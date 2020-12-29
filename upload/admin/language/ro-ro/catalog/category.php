546b520022601652e119c15a4ddee5e2c9a8cc60
Constantin DOBRE <admin@harddex.ro>
2020-12-29 13:50:07 +0800

    New translations category.php (Romanian)

--- upload/admin/language/ro-ro/catalog/category.php
+++ upload/admin/language/ro-ro/catalog/category.php
@@ -18,7 +18,7 @@ $_['column_action']          = 'Acţiune';
 // Entry
 $_['entry_name']             = 'Nume Categorie';
 $_['entry_description']      = 'Descriere';
-$_['entry_meta_title']       = 'MetaTag Titlu';
+$_['entry_meta_title'] 	     = 'MetaTag Titlu';
 $_['entry_meta_keyword']     = 'MetaTag Cuvinte Cheie';
 $_['entry_meta_description'] = 'MetaTag Descriere';
 $_['entry_store']            = 'Magazine';
@@ -41,7 +41,7 @@ $_['help_column']            = 'Numărul de coloane de utilizat pentru cele 3 ca
 $_['error_warning']          = 'Atenție: Vă rugăm să verificați cu atenție formularul pentru erori!';
 $_['error_permission']       = 'Atenție: Nu aveți permisiunea de a modifica categoriile!';
 $_['error_name']             = 'Numele categoriei trebuie să aibă între 1 și 255 de caractere!';
-$_['error_meta_title']       = 'MetaTag Titlu trebuie să aibă mai mult de 1 și mai puțin de 255 de caractere!';
+$_['error_meta_title']       = 'MetaTag Titlu trebuie să fie mai mare de 1 și mai mic de 255 de caractere!';
 $_['error_keyword']          = 'URL SEO deja utilizat!';
 $_['error_unique']           = 'URL-ul SEO trebuie să fie unic!';
 $_['error_parent']           = 'Categoria părinte pe care ați ales-o este un copil al celei actuale!';
