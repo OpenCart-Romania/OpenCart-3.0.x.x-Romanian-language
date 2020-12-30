039d8a9fe8f47e97c513d29209ae45b79dea460c
Constantin DOBRE <admin@harddex.ro>
2020-12-30 13:01:20 +0800

    New translations information.php (Romanian)

--- upload/admin/language/ro-ro/catalog/information.php
+++ upload/admin/language/ro-ro/catalog/information.php
@@ -3,44 +3,44 @@
 $_['heading_title']          = 'Informaţii';
 
 // Text
-$_['text_success']           = 'Succes: Ai modificat Informaţii!';
+$_['text_success']           = 'Succes: Ați modificat Informaţii!';
 $_['text_list']              = 'Listă Informaţii';
-$_['text_add']               = 'Adaugă Informații';
+$_['text_add']               = 'Adăugați Informații';
 $_['text_edit']              = 'Editați Informații';
 $_['text_default']           = 'Implicit';
-$_['text_keyword']           = 'Nu utilizaţi spaţii, în schimb înlocuiţi-le cu  -  și asiguraţi-vă că acest cuvânt cheie este unic global.';
+$_['text_keyword']           = 'Nu utilizaţi spaţii, în schimb înlocuiţi-le cu - și asigurați-vă că URL-ul SEO este unic la nivel global.';
 
 // Column
 $_['column_title']           = 'Titlu Informaţii';
-$_['column_sort_order']      = 'Ordinea de sortare';
+$_['column_sort_order']      = 'Ordine Sortare';
 $_['column_action']          = 'Acţiune';
 
 // Entry
 $_['entry_title']            = 'Titlu Informaţii';
 $_['entry_description']      = 'Descriere';
-$_['entry_meta_title']       = 'MetaTag Titlu';
-$_['entry_meta_keyword']     = 'MetaTag cuvinte cheie';
+$_['entry_meta_title']       = 'Meta Tag Titlu';
+$_['entry_meta_keyword']     = 'Meta Tag Cuvinte Cheie';
 $_['entry_meta_description'] = 'MetaTag Descriere';
 $_['entry_store']            = 'Magazine';
-$_['entry_keyword']          = 'Cuvânt cheie pentru SEO';
+$_['entry_keyword']          = 'Cuvânt Cheie';
 $_['entry_bottom']           = 'Jos';
-$_['entry_status']           = 'Starea';
-$_['entry_sort_order']       = 'Ordinea de sortare';
-$_['entry_layout']           = 'Suprascrie aspectul general';
+$_['entry_status']           = 'Stare';
+$_['entry_sort_order']       = 'Ordine Sortare';
+$_['entry_layout']           = 'Suprascriere Aspect';
 
 // Help
-$_['help_bottom']            = 'Afișare în subsolul paginii.';
+$_['help_bottom']            = 'Afișeaza în subsolul paginii.';
 
 // Error
-$_['error_warning']          = 'Avertisment: Vă rugăm să verificaţi cu atenție formularul pentru erori!';
-$_['error_permission']       = 'Avertisment: Nu aveți permisiunea de a modifica Informații!';
-$_['error_title']            = 'Titlu paginii de Informații trebuie să aibă între 3 şi 64 de caractere!';
+$_['error_warning']          = 'Atenție: Vă rugăm să verificați cu atenție formularul pentru erori!';
+$_['error_permission']       = 'Atenție: Nu aveți permisiunea de a modifica informații!';
+$_['error_title']            = 'Titlu Informaţii trebuie să fie între 1 şi 64 de caractere!';
 $_['error_description']      = 'Descrierea trebuie să conțină mai mult de 3 caractere!';
-$_['error_meta_title']       = 'MetaTag Titlu trebuie să aibă între 3 și 255 de caractere!';
-$_['error_keyword']          = 'Acest cuvânt cheie SEO este deja folosit!';
-$_['error_unique']           = 'SEO URL must be unique!';
-$_['error_account']          = 'Avertisment: Această pagină de informaţii nu poate fi ştersă deoarece este asociată cu termenii de cont ai magazinului!';
-$_['error_checkout']         = 'Avertisment: Această pagină de informaţii nu poate fi ştersă deoarece este asociată cu termenii de comandă ai magazinului!';
-$_['error_affiliate']        = 'Avertisment: Această pagină de informaţii nu poate fi ştersă deoarece este asociată cu termenii de afiliere ai magazinului!';
-$_['error_return']           = 'Avertisment: Această pagină de informaţii nu poate fi ştersă deoarece este asociată cu termenii de returnare ai magazinului!';
-$_['error_store']            = 'Avertisment: Această pagină de informaţii nu poate fi ștearsă deoarece este utiliză în prezent de %s magazine!';
+$_['error_meta_title']       = 'Meta Tag Titlu trebuie să fie mai mare de 1 și mai mic de 255 de caractere!';
+$_['error_keyword']          = 'URL-ul SEO este deja utilizat!';
+$_['error_unique']           = 'URL-ul SEO trebuie să fie unic!';
+$_['error_account']          = 'Atenție: Această pagină de informații nu poate fi ștearsă deoarece în prezent este asociată cu termenii de cont ai magazinului!';
+$_['error_checkout']         = 'Atenție: Această pagină de informaţii nu poate fi ștearsă deoarece în prezent este asociată cu termenii de comandă ai magazinului!';
+$_['error_affiliate']        = 'Atenție: Această pagină de informaţii nu poate fi ștearsă deoarece în prezent este asociată cu termenii de afiliere ai magazinului!';
+$_['error_return']           = 'Atenție: Această pagină de informaţii nu poate fi ștearsă deoarece în prezent este asociată cu termenii de returnare ai magazinului!';
+$_['error_store']            = 'Atenție: Această pagină de informaţii nu poate fi ștearsă deoarece în prezent este utilizată de %s magazine!';
