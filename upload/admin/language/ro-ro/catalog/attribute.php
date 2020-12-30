cf6dfa596ccc2eed0bb2ecf0d9a596f3aa8e957e
Constantin DOBRE <admin@harddex.ro>
2020-12-30 12:56:34 +0800

    New translations attribute.php (Romanian)

--- upload/admin/language/ro-ro/catalog/attribute.php
+++ upload/admin/language/ro-ro/catalog/attribute.php
@@ -5,22 +5,22 @@ $_['heading_title']          = 'Atribute';
 // Text
 $_['text_success']           = 'Succes: Ați modificat atributele!';
 $_['text_list']              = 'Listă Atribute';
-$_['text_add']               = 'Adaugă Atribut';
+$_['text_add']               = 'Adăugati Atribut';
 $_['text_edit']              = 'Editați Atribut';
 
 // Column
 $_['column_name']            = 'Nume Atribut';
-$_['column_attribute_group'] = 'Grup de Atribute';
-$_['column_sort_order']      = 'Ordinea de Sortare';
+$_['column_attribute_group'] = 'Grup Atribute';
+$_['column_sort_order']      = 'Ordine Sortare';
 $_['column_action']          = 'Acțiune';
 
 // Entry
 $_['entry_name']             = 'Nume Atribut';
-$_['entry_attribute_group']  = 'Grup de Atribute';
-$_['entry_sort_order']       = 'Ordinea de Sortare';
+$_['entry_attribute_group']  = 'Grup Atribute';
+$_['entry_sort_order']       = 'Ordine Sortare';
 
 // Error
-$_['error_permission']       = 'Atenție: Nu aveți permisiunea de a modifica atributele!';
-$_['error_attribute_group']  = 'Grup de Atribute Necesar!';
-$_['error_name']             = 'Numele Atributului trebuie să aibă între 1 și 64 de caractere!';
+$_['error_permission']       = 'Atenție: Nu aveți permisiunea de a modifica atribute!';
+$_['error_attribute_group']  = 'Grup Atribute Necesar!';
+$_['error_name']             = 'Nume Atribut trebuie să fie între 1 și 64 de caractere!';
 $_['error_product']          = 'Atenție: Acest atribut nu poate fi șters deoarece în prezent este asociat cu %s produse!';
