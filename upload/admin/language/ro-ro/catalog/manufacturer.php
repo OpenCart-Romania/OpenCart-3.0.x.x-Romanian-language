eb055d4ba1edc88f205780597bf4ce2cb75e2be3
Constantin DOBRE <admin@harddex.ro>
2020-12-30 13:01:18 +0800

    New translations manufacturer.php (Romanian)

--- upload/admin/language/ro-ro/catalog/manufacturer.php
+++ upload/admin/language/ro-ro/catalog/manufacturer.php
@@ -3,31 +3,31 @@
 $_['heading_title']     = 'Producători';
 
 // Text
-$_['text_success']      = 'Succes: Ai modificat Producătorii!';
+$_['text_success']      = 'Succes: Ați modificat producătorii!';
 $_['text_list']         = 'Listă Producători';
-$_['text_add']          = 'Adaugă Producător';
-$_['text_edit']         = 'Editează Producător';
+$_['text_add']          = 'Adăugați Producător';
+$_['text_edit']         = 'Editați Producător';
 $_['text_default']      = 'Implicit';
-$_['text_percent']      = 'Procent';
-$_['text_amount']       = 'Sumă fixă';
-$_['text_keyword']      = 'Nu utilizaţi spaţii, în schimb înlocuiţi-le cu  -  și asiguraţi-vă că acest cuvânt cheie este unic global.';
+$_['text_percent']      = 'Procentaj';
+$_['text_amount']       = 'Cantitate Fixă';
+$_['text_keyword']      = 'Nu utilizaţi spaţii, în schimb înlocuiţi-le cu - și asigurați-vă că URL-ul SEO este unic la nivel global.';
 
 // Column
-$_['column_name']       = 'Numele Producătorului';
-$_['column_sort_order'] = 'Ordinea aranjării';
+$_['column_name']       = 'Nume Producător';
+$_['column_sort_order'] = 'Ordine Sortare';
 $_['column_action']     = 'Acţiune';
 
 // Entry
-$_['entry_name']        = 'Numele Producătorului';
+$_['entry_name']        = 'Nume Producător';
 $_['entry_store']       = 'Magazine';
-$_['entry_keyword']     = 'Cuvânt cheie pentru SEO';
+$_['entry_keyword']     = 'Cuvânt Cheie';
 $_['entry_image']       = 'Imagine';
-$_['entry_sort_order']  = 'Ordinea aranjării';
+$_['entry_sort_order']  = 'Ordine Sortare';
 $_['entry_type']        = 'Tip';
 
 // Error
-$_['error_permission']  = 'Atenție: Nu aveţi permisiunea de a modifica Producătorii!';
-$_['error_name']        = 'Numele Producătorului trebuie să aibă între 2 și 64 de caractere!';
-$_['error_keyword']     = 'Acest cuvânt cheie SEO este deja folosit!';
-$_['error_unique']      = 'SEO URL must be unique!';
+$_['error_permission']  = 'Atenție: Nu aveţi permisiunea de a modifica producătorii!';
+$_['error_name']        = 'Nume Producător trebuie să fie între 1 și 64 de caractere!';
+$_['error_keyword']     = 'URL-ul SEO este deja utilizat!';
+$_['error_unique']      = 'URL-ul SEO trebuie să fie unic!';
 $_['error_product']     = 'Atenție: Acest producător nu poate fi șters deoarece în prezent este asociat cu %s produse!';
