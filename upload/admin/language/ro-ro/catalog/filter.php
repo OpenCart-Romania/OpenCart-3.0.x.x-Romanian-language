3a1e354b68697c06725c4675eb6d204a0ec73e8d
Constantin DOBRE <admin@harddex.ro>
2020-12-30 13:01:21 +0800

    New translations filter.php (Romanian)

--- upload/admin/language/ro-ro/catalog/filter.php
+++ upload/admin/language/ro-ro/catalog/filter.php
@@ -3,24 +3,24 @@
 $_['heading_title']     = 'Filtre';
 
 // Text
-$_['text_success']      = 'Succes: Ai modificat filtrele!';
-$_['text_list']         = 'Listă de filtre';
-$_['text_add']          = 'Adaugă Filtru';
-$_['text_edit']         = 'Editează Filtru';
-$_['text_group']        = 'Grupuri de Filtre';
+$_['text_success']      = 'Succes: Ați modificat filtrele!';
+$_['text_list']         = 'Listă Filtre';
+$_['text_add']          = 'Adăugati Filtru';
+$_['text_edit']         = 'Editați Filtru';
+$_['text_group']        = 'Grup Filtre';
 $_['text_value']        = 'Valori Filtru';
 
 // Column
-$_['column_group']      = 'Grupuri de Filtre';
-$_['column_sort_order'] = 'Ordinea de sortare';
+$_['column_group']      = 'Grup Filtre';
+$_['column_sort_order'] = 'Ordine Sortare';
 $_['column_action']     = 'Acţiune';
 
 // Entry
-$_['entry_group']       = 'Denumirea Grupului de Filtre';
-$_['entry_name']        = 'Denumire Filtru';
-$_['entry_sort_order']  = 'Ordinea de sortare';
+$_['entry_group']       = 'Nume Grup Filtre';
+$_['entry_name']        = 'Nume Filtru';
+$_['entry_sort_order']  = 'Ordinea de Sortare';
 
 // Error
-$_['error_permission']  = 'Avertisment: Nu aveţi permisiunea de a modifica filtrele!';
-$_['error_group']       = 'Denumirea Grupului de Filtre trebuie să aibă între 1 și 64 de caractere!';
-$_['error_name']        = 'Denumirea Filtrului trebuie să aibă între 1 și 64 de caractere!';
+$_['error_permission']  = 'Atentie: Nu aveţi permisiunea de a modifica filtre!';
+$_['error_group']       = 'Nume Grup Filtre trebuie să fie între 1 și 64 de caractere!';
+$_['error_name']        = 'Nume Filtru trebuie să fie între 1 și 64 de caractere!';
