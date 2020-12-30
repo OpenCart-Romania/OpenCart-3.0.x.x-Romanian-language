91d70976dcadd592039dfd39e8446c59dae85842
Constantin DOBRE <admin@harddex.ro>
2020-12-30 13:01:16 +0800

    New translations option.php (Romanian)

--- upload/admin/language/ro-ro/catalog/option.php
+++ upload/admin/language/ro-ro/catalog/option.php
@@ -3,39 +3,39 @@
 $_['heading_title']      = 'Opţiuni';
 
 // Text
-$_['text_success']       = 'Succes: Ai modificat Opţiunile!';
+$_['text_success']       = 'Succes: Ați modificat opţiunile!';
 $_['text_list']          = 'Listă Opțiuni';
-$_['text_add']           = 'Adaugă Opţiune';
-$_['text_edit']          = 'Editează Opţiune';
-$_['text_choose']        = 'Alege';
-$_['text_select']        = 'Selectează';
-$_['text_radio']         = 'Buton Radio';
-$_['text_checkbox']      = 'Casetă de Selectare';
-$_['text_input']         = 'Intrare';
+$_['text_add']           = 'Adăugați Opţiune';
+$_['text_edit']          = 'Editați Opţiune';
+$_['text_choose']        = 'Alegeți';
+$_['text_select']        = 'Select';
+$_['text_radio']         = 'Radio';
+$_['text_checkbox']      = 'Checkbox';
+$_['text_input']         = 'Input';
 $_['text_text']          = 'Text';
-$_['text_textarea']      = 'Spațiu Text';
+$_['text_textarea']      = 'Textarea';
 $_['text_file']          = 'Fişier';
 $_['text_date']          = 'Dată';
 $_['text_datetime']      = 'Dată &amp; Oră';
 $_['text_time']          = 'Oră';
-$_['text_option']        = 'Option';
-$_['text_value']         = 'Option Values';
+$_['text_option']        = 'Opţiune';
+$_['text_value']         = 'Valori Opțiune';
 
 // Column
-$_['column_name']        = 'Numele Opțiunii';
-$_['column_sort_order']  = 'Ordinea aranjării';
+$_['column_name']        = 'Nume Opțiune';
+$_['column_sort_order']  = 'Ordine Sortare';
 $_['column_action']      = 'Acţiune';
 
 // Entry
-$_['entry_name']         = 'Numele Opțiunii';
+$_['entry_name']         = 'Nume Opțiune';
 $_['entry_type']         = 'Tip';
 $_['entry_option_value'] = 'Nume Valoare Opțiune';
 $_['entry_image']        = 'Imagine';
-$_['entry_sort_order']   = 'Ordinea aranjării';
+$_['entry_sort_order']   = 'Ordine Sortare';
 
 // Error
-$_['error_permission']   = 'Atenție: Nu ai permisiunea să modifici Opţiunile!';
-$_['error_name']         = 'Numele Opţiunii trebuie să aibă între 1 şi 128 de caractere!';
-$_['error_type']         = 'Atenție: Valori Opțiuni necesare!';
-$_['error_option_value'] = 'Nume Valoare Opţiune trebuie să aibă între 1 şi 128 de caractere!';
-$_['error_product']      = 'Atenție: Această Opțiune nu poate fi ștearsă deoarece este asociată cu  %s produs(e)!';
+$_['error_permission']   = 'Atenţie: Nu aveţi permisiunea de a modifica opţiunile!';
+$_['error_name']         = 'Nume Opțiune trebuie să fie între 1 şi 128 de caractere!';
+$_['error_type']         = 'Atenție: Valori Opțiune necesare!';
+$_['error_option_value'] = 'Nume Valoare Opţiune trebuie să fie între 1 şi 128 de caractere!';
+$_['error_product']      = 'Atenție: Această opțiune nu poate fi ștearsă deoarece în prezent este asociată cu %s produse!';
