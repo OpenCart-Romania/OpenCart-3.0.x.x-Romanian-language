78974213c6730947dce55b5bbcc7d7a9f463bb49
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:55:19 +0800

    New translations analytics.php (Romanian)

--- upload/admin/language/ro-ro/extension/extension/analytics.php
+++ upload/admin/language/ro-ro/extension/extension/analytics.php
@@ -3,13 +3,13 @@
 $_['heading_title']    = 'Analytics';
 
 // Text
-$_['text_success']     = 'Success: You have modified analytics!';
+$_['text_success']     = 'Succes: Ai modificat statisticile!';
 $_['text_list']        = 'Analytics List';
 
 // Column
 $_['column_name']      = 'Analytics Name';
-$_['column_status']    = 'Status';
-$_['column_action']    = 'Action';
+$_['column_status']    = 'Stare';
+$_['column_action']    = 'Acțiune';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify analytics!';
