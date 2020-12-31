c53784761a7ac0737dce1d396a723e65adae047d
Constantin DOBRE <admin@harddex.ro>
2020-12-31 15:01:56 +0800

    New translations order.php (Romanian)

--- upload/admin/language/ro-ro/sale/order.php
+++ upload/admin/language/ro-ro/sale/order.php
@@ -1,27 +1,27 @@
 <?php
 // Heading
-$_['heading_title']              = 'Orders';
+$_['heading_title']              = 'Comenzi';
 
 // Text
 $_['text_success']               = 'Success: You have modified orders!';
 $_['text_list']                  = 'Order List';
 $_['text_add']                   = 'Add Order';
 $_['text_edit']                  = 'Edit Order';
-$_['text_filter']                = 'Filter';
+$_['text_filter']                = 'Filtru';
 $_['text_order_detail']          = 'Order Details';
 $_['text_customer_detail']       = 'Customer Details';
-$_['text_option']                = 'Options';
+$_['text_option']                = 'Opţiuni';
 $_['text_store']                 = 'Store';
-$_['text_date_added']            = 'Date Added';
+$_['text_date_added']            = 'Data Adăugării';
 $_['text_payment_method']        = 'Payment Method';
 $_['text_shipping_method']       = 'Shipping Method';
 $_['text_customer']              = 'Customer';
-$_['text_customer_group']        = 'Customer Group';
+$_['text_customer_group']        = 'Grup Clienți';
 $_['text_email']                 = 'E-Mail';
 $_['text_telephone']             = 'Telephone';
 $_['text_fax']                   = 'Fax';
 $_['text_invoice']               = 'Invoice';
-$_['text_reward']                = 'Reward Points';
+$_['text_reward']                = 'Puncte de Recompensă';
 $_['text_affiliate']             = 'Affiliate';
 $_['text_order']                 = 'Order (#%s)';
 $_['text_payment_address']       = 'Payment Address';
@@ -48,7 +48,7 @@ $_['text_jan']                   = 'JAN:';
 $_['text_isbn']                  = 'ISBN:';
 $_['text_mpn']                   = 'MPN:';
 $_['text_missing']               = 'Missing Orders';
-$_['text_default']               = 'Default';
+$_['text_default']               = 'Implicit';
 $_['text_product']               = 'Add Product(s)';
 $_['text_voucher']               = 'Add Voucher(s)';
 $_['text_shipping']              = 'Shipping';
@@ -58,39 +58,39 @@ $_['text_reward_removed']        = 'Success: Reward points removed!';
 $_['text_commission_added']      = 'Success: Commission added!';
 $_['text_commission_removed']    = 'Success: Commission removed!';
 $_['text_restock']               = 'Success: Products have been restocked!';
-$_['text_upload']                = 'Your file was successfully uploaded!';
+$_['text_upload']                = 'Fișierul tău a fost încărcat cu succes!';
 $_['text_picklist']              = 'Dispatch Note';
 
 // Column
-$_['column_order_id']            = 'Order ID';
+$_['column_order_id']            = 'ID Comandă';
 $_['column_customer']            = 'Customer';
-$_['column_status']              = 'Status';
-$_['column_date_added']          = 'Date Added';
+$_['column_status']              = 'Stare';
+$_['column_date_added']          = 'Data Adăugării';
 $_['column_date_modified']       = 'Date Modified';
 $_['column_total']               = 'Total';
-$_['column_product']             = 'Product';
+$_['column_product']             = 'Produs';
 $_['column_model']               = 'Model';
-$_['column_quantity']            = 'Quantity';
+$_['column_quantity']            = 'Cantitate';
 $_['column_price']               = 'Unit Price';
 $_['column_comment']             = 'Comment';
 $_['column_notify']              = 'Customer Notified';
-$_['column_location']            = 'Location';
+$_['column_location']            = 'Locație';
 $_['column_reference']           = 'Reference';
-$_['column_action']              = 'Action';
+$_['column_action']              = 'Acțiune';
 $_['column_weight']              = 'Product Weight';
 
 // Entry
 $_['entry_store']                = 'Store';
 $_['entry_customer']             = 'Customer';
-$_['entry_customer_group']       = 'Customer Group';
-$_['entry_firstname']            = 'First Name';
-$_['entry_lastname']             = 'Last Name';
+$_['entry_customer_group']       = 'Grup Clienți';
+$_['entry_firstname']            = 'Prenume';
+$_['entry_lastname']             = 'Nume de Familie';
 $_['entry_email']                = 'E-Mail';
 $_['entry_telephone']            = 'Telephone';
 $_['entry_address']              = 'Choose Address';
 $_['entry_company']              = 'Company';
-$_['entry_address_1']            = 'Address 1';
-$_['entry_address_2']            = 'Address 2';
+$_['entry_address_1']            = 'Adresă 1';
+$_['entry_address_2']            = 'Adresă 2';
 $_['entry_city']                 = 'City';
 $_['entry_postcode']             = 'Postcode';
 $_['entry_country']              = 'Country';
@@ -98,7 +98,7 @@ $_['entry_zone']                 = 'Region / State';
 $_['entry_zone_code']            = 'Region / State Code';
 $_['entry_product']              = 'Choose Product';
 $_['entry_option']               = 'Choose Option(s)';
-$_['entry_quantity']             = 'Quantity';
+$_['entry_quantity']             = 'Cantitate';
 $_['entry_to_name']              = 'Recipient\'s Name';
 $_['entry_to_email']             = 'Recipient\'s E-mail';
 $_['entry_from_name']            = 'Sender\'s Name';
@@ -117,16 +117,16 @@ $_['entry_payment_method']       = 'Payment Method';
 $_['entry_coupon']               = 'Coupon';
 $_['entry_voucher']              = 'Voucher';
 $_['entry_reward']               = 'Reward';
-$_['entry_order_id']             = 'Order ID';
+$_['entry_order_id']             = 'ID Comandă';
 $_['entry_total']                = 'Total';
-$_['entry_date_added']           = 'Date Added';
+$_['entry_date_added']           = 'Data Adăugării';
 $_['entry_date_modified']        = 'Date Modified';
 
 // Help
 $_['help_override']              = 'If the customers order is being blocked from changing the order status due to an anti-fraud extension enable override.';
 
 // Error
-$_['error_warning']              = 'Warning: Please check the form carefully for errors!';
+$_['error_warning']              = 'Atenție: Te rugăm să verifici cu atenție formularul pentru erori!';
 $_['error_permission']           = 'Warning: You do not have permission to modify orders!';
 $_['error_action']               = 'Warning: Could not complete this action!';
-$_['error_filetype']             = 'Invalid file type!';
+$_['error_filetype']             = 'Tip fișier nevalid!';
