18d03b99412a026120cf67b8e3ee4ce1b65549e2
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:54:40 +0800

    New translations theme.php (Romanian)

--- upload/admin/language/ro-ro/extension/extension/theme.php
+++ upload/admin/language/ro-ro/extension/extension/theme.php
@@ -3,13 +3,13 @@
 $_['heading_title']    = 'Themes';
 
 // Text
-$_['text_success']     = 'Success: You have modified themes!';
+$_['text_success']     = 'Succes: Ai modificat temele!';
 $_['text_list']        = 'Theme List';
 
 // Column
 $_['column_name']      = 'Theme Name';
-$_['column_status']    = 'Status';
-$_['column_action']    = 'Action';
+$_['column_status']    = 'Stare';
+$_['column_action']    = 'Acțiune';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify themes!';
