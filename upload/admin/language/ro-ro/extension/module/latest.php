e1c162244de8de2fbd5dcba0786b982551e15308
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:52:05 +0800

    New translations latest.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/latest.php
+++ upload/admin/language/ro-ro/extension/module/latest.php
@@ -3,16 +3,16 @@
 $_['heading_title']    = 'Latest';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified latest module!';
 $_['text_edit']        = 'Edit Latest Module';
 
 // Entry
 $_['entry_name']       = 'Module Name';
 $_['entry_limit']      = 'Limit';
-$_['entry_width']      = 'Width';
-$_['entry_height']     = 'Height';
-$_['entry_status']     = 'Status';
+$_['entry_width']      = 'Lăţime';
+$_['entry_height']     = 'Înălţime';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify latest module!';
