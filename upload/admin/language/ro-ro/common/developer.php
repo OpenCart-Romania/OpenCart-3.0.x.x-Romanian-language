cb369ab45577814eebc542b4121877e3832c7c63
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:54:01 +0800

    New translations developer.php (Romanian)

--- upload/admin/language/ro-ro/common/developer.php
+++ upload/admin/language/ro-ro/common/developer.php
@@ -3,10 +3,10 @@
 $_['heading_title']    = 'Setări Dezvoltator';
 
 // Text
-$_['text_success']     = 'Succes: Ați modificat setările dezvoltatorului!';
+$_['text_success']     = 'Succes: Ai modificat setările dezvoltatorului!';
 $_['text_theme']       = 'temă';
 $_['text_sass']        = 'SASS';
-$_['text_cache']       = 'Succes: Ați șters cache-ul %s!';
+$_['text_cache']       = 'Succes: Ai curățat cache-ul %s!';
 
 // Column
 $_['column_component'] = 'Componentă';
@@ -22,4 +22,4 @@ $_['button_on']        = 'Pornit';
 $_['button_off']       = 'Oprit';
 
 // Error
-$_['error_permission'] = 'Atenție: Nu aveți permisiunea de a modifica setările dezvoltatorului!';
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica setările dezvoltatorului!';
