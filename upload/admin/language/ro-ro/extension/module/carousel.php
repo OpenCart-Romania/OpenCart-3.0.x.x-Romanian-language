eb71cf78cbaa5ed41c165b6160a65d7b55443d0d
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:52:22 +0800

    New translations carousel.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/carousel.php
+++ upload/admin/language/ro-ro/extension/module/carousel.php
@@ -3,16 +3,16 @@
 $_['heading_title']    = 'Carousel';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified carousel module!';
 $_['text_edit']        = 'Edit Carousel Module';
 
 // Entry
 $_['entry_name']       = 'Module Name';
 $_['entry_banner']     = 'Banner';
-$_['entry_width']      = 'Width';
-$_['entry_height']     = 'Height';
-$_['entry_status']     = 'Status';
+$_['entry_width']      = 'Lăţime';
+$_['entry_height']     = 'Înălţime';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify carousel module!';
