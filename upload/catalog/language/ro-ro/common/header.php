7aefdc36b452fcab30b854cd9d29817f2836b99f
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:56:06 +0800

    New translations header.php (Romanian)

--- upload/catalog/language/ro-ro/common/header.php
+++ upload/catalog/language/ro-ro/common/header.php
@@ -1,16 +1,16 @@
 <?php
 // Text
-$_['text_home']          = 'Home';
+$_['text_home']          = 'Acasă';
 $_['text_wishlist']      = 'Wish List (%s)';
 $_['text_shopping_cart'] = 'Shopping Cart';
-$_['text_category']      = 'Categories';
-$_['text_account']       = 'My Account';
+$_['text_category']      = 'Categorii';
+$_['text_account']       = 'Contul Meu';
 $_['text_register']      = 'Register';
-$_['text_login']         = 'Login';
+$_['text_login']         = 'Autentificare';
 $_['text_order']         = 'Order History';
 $_['text_transaction']   = 'Transactions';
-$_['text_download']      = 'Downloads';
-$_['text_logout']        = 'Logout';
+$_['text_download']      = 'Descărcări';
+$_['text_logout']        = 'Deconectare';
 $_['text_checkout']      = 'Checkout';
-$_['text_search']        = 'Search';
+$_['text_search']        = 'Căutare';
 $_['text_all']           = 'Show All';
