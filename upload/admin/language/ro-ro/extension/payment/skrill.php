8abd15e51f964ab65b86234aa11e40efc96b8a59
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:59:31 +0800

    New translations skrill.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/skrill.php
+++ upload/admin/language/ro-ro/extension/payment/skrill.php
@@ -3,7 +3,7 @@
 $_['heading_title']			  = 'Skrill';
 
 // Text
-$_['text_extension']		  = 'Extensions';
+$_['text_extension']		  = 'Extensii';
 $_['text_success']			  = 'Success: You have modified the Skrill details.';
 $_['text_edit']               = 'Edit Skrill';
 $_['text_skrill']	     	  = '<a href="https://content.skrill.com/en/ecommerce-solutions/opencart/" target="_blank"><img src="view/image/payment/skrill.png" alt="Skrill" title="Skrill" style="border: 1px solid #EEEEEE;" /></a>';
@@ -18,8 +18,8 @@ $_['entry_canceled_status']	  = 'Canceled Status';
 $_['entry_failed_status']	  = 'Failed Status';
 $_['entry_chargeback_status'] = 'Chargeback Status';
 $_['entry_geo_zone']		  = 'Geo Zone';
-$_['entry_status']			  = 'Status';
-$_['entry_sort_order']		  = 'Sort Order';
+$_['entry_status']			  = 'Stare';
+$_['entry_sort_order']		  = 'Ordinea de Sortare';
 
 // Help
 $_['help_total']			  = 'The checkout total the order must reach before this payment method becomes active.';
