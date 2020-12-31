ab438c53c8f1625ff799b0e5f96036eb14e37f2a
Constantin DOBRE <admin@harddex.ro>
2020-12-31 15:01:25 +0800

    New translations event.php (Romanian)

--- upload/admin/language/ro-ro/marketplace/event.php
+++ upload/admin/language/ro-ro/marketplace/event.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']     = 'Events';
+$_['heading_title']     = 'Evenimente';
 
 // Text
 $_['text_success']      = 'Success: You have modified events!';
@@ -8,13 +8,13 @@ $_['text_list']         = 'Event List';
 $_['text_event']        = 'Events are used by extensions to override the default functionality of your store. If you have issues you can disable or enable the events here.';
 $_['text_info']         = 'Event Information';
 $_['text_trigger']      = 'Trigger';
-$_['text_action']       = 'Action';
+$_['text_action']       = 'Acțiune';
 
 // Column
 $_['column_code']       = 'Event Code';
-$_['column_status']     = 'Status';
-$_['column_sort_order'] = 'Sort Order';
-$_['column_action']     = 'Action';
+$_['column_status']     = 'Stare';
+$_['column_sort_order'] = 'Ordinea de Sortare';
+$_['column_action']     = 'Acțiune';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify extensions!';
