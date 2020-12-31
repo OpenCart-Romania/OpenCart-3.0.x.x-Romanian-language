09bf4da442a7d15b735a0b058fe9992c43807f7d
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:51:49 +0800

    New translations store.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/store.php
+++ upload/admin/language/ro-ro/extension/module/store.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']    = 'Store';
+$_['heading_title']    = 'Magazin';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified store module!';
 $_['text_edit']        = 'Edit Store Module';
 
 // Entry
 $_['entry_admin']      = 'Admin Users Only';
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Stare';
 
 //Help
 $_['help_admin']       = 'If yes, then multi store list will be visible only when admin user login!';
