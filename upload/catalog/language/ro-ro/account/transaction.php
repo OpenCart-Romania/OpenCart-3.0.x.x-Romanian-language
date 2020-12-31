ec5861425882c54aa1c19c43d4a8b84b516464ad
Constantin DOBRE <admin@harddex.ro>
2020-12-31 15:00:42 +0800

    New translations transaction.php (Romanian)

--- upload/catalog/language/ro-ro/account/transaction.php
+++ upload/catalog/language/ro-ro/account/transaction.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']      = 'Your Transactions';
+$_['heading_title']      = 'Tranzacțiile Dvs.';
 
 // Column
-$_['column_date_added']  = 'Date Added';
-$_['column_description'] = 'Description';
+$_['column_date_added']  = 'Data Adăugării';
+$_['column_description'] = 'Descriere';
 $_['column_amount']      = 'Amount (%s)';
 
 // Text
-$_['text_account']       = 'Account';
-$_['text_transaction']   = 'Your Transactions';
+$_['text_account']       = 'Cont';
+$_['text_transaction']   = 'Tranzacțiile Dvs.';
 $_['text_total']         = 'Your current balance is:';
 $_['text_empty']         = 'You do not have any transactions!';
