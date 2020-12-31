3c57bab16c6b33e02ae4fa69a7033601dbfa4125
Constantin DOBRE <admin@harddex.ro>
2020-12-31 15:00:20 +0800

    New translations forgotten.php (Romanian)

--- upload/catalog/language/ro-ro/account/forgotten.php
+++ upload/catalog/language/ro-ro/account/forgotten.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title']   = 'Forgot Your Password?';
+$_['heading_title']   = 'Ți-ai Uitat Parola?';
 
 // Text
-$_['text_account']    = 'Account';
-$_['text_forgotten']  = 'Forgotten Password';
-$_['text_your_email'] = 'Your E-Mail Address';
-$_['text_email']      = 'Enter the e-mail address associated with your account. Click submit to have a password reset link e-mailed to you.';
+$_['text_account']    = 'Cont';
+$_['text_forgotten']  = 'Parolă Uitată';
+$_['text_your_email'] = 'Adresa Ta de E-Mail';
+$_['text_email']      = 'Introdu adresa de e-mail asociată contului tau. Click pe "Trimite" pentru a-ti trimite prin e-mail un link de resetare a parolei.';
 $_['text_success']    = 'An email with a confirmation link has been sent your email address.';
 
 // Entry
-$_['entry_email']     = 'E-Mail Address';
-$_['entry_password']  = 'New Password';
-$_['entry_confirm']   = 'Confirm';
+$_['entry_email']     = 'Adresă de E-Mail';
+$_['entry_password']  = 'Parolă Nouă';
+$_['entry_confirm']   = 'Confirmare';
 
 // Error
-$_['error_email']     = 'Warning: The E-Mail Address was not found in our records, please try again!';
+$_['error_email']     = 'Atenție: Adresa de E-Mail nu a fost găsită în înregistrările noastre, te rugăm să încerci din nou!';
 $_['error_approved']  = 'Warning: Your account requires approval before you can login.';
-$_['error_password']  = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']   = 'Password and password confirmation do not match!';
+$_['error_password']  = 'Parola trebuie să fie între 4 și 20 de caractere!';
+$_['error_confirm']   = 'Parola și confirmarea parolei nu se potrivesc!';
