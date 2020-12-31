2054e4055f18ee788fdf7791920da165537d7f9b
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:54:06 +0800

    New translations review.php (Romanian)

--- upload/admin/language/ro-ro/catalog/review.php
+++ upload/admin/language/ro-ro/catalog/review.php
@@ -1,19 +1,19 @@
 <?php
 // Heading
-$_['heading_title']     = 'Opinii';
+$_['heading_title']     = 'Recenzii';
 
 // Text
-$_['text_success']      = 'Succes: Ai modificat Opiniile!';
-$_['text_list']         = 'Listă Opinii';
-$_['text_add']          = 'Adaugă Opinie';
-$_['text_edit']         = 'Editează Opinie';
+$_['text_success']      = 'Succes: Ai modificat recenziile!';
+$_['text_list']         = 'Listă Recenzii';
+$_['text_add']          = 'Adaugă Recenzie';
+$_['text_edit']         = 'Editează Recenzie';
 $_['text_filter']       = 'Filtru';
 
 // Column
 $_['column_product']    = 'Produs';
 $_['column_author']     = 'Autor';
-$_['column_rating']     = 'Notă';
-$_['column_status']     = 'Starea';
+$_['column_rating']     = 'Evaluare';
+$_['column_status']     = 'Stare';
 $_['column_date_added'] = 'Data Adăugării';
 $_['column_action']     = 'Acţiune';
 
@@ -21,7 +21,7 @@ $_['column_action']     = 'Acţiune';
 $_['entry_product']     = 'Produs';
 $_['entry_author']      = 'Autor';
 $_['entry_rating']      = 'Notă';
-$_['entry_status']      = 'Starea';
+$_['entry_status']      = 'Stare';
 $_['entry_text']        = 'Text';
 $_['entry_date_added']  = 'Data Adăugării';
 
@@ -29,8 +29,8 @@ $_['entry_date_added']  = 'Data Adăugării';
 $_['help_product']      = '(Completare automată)';
 
 // Error
-$_['error_permission']  = 'Atenție: Nu ai permisiunea să modifici Opiniile!';
+$_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica recenziile!';
 $_['error_product']     = 'Produs necesar!';
 $_['error_author']      = 'Autor trebuie să fie între 3 și 64 de caractere!';
-$_['error_text']        = 'Textul Opiniei dvs. trebuie să conțină cel puțin un caracter!';
-$_['error_rating']      = 'Evaluarea Produsului este necesară!';
+$_['error_text']        = 'Textul Recenziei trebuie să aibă cel puțin 1 caracter!';
+$_['error_rating']      = 'Evaluare Recenzie necesară!';
