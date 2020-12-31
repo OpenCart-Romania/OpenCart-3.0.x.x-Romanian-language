803673946ebef7e4628e656fa9ef35c720fef5d1
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:59:57 +0800

    New translations product_purchased.php (Romanian)

--- upload/admin/language/ro-ro/extension/report/product_purchased.php
+++ upload/admin/language/ro-ro/extension/report/product_purchased.php
@@ -3,26 +3,26 @@
 $_['heading_title']     = 'Products Purchased Report';
 
 // Text
-$_['text_extension']    = 'Extensions';
+$_['text_extension']    = 'Extensii';
 $_['text_edit']         = 'Edit Products Purchased Report';
 $_['text_success']      = 'Success: You have modified products purchased report!';
-$_['text_filter']       = 'Filter';
+$_['text_filter']       = 'Filtru';
 $_['text_all_status']   = 'All Statuses';
 
 // Column
-$_['column_date_start'] = 'Date Start';
-$_['column_date_end']   = 'Date End';
-$_['column_name']       = 'Product Name';
+$_['column_date_start'] = 'Dată de Început';
+$_['column_date_end']   = 'Dată de Sfârșit';
+$_['column_name']       = 'Nume Produs';
 $_['column_model']      = 'Model';
-$_['column_quantity']   = 'Quantity';
+$_['column_quantity']   = 'Cantitate';
 $_['column_total']      = 'Total';
 
 // Entry
-$_['entry_date_start']  = 'Date Start';
-$_['entry_date_end']    = 'Date End';
+$_['entry_date_start']  = 'Dată de Început';
+$_['entry_date_end']    = 'Dată de Sfârșit';
 $_['entry_status']      = 'Status';
-$_['entry_status']      = 'Status';
-$_['entry_sort_order']  = 'Sort Order';
+$_['entry_status']      = 'Stare';
+$_['entry_sort_order']  = 'Ordinea de Sortare';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify products purchased report!';
