1df1d83730435110f683e2fb48ec8e018c193805
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:54:13 +0800

    New translations manufacturer.php (Romanian)

--- upload/admin/language/ro-ro/catalog/manufacturer.php
+++ upload/admin/language/ro-ro/catalog/manufacturer.php
@@ -3,18 +3,18 @@
 $_['heading_title']     = 'Producători';
 
 // Text
-$_['text_success']      = 'Succes: Ați modificat producătorii!';
+$_['text_success']      = 'Succes: Ai modificat producătorii!';
 $_['text_list']         = 'Listă Producători';
-$_['text_add']          = 'Adăugați Producător';
-$_['text_edit']         = 'Editați Producător';
+$_['text_add']          = 'Adaugă Producător';
+$_['text_edit']         = 'Editează Producător';
 $_['text_default']      = 'Implicit';
 $_['text_percent']      = 'Procentaj';
 $_['text_amount']       = 'Cantitate Fixă';
-$_['text_keyword']      = 'Nu utilizaţi spaţii, în schimb înlocuiţi-le cu - și asigurați-vă că URL-ul SEO este unic la nivel global.';
+$_['text_keyword']      = 'Nu utiliza spații, în schimb înlocuieste spațiile cu - și asigură-te că SEO URL este unic global.';
 
 // Column
 $_['column_name']       = 'Nume Producător';
-$_['column_sort_order'] = 'Ordine Sortare';
+$_['column_sort_order'] = 'Ordinea de Sortare';
 $_['column_action']     = 'Acţiune';
 
 // Entry
@@ -22,12 +22,12 @@ $_['entry_name']        = 'Nume Producător';
 $_['entry_store']       = 'Magazine';
 $_['entry_keyword']     = 'Cuvânt Cheie';
 $_['entry_image']       = 'Imagine';
-$_['entry_sort_order']  = 'Ordine Sortare';
+$_['entry_sort_order']  = 'Ordinea de Sortare';
 $_['entry_type']        = 'Tip';
 
 // Error
-$_['error_permission']  = 'Atenție: Nu aveţi permisiunea de a modifica producătorii!';
+$_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica producătorii!';
 $_['error_name']        = 'Nume Producător trebuie să fie între 1 și 64 de caractere!';
-$_['error_keyword']     = 'URL-ul SEO este deja utilizat!';
-$_['error_unique']      = 'URL-ul SEO trebuie să fie unic!';
+$_['error_keyword']     = 'SEO URL este deja utilizat!';
+$_['error_unique']      = 'SEO URL trebuie să fie unic!';
 $_['error_product']     = 'Atenție: Acest producător nu poate fi șters deoarece în prezent este asociat cu %s produse!';
