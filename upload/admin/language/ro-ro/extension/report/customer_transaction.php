b55d493d487d7e6c1f871820a4956d39eb494716
Constantin DOBRE <admin@harddex.ro>
2020-12-31 15:00:01 +0800

    New translations customer_transaction.php (Romanian)

--- upload/admin/language/ro-ro/extension/report/customer_transaction.php
+++ upload/admin/language/ro-ro/extension/report/customer_transaction.php
@@ -3,25 +3,25 @@
 $_['heading_title']         = 'Customer Transaction Report';
 
 // Column
-$_['text_extension']        = 'Extensions';
+$_['text_extension']        = 'Extensii';
 $_['text_edit']             = 'Edit Customer Transaction Report';
 $_['text_success']          = 'Success: You have modified customer credit report!';
-$_['text_filter']           = 'Filter';
+$_['text_filter']           = 'Filtru';
 
 // Column
 $_['column_customer']       = 'Customer Name';
 $_['column_email']          = 'E-Mail';
-$_['column_customer_group'] = 'Customer Group';
-$_['column_status']         = 'Status';
+$_['column_customer_group'] = 'Grup Clienți';
+$_['column_status']         = 'Stare';
 $_['column_total']          = 'Total';
-$_['column_action']         = 'Action';
+$_['column_action']         = 'Acțiune';
 
 // Entry
-$_['entry_date_start']      = 'Date Start';
-$_['entry_date_end']        = 'Date End';
+$_['entry_date_start']      = 'Dată de Început';
+$_['entry_date_end']        = 'Dată de Sfârșit';
 $_['entry_customer']        = 'Customer';
-$_['entry_status']          = 'Status';
-$_['entry_sort_order']      = 'Sort Order';
+$_['entry_status']          = 'Stare';
+$_['entry_sort_order']      = 'Ordinea de Sortare';
 
 // Error
 $_['error_permission']      = 'Warning: You do not have permission to modify customer credit report!';
