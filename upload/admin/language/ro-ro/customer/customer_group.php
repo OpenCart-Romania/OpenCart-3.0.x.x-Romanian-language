8964221dcc71165c7e2e69af3f01e62403d18318
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:53:42 +0800

    New translations customer_group.php (Romanian)

--- upload/admin/language/ro-ro/customer/customer_group.php
+++ upload/admin/language/ro-ro/customer/customer_group.php
@@ -1,29 +1,29 @@
 <?php
 // Heading
-$_['heading_title']     = 'Customer Groups';
+$_['heading_title']     = 'Grupuri de Clienți';
 
 // Text
-$_['text_success']      = 'Success: You have modified customer groups!';
+$_['text_success']      = 'Succes: Ai modificat grupurile de clienți!';
 $_['text_list']         = 'Customer Group List';
 $_['text_add']          = 'Add Customer Group';
 $_['text_edit']         = 'Edit Customer Group';
 
 // Column
 $_['column_name']       = 'Customer Group Name';
-$_['column_sort_order'] = 'Sort Order';
-$_['column_action']     = 'Action';
+$_['column_sort_order'] = 'Ordinea de Sortare';
+$_['column_action']     = 'Acțiune';
 
 // Entry
 $_['entry_name']        = 'Customer Group Name';
-$_['entry_description'] = 'Description';
+$_['entry_description'] = 'Descriere';
 $_['entry_approval']    = 'Approve New Customers';
-$_['entry_sort_order']  = 'Sort Order';
+$_['entry_sort_order']  = 'Ordinea de Sortare';
 
 // Help
 $_['help_approval']     = 'Customers must be approved by an administrator before they can login.';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify customer groups!';
+$_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica grupurile de clienți!';
 $_['error_name']        = 'Customer Group Name must be between 3 and 32 characters!';
 $_['error_default']     = 'Warning: This customer group cannot be deleted as it is currently assigned as the default store customer group!';
 $_['error_store']       = 'Warning: This customer group cannot be deleted as it is currently assigned to %s stores!';
