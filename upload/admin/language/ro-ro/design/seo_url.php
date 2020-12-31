d0a037e3ee7b0f69f25265a2147a91a648ce6044
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:53:37 +0800

    New translations seo_url.php (Romanian)

--- upload/admin/language/ro-ro/design/seo_url.php
+++ upload/admin/language/ro-ro/design/seo_url.php
@@ -3,28 +3,28 @@
 $_['heading_title']    = 'SEO URL';
 
 // Text
-$_['text_success']     = 'Success: You have modified SEO URL!';
-$_['text_list']        = 'SEO URL List';
-$_['text_add']         = 'Add SEO URL';
-$_['text_edit']        = 'Edit SEO URL';
-$_['text_filter']      = 'Filter';
-$_['text_default']     = 'Default';
+$_['text_success']     = 'Succes: Ai modificat SEO URL!';
+$_['text_list']        = 'Listă SEO URL';
+$_['text_add']         = 'Adaugă SEO URL';
+$_['text_edit']        = 'Editează SEO URL';
+$_['text_filter']      = 'Filtru';
+$_['text_default']     = 'Implicit';
 
 // Column
 $_['column_query']     = 'Query';
-$_['column_keyword']   = 'Keyword';
-$_['column_store']     = 'Store';
+$_['column_keyword']   = 'Cuvânt Cheie';
+$_['column_store']     = 'Magazin';
 $_['column_language']  = 'Language';
-$_['column_action']    = 'Action';
+$_['column_action']    = 'Acțiune';
 
 // Entry
 $_['entry_query']      = 'Query';
-$_['entry_keyword']    = 'Keyword';
-$_['entry_store']      = 'Store';
+$_['entry_keyword']    = 'Cuvânt Cheie';
+$_['entry_store']      = 'Magazin';
 $_['entry_language']   = 'Language';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify SEO URL!';
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica SEO URL!';
 $_['error_query']      = 'Query must be between 3 and 64 characters!';
 $_['error_keyword']    = 'Keyword must be between 3 and 64 characters!';
-$_['error_exists']     = 'Keyword already in use!';
+$_['error_exists']     = 'Cuvânt Cheie deja utilizat!';
