0da3d219e22118725ad7d5cb4acf4e14ec6686f3
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:54:58 +0800

    New translations online.php (Romanian)

--- upload/admin/language/ro-ro/extension/dashboard/online.php
+++ upload/admin/language/ro-ro/extension/dashboard/online.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']    = 'People Online';
+$_['heading_title']    = 'Oameni Online';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified dashboard online!';
-$_['text_edit']        = 'Edit Dashboard Online';
-$_['text_view']        = 'View more...';
+$_['text_extension']   = 'Extensii';
+$_['text_success']     = 'Succes: Ai modificat panoul online!';
+$_['text_edit']        = 'Editează Panoul Online';
+$_['text_view']        = 'Vezi mai mult...';
 
 // Entry
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordinea de Sortare';
+$_['entry_width']      = 'Lăţime';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify dashboard online!';
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica panoul online!';
