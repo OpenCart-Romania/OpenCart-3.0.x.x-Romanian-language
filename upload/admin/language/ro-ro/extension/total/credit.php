9d64cc86371a32f66e8035f003e53578a8922287
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:58:25 +0800

    New translations credit.php (Romanian)

--- upload/admin/language/ro-ro/extension/total/credit.php
+++ upload/admin/language/ro-ro/extension/total/credit.php
@@ -3,13 +3,13 @@
 $_['heading_title']    = 'Store Credit';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified store credit total!';
 $_['text_edit']        = 'Edit Store Credit Total';
 
 // Entry
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordinea de Sortare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify store credit total!';
