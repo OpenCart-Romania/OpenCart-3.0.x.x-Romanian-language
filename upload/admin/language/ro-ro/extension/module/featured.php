25c14c82bf7cc5575a390cbfeb964f580f51d66b
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:52:17 +0800

    New translations featured.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/featured.php
+++ upload/admin/language/ro-ro/extension/module/featured.php
@@ -3,20 +3,20 @@
 $_['heading_title']    = 'Featured';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified featured module!';
 $_['text_edit']        = 'Edit Featured Module';
 
 // Entry
 $_['entry_name']       = 'Module Name';
-$_['entry_product']    = 'Products';
+$_['entry_product']    = 'Produse';
 $_['entry_limit']      = 'Limit';
-$_['entry_width']      = 'Width';
-$_['entry_height']     = 'Height';
-$_['entry_status']     = 'Status';
+$_['entry_width']      = 'Lăţime';
+$_['entry_height']     = 'Înălţime';
+$_['entry_status']     = 'Stare';
 
 // Help
-$_['help_product']     = '(Autocomplete)';
+$_['help_product']     = '(Completare automată)';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify featured module!';
