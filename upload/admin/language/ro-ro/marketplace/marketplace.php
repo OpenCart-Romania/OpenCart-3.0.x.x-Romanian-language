288b0ac41c07f6736c085db0cec516ad87dd6fd0
Constantin DOBRE <admin@harddex.ro>
2020-12-31 15:01:11 +0800

    New translations marketplace.php (Romanian)

--- upload/admin/language/ro-ro/marketplace/marketplace.php
+++ upload/admin/language/ro-ro/marketplace/marketplace.php
@@ -5,55 +5,55 @@ $_['heading_title']      = 'Extension Marketplace';
 // Text
 $_['text_success']       = 'Success: You have modified extensions!';
 $_['text_list']          = 'Extension List';
-$_['text_filter']        = 'Filter';
+$_['text_filter']        = 'Filtru';
 $_['text_search']        = 'Search for extensions and themes';
-$_['text_category']      = 'Categories';
+$_['text_category']      = 'Categorii';
 $_['text_all']           = 'All';
 $_['text_theme']         = 'Themes';
 $_['text_marketplace']   = 'Marketplaces';
-$_['text_language']      = 'Languages';
+$_['text_language']      = 'Limbi';
 $_['text_payment']       = 'Payment';
 $_['text_shipping']      = 'Shipping';
 $_['text_module']        = 'Modules';
 $_['text_total']         = 'Order Totals';
 $_['text_feed']          = 'Feeds';
-$_['text_report']        = 'Reports';
+$_['text_report']        = 'Rapoarte';
 $_['text_other']         = 'Other';
 $_['text_free']          = 'Free';
 $_['text_paid']          = 'Paid';
 $_['text_purchased']     = 'Purchased';
 $_['text_date_modified'] = 'Date Modified';
-$_['text_date_added']    = 'Date Added';
-$_['text_rating']        = 'Rating';
+$_['text_date_added']    = 'Data Adăugării';
+$_['text_rating']        = 'Evaluare';
 $_['text_reviews']       = 'reviews';
 $_['text_compatibility'] = 'Compatibility';
-$_['text_downloaded']    = 'Downloaded';
+$_['text_downloaded']    = 'Descărcat';
 $_['text_member_since']  = 'Member since:';
-$_['text_price']         = 'Price';
+$_['text_price']         = 'Preț';
 $_['text_partner']       = 'Developed by OpenCart Partner';
 $_['text_support']       = '12 Months free support';
 $_['text_documentation'] = 'Documentation Included';
-$_['text_sales']         = 'Sales';
+$_['text_sales']         = 'Vânzări';
 $_['text_comment']       = 'Comments';
-$_['text_download']      = 'Downloading';
+$_['text_download']      = 'Descarcă';
 $_['text_install']       = 'Installing';
 $_['text_comment_add']   = 'Leave your comment';
 $_['text_write']         = 'Write your comment here..';
 $_['text_purchase']      = 'Please confirm who you are!';
 $_['text_pin']           = 'Please enter your 4 digit PIN number. This PIN number is to protect your account.';
 $_['text_secure']        = 'Do not give your PIN to any one including developers. If you require an extension seller to install an extension for you then you should email them the required extension.';
-$_['text_name']          = 'Download Name';
+$_['text_name']          = 'Nume Descărcare';
 $_['text_progress']      = 'Progress';
 $_['text_available']     = 'Available Installs';
-$_['text_action']        = 'Action';
+$_['text_action']        = 'Acțiune';
 
 // Entry
 $_['entry_pin']          = 'PIN';
 
 // Tab
-$_['tab_description']    = 'Description';
-$_['tab_documentation']  = 'Documentation';
-$_['tab_download']       = 'Download';
+$_['tab_description']    = 'Descriere';
+$_['tab_documentation']  = 'Documentație';
+$_['tab_download']       = 'Descărcare';
 $_['tab_comment']        = 'Comment';
 
 // Button
