a467d93303f02ea78835fa7b442ccf0acc993c8e
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:54:16 +0800

    New translations filter.php (Romanian)

--- upload/admin/language/ro-ro/catalog/filter.php
+++ upload/admin/language/ro-ro/catalog/filter.php
@@ -3,16 +3,16 @@
 $_['heading_title']     = 'Filtre';
 
 // Text
-$_['text_success']      = 'Succes: Ați modificat filtrele!';
+$_['text_success']      = 'Succes: Ai modificat filtrele!';
 $_['text_list']         = 'Listă Filtre';
-$_['text_add']          = 'Adăugati Filtru';
-$_['text_edit']         = 'Editați Filtru';
+$_['text_add']          = 'Adaugă Filtru';
+$_['text_edit']         = 'Editează Filtru';
 $_['text_group']        = 'Grup Filtre';
 $_['text_value']        = 'Valori Filtru';
 
 // Column
 $_['column_group']      = 'Grup Filtre';
-$_['column_sort_order'] = 'Ordine Sortare';
+$_['column_sort_order'] = 'Ordinea de Sortare';
 $_['column_action']     = 'Acţiune';
 
 // Entry
@@ -21,6 +21,6 @@ $_['entry_name']        = 'Nume Filtru';
 $_['entry_sort_order']  = 'Ordinea de Sortare';
 
 // Error
-$_['error_permission']  = 'Atentie: Nu aveţi permisiunea de a modifica filtre!';
+$_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica filtrele!';
 $_['error_group']       = 'Nume Grup Filtre trebuie să fie între 1 și 64 de caractere!';
 $_['error_name']        = 'Nume Filtru trebuie să fie între 1 și 64 de caractere!';
