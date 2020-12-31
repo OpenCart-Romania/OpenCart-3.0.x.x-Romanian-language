e94a65fd86fbc6ddfc677ee342d987ca02d6546c
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:54:56 +0800

    New translations captcha.php (Romanian)

--- upload/admin/language/ro-ro/extension/extension/captcha.php
+++ upload/admin/language/ro-ro/extension/extension/captcha.php
@@ -3,13 +3,13 @@
 $_['heading_title']    = 'Captchas';
 
 // Text
-$_['text_success']     = 'Success: You have modified captchas!';
+$_['text_success']     = 'Succes: Ai modificat captchas!';
 $_['text_list']        = 'Captcha List';
 
 // Column
 $_['column_name']      = 'Captcha Name';
-$_['column_status']    = 'Status';
-$_['column_action']    = 'Action';
+$_['column_status']    = 'Stare';
+$_['column_action']    = 'Acțiune';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify captchas!';
