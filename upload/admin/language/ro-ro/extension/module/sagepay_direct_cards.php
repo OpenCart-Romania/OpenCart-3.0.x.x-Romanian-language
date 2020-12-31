b413ee3434f8e8a33444f18e2bfc45ae7af65003
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:51:57 +0800

    New translations sagepay_direct_cards.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/sagepay_direct_cards.php
+++ upload/admin/language/ro-ro/extension/module/sagepay_direct_cards.php
@@ -2,12 +2,12 @@
 // Heading
 $_['heading_title']    = 'Sagepay Direct Card Management';
 
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified Sagepay Direct Card Management module!';
 $_['text_edit']        = 'Edit Sagepay Direct Card Management Module';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify Sagepay Direct Card Management module!';
