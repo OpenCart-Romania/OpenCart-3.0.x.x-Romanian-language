ef12d720c87574ec67e9e1faea306c4fcd19ea5e
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:52:02 +0800

    New translations cardinity.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/cardinity.php
+++ upload/admin/language/ro-ro/extension/payment/cardinity.php
@@ -3,7 +3,7 @@
 $_['heading_title']			= 'Cardinity';
 
 // Text
-$_['text_extension']		= 'Extensions';
+$_['text_extension']		= 'Extensii';
 $_['text_success']			= 'Success: You have modified Cardinity payment module!';
 $_['text_edit']             = 'Edit Cardinity';
 $_['text_cardinity']		= '<a href="http://cardinity.com/?crdp=opencart" target="_blank"><img src="view/image/payment/cardinity.png" alt="Cardinity" title="Cardinity" style="border: 1px solid #EEEEEE;" /></a>';
@@ -18,19 +18,19 @@ $_['text_error_generic']	= 'Error: There was an error with your request. Please 
 
 // Column
 $_['column_refund']			= 'Refund';
-$_['column_date']			= 'Date';
+$_['column_date']			= 'Dată';
 $_['column_refund_history'] = 'Refund History';
-$_['column_action']			= 'Action';
-$_['column_status']			= 'Status';
+$_['column_action']			= 'Acțiune';
+$_['column_status']			= 'Stare';
 $_['column_amount']			= 'Amount';
-$_['column_description']	= 'Description';
+$_['column_description']	= 'Descriere';
 
 // Entry
 $_['entry_total']			= 'Total';
 $_['entry_order_status']	= 'Order Status';
 $_['entry_geo_zone']		= 'Geo Zone';
-$_['entry_status']			= 'Status';
-$_['entry_sort_order']		= 'Sort Order';
+$_['entry_status']			= 'Stare';
+$_['entry_sort_order']		= 'Ordinea de Sortare';
 $_['entry_key']				= 'Key';
 $_['entry_secret']			= 'Secret';
 $_['entry_debug']			= 'Debug';
@@ -49,4 +49,4 @@ $_['error_composer']		= 'Unable to load Cardinity SDK. Please download a compile
 $_['error_php_version']		= 'Minimum version of PHP 5.4.0 is required!';
 $_['error_permission']		= 'Warning: You do not have permission to modify payment Cardinity!';
 $_['error_connection']		= 'There was a problem establishing a connection to the Cardinity API. Please check your Key and Secret settings.';
-$_['error_warning']			= 'Warning: Please check the form carefully for errors!';
+$_['error_warning']			= 'Atenție: Te rugăm să verifici cu atenție formularul pentru erori!';
