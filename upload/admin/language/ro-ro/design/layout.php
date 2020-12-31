0703c28ea07358f28a940f69e48c5d81d650de4b
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:53:39 +0800

    New translations layout.php (Romanian)

--- upload/admin/language/ro-ro/design/layout.php
+++ upload/admin/language/ro-ro/design/layout.php
@@ -1,16 +1,16 @@
 <?php
 // Heading
-$_['heading_title']       = 'Layouts';
+$_['heading_title']       = 'Layout-uri';
 
 // Text
-$_['text_success']        = 'Success: You have modified layouts!';
+$_['text_success']        = 'Succes: Ai modificat layout-urile!';
 $_['text_list']           = 'Layout List';
-$_['text_add']            = 'Add Layout';
+$_['text_add']            = 'Adaugă Layout';
 $_['text_edit']           = 'Edit Layout';
 $_['text_remove']         = 'Remove';
 $_['text_route']          = 'Choose the store and routes to be used with this layout';
 $_['text_module']         = 'Choose the position of the modules';
-$_['text_default']        = 'Default';
+$_['text_default']        = 'Implicit';
 $_['text_content_top']    = 'Content Top';
 $_['text_content_bottom'] = 'Content Bottom';
 $_['text_column_left']    = 'Column Left';
@@ -18,7 +18,7 @@ $_['text_column_right']   = 'Column Right';
 
 // Column
 $_['column_name']         = 'Layout Name';
-$_['column_action']       = 'Action';
+$_['column_action']       = 'Acțiune';
 
 // Entry
 $_['entry_name']          = 'Layout Name';
@@ -27,7 +27,7 @@ $_['entry_route']         = 'Route';
 $_['entry_module']        = 'Module';
 
 // Error
-$_['error_permission']    = 'Warning: You do not have permission to modify layouts!';
+$_['error_permission']    = 'Atenție: Nu ai permisiunea de a modifica layout-urile!';
 $_['error_name']          = 'Layout Name must be between 3 and 64 characters!';
 $_['error_default']       = 'Warning: This layout cannot be deleted as it is currently assigned as the default store layout!';
 $_['error_store']         = 'Warning: This layout cannot be deleted as it is currently assigned to %s stores!';
