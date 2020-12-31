e75cd26341d5e29611804accd35d1690d743dcb2
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:52:58 +0800

    New translations paymate.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/paymate.php
+++ upload/admin/language/ro-ro/extension/payment/paymate.php
@@ -3,20 +3,20 @@
 $_['heading_title']		 = 'Paymate';
 
 // Text
-$_['text_extension']	 = 'Extensions';
+$_['text_extension']	 = 'Extensii';
 $_['text_success']		 = 'Success: You have modified Paymate account details!';
 $_['text_edit']          = 'Edit Paymate';
 $_['text_paymate']		 = '<img src="view/image/payment/paymate.png" alt="Paymate" title="Paymate" style="border: 1px solid #EEEEEE;" />';
 
 // Entry
 $_['entry_username']	 = 'Paymate Username';
-$_['entry_password']	 = 'Password';
+$_['entry_password']	 = 'Parolă';
 $_['entry_test']		 = 'Test Mode';
 $_['entry_total']		 = 'Total';
 $_['entry_order_status'] = 'Order Status';
 $_['entry_geo_zone']	 = 'Geo Zone';
-$_['entry_status']		 = 'Status';
-$_['entry_sort_order']	 = 'Sort Order';
+$_['entry_status']		 = 'Stare';
+$_['entry_sort_order']	 = 'Ordinea de Sortare';
 
 // Help
 $_['help_password']		 = 'Just use some random password. This will be used to make sure the payment information is not interfered with after being sent to the payment gateway.';
