8ceee5064aa2d2492e230b1aba311ee7d8ff82a3
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:59:10 +0800

    New translations wechat_pay.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/wechat_pay.php
+++ upload/admin/language/ro-ro/extension/payment/wechat_pay.php
@@ -11,7 +11,7 @@
 $_['heading_title']                  = 'Wechat Pay';
 
 // Text
-$_['text_extension']                 = 'Extensions';
+$_['text_extension']                 = 'Extensii';
 $_['text_success']                   = 'Success: You have modified Wechat account details!';
 $_['text_edit']                      = 'Edit Wechat Pay';
 $_['text_wechat_pay']                = '<a target="_BLANK" href="https://pay.weixin.qq.com"><img src="view/image/payment/wechat.png" alt="Wechat Pay Website" title="Wechat Pay Website" style="border: 1px solid #EEEEEE;" /></a>';
@@ -26,8 +26,8 @@ $_['entry_total']                    = 'Total';
 $_['entry_currency']                 = 'Currency';
 $_['entry_completed_status']         = 'Completed Status';
 $_['entry_geo_zone']                 = 'Geo Zone';
-$_['entry_status']                   = 'Status';
-$_['entry_sort_order']               = 'Sort Order';
+$_['entry_status']                   = 'Stare';
+$_['entry_sort_order']               = 'Ordinea de Sortare';
 
 // Help
 $_['help_total']                     = 'The checkout total the order must reach before this payment method becomes active';
