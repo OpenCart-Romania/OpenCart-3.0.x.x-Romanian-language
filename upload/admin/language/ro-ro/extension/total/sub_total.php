614abadcb8acf3f5b8b0bb4c642c424dae6b0cfe
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:58:09 +0800

    New translations sub_total.php (Romanian)

--- upload/admin/language/ro-ro/extension/total/sub_total.php
+++ upload/admin/language/ro-ro/extension/total/sub_total.php
@@ -3,13 +3,13 @@
 $_['heading_title']    = 'Sub-Total';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified sub-total total!';
 $_['text_edit']        = 'Edit Sub-Total Total';
 
 // Entry
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordinea de Sortare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify sub-total total!';
