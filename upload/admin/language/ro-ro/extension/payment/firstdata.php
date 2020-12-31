0ec1214ed83b4c405702c5e1245919fe8466c8ce
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:53:20 +0800

    New translations firstdata.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/firstdata.php
+++ upload/admin/language/ro-ro/extension/payment/firstdata.php
@@ -3,13 +3,13 @@
 $_['heading_title']					 = 'First Data EMEA Connect (3DSecure enabled)';
 
 // Text
-$_['text_extension']				 = 'Extensions';
+$_['text_extension']				 = 'Extensii';
 $_['text_success']					 = 'Success: You have modified First Data account details!';
 $_['text_edit']                      = 'Edit First Data EMEA Connect (3DSecure enabled)';
 $_['text_notification_url']			 = 'Notification URL';
 $_['text_live']						 = 'Live';
 $_['text_demo']						 = 'Demo';
-$_['text_enabled']					 = 'Enabled';
+$_['text_enabled']					 = 'Activat';
 $_['text_merchant_id']				 = 'Store ID';
 $_['text_secret']					 = 'Shared secret';
 $_['text_capture_ok']				 = 'Capture was successful';
@@ -28,7 +28,7 @@ $_['text_order_total']				 = 'Total authorised';
 $_['text_total_captured']			 = 'Total captured';
 $_['text_transactions']				 = 'Transactions';
 $_['text_column_amount']			 = 'Amount';
-$_['text_column_type']				 = 'Type';
+$_['text_column_type']				 = 'Tip';
 $_['text_column_date_added']		 = 'Created';
 $_['text_confirm_void']				 = 'Are you sure you want to void the payment?';
 $_['text_confirm_capture']			 = 'Are you sure you want to capture the payment?';
@@ -37,9 +37,9 @@ $_['text_confirm_capture']			 = 'Are you sure you want to capture the payment?';
 $_['entry_merchant_id']				 = 'Store ID';
 $_['entry_secret']					 = 'Shared secret';
 $_['entry_total']					 = 'Total';
-$_['entry_sort_order']				 = 'Sort order';
+$_['entry_sort_order']				 = 'Ordinea de Sortare';
 $_['entry_geo_zone']				 = 'Geo zone';
-$_['entry_status']					 = 'Status';
+$_['entry_status']					 = 'Stare';
 $_['entry_debug']					 = 'Debug logging';
 $_['entry_live_demo']				 = 'Live / Demo';
 $_['entry_auto_settle']			  	 = 'Settlement type';
