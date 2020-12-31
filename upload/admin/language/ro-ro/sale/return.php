37efa6017947af671badb56ccd7b27ef4f2a81a2
Constantin DOBRE <admin@harddex.ro>
2020-12-31 15:01:52 +0800

    New translations return.php (Romanian)

--- upload/admin/language/ro-ro/sale/return.php
+++ upload/admin/language/ro-ro/sale/return.php
@@ -12,52 +12,52 @@ $_['text_unopened']        = 'Unopened';
 $_['text_order']           = 'Order Information';
 $_['text_product']         = 'Product Information &amp; Reason for Return';
 $_['text_history']         = 'History';
-$_['text_history_add']     = 'Add History';
+$_['text_history_add']     = 'Adaugă Istoric';
 
 // Column
 $_['column_return_id']     = 'Return ID';
-$_['column_order_id']      = 'Order ID';
+$_['column_order_id']      = 'ID Comandă';
 $_['column_customer']      = 'Customer';
-$_['column_product']       = 'Product';
+$_['column_product']       = 'Produs';
 $_['column_model']         = 'Model';
-$_['column_status']        = 'Status';
-$_['column_date_added']    = 'Date Added';
+$_['column_status']        = 'Stare';
+$_['column_date_added']    = 'Data Adăugării';
 $_['column_date_modified'] = 'Date Modified';
 $_['column_comment']       = 'Comment';
 $_['column_notify']        = 'Customer Notified';
-$_['column_action']        = 'Action';
+$_['column_action']        = 'Acțiune';
 
 // Entry
 $_['entry_customer']       = 'Customer';
-$_['entry_order_id']       = 'Order ID';
+$_['entry_order_id']       = 'ID Comandă';
 $_['entry_date_ordered']   = 'Order Date';
-$_['entry_firstname']      = 'First Name';
-$_['entry_lastname']       = 'Last Name';
+$_['entry_firstname']      = 'Prenume';
+$_['entry_lastname']       = 'Nume de Familie';
 $_['entry_email']          = 'E-Mail';
 $_['entry_telephone']      = 'Telephone';
-$_['entry_product']        = 'Product';
+$_['entry_product']        = 'Produs';
 $_['entry_model']          = 'Model';
-$_['entry_quantity']       = 'Quantity';
+$_['entry_quantity']       = 'Cantitate';
 $_['entry_opened']         = 'Opened';
 $_['entry_comment']        = 'Comment';
 $_['entry_return_reason']  = 'Return Reason';
 $_['entry_return_action']  = 'Return Action';
-$_['entry_return_status']  = 'Return Status';
+$_['entry_return_status']  = 'Status';
 $_['entry_notify']         = 'Notify Customer';
 $_['entry_return_id']      = 'Return ID';
-$_['entry_date_added']     = 'Date Added';
+$_['entry_date_added']     = 'Data Adăugării';
 $_['entry_date_modified']  = 'Date Modified';
 
 // Help
-$_['help_product']         = '(Autocomplete)';
+$_['help_product']         = '(Completare automată)';
 
 // Error
-$_['error_warning']        = 'Warning: Please check the form carefully for errors!';
+$_['error_warning']        = 'Atenție: Te rugăm să verifici cu atenție formularul pentru erori!';
 $_['error_permission']     = 'Warning: You do not have permission to modify returns!';
 $_['error_order_id']       = 'Order ID required!';
-$_['error_firstname']      = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']       = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']          = 'E-Mail Address does not appear to be valid!';
+$_['error_firstname']      = 'Prenumele trebuie să fie între 1 și 32 de caractere!';
+$_['error_lastname']       = 'Numele de Familie trebuie să fie între 1 și 32 de caractere!';
+$_['error_email']          = 'Adresa E-Mail nu pare să fie validă!';
 $_['error_telephone']      = 'Telephone must be between 3 and 32 characters!';
-$_['error_product']        = 'Product Name must be greater than 3 and less than 255 characters!';
-$_['error_model']          = 'Product Model must be greater than 3 and less than 64 characters!';
+$_['error_product']        = 'Nume Produs trebuie să fie mai mare de 3 și mai mic de 255 de caractere!';
+$_['error_model']          = 'Model Produs trebuie să fie mai mare de 3 și mai mic de 64 de caractere!';
