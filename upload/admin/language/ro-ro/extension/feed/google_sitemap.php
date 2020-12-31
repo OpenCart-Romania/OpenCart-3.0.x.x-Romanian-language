c2573ef5ff04dab998f27f0d00fb9ee18385a02b
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:54:34 +0800

    New translations google_sitemap.php (Romanian)

--- upload/admin/language/ro-ro/extension/feed/google_sitemap.php
+++ upload/admin/language/ro-ro/extension/feed/google_sitemap.php
@@ -3,12 +3,12 @@
 $_['heading_title']    = 'Google Sitemap';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified Google Sitemap feed!';
 $_['text_edit']        = 'Edit Google Sitemap';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Stare';
 $_['entry_data_feed']  = 'Data Feed Url';
 
 // Error
