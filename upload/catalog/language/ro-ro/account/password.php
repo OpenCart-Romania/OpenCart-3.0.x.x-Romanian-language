18722596d6afab4259eb1a9d643a44d1a6d800bb
Constantin DOBRE <admin@harddex.ro>
2020-12-31 15:00:29 +0800

    New translations password.php (Romanian)

--- upload/catalog/language/ro-ro/account/password.php
+++ upload/catalog/language/ro-ro/account/password.php
@@ -3,14 +3,14 @@
 $_['heading_title']  = 'Change Password';
 
 // Text
-$_['text_account']   = 'Account';
+$_['text_account']   = 'Cont';
 $_['text_password']  = 'Your Password';
-$_['text_success']   = 'Success: Your password has been successfully updated.';
+$_['text_success']   = 'Succes: Parola ta a fost actualizată cu succes.';
 
 // Entry
-$_['entry_password'] = 'Password';
+$_['entry_password'] = 'Parolă';
 $_['entry_confirm']  = 'Password Confirm';
 
 // Error
-$_['error_password'] = 'Password must be between 4 and 20 characters!';
+$_['error_password'] = 'Parola trebuie să fie între 4 și 20 de caractere!';
 $_['error_confirm']  = 'Password confirmation does not match password!';
