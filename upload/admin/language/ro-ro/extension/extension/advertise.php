bf827864ac805bf6f5e37ef81287643baef54d7b
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:55:17 +0800

    New translations advertise.php (Romanian)

--- upload/admin/language/ro-ro/extension/extension/advertise.php
+++ upload/admin/language/ro-ro/extension/extension/advertise.php
@@ -6,12 +6,12 @@ $_['heading_title'] = "Advertising";
 
 // Columns
 $_['column_name']   = "Advertising Name";
-$_['column_status'] = "Status";
-$_['column_action'] = "Action";
+$_['column_status'] = "Stare";
+$_['column_action'] = "Acțiune";
 
 
 // Text
-$_['text_success']   = "<strong>Success:</strong> You have modified advertising!";
+$_['text_success']   = "<strong>Succes:</strong> Ai modificat publicitatea!";
 
 // Error
 $_['error_adblock'] = "It looks like you are using an ad blocker. In order to use this Advertising section, please disable your ad blocker for your OpenCart admin panel.";
