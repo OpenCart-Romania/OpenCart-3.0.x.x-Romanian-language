07310f94646804c5711c224097450d3ebf3ab7cd
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:58:27 +0800

    New translations handling.php (Romanian)

--- upload/admin/language/ro-ro/extension/total/handling.php
+++ upload/admin/language/ro-ro/extension/total/handling.php
@@ -3,16 +3,16 @@
 $_['heading_title']    = 'Handling Fee';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified handling fee total!';
 $_['text_edit']        = 'Edit Handling Fee Total';
 
 // Entry
 $_['entry_total']      = 'Order Total';
 $_['entry_fee']        = 'Fee';
-$_['entry_tax_class']  = 'Tax Class';
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_tax_class']  = 'Clasă de Taxare';
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordinea de Sortare';
 
 // Help
 $_['help_total']       = 'The checkout total the order must reach before this order total becomes active.';
