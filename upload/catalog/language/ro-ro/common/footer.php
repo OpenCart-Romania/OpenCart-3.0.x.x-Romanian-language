bfe0d1bf7738e28e2e20d42cb98a5a7137a5117e
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:56:04 +0800

    New translations footer.php (Romanian)

--- upload/catalog/language/ro-ro/common/footer.php
+++ upload/catalog/language/ro-ro/common/footer.php
@@ -1,16 +1,16 @@
 <?php
 // Text
-$_['text_information']  = 'Information';
+$_['text_information']  = 'Informaţii';
 $_['text_service']      = 'Customer Service';
 $_['text_extra']        = 'Extras';
 $_['text_contact']      = 'Contact Us';
-$_['text_return']       = 'Returns';
+$_['text_return']       = 'Returnări';
 $_['text_sitemap']      = 'Site Map';
 $_['text_manufacturer'] = 'Brands';
 $_['text_voucher']      = 'Gift Certificates';
 $_['text_affiliate']    = 'Affiliate';
 $_['text_special']      = 'Specials';
-$_['text_account']      = 'My Account';
+$_['text_account']      = 'Contul Meu';
 $_['text_order']        = 'Order History';
 $_['text_wishlist']     = 'Wish List';
 $_['text_newsletter']   = 'Newsletter';
