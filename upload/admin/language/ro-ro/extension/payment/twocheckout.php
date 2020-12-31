4c028a5f80549f9bfbfb259fe6d95cc33f8ccb84
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:59:34 +0800

    New translations twocheckout.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/twocheckout.php
+++ upload/admin/language/ro-ro/extension/payment/twocheckout.php
@@ -3,7 +3,7 @@
 $_['heading_title']		 = '2Checkout';
 
 // Text
-$_['text_extension']	 = 'Extensions';
+$_['text_extension']	 = 'Extensii';
 $_['text_success']		 = 'Success: You have modified 2Checkout account details!';
 $_['text_edit']          = 'Edit 2Checkout';
 $_['text_twocheckout']	 = '<a href="https://www.2checkout.com/2co/affiliate?affiliate=1596408" target="_blank"><img src="view/image/payment/2checkout.png" alt="2Checkout" title="2Checkout" style="border: 1px solid #EEEEEE;" /></a>';
@@ -16,8 +16,8 @@ $_['entry_test']		 = 'Test Mode';
 $_['entry_total']		 = 'Total';
 $_['entry_order_status'] = 'Order Status';
 $_['entry_geo_zone']	 = 'Geo Zone';
-$_['entry_status']		 = 'Status';
-$_['entry_sort_order']	 = 'Sort Order';
+$_['entry_status']		 = 'Stare';
+$_['entry_sort_order']	 = 'Ordinea de Sortare';
 
 // Help
 $_['help_secret']		 = 'The secret word to confirm transactions with (must be the same as defined on the merchant account configuration page).';
