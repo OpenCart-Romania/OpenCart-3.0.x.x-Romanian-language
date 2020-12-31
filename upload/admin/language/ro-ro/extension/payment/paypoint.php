16ddffe799cad9ed1d3f8beab69018a550de5753
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:52:56 +0800

    New translations paypoint.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/paypoint.php
+++ upload/admin/language/ro-ro/extension/payment/paypoint.php
@@ -3,7 +3,7 @@
 $_['heading_title']		 = 'PayPoint';
 
 // Text
-$_['text_extension']	 = 'Extensions';
+$_['text_extension']	 = 'Extensii';
 $_['text_success']		 = 'Success: You have modified PayPoint account details!';
 $_['text_edit']          = 'Edit PayPoint';
 $_['text_paypoint']		 = '<a href="https://www.paypoint.net/partners/opencart" target="_blank"><img src="view/image/payment/paypoint.png" alt="PayPoint" title="PayPoint" style="border: 1px solid #EEEEEE;" /></a>';
@@ -18,8 +18,8 @@ $_['entry_test']		 = 'Test Mode';
 $_['entry_total']		 = 'Total';
 $_['entry_order_status'] = 'Order Status';
 $_['entry_geo_zone']	 = 'Geo Zone';
-$_['entry_status']		 = 'Status';
-$_['entry_sort_order']	 = 'Sort Order';
+$_['entry_status']		 = 'Stare';
+$_['entry_sort_order']	 = 'Ordinea de Sortare';
 
 // Help
 $_['help_password']		 = 'Leave empty if you do not have "Digest Key Authentication" enabled on your account.';
