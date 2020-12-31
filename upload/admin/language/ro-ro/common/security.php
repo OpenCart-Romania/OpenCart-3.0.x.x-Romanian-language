3bd6b1304865390410a8901402a441298bdfc357
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:53:49 +0800

    New translations security.php (Romanian)

--- upload/admin/language/ro-ro/common/security.php
+++ upload/admin/language/ro-ro/common/security.php
@@ -3,16 +3,16 @@
 $_['heading_title']    = 'Notificare Importantă de Securitate!';
 
 // Text
-$_['text_success']     = 'Succes: Ați modificat directorul "storage"!';
-$_['text_admin']       = 'Editați admin/config.php și modificați';
-$_['text_security']    = 'Este foarte important să mutați directorul "storage" în afara directorului web (e.g. public_html, www or htdocs).';
-$_['text_choose']      = 'Alegeți cum să mutați directorul de stocare';
+$_['text_success']     = 'Succes: Ai modificat directorul "storage"!';
+$_['text_admin']       = 'Editează admin/config.php și modifică';
+$_['text_security']    = 'Este foarte important să muți directorul "storage" în afara directorului web (e.g. public_html, www or htdocs).';
+$_['text_choose']      = 'Alege cum să muți directorul de stocare';
 $_['text_automatic']   = 'Mutare automată';
 $_['text_manual']      = 'Mutare manuală';
 $_['text_move']        = 'Mutare';
 $_['text_to']          = 'către';
-$_['text_config']      = 'Editați modificarea config.php';
-$_['text_admin']       = 'Edit admin/config.php and change';
+$_['text_config']      = 'Editează modificarea config.php';
+$_['text_admin']       = 'Editează admin/config.php și modifică';
 
 // Entry
 $_['entry_directory']  = 'Director';
@@ -22,8 +22,8 @@ $_['button_move']      = 'Mutare';
 $_['button_manual']    = 'Manual';
 
 // Error
-$_['error_permission'] = 'Atenție: Nu aveți permisiunea de a modifica directorul "storage"!';
-$_['error_path']       = 'Atenție: Cale nevalidă!';
-$_['error_directory']  = 'Atenție: Director nevalid!';
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica directorul "storage"!';
+$_['error_path']       = 'Atenție: Cale invalidă!';
+$_['error_directory']  = 'Atenție: Director invalid!';
 $_['error_exists']     = 'Atenție: Directorul există deja!';
 $_['error_writable']   = 'Atenție: config.php and admin/config.php trebuie să fie editabile!';
