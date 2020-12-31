ead83b27e943eb5237b8c0a43fd59bc6af103823
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:54:18 +0800

    New translations download.php (Romanian)

--- upload/admin/language/ro-ro/catalog/download.php
+++ upload/admin/language/ro-ro/catalog/download.php
@@ -3,11 +3,11 @@
 $_['heading_title']     = 'Descărcări';
 
 // Text
-$_['text_success']      = 'Succes: Ați modificat descărcările!';
+$_['text_success']      = 'Succes: Ai modificat descărcările!';
 $_['text_list']         = 'Lista de Descărcări';
-$_['text_add']          = 'Adăugați Descărcare';
-$_['text_edit']         = 'Editați Descărcare';
-$_['text_upload']       = 'Fișierul dvs. a fost încărcat cu succes!';
+$_['text_add']          = 'Adaugă Descărcare';
+$_['text_edit']         = 'Editează Descărcare';
+$_['text_upload']       = 'Fișierul tău a fost încărcat cu succes!';
 
 // Column
 $_['column_name']       = 'Nume Descărcare';
@@ -20,11 +20,11 @@ $_['entry_filename']    = 'Nume Fișier';
 $_['entry_mask']        = 'Mască';
 
 // Help
-$_['help_filename']     = 'Puteți încărca prin butonul de încărcare sau utilizați FTP pentru a încărca în directorul de descărcare și introduceți detaliile de mai jos.';
-$_['help_mask']         = 'Se recomandă ca numele fișierului și masca să fie diferite pentru a opri persoanele care încearcă să se conecteze direct la descărcările dvs.';
+$_['help_filename']     = 'Poți încărca prin butonul de încărcare sau utilizeaza FTP pentru a încărca în directorul de descărcare și introdu detaliile de mai jos.';
+$_['help_mask']         = 'Se recomandă ca numele fișierului și masca să fie diferite pentru a opri persoanele care încearcă să se conecteze direct la descărcările tale.';
 
 // Error
-$_['error_permission']  = 'Atenție: Nu aveți permisiunea de a modifica descărcări!';
+$_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica descărcările!';
 $_['error_name']        = 'Nume Descărcare trebuie să fie între 3 și 64 de caractere!';
 $_['error_upload']      = 'Încărcare necesară!';
 $_['error_filename']    = 'Nume Fișier trebuie să fie între 3 și 128 de caractere!';
