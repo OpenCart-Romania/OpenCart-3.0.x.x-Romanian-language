988b66bee6b6f7283ececa8c647dcd743cc80c06
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:59:54 +0800

    New translations product_viewed.php (Romanian)

--- upload/admin/language/ro-ro/extension/report/product_viewed.php
+++ upload/admin/language/ro-ro/extension/report/product_viewed.php
@@ -3,19 +3,19 @@
 $_['heading_title']    = 'Products Viewed Report';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_edit']        = 'Edit Products Viewed Report';
 $_['text_success']     = 'Success: You have reset the products viewed report!';
 
 // Column
-$_['column_name']      = 'Product Name';
+$_['column_name']      = 'Nume Produs';
 $_['column_model']     = 'Model';
 $_['column_viewed']    = 'Viewed';
 $_['column_percent']   = 'Percent';
 
 // Entry
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordinea de Sortare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify products viewed report!';
