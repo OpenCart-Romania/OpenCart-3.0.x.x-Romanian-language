dff647ac2594f609ceedfcb7e6371fb008a1d240
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:52:18 +0800

    New translations divido_calculator.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/divido_calculator.php
+++ upload/admin/language/ro-ro/extension/module/divido_calculator.php
@@ -3,12 +3,12 @@
 $_['heading_title']    = 'Divido Product Page Calculator';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified Divido Product Page Calculator!';
 $_['text_edit']        = 'Edit Divido Product Page Calculator';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify module Divido Product Page Calculator!';
