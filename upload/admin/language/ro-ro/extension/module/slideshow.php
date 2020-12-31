7a012b994c8c00a57e31e93e62890559536e2922
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:51:53 +0800

    New translations slideshow.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/slideshow.php
+++ upload/admin/language/ro-ro/extension/module/slideshow.php
@@ -3,16 +3,16 @@
 $_['heading_title']    = 'Slideshow';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified slideshow module!';
 $_['text_edit']        = 'Edit Slideshow Module';
 
 // Entry
 $_['entry_name']       = 'Module Name';
 $_['entry_banner']     = 'Banner';
-$_['entry_width']      = 'Width';
-$_['entry_height']     = 'Height';
-$_['entry_status']     = 'Status';
+$_['entry_width']      = 'Lăţime';
+$_['entry_height']     = 'Înălţime';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify slideshow module!';
