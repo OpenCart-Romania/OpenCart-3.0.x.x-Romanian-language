fe95b511fcb0a45726803ea0aa808ac2d90735e9
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:53:17 +0800

    New translations free_checkout.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/free_checkout.php
+++ upload/admin/language/ro-ro/extension/payment/free_checkout.php
@@ -3,14 +3,14 @@
 $_['heading_title']      = 'Free Checkout';
 
 // Text
-$_['text_extension']     = 'Extensions';
+$_['text_extension']     = 'Extensii';
 $_['text_success']       = 'Success: You have modified Free Checkout payment module!';
 $_['text_edit']          = 'Edit Free Checkout';
 
 // Entry
 $_['entry_order_status'] = 'Order Status';
-$_['entry_status']       = 'Status';
-$_['entry_sort_order']   = 'Sort Order';
+$_['entry_status']       = 'Stare';
+$_['entry_sort_order']   = 'Ordinea de Sortare';
 
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify payment Free Checkout!';
