3f2ac44d3cbaa620e5f45aa61c5227c18f50b0be
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:59:52 +0800

    New translations sale_order.php (Romanian)

--- upload/admin/language/ro-ro/extension/report/sale_order.php
+++ upload/admin/language/ro-ro/extension/report/sale_order.php
@@ -3,10 +3,10 @@
 $_['heading_title']     = 'Sales Report';
 
 // Text
-$_['text_extension']    = 'Extensions';
+$_['text_extension']    = 'Extensii';
 $_['text_edit']         = 'Edit Sales Report';
 $_['text_success']      = 'Success: You have modified sales report!';
-$_['text_filter']       = 'Filter';
+$_['text_filter']       = 'Filtru';
 $_['text_year']         = 'Years';
 $_['text_month']        = 'Months';
 $_['text_week']         = 'Weeks';
@@ -14,20 +14,20 @@ $_['text_day']          = 'Days';
 $_['text_all_status']   = 'All Statuses';
 
 // Column
-$_['column_date_start'] = 'Date Start';
-$_['column_date_end']   = 'Date End';
+$_['column_date_start'] = 'Dată de Început';
+$_['column_date_end']   = 'Dată de Sfârșit';
 $_['column_orders']     = 'No. Orders';
 $_['column_products']   = 'No. Products';
 $_['column_tax']        = 'Tax';
 $_['column_total']      = 'Total';
 
 // Entry
-$_['entry_date_start']  = 'Date Start';
-$_['entry_date_end']    = 'Date End';
+$_['entry_date_start']  = 'Dată de Început';
+$_['entry_date_end']    = 'Dată de Sfârșit';
 $_['entry_group']       = 'Group By';
 $_['entry_status']      = 'Status';
-$_['entry_status']      = 'Status';
-$_['entry_sort_order']  = 'Sort Order';
+$_['entry_status']      = 'Stare';
+$_['entry_sort_order']  = 'Ordinea de Sortare';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify sales report!';
