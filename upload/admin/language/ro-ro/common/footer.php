1598b7bfe78269c3f53508208cff8ec84374ff10
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:53:56 +0800

    New translations footer.php (Romanian)

--- upload/admin/language/ro-ro/common/footer.php
+++ upload/admin/language/ro-ro/common/footer.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' Toate drepturile rezervate.';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' Toate Drepturile Rezervate.';
 $_['text_version'] = 'Versiunea %s';
