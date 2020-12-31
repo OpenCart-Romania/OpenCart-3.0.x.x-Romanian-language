618176138ca057ad67cc6ed7db1ccace1f553a63
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:54:21 +0800

    New translations category.php (Romanian)

--- upload/admin/language/ro-ro/catalog/category.php
+++ upload/admin/language/ro-ro/catalog/category.php
@@ -3,24 +3,24 @@
 $_['heading_title']          = 'Categorii';
 
 // Text
-$_['text_success']           = 'Succes: Ați modificat categoriile!';
+$_['text_success']           = 'Succes: Ai modificat categoriile!';
 $_['text_list']              = 'Listă Categorii';
-$_['text_add']               = 'Adăugați Categorie';
-$_['text_edit']              = 'Editați Categorie';
+$_['text_add']               = 'Adaugă Categorie';
+$_['text_edit']              = 'Editează Categorie';
 $_['text_default']           = 'Implicit';
-$_['text_keyword']           = 'Nu utilizați spații, înlocuiți spațiile cu - și asigurați-vă că URL-ul SEO este unic la nivel global.';
+$_['text_keyword']           = 'Nu utiliza spații, în schimb înlocuieste spațiile cu - și asigură-te că SEO URL este unic global.';
 
 // Column
 $_['column_name']            = 'Nume Categorie';
-$_['column_sort_order']      = 'Ordine Sortare';
+$_['column_sort_order']      = 'Ordinea de Sortare';
 $_['column_action']          = 'Acţiune';
 
 // Entry
 $_['entry_name']             = 'Nume Categorie';
 $_['entry_description']      = 'Descriere';
-$_['entry_meta_title'] 	     = 'MetaTag Titlu';
-$_['entry_meta_keyword']     = 'MetaTag Cuvinte Cheie';
-$_['entry_meta_description'] = 'MetaTag Descriere';
+$_['entry_meta_title'] 	     = 'Meta Tag Titlu';
+$_['entry_meta_keyword']     = 'Meta Tag Cuvinte Cheie';
+$_['entry_meta_description'] = 'Meta Tag Descriere';
 $_['entry_store']            = 'Magazine';
 $_['entry_keyword']          = 'Cuvânt Cheie';
 $_['entry_parent']           = 'Părinte';
@@ -34,14 +34,14 @@ $_['entry_layout']           = 'Suprascriere Aspect';
 
 // Help
 $_['help_filter']            = '(Completare automată)';
-$_['help_top']               = 'Afișați în bara de meniu de sus. Funcționează numai pentru categoriile părinte de top.';
+$_['help_top']               = 'Afișează în bara de meniu de sus. Funcționează numai pentru categoriile părinte de top.';
 $_['help_column']            = 'Numărul de coloane de utilizat pentru cele 3 categorii inferioare. Funcționează numai pentru categoriile părinte de top.';
 
 // Error
-$_['error_warning']          = 'Atenție: Vă rugăm să verificați cu atenție formularul pentru erori!';
-$_['error_permission']       = 'Atenție: Nu aveți permisiunea de a modifica categorii!';
+$_['error_warning']          = 'Atenție: Te rugăm să verifici cu atenție formularul pentru erori!';
+$_['error_permission']       = 'Atenție: Nu ai permisiunea de a modifica categoriile!';
 $_['error_name']             = 'Numele categoriei trebuie să fie între 1 și 255 de caractere!';
 $_['error_meta_title']       = 'MetaTag Titlu trebuie să fie mai mare de 1 și mai mic de 255 de caractere!';
-$_['error_keyword']          = 'URL-ul SEO este deja utilizat!';
-$_['error_unique']           = 'URL-ul SEO trebuie să fie unic!';
+$_['error_keyword']          = 'SEO URL este deja utilizat!';
+$_['error_unique']           = 'SEO URL trebuie să fie unic!';
 $_['error_parent']           = 'Categoria părinte pe care ați ales-o este un copil al celei curente!';
