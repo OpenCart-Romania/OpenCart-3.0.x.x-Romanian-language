fee40ad431ba18175f6ad7200bd6089848031aa3
Constantin DOBRE <admin@harddex.ro>
2020-12-31 15:01:01 +0800

    New translations register.php (Romanian)

--- upload/catalog/language/ro-ro/affiliate/register.php
+++ upload/catalog/language/ro-ro/affiliate/register.php
@@ -3,7 +3,7 @@
 $_['heading_title']             = 'Affiliate Program';
 
 // Text
-$_['text_account']              = 'Account';
+$_['text_account']              = 'Cont';
 $_['text_register']             = 'Affiliate Register';
 $_['text_account_already']      = 'If you already have an account with us, please login at the <a href="%s">login page</a>.';
 $_['text_signup']               = 'To create an affiliate account, fill in the form below ensuring you complete all the required fields:';
@@ -17,9 +17,9 @@ $_['text_bank']                 = 'Bank Transfer';
 $_['text_agree']                = 'I have read and agree to the <a href="%s" class="agree"><b>%s</b></a>';
 
 // Entry
-$_['entry_customer_group']      = 'Customer Group';
-$_['entry_firstname']           = 'First Name';
-$_['entry_lastname']            = 'Last Name';
+$_['entry_customer_group']      = 'Grup Clienți';
+$_['entry_firstname']           = 'Prenume';
+$_['entry_lastname']            = 'Nume de Familie';
 $_['entry_email']               = 'E-Mail';
 $_['entry_telephone']           = 'Telephone';
 $_['entry_company']             = 'Company';
@@ -27,26 +27,26 @@ $_['entry_website']             = 'Web Site';
 $_['entry_tax']                 = 'Tax ID';
 $_['entry_payment']             = 'Payment Method';
 $_['entry_cheque']              = 'Cheque Payee Name';
-$_['entry_paypal']              = 'PayPal Email Account';
+$_['entry_paypal']              = 'Email Cont PayPal';
 $_['entry_bank_name']           = 'Bank Name';
 $_['entry_bank_branch_number']  = 'ABA/BSB number (Branch Number)';
 $_['entry_bank_swift_code']     = 'SWIFT Code';
-$_['entry_bank_account_name']   = 'Account Name';
-$_['entry_bank_account_number'] = 'Account Number';
-$_['entry_password']            = 'Password';
+$_['entry_bank_account_name']   = 'Nume Cont';
+$_['entry_bank_account_number'] = 'Număr Cont';
+$_['entry_password']            = 'Parolă';
 $_['entry_confirm']             = 'Password Confirm';
 
 // Error
-$_['error_exists']              = 'Warning: E-Mail Address is already registered!';
-$_['error_firstname']           = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']            = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']               = 'E-Mail Address does not appear to be valid!';
+$_['error_exists']              = 'Atenție: Adresa E-Mail este deja înregistrată!';
+$_['error_firstname']           = 'Prenumele trebuie să fie între 1 și 32 de caractere!';
+$_['error_lastname']            = 'Numele de Familie trebuie să fie între 1 și 32 de caractere!';
+$_['error_email']               = 'Adresa E-Mail nu pare să fie validă!';
 $_['error_telephone']           = 'Telephone must be between 3 and 32 characters!';
 $_['error_custom_field']        = '%s required!';
 $_['error_cheque']              = 'Cheque Payee Name required!';
 $_['error_paypal']              = 'PayPal Email Address does not appear to be valid!';
-$_['error_bank_account_name']   = 'Account Name required!';
-$_['error_bank_account_number'] = 'Account Number required!';
-$_['error_password']            = 'Password must be between 4 and 20 characters!';
+$_['error_bank_account_name']   = 'Nume Cont necesar!';
+$_['error_bank_account_number'] = 'Număr Cont necesar!';
+$_['error_password']            = 'Parola trebuie să fie între 4 și 20 de caractere!';
 $_['error_confirm']             = 'Password confirmation does not match password!';
 $_['error_agree']               = 'Warning: You must agree to the %s!';
