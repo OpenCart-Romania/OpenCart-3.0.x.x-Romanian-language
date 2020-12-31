e491a07ee6f7d4a090f6c737cab7ec6ae03156aa
Constantin DOBRE <admin@harddex.ro>
2020-12-31 15:01:07 +0800

    New translations affiliate.php (Romanian)

--- upload/catalog/language/ro-ro/account/affiliate.php
+++ upload/catalog/language/ro-ro/account/affiliate.php
@@ -3,9 +3,9 @@
 $_['heading_title']             = 'Your Affiliate Information';
 
 // Text
-$_['text_account']              = 'Account';
+$_['text_account']              = 'Cont';
 $_['text_affiliate']            = 'Affiliate';
-$_['text_my_affiliate']         = 'My Affiliate Account';
+$_['text_my_affiliate']         = 'Contul Meu de Afiliat';
 $_['text_payment']              = 'Payment Information';
 $_['text_cheque']               = 'Cheque';
 $_['text_paypal']               = 'PayPal';
@@ -19,17 +19,17 @@ $_['entry_website']             = 'Web Site';
 $_['entry_tax']                 = 'Tax ID';
 $_['entry_payment']             = 'Payment Method';
 $_['entry_cheque']              = 'Cheque Payee Name';
-$_['entry_paypal']              = 'PayPal Email Account';
+$_['entry_paypal']              = 'Email Cont PayPal';
 $_['entry_bank_name']           = 'Bank Name';
 $_['entry_bank_branch_number']  = 'ABA/BSB number (Branch Number)';
 $_['entry_bank_swift_code']     = 'SWIFT Code';
-$_['entry_bank_account_name']   = 'Account Name';
-$_['entry_bank_account_number'] = 'Account Number';
+$_['entry_bank_account_name']   = 'Nume Cont';
+$_['entry_bank_account_number'] = 'Număr Cont';
 
 // Error
 $_['error_agree']               = 'Warning: You must agree to the %s!';
 $_['error_cheque']              = 'Cheque Payee Name required!';
 $_['error_paypal']              = 'PayPal Email Address does not appear to be valid!';
-$_['error_bank_account_name']   = 'Account Name required!';
-$_['error_bank_account_number'] = 'Account Number required!';
+$_['error_bank_account_name']   = 'Nume Cont necesar!';
+$_['error_bank_account_number'] = 'Număr Cont necesar!';
 $_['error_custom_field']        = '%s required!';
