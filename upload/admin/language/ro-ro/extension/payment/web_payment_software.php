8e059299cf91c568c6f95ca1393895e17c3972f5
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:59:36 +0800

    New translations web_payment_software.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/web_payment_software.php
+++ upload/admin/language/ro-ro/extension/payment/web_payment_software.php
@@ -3,7 +3,7 @@
 $_['heading_title']				= 'Web Payment Software';
 
 // Text
-$_['text_extension']			= 'Extensions';
+$_['text_extension']			= 'Extensii';
 $_['text_success']				= 'Success: You have modified Web Payment Software account details!';
 $_['text_edit']                 = 'Edit AWeb Payment Software';
 $_['text_web_payment_software']	= '<a href="http://www.web-payment-software.com/" target="_blank"><img src="view/image/payment/wps-logo.jpg" alt="Web Payment Software" title="Web Payment Software" style="border: 1px solid #EEEEEE;" /></a>';
@@ -20,8 +20,8 @@ $_['entry_method']				= 'Transaction Method';
 $_['entry_total']				= 'Total';
 $_['entry_order_status']		= 'Order Status';
 $_['entry_geo_zone']			= 'Geo Zone';
-$_['entry_status']				= 'Status';
-$_['entry_sort_order']			= 'Sort Order';
+$_['entry_status']				= 'Stare';
+$_['entry_sort_order']			= 'Ordinea de Sortare';
 
 // Help
 $_['help_total']				= 'The checkout total the order must reach before this payment method becomes active.';
