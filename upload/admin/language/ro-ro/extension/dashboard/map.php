54ca24bbb8b7c7665341851e79c263e8c8789fff
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:55:11 +0800

    New translations map.php (Romanian)

--- upload/admin/language/ro-ro/extension/dashboard/map.php
+++ upload/admin/language/ro-ro/extension/dashboard/map.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']    = 'World Map';
+$_['heading_title']    = 'Harta Lumii';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified dashboard map!';
-$_['text_edit']        = 'Edit Dashboard Map';
-$_['text_order']       = 'Orders';
-$_['text_sale']        = 'Sales';
+$_['text_extension']   = 'Extensii';
+$_['text_success']     = 'Succes: Ai modificat panoul hartă!';
+$_['text_edit']        = 'Editează Panoul Hartă';
+$_['text_order']       = 'Comenzi';
+$_['text_sale']        = 'Vânzări';
 
 // Entry
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordinea de Sortare';
+$_['entry_width']      = 'Lăţime';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify dashboard map!';
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica panoul hartă!';
