a60ab0978fe874fe83470e2176b96114b7ee7d2a
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:53:30 +0800

    New translations forgotten.php (Romanian)

--- upload/admin/language/ro-ro/common/forgotten.php
+++ upload/admin/language/ro-ro/common/forgotten.php
@@ -1,12 +1,12 @@
 <?php
 // Heading
-$_['heading_title']   = 'V-ați Uitat Parola?';
+$_['heading_title']   = 'Ți-ai Uitat Parola?';
 
 // Text
 $_['text_forgotten']  = 'Parolă Uitată';
-$_['text_your_email'] = 'Adresa Dvs. de E-Mail';
-$_['text_email']      = 'Introduceți adresa de e-mail asociată contului dvs. Faceți clic pe "Trimite" pentru a vă trimite prin e-mail un link de resetare a parolei.';
-$_['text_success']    = 'Un e-mail cu un link de confirmare a fost trimis la adresa dvs. de e-mail de administrare.';
+$_['text_your_email'] = 'Adresa Ta de E-Mail';
+$_['text_email']      = 'Introdu adresa de e-mail asociată contului tau. Click pe "Trimite" pentru a-ti trimite prin e-mail un link de resetare a parolei.';
+$_['text_success']    = 'Un email cu un link de confirmare a fost trimis la adresa ta de email de admin.';
 
 // Entry
 $_['entry_email']     = 'Adresă de E-Mail';
@@ -14,6 +14,6 @@ $_['entry_password']  = 'Parolă Nouă';
 $_['entry_confirm']   = 'Confirmare';
 
 // Error
-$_['error_email']     = 'Atenție: Adresa de E-Mail nu a fost găsită în înregistrările noastre, vă rugăm să încercați din nou!';
-$_['error_password']  = 'Parola trebuie să aibă între 4 și 20 de caractere!';
+$_['error_email']     = 'Atenție: Adresa de E-Mail nu a fost găsită în înregistrările noastre, te rugăm să încerci din nou!';
+$_['error_password']  = 'Parola trebuie să fie între 4 și 20 de caractere!';
 $_['error_confirm']   = 'Parola și confirmarea parolei nu se potrivesc!';
