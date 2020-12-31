9b2d00a4434d4fa4b2e8a1690d8b2b9e2092fc4c
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:52:07 +0800

    New translations klarna_checkout_module.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/klarna_checkout_module.php
+++ upload/admin/language/ro-ro/extension/module/klarna_checkout_module.php
@@ -3,11 +3,11 @@
 $_['heading_title']	   = 'Klarna Checkout';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']	   = 'Success: You have modified module Klarna Checkout!';
 
 //Entry
-$_['entry_status']	   = 'Status';
+$_['entry_status']	   = 'Stare';
 
 //Error
 $_['error_permission'] = 'Warning: You do not have permission to modify Klarna Checkout module!';
