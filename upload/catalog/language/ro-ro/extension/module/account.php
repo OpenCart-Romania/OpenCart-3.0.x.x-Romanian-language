57206faef4f39e514b6f39e5e43169a13dc12052
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:56:35 +0800

    New translations account.php (Romanian)

--- upload/catalog/language/ro-ro/extension/module/account.php
+++ upload/catalog/language/ro-ro/extension/module/account.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title']    = 'Account';
+$_['heading_title']    = 'Cont';
 
 // Text
 $_['text_register']    = 'Register';
-$_['text_login']       = 'Login';
-$_['text_logout']      = 'Logout';
-$_['text_forgotten']   = 'Forgotten Password';
-$_['text_account']     = 'My Account';
+$_['text_login']       = 'Autentificare';
+$_['text_logout']      = 'Deconectare';
+$_['text_forgotten']   = 'Parolă Uitată';
+$_['text_account']     = 'Contul Meu';
 $_['text_edit']        = 'Edit Account';
-$_['text_password']    = 'Password';
-$_['text_address']     = 'Address Book';
+$_['text_password']    = 'Parolă';
+$_['text_address']     = 'Agendă';
 $_['text_wishlist']    = 'Wish List';
 $_['text_order']       = 'Order History';
-$_['text_download']    = 'Downloads';
-$_['text_reward']      = 'Reward Points';
-$_['text_return']      = 'Returns';
+$_['text_download']    = 'Descărcări';
+$_['text_reward']      = 'Puncte de Recompensă';
+$_['text_return']      = 'Returnări';
 $_['text_transaction'] = 'Transactions';
 $_['text_newsletter']  = 'Newsletter';
-$_['text_recurring']   = 'Recurring payments';
+$_['text_recurring']   = 'Plăţi recurente';
