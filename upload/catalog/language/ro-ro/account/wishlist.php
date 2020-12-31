8ec14d1840b3deee4d7c951e6d96f6413c046597
Constantin DOBRE <admin@harddex.ro>
2020-12-31 15:01:05 +0800

    New translations wishlist.php (Romanian)

--- upload/catalog/language/ro-ro/account/wishlist.php
+++ upload/catalog/language/ro-ro/account/wishlist.php
@@ -3,7 +3,7 @@
 $_['heading_title'] = 'My Wish List';
 
 // Text
-$_['text_account']  = 'Account';
+$_['text_account']  = 'Cont';
 $_['text_instock']  = 'In Stock';
 $_['text_wishlist'] = 'Wish List (%s)';
 $_['text_login']    = 'You must <a href="%s">login</a> or <a href="%s">create an account</a> to save <a href="%s">%s</a> to your <a href="%s">wish list</a>!';
@@ -12,9 +12,9 @@ $_['text_remove']   = 'Success: You have modified your wish list!';
 $_['text_empty']    = 'Your wish list is empty.';
 
 // Column
-$_['column_image']  = 'Image';
-$_['column_name']   = 'Product Name';
+$_['column_image']  = 'Imagine';
+$_['column_name']   = 'Nume Produs';
 $_['column_model']  = 'Model';
 $_['column_stock']  = 'Stock';
 $_['column_price']  = 'Unit Price';
-$_['column_action'] = 'Action';
+$_['column_action'] = 'Acțiune';
