5924fa55f165313c284cd4c258871af4e0b0bfdc
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:51:51 +0800

    New translations special.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/special.php
+++ upload/admin/language/ro-ro/extension/module/special.php
@@ -3,16 +3,16 @@
 $_['heading_title']    = 'Specials';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified module specials!';
 $_['text_edit']        = 'Edit Specials Module';
 
 // Entry
 $_['entry_name']       = 'Module Name';
 $_['entry_limit']      = 'Limit';
-$_['entry_width']      = 'Width';
-$_['entry_height']     = 'Height';
-$_['entry_status']     = 'Status';
+$_['entry_width']      = 'Lăţime';
+$_['entry_height']     = 'Înălţime';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify specials module!';
