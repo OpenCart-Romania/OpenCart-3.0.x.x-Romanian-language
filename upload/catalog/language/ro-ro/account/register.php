f253b1c26b983599f5360d8d3ee7aaf199980df4
Constantin DOBRE <admin@harddex.ro>
2020-12-31 15:00:33 +0800

    New translations register.php (Romanian)

--- upload/catalog/language/ro-ro/account/register.php
+++ upload/catalog/language/ro-ro/account/register.php
@@ -3,7 +3,7 @@
 $_['heading_title']        = 'Register Account';
 
 // Text
-$_['text_account']         = 'Account';
+$_['text_account']         = 'Cont';
 $_['text_register']        = 'Register';
 $_['text_account_already'] = 'If you already have an account with us, please login at the <a href="%s">login page</a>.';
 $_['text_your_details']    = 'Your Personal Details';
@@ -12,22 +12,22 @@ $_['text_your_password']   = 'Your Password';
 $_['text_agree']           = 'I have read and agree to the <a href="%s" class="agree"><b>%s</b></a>';
 
 // Entry
-$_['entry_customer_group'] = 'Customer Group';
-$_['entry_firstname']      = 'First Name';
-$_['entry_lastname']       = 'Last Name';
+$_['entry_customer_group'] = 'Grup Clienți';
+$_['entry_firstname']      = 'Prenume';
+$_['entry_lastname']       = 'Nume de Familie';
 $_['entry_email']          = 'E-Mail';
 $_['entry_telephone']      = 'Telephone';
 $_['entry_newsletter']     = 'Subscribe';
-$_['entry_password']       = 'Password';
+$_['entry_password']       = 'Parolă';
 $_['entry_confirm']        = 'Password Confirm';
 
 // Error
-$_['error_exists']         = 'Warning: E-Mail Address is already registered!';
-$_['error_firstname']      = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']       = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']          = 'E-Mail Address does not appear to be valid!';
+$_['error_exists']         = 'Atenție: Adresa E-Mail este deja înregistrată!';
+$_['error_firstname']      = 'Prenumele trebuie să fie între 1 și 32 de caractere!';
+$_['error_lastname']       = 'Numele de Familie trebuie să fie între 1 și 32 de caractere!';
+$_['error_email']          = 'Adresa E-Mail nu pare să fie validă!';
 $_['error_telephone']      = 'Telephone must be between 3 and 32 characters!';
 $_['error_custom_field']   = '%s required!';
-$_['error_password']       = 'Password must be between 4 and 20 characters!';
+$_['error_password']       = 'Parola trebuie să fie între 4 și 20 de caractere!';
 $_['error_confirm']        = 'Password confirmation does not match password!';
 $_['error_agree']          = 'Warning: You must agree to the %s!';
