28e7139b8172566ca9c1c2578fbc8d0e37a1c75e
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:54:11 +0800

    New translations option.php (Romanian)

--- upload/admin/language/ro-ro/catalog/option.php
+++ upload/admin/language/ro-ro/catalog/option.php
@@ -3,12 +3,12 @@
 $_['heading_title']      = 'Opţiuni';
 
 // Text
-$_['text_success']       = 'Succes: Ați modificat opţiunile!';
+$_['text_success']       = 'Succes: Ai modificat opţiunile!';
 $_['text_list']          = 'Listă Opțiuni';
-$_['text_add']           = 'Adăugați Opţiune';
-$_['text_edit']          = 'Editați Opţiune';
-$_['text_choose']        = 'Alegeți';
-$_['text_select']        = 'Select';
+$_['text_add']           = 'Adaugă Opţiune';
+$_['text_edit']          = 'Editează Opţiune';
+$_['text_choose']        = 'Alege';
+$_['text_select']        = 'Selectează';
 $_['text_radio']         = 'Radio';
 $_['text_checkbox']      = 'Checkbox';
 $_['text_input']         = 'Input';
@@ -23,7 +23,7 @@ $_['text_value']         = 'Valori Opțiune';
 
 // Column
 $_['column_name']        = 'Nume Opțiune';
-$_['column_sort_order']  = 'Ordine Sortare';
+$_['column_sort_order']  = 'Ordinea de Sortare';
 $_['column_action']      = 'Acţiune';
 
 // Entry
@@ -31,10 +31,10 @@ $_['entry_name']         = 'Nume Opțiune';
 $_['entry_type']         = 'Tip';
 $_['entry_option_value'] = 'Nume Valoare Opțiune';
 $_['entry_image']        = 'Imagine';
-$_['entry_sort_order']   = 'Ordine Sortare';
+$_['entry_sort_order']   = 'Ordinea de Sortare';
 
 // Error
-$_['error_permission']   = 'Atenţie: Nu aveţi permisiunea de a modifica opţiunile!';
+$_['error_permission']   = 'Atenție: Nu ai permisiunea de a modifica opțiunile!';
 $_['error_name']         = 'Nume Opțiune trebuie să fie între 1 şi 128 de caractere!';
 $_['error_type']         = 'Atenție: Valori Opțiune necesare!';
 $_['error_option_value'] = 'Nume Valoare Opţiune trebuie să fie între 1 şi 128 de caractere!';
