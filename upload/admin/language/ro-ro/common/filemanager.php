e54e1a2a8ea47e0e5f3e878793a98568cf86d097
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:53:59 +0800

    New translations filemanager.php (Romanian)

--- upload/admin/language/ro-ro/common/filemanager.php
+++ upload/admin/language/ro-ro/common/filemanager.php
@@ -1,11 +1,11 @@
 <?php
 // Heading
-$_['heading_title']    = 'Manager de Imagini';
+$_['heading_title']    = 'Manager Imagini';
 
 // Text
-$_['text_uploaded']    = 'Succes: Fișierul dvs. a fost încărcat!';
+$_['text_uploaded']    = 'Succes: Fișierul tău a fost încărcat!';
 $_['text_directory']   = 'Succes: Director creat!';
-$_['text_delete']      = 'Succes: Fișierul sau directorul dvs. a fost șters!';
+$_['text_delete']      = 'Succes: Fișierul sau directorul tău a fost șters!';
 
 // Entry
 $_['entry_search']     = 'Căutare..';
@@ -17,7 +17,7 @@ $_['error_filename']   = 'Atenție: Numele fișierului trebuie să fie între 3 
 $_['error_folder']     = 'Atenție: Numele directorului trebuie să fie între 3 și 255!';
 $_['error_exists']     = 'Atenție: Există deja un fișier sau director cu același nume!';
 $_['error_directory']  = 'Atenție: Directorul nu există!';
-$_['error_filesize']   = 'Atenție: Dimensiunea fișierului este incorectă!';
-$_['error_filetype']   = 'Atenție: Tipul de fișier este incorect!';
+$_['error_filesize']   = 'Atenție: Mărime fișier incorectă!';
+$_['error_filetype']   = 'Atenție: Tip fișier incorect!';
 $_['error_upload']     = 'Atenție: Fișierul nu a putut fi încărcat dintr-un motiv necunoscut!';
-$_['error_delete']     = 'Atenție: Nu puteți șterge acest director!';
+$_['error_delete']     = 'Atenție: Nu poți șterge acest director!';
