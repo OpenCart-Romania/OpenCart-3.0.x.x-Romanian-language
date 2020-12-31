8c8d5c1499797d32645132b5ae789fa967ae257e
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:53:48 +0800

    New translations custom_field.php (Romanian)

--- upload/admin/language/ro-ro/customer/custom_field.php
+++ upload/admin/language/ro-ro/customer/custom_field.php
@@ -1,25 +1,25 @@
 <?php
 // Heading
-$_['heading_title']        = 'Custom Fields';
+$_['heading_title']        = 'Câmpuri Personalizate';
 
 // Text
-$_['text_success']         = 'Success: You have modified custom fields!';
+$_['text_success']         = 'Succes: Ai modificat câmpurile personalizate!';
 $_['text_list']            = 'Custom Field List';
 $_['text_add']             = 'Add Custom Field';
 $_['text_edit']            = 'Edit Custom Field';
-$_['text_choose']          = 'Choose';
-$_['text_select']          = 'Select';
+$_['text_choose']          = 'Alege';
+$_['text_select']          = 'Selectează';
 $_['text_radio']           = 'Radio';
 $_['text_checkbox']        = 'Checkbox';
 $_['text_input']           = 'Input';
 $_['text_text']            = 'Text';
 $_['text_textarea']        = 'Textarea';
-$_['text_file']            = 'File';
-$_['text_date']            = 'Date';
-$_['text_datetime']        = 'Date &amp; Time';
-$_['text_time']            = 'Time';
-$_['text_account']         = 'Account';
-$_['text_address']         = 'Address';
+$_['text_file']            = 'Fişier';
+$_['text_date']            = 'Dată';
+$_['text_datetime']        = 'Dată &amp; Oră';
+$_['text_time']            = 'Oră';
+$_['text_account']         = 'Cont';
+$_['text_address']         = 'Adresă';
 $_['text_affiliate']       = 'Affiliate';
 $_['text_regex']           = 'Regex';
 $_['text_custom_field']    = 'Custom Field';
@@ -27,29 +27,29 @@ $_['text_value']           = 'Custom Field Values';
 
 // Column
 $_['column_name']          = 'Custom Field Name';
-$_['column_location']      = 'Location';
-$_['column_type']          = 'Type';
-$_['column_sort_order']    = 'Sort Order';
-$_['column_action']        = 'Action';
+$_['column_location']      = 'Locație';
+$_['column_type']          = 'Tip';
+$_['column_sort_order']    = 'Ordinea de Sortare';
+$_['column_action']        = 'Acțiune';
 
 // Entry
 $_['entry_name']           = 'Custom Field Name';
-$_['entry_location']       = 'Location';
-$_['entry_type']           = 'Type';
+$_['entry_location']       = 'Locație';
+$_['entry_type']           = 'Tip';
 $_['entry_value']          = 'Value';
 $_['entry_validation']     = 'Validation';
 $_['entry_custom_value']   = 'Custom Field Value Name';
-$_['entry_customer_group'] = 'Customer Group';
-$_['entry_required']       = 'Required';
-$_['entry_status']         = 'Status';
-$_['entry_sort_order']     = 'Sort Order';
+$_['entry_customer_group'] = 'Grup Clienți';
+$_['entry_required']       = 'Necesar';
+$_['entry_status']         = 'Stare';
+$_['entry_sort_order']     = 'Ordinea de Sortare';
 
 // Help
 $_['help_regex']           = 'Use regex. E.g: /[a-zA-Z0-9_-]/';
 $_['help_sort_order']      = 'Use minus to count backwards from the last field in the set.';
 
 // Error
-$_['error_permission']     = 'Warning: You do not have permission to modify custom fields!';
+$_['error_permission']     = 'Atenție: Nu ai permisiunea de a modifica câmpurile personalizate!';
 $_['error_name']           = 'Custom Field Name must be between 1 and 128 characters!';
 $_['error_type']           = 'Warning: Custom Field Values required!';
 $_['error_custom_value']   = 'Custom Value Name must be between 1 and 128 characters!';
