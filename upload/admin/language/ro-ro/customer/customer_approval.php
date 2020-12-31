51aa85173b1578ae20a13d621c53961cce2f36e0
Constantin DOBRE <admin@harddex.ro>
2020-12-31 14:53:44 +0800

    New translations customer_approval.php (Romanian)

--- upload/admin/language/ro-ro/customer/customer_approval.php
+++ upload/admin/language/ro-ro/customer/customer_approval.php
@@ -1,28 +1,28 @@
 <?php
 // Heading
-$_['heading_title']         = 'Customer Approvals';
+$_['heading_title']         = 'Aprobări Clienți';
 
 // Text
-$_['text_success']          = 'Success: You have modified customer approvals!';
+$_['text_success']          = 'Succes: Ai modificat aprobările clienților!';
 $_['text_list']             = 'Customer Approval List';
-$_['text_default']          = 'Default';
+$_['text_default']          = 'Implicit';
 $_['text_customer']         = 'Customer';
 $_['text_affiliate']        = 'Affiliate';
 
 // Column
 $_['column_name']           = 'Customer Name';
 $_['column_email']          = 'E-Mail';
-$_['column_customer_group'] = 'Customer Group';
-$_['column_type']           = 'Type';
-$_['column_date_added']     = 'Date Added';
-$_['column_action']         = 'Action';
+$_['column_customer_group'] = 'Grup Clienți';
+$_['column_type']           = 'Tip';
+$_['column_date_added']     = 'Data Adăugării';
+$_['column_action']         = 'Acțiune';
 
 // Entry
 $_['entry_name']            = 'Customer Name';
 $_['entry_email']           = 'E-Mail';
-$_['entry_customer_group']  = 'Customer Group';
-$_['entry_type']            = 'Type';
-$_['entry_date_added']      = 'Date Added';
+$_['entry_customer_group']  = 'Grup Clienți';
+$_['entry_type']            = 'Tip';
+$_['entry_date_added']      = 'Data Adăugării';
 
 // Error
-$_['error_permission']      = 'Warning: You do not have permission to modify customer approvals!';
+$_['error_permission']      = 'Atenție: Nu ai permisiunea de a modifica aprobările clienților!';
