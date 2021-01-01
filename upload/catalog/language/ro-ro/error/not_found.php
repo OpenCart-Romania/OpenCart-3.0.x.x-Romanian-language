8deeaeed2a515beecdff5d93e3424e3c0c21a2e3
Constantin DOBRE <admin@harddex.ro>
2021-01-01 14:50:30 +0800

    New translations not_found.php (Romanian)

--- upload/catalog/language/ro-ro/error/not_found.php
+++ upload/catalog/language/ro-ro/error/not_found.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title'] = 'The page you requested cannot be found!';
+$_['heading_title'] = 'Pagina pe care ai solicitat-o nu a putut fi găsită!';
 
 // Text
-$_['text_error']    = 'The page you requested cannot be found.';
+$_['text_error']    = 'Pagina pe care ai solicitat-o nu a putut fi găsită.';
