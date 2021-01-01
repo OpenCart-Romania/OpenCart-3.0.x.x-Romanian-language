bba3515d66a8048f932087bf82329df03607d9f0
Constantin DOBRE <admin@harddex.ro>
2021-01-01 14:51:54 +0800

    New translations order.php (Romanian)

--- upload/catalog/language/ro-ro/account/order.php
+++ upload/catalog/language/ro-ro/account/order.php
@@ -1,28 +1,28 @@
 <?php
 // Heading
-$_['heading_title']         = 'Order History';
+$_['heading_title']         = 'Istoric Comenzi';
 
 // Text
 $_['text_account']          = 'Cont';
-$_['text_order']            = 'Order Information';
-$_['text_order_detail']     = 'Order Details';
-$_['text_invoice_no']       = 'Invoice No.:';
-$_['text_order_id']         = 'Order ID:';
-$_['text_date_added']       = 'Date Added:';
-$_['text_shipping_address'] = 'Shipping Address';
-$_['text_shipping_method']  = 'Shipping Method:';
-$_['text_payment_address']  = 'Payment Address';
-$_['text_payment_method']   = 'Payment Method:';
-$_['text_comment']          = 'Order Comments';
-$_['text_history']          = 'Order History';
-$_['text_success']          = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">shopping cart</a>!';
-$_['text_empty']            = 'You have not made any previous orders!';
-$_['text_error']            = 'The order you requested could not be found!';
+$_['text_order']            = 'Informații Comandă';
+$_['text_order_detail']     = 'Detalii Comandă';
+$_['text_invoice_no']       = 'Factură Nr.:';
+$_['text_order_id']         = 'ID Comandă:';
+$_['text_date_added']       = 'Data Adăugării:';
+$_['text_shipping_address'] = 'Adresă Livrare';
+$_['text_shipping_method']  = 'Metodă Livrare:';
+$_['text_payment_address']  = 'Adresă Plată';
+$_['text_payment_method']   = 'Metodă de Plată:';
+$_['text_comment']          = 'Comentarii Comandă';
+$_['text_history']          = 'Istoric Comenzi';
+$_['text_success']          = 'Succes: Ai adăugat <a href="%s">%s</a> la <a href="%s">coșul tău de cumpărături</a>!';
+$_['text_empty']            = 'Nu ai făcut nicio comandă anterior!';
+$_['text_error']            = 'Comanda pe care ai solicitat-o nu a putut fi găsită!';
 
 // Column
 $_['column_order_id']       = 'ID Comandă';
-$_['column_customer']       = 'Customer';
-$_['column_product']        = 'No. of Products';
+$_['column_customer']       = 'Client';
+$_['column_product']        = 'Nr. de Produse';
 $_['column_name']           = 'Nume Produs';
 $_['column_model']          = 'Model';
 $_['column_quantity']       = 'Cantitate';
@@ -31,7 +31,7 @@ $_['column_total']          = 'Total';
 $_['column_action']         = 'Acțiune';
 $_['column_date_added']     = 'Data Adăugării';
 $_['column_status']         = 'Stare';
-$_['column_comment']        = 'Comment';
+$_['column_comment']        = 'Comentariu';
 
 // Error
-$_['error_reorder']         = '%s is not currently available to be reordered.';
+$_['error_reorder']         = '%s nu este disponibil în prezent pentru a fi comandat din nou.';
