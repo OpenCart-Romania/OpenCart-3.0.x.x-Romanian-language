6c99e9e17344b2c6a3e4a65cdba90dc3d960e38e
Constantin DOBRE <admin@harddex.ro>
2021-01-01 14:50:18 +0800

    New translations success.php (Romanian)

--- upload/catalog/language/ro-ro/checkout/success.php
+++ upload/catalog/language/ro-ro/checkout/success.php
@@ -6,5 +6,5 @@ $_['heading_title']        = 'Your order has been placed!';
 $_['text_basket']          = 'Shopping Cart';
 $_['text_checkout']        = 'Checkout';
 $_['text_success']         = 'Success';
-$_['text_customer']        = '<p>Your order has been successfully processed!</p><p>You can view your order history by going to the <a href="%s">my account</a> page and by clicking on <a href="%s">history</a>.</p><p>If your purchase has an associated download, you can go to the account <a href="%s">downloads</a> page to view them.</p><p>Please direct any questions you have to the <a href="%s">store owner</a>.</p><p>Thanks for shopping with us online!</p>';
+$_['text_customer']        = '<p>Comanda ta a fost procesata cu succes!</p><p>Poți vizualiza istoricul comenzilor mergând la pagina <a href="%s">contul meu</a> și dând click pe <a href="%s">istoric</a>.</p><p>Dacă achiziția are o descărcare asociată, poți merge la pagina <a href="%s">descărcări</a> pentru a o vizualiza.</p><p>Te rugăm să direcţionezi orice întrebare către proprietarul magazinului <a href="%s"></a>.</p><p>Mulțumim pentru cumpărături online cu noi!</p>';
 $_['text_guest']           = '<p>Your order has been successfully processed!</p><p>Please direct any questions you have to the <a href="%s">store owner</a>.</p><p>Thanks for shopping with us online!</p>';
