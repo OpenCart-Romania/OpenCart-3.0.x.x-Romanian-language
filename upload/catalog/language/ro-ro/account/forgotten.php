57e5906f4021f9b718109140a7b1c88cbcb0f284
Constantin DOBRE <admin@harddex.ro>
2021-01-01 14:51:47 +0800

    New translations forgotten.php (Romanian)

--- upload/catalog/language/ro-ro/account/forgotten.php
+++ upload/catalog/language/ro-ro/account/forgotten.php
@@ -7,7 +7,7 @@ $_['text_account']    = 'Cont';
 $_['text_forgotten']  = 'Parolă Uitată';
 $_['text_your_email'] = 'Adresa Ta de E-Mail';
 $_['text_email']      = 'Introdu adresa de e-mail asociată contului tau. Click pe "Trimite" pentru a-ti trimite prin e-mail un link de resetare a parolei.';
-$_['text_success']    = 'An email with a confirmation link has been sent your email address.';
+$_['text_success']    = 'Un email cu un link de confirmare a fost trimis la adresa ta de email.';
 
 // Entry
 $_['entry_email']     = 'Adresă de E-Mail';
@@ -16,6 +16,6 @@ $_['entry_confirm']   = 'Confirmare';
 
 // Error
 $_['error_email']     = 'Atenție: Adresa de E-Mail nu a fost găsită în înregistrările noastre, te rugăm să încerci din nou!';
-$_['error_approved']  = 'Warning: Your account requires approval before you can login.';
+$_['error_approved']  = 'Atenție: Contul tău necesită aprobare înainte de a te putea autentifica.';
 $_['error_password']  = 'Parola trebuie să fie între 4 și 20 de caractere!';
 $_['error_confirm']   = 'Parola și confirmarea parolei nu se potrivesc!';
