f9e322580852b15f0105f0b197ca19852e660c7c
Constantin DOBRE <admin@harddex.ro>
2021-01-01 14:51:58 +0800

    New translations step_3.php (Romanian)

--- upload/install/language/ro-ro/install/step_3.php
+++ upload/install/language/ro-ro/install/step_3.php
@@ -24,13 +24,13 @@ $_['entry_email']            = 'E-Mail';
 
 // Error
 $_['error_db_driver']        = 'Database Driver required!';
-$_['error_db_hostname']      = 'Hostname required!';
-$_['error_db_username']      = 'Username required!';
+$_['error_db_hostname']      = 'Hostname necesar!';
+$_['error_db_username']      = 'Nume utilizator necesar!';
 $_['error_db_database']      = 'Database Name required!';
 $_['error_db_port']          = 'Database Port required!';
 $_['error_db_prefix']        = 'DB Prefix can only contain lowercase characters in the a-z range, 0-9 and underscores';
 $_['error_db_connect']       = 'Error: Could not connect to the database please make sure the database server, username and password is correct!';
-$_['error_username']         = 'Username required!';
+$_['error_username']         = 'Nume utilizator necesar!';
 $_['error_password']         = 'Password required!';
 $_['error_email']            = 'Adresa E-Mail nu pare să fie validă!';
 $_['error_config']           = 'Error: Could not write to config.php please check you have set the correct permissions on: ';
