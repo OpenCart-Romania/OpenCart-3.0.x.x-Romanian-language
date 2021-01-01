4ef644a40dcce338927ca23388101e52948d2702
Constantin DOBRE <admin@harddex.ro>
2021-01-01 14:51:52 +0800

    New translations newsletter.php (Romanian)

--- upload/catalog/language/ro-ro/account/newsletter.php
+++ upload/catalog/language/ro-ro/account/newsletter.php
@@ -1,11 +1,11 @@
 <?php
 // Heading
-$_['heading_title']    = 'Newsletter Subscription';
+$_['heading_title']    = 'Abonare la Newsletter';
 
 // Text
 $_['text_account']     = 'Cont';
 $_['text_newsletter']  = 'Newsletter';
-$_['text_success']     = 'Success: Your newsletter subscription has been successfully updated!';
+$_['text_success']     = 'Succes: Abonarea ta la newsletter ta a fost actualizată cu succes!';
 
 // Entry
-$_['entry_newsletter'] = 'Subscribe';
+$_['entry_newsletter'] = 'Abonează-te';
