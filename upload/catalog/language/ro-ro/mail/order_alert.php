1d824dc89d50616ab095994e0c9ca5a61cb9b168
Constantin DOBRE <admin@harddex.ro>
2021-01-01 14:50:52 +0800

    New translations order_alert.php (Romanian)

--- upload/catalog/language/ro-ro/mail/order_alert.php
+++ upload/catalog/language/ro-ro/mail/order_alert.php
@@ -3,7 +3,7 @@
 $_['text_subject']      = '%s - Order %s';
 $_['text_received']     = 'You have received an order.';
 $_['text_order_id']     = 'Order ID:';
-$_['text_date_added']   = 'Date Added:';
+$_['text_date_added']   = 'Data Adăugării:';
 $_['text_order_status'] = 'Order Status:';
 $_['text_product']      = 'Produse';
 $_['text_total']        = 'Totaluri';
