e48c2cb725d407e20e3a4ffba218a70da5c09088
Constantin DOBRE <admin@harddex.ro>
2021-01-01 14:50:09 +0800

    New translations register.php (Romanian)

--- upload/catalog/language/ro-ro/affiliate/register.php
+++ upload/catalog/language/ro-ro/affiliate/register.php
@@ -5,16 +5,16 @@ $_['heading_title']             = 'Affiliate Program';
 // Text
 $_['text_account']              = 'Cont';
 $_['text_register']             = 'Affiliate Register';
-$_['text_account_already']      = 'If you already have an account with us, please login at the <a href="%s">login page</a>.';
+$_['text_account_already']      = 'Dacă ai deja un cont la noi, te rugăm să te autentifici pe <a href="%s">pagina de autentificare</a>.';
 $_['text_signup']               = 'To create an affiliate account, fill in the form below ensuring you complete all the required fields:';
-$_['text_your_details']         = 'Your Personal Details';
+$_['text_your_details']         = 'Detaliile Tale Personale';
 $_['text_your_address']         = 'Your Address Details';
-$_['text_your_affiliate']       = 'Your Affiliate Information';
-$_['text_your_password']        = 'Your Password';
+$_['text_your_affiliate']       = 'Informațiile Tale de Afiliat';
+$_['text_your_password']        = 'Parola Ta';
 $_['text_cheque']               = 'Cheque';
 $_['text_paypal']               = 'PayPal';
 $_['text_bank']                 = 'Bank Transfer';
-$_['text_agree']                = 'I have read and agree to the <a href="%s" class="agree"><b>%s</b></a>';
+$_['text_agree']                = 'Am citit și sunt de acord cu <a href="%s" class="agree"><b>%s</b></a>';
 
 // Entry
 $_['entry_customer_group']      = 'Grup Clienți';
@@ -24,7 +24,7 @@ $_['entry_email']               = 'E-Mail';
 $_['entry_telephone']           = 'Telephone';
 $_['entry_company']             = 'Company';
 $_['entry_website']             = 'Web Site';
-$_['entry_tax']                 = 'Tax ID';
+$_['entry_tax']                 = 'C.U.I. / Cod TVA';
 $_['entry_payment']             = 'Payment Method';
 $_['entry_cheque']              = 'Cheque Payee Name';
 $_['entry_paypal']              = 'Email Cont PayPal';
@@ -34,7 +34,7 @@ $_['entry_bank_swift_code']     = 'SWIFT Code';
 $_['entry_bank_account_name']   = 'Nume Cont';
 $_['entry_bank_account_number'] = 'Număr Cont';
 $_['entry_password']            = 'Parolă';
-$_['entry_confirm']             = 'Password Confirm';
+$_['entry_confirm']             = 'Confirmă Parola';
 
 // Error
 $_['error_exists']              = 'Atenție: Adresa E-Mail este deja înregistrată!';
@@ -44,9 +44,9 @@ $_['error_email']               = 'Adresa E-Mail nu pare să fie validă!';
 $_['error_telephone']           = 'Telephone must be between 3 and 32 characters!';
 $_['error_custom_field']        = '%s required!';
 $_['error_cheque']              = 'Cheque Payee Name required!';
-$_['error_paypal']              = 'PayPal Email Address does not appear to be valid!';
+$_['error_paypal']              = 'Adresa Email PayPal nu pare să fie validă!';
 $_['error_bank_account_name']   = 'Nume Cont necesar!';
 $_['error_bank_account_number'] = 'Număr Cont necesar!';
 $_['error_password']            = 'Parola trebuie să fie între 4 și 20 de caractere!';
-$_['error_confirm']             = 'Password confirmation does not match password!';
-$_['error_agree']               = 'Warning: You must agree to the %s!';
+$_['error_confirm']             = 'Confirmarea parolei nu se potrivește cu parola!';
+$_['error_agree']               = 'Atenție: Trebuie să fii de acord cu %s!';
