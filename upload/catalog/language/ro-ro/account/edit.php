af0e86914e110f4c73296419e4d4da2d69a491b5
Constantin DOBRE <admin@harddex.ro>
2021-01-01 14:51:46 +0800

    New translations edit.php (Romanian)

--- upload/catalog/language/ro-ro/account/edit.php
+++ upload/catalog/language/ro-ro/account/edit.php
@@ -1,23 +1,23 @@
 <?php
 // Heading
-$_['heading_title']      = 'My Account Information';
+$_['heading_title']      = 'Informații Contul Meu';
 
 // Text
 $_['text_account']       = 'Cont';
 $_['text_edit']          = 'Editează Informație';
-$_['text_your_details']  = 'Your Personal Details';
-$_['text_success']       = 'Success: Your account has been successfully updated.';
+$_['text_your_details']  = 'Detaliile Tale Personale';
+$_['text_success']       = 'Succes: Contul tau a fost actualizat cu succes.';
 
 // Entry
 $_['entry_firstname']    = 'Prenume';
 $_['entry_lastname']     = 'Nume de Familie';
 $_['entry_email']        = 'E-Mail';
-$_['entry_telephone']    = 'Telephone';
+$_['entry_telephone']    = 'Telefon';
 
 // Error
-$_['error_exists']       = 'Warning: E-Mail address is already registered!';
+$_['error_exists']       = 'Atenție: Adresa E-Mail este deja înregistrată!';
 $_['error_firstname']    = 'Prenumele trebuie să fie între 1 și 32 de caractere!';
 $_['error_lastname']     = 'Numele de Familie trebuie să fie între 1 și 32 de caractere!';
 $_['error_email']        = 'Adresa E-Mail nu pare să fie validă!';
-$_['error_telephone']    = 'Telephone must be between 3 and 32 characters!';
-$_['error_custom_field'] = '%s required!';
+$_['error_telephone']    = 'Telefon trebuie să fie între 3 și 32 de caractere!';
+$_['error_custom_field'] = '%s necesar!';
