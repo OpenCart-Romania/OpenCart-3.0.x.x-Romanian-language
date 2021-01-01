c1ca61f9bcb44fe757a0f48378ec8f9aa1f38fdf
Constantin DOBRE <admin@harddex.ro>
2021-01-01 14:50:43 +0800

    New translations squareup.php (Romanian)

--- upload/catalog/language/ro-ro/extension/recurring/squareup.php
+++ upload/catalog/language/ro-ro/extension/recurring/squareup.php
@@ -3,7 +3,7 @@
 $_['text_title']                = 'Square';
 $_['text_canceled']             = 'Success: You have succesfully canceled this payment! We sent you a confirmation e-mail.';
 $_['text_confirm_cancel']       = 'Are you sure you want to cancel the recurring payments?';
-$_['text_order_history_cancel'] = 'You canceled your recurring profile. Your card will no longer be charged.';
+$_['text_order_history_cancel'] = 'Ai anulat profilul tău recurent. Cardul tău nu va mai fi debitat.';
 
 // Button
 $_['button_cancel']             = 'Cancel Recurring Payment';
