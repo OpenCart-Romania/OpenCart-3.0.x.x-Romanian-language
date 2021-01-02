d80fc842dc86d644c743a4eccbdbe1efc9d4e24e
Constantin DOBRE <admin@harddex.ro>
2021-01-02 20:52:00 +0800

    New translations wishlist.php (Romanian)

--- upload/catalog/language/ro-ro/account/wishlist.php
+++ upload/catalog/language/ro-ro/account/wishlist.php
@@ -1,20 +1,20 @@
 <?php
 // Heading
-$_['heading_title'] = 'My Wish List';
+$_['heading_title'] = 'Lista Mea de Dorințe';
 
 // Text
 $_['text_account']  = 'Cont';
-$_['text_instock']  = 'In Stock';
-$_['text_wishlist'] = 'Wish List (%s)';
-$_['text_login']    = 'You must <a href="%s">login</a> or <a href="%s">create an account</a> to save <a href="%s">%s</a> to your <a href="%s">wish list</a>!';
-$_['text_success']  = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">wish list</a>!';
-$_['text_remove']   = 'Success: You have modified your wish list!';
-$_['text_empty']    = 'Your wish list is empty.';
+$_['text_instock']  = 'În Stoc';
+$_['text_wishlist'] = 'Listă de Dorințe (%s)';
+$_['text_login']    = 'Trebuie să <a href="%s">te autentifici</a> sau <a href="%s">să creezi un cont</a> pentru a salva <a href="%s">%s</a> în <a href="%s">lista ta de dorințe</a>!';
+$_['text_success']  = 'Succes: Ai adăugat <a href="%s">%s</a> la <a href="%s">lista ta de dorințe</a>!';
+$_['text_remove']   = 'Succes: Ți-ai modificat lista de dorințe!';
+$_['text_empty']    = 'Lista ta de dorințe este goala.';
 
 // Column
 $_['column_image']  = 'Imagine';
 $_['column_name']   = 'Nume Produs';
 $_['column_model']  = 'Model';
-$_['column_stock']  = 'Stock';
-$_['column_price']  = 'Unit Price';
+$_['column_stock']  = 'Stoc';
+$_['column_price']  = 'Preț Unitar';
 $_['column_action'] = 'Acțiune';
