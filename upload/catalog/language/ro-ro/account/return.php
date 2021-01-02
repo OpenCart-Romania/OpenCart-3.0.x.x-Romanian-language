b5e270772c1bdfb12c7d747d610bd97442b52099
Constantin DOBRE <admin@harddex.ro>
2021-01-02 18:51:41 +0800

    New translations return.php (Romanian)

--- upload/catalog/language/ro-ro/account/return.php
+++ upload/catalog/language/ro-ro/account/return.php
@@ -1,52 +1,52 @@
 <?php
 // Heading
-$_['heading_title']      = 'Product Returns';
+$_['heading_title']      = 'Returnări Produse';
 
 // Text
 $_['text_account']       = 'Cont';
-$_['text_return']        = 'Return Information';
-$_['text_return_detail'] = 'Return Details';
+$_['text_return']        = 'Informații Returnare';
+$_['text_return_detail'] = 'Detalii Returnare';
 $_['text_description']   = 'Please complete the form below to request an RMA number.';
-$_['text_order']         = 'Order Information';
-$_['text_product']       = 'Product Information';
-$_['text_reason']        = 'Reason for Return';
+$_['text_order']         = 'Informații Comandă';
+$_['text_product']       = 'Informații Produs';
+$_['text_reason']        = 'Motivul Returnării';
 $_['text_message']       = '<p>Thank you for submitting your return request. Your request has been sent to the relevant department for processing.</p><p> You will be notified via e-mail as to the status of your request.</p>';
-$_['text_return_id']     = 'Return ID:';
-$_['text_order_id']      = 'Order ID:';
-$_['text_date_ordered']  = 'Order Date:';
+$_['text_return_id']     = 'ID Returnare:';
+$_['text_order_id']      = 'ID Comandă:';
+$_['text_date_ordered']  = 'Data Comenzii:';
 $_['text_status']        = 'Stare:';
 $_['text_date_added']    = 'Data Adăugării:';
-$_['text_comment']       = 'Return Comments';
-$_['text_history']       = 'Return History';
+$_['text_comment']       = 'Comentarii Returnare';
+$_['text_history']       = 'Istoric Returnare';
 $_['text_empty']         = 'You have not made any previous returns!';
 $_['text_agree']         = 'Am citit și sunt de acord cu <a href="%s" class="agree"><b>%s</b></a>';
 
 // Column
-$_['column_return_id']   = 'Return ID';
+$_['column_return_id']   = 'ID Returnare';
 $_['column_order_id']    = 'ID Comandă';
 $_['column_status']      = 'Stare';
 $_['column_date_added']  = 'Data Adăugării';
-$_['column_customer']    = 'Customer';
+$_['column_customer']    = 'Client';
 $_['column_product']     = 'Nume Produs';
 $_['column_model']       = 'Model';
 $_['column_quantity']    = 'Cantitate';
 $_['column_price']       = 'Preț';
-$_['column_opened']      = 'Opened';
-$_['column_comment']     = 'Comment';
-$_['column_reason']      = 'Reason';
+$_['column_opened']      = 'Deschis';
+$_['column_comment']     = 'Comentariu';
+$_['column_reason']      = 'Motiv';
 $_['column_action']      = 'Acțiune';
 
 // Entry
 $_['entry_order_id']     = 'ID Comandă';
-$_['entry_date_ordered'] = 'Order Date';
+$_['entry_date_ordered'] = 'Data Comenzii';
 $_['entry_firstname']    = 'Prenume';
 $_['entry_lastname']     = 'Nume de Familie';
 $_['entry_email']        = 'E-Mail';
-$_['entry_telephone']    = 'Telephone';
+$_['entry_telephone']    = 'Telefon';
 $_['entry_product']      = 'Nume Produs';
-$_['entry_model']        = 'Product Code';
+$_['entry_model']        = 'Cod Produs';
 $_['entry_quantity']     = 'Cantitate';
-$_['entry_reason']       = 'Reason for Return';
+$_['entry_reason']       = 'Motivul Returnării';
 $_['entry_opened']       = 'Product is opened';
 $_['entry_fault_detail'] = 'Faulty or other details';
 
