3b36f074adcc7a47a93e384c995d051fdda5c0ce
Constantin DOBRE <admin@harddex.ro>
2021-01-02 20:52:43 +0800

    New translations sitemap.php (Romanian)

--- upload/catalog/language/ro-ro/information/sitemap.php
+++ upload/catalog/language/ro-ro/information/sitemap.php
@@ -3,14 +3,14 @@
 $_['heading_title']    = 'Hartă Site';
 
 // Text
-$_['text_special']     = 'Special Offers';
+$_['text_special']     = 'Oferte Speciale';
 $_['text_account']     = 'Contul Meu';
-$_['text_edit']        = 'Account Information';
+$_['text_edit']        = 'Informații Cont';
 $_['text_password']    = 'Parolă';
 $_['text_address']     = 'Agendă';
 $_['text_history']     = 'Istoric Comenzi';
 $_['text_download']    = 'Descărcări';
-$_['text_cart']        = 'Shopping Cart';
+$_['text_cart']        = 'Coș de Cumpărături';
 $_['text_checkout']    = 'Checkout';
 $_['text_search']      = 'Căutare';
 $_['text_information'] = 'Informaţii';
