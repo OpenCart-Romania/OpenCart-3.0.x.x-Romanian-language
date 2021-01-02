1a46ac3a98908f011bdbc44250fefe773157bb4b
Constantin DOBRE <admin@harddex.ro>
2021-01-02 20:52:13 +0800

    New translations return.php (Romanian)

--- upload/catalog/language/ro-ro/account/return.php
+++ upload/catalog/language/ro-ro/account/return.php
@@ -6,11 +6,11 @@ $_['heading_title']      = 'Returnări Produse';
 $_['text_account']       = 'Cont';
 $_['text_return']        = 'Informații Returnare';
 $_['text_return_detail'] = 'Detalii Returnare';
-$_['text_description']   = 'Please complete the form below to request an RMA number.';
+$_['text_description']   = 'Te rugăm să completezi formularul de mai jos pentru a solicita un număr RMA.';
 $_['text_order']         = 'Informații Comandă';
 $_['text_product']       = 'Informații Produs';
 $_['text_reason']        = 'Motivul Returnării';
-$_['text_message']       = '<p>Thank you for submitting your return request. Your request has been sent to the relevant department for processing.</p><p> You will be notified via e-mail as to the status of your request.</p>';
+$_['text_message']       = '<p>Iți mulțumim pentru trimiterea cererii de returnare. Cererea ta a fost trimisă departamentului relevant pentru prelucrare.</p><p> Vei fi notificat prin e-mail cu privire la starea cererii tale.</p>';
 $_['text_return_id']     = 'ID Returnare:';
 $_['text_order_id']      = 'ID Comandă:';
 $_['text_date_ordered']  = 'Data Comenzii:';
@@ -18,7 +18,7 @@ $_['text_status']        = 'Stare:';
 $_['text_date_added']    = 'Data Adăugării:';
 $_['text_comment']       = 'Comentarii Returnare';
 $_['text_history']       = 'Istoric Returnare';
-$_['text_empty']         = 'You have not made any previous returns!';
+$_['text_empty']         = 'Nu ai făcut nicio returnare anterior!';
 $_['text_agree']         = 'Am citit și sunt de acord cu <a href="%s" class="agree"><b>%s</b></a>';
 
 // Column
@@ -47,17 +47,17 @@ $_['entry_product']      = 'Nume Produs';
 $_['entry_model']        = 'Cod Produs';
 $_['entry_quantity']     = 'Cantitate';
 $_['entry_reason']       = 'Motivul Returnării';
-$_['entry_opened']       = 'Product is opened';
-$_['entry_fault_detail'] = 'Faulty or other details';
+$_['entry_opened']       = 'Produsul este deschis';
+$_['entry_fault_detail'] = 'Defect sau alte detalii';
 
 // Error
-$_['text_error']         = 'The returns you requested could not be found!';
-$_['error_order_id']     = 'Order ID required!';
+$_['text_error']         = 'Returnarea pe care ai solicitat-o nu a putut fi găsită!';
+$_['error_order_id']     = 'ID Comandă necesar!';
 $_['error_firstname']    = 'Prenumele trebuie să fie între 1 și 32 de caractere!';
 $_['error_lastname']     = 'Numele de Familie trebuie să fie între 1 și 32 de caractere!';
 $_['error_email']        = 'Adresa E-Mail nu pare să fie validă!';
-$_['error_telephone']    = 'Telephone must be between 3 and 32 characters!';
+$_['error_telephone']    = 'Telefon trebuie să fie între 3 și 32 de caractere!';
 $_['error_product']      = 'Nume Produs trebuie să fie mai mare de 3 și mai mic de 255 de caractere!';
 $_['error_model']        = 'Model Produs trebuie să fie mai mare de 3 și mai mic de 64 de caractere!';
-$_['error_reason']       = 'You must select a return product reason!';
+$_['error_reason']       = 'Trebuie sa selectezi un motiv returnare produs!';
 $_['error_agree']        = 'Atenție: Trebuie să fii de acord cu %s!';
