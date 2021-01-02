fecfb5be8794fe013db40c3bdeb7209d0641c157
Constantin DOBRE <admin@harddex.ro>
2021-01-02 23:31:44 +0800

    New translations laybuy.php (Romanian)

--- upload/catalog/language/ro-ro/extension/payment/laybuy.php
+++ upload/catalog/language/ro-ro/extension/payment/laybuy.php
@@ -26,4 +26,4 @@ $_['entry_initial']			= 'Initial Payment';
 $_['entry_months']			= 'Months';
 
 // Button
-$_['button_confirm']		= 'Confirmați Comanda';
+$_['button_confirm']		= 'Confirmă Comanda';
