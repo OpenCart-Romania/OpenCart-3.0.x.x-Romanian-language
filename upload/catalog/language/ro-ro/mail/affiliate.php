09389c9065f70f19a6aa3fd69d7f87a8cd3bef1f
Constantin DOBRE <admin@harddex.ro>
2021-01-04 01:02:01 +0800

    New translations affiliate.php (Romanian)

--- upload/catalog/language/ro-ro/mail/affiliate.php
+++ upload/catalog/language/ro-ro/mail/affiliate.php
@@ -1,12 +1,12 @@
 <?php
 // Text
-$_['text_subject']        = '%s - Affiliate Program';
+$_['text_subject']        = '%s - Program de Afiliere';
 $_['text_welcome']        = 'Thank you for joining the %s Affiliate Program!';
 $_['text_login']          = 'Your account has now been created and you can log in by using your e-mail address and password by visiting our website or at the following URL:';
 $_['text_approval']       = 'Your account must be approved before you can login. Once approved you can log in by using your e-mail address and password by visiting our website or at the following URL:';
 $_['text_service']        = 'Upon logging in, you will be able to generate tracking codes, track commission payments and edit your account information.';
 $_['text_thanks']         = 'Thanks,';
-$_['text_new_affiliate']  = 'New Affiliate';
+$_['text_new_affiliate']  = 'Afiliat Nou';
 $_['text_signup']         = 'A new affiliate has signed up:';
 $_['text_website']        = 'Web Site:';
 $_['text_customer_group'] = 'Customer Group:';
