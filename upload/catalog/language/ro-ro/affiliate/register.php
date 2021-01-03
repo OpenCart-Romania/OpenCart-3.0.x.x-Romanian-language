803b197f2194c30a1b42673e993542fb4d3c0033
Constantin DOBRE <admin@harddex.ro>
2021-01-04 01:01:58 +0800

    New translations register.php (Romanian)

--- upload/catalog/language/ro-ro/affiliate/register.php
+++ upload/catalog/language/ro-ro/affiliate/register.php
@@ -1,19 +1,19 @@
 <?php
 // Heading
-$_['heading_title']             = 'Affiliate Program';
+$_['heading_title']             = 'Program de Afiliere';
 
 // Text
 $_['text_account']              = 'Cont';
-$_['text_register']             = 'Affiliate Register';
+$_['text_register']             = 'Registru Afiliați';
 $_['text_account_already']      = 'Dacă ai deja un cont la noi, te rugăm să te autentifici pe <a href="%s">pagina de autentificare</a>.';
-$_['text_signup']               = 'To create an affiliate account, fill in the form below ensuring you complete all the required fields:';
+$_['text_signup']               = 'Pentru a crea un cont de afiliat, completați formularul de mai jos asigurându-vă că completați toate câmpurile necesare:';
 $_['text_your_details']         = 'Detaliile Tale Personale';
-$_['text_your_address']         = 'Your Address Details';
+$_['text_your_address']         = 'Detaliile Adresei Tale';
 $_['text_your_affiliate']       = 'Informațiile Tale de Afiliat';
 $_['text_your_password']        = 'Parola Ta';
-$_['text_cheque']               = 'Cheque';
+$_['text_cheque']               = 'Cec';
 $_['text_paypal']               = 'PayPal';
-$_['text_bank']                 = 'Bank Transfer';
+$_['text_bank']                 = 'Transfer Bancar';
 $_['text_agree']                = 'Am citit și sunt de acord cu <a href="%s" class="agree"><b>%s</b></a>';
 
 // Entry
@@ -21,16 +21,16 @@ $_['entry_customer_group']      = 'Grup Clienți';
 $_['entry_firstname']           = 'Prenume';
 $_['entry_lastname']            = 'Nume de Familie';
 $_['entry_email']               = 'E-Mail';
-$_['entry_telephone']           = 'Telephone';
-$_['entry_company']             = 'Company';
+$_['entry_telephone']           = 'Telefon';
+$_['entry_company']             = 'Companie';
 $_['entry_website']             = 'Web Site';
 $_['entry_tax']                 = 'C.U.I. / Cod TVA';
-$_['entry_payment']             = 'Payment Method';
-$_['entry_cheque']              = 'Cheque Payee Name';
+$_['entry_payment']             = 'Metodă de Plată';
+$_['entry_cheque']              = 'Nume Beneficiar Cec';
 $_['entry_paypal']              = 'Email Cont PayPal';
-$_['entry_bank_name']           = 'Bank Name';
-$_['entry_bank_branch_number']  = 'ABA/BSB number (Branch Number)';
-$_['entry_bank_swift_code']     = 'SWIFT Code';
+$_['entry_bank_name']           = 'Nume Bancă';
+$_['entry_bank_branch_number']  = 'Număr ABA/BSB (Număr Filială)';
+$_['entry_bank_swift_code']     = 'Cod SWIFT';
 $_['entry_bank_account_name']   = 'Nume Cont';
 $_['entry_bank_account_number'] = 'Număr Cont';
 $_['entry_password']            = 'Parolă';
@@ -41,9 +41,9 @@ $_['error_exists']              = 'Atenție: Adresa E-Mail este deja înregistra
 $_['error_firstname']           = 'Prenumele trebuie să fie între 1 și 32 de caractere!';
 $_['error_lastname']            = 'Numele de Familie trebuie să fie între 1 și 32 de caractere!';
 $_['error_email']               = 'Adresa E-Mail nu pare să fie validă!';
-$_['error_telephone']           = 'Telephone must be between 3 and 32 characters!';
-$_['error_custom_field']        = '%s required!';
-$_['error_cheque']              = 'Cheque Payee Name required!';
+$_['error_telephone']           = 'Telefon trebuie să fie între 3 și 32 de caractere!';
+$_['error_custom_field']        = '%s necesar!';
+$_['error_cheque']              = 'Nume Beneficiar Cec necesar!';
 $_['error_paypal']              = 'Adresa Email PayPal nu pare să fie validă!';
 $_['error_bank_account_name']   = 'Nume Cont necesar!';
 $_['error_bank_account_number'] = 'Număr Cont necesar!';
