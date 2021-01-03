6a9cea236ea21cd4ad356f07851920421da7ef65
Constantin DOBRE <admin@harddex.ro>
2021-01-04 01:01:59 +0800

    New translations success.php (Romanian)

--- upload/catalog/language/ro-ro/affiliate/success.php
+++ upload/catalog/language/ro-ro/affiliate/success.php
@@ -1,9 +1,9 @@
 <?php
 // Heading
-$_['heading_title'] = 'Your Affiliate Account Has Been Created!';
+$_['heading_title'] = 'Contul Tău de Afiliat A Fost Creat!';
 
 // Text
 $_['text_message']  = '<p>Congratulations! Your new account has been successfully created!</p> <p>You are now a member of %s affiliates.</p> <p>If you have ANY questions about the operation of this affiliate system, please e-mail the store owner.</p> <p>A confirmation has been sent to the provided e-mail address. If you have not received it within the hour, please <a href="%s">contact us</a>.</p>';
 $_['text_approval'] = '<p>Thank you for registering for an affiliate account with %s!</p><p>You will be notified by e-mail once your account has been activated by the store owner.</p><p>If you have ANY questions about the operation of this affiliate system, please <a href="%s">contact the store owner</a>.</p>';
 $_['text_account']  = 'Cont';
-$_['text_success']  = 'Success';
+$_['text_success']  = 'Succes';
