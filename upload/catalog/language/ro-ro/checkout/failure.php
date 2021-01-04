a3f73c64ca923634bec2b23ec146421cb7ca62a8
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:52:27 +0800

    New translations failure.php (Romanian)

--- upload/catalog/language/ro-ro/checkout/failure.php
+++ upload/catalog/language/ro-ro/checkout/failure.php
@@ -1,11 +1,11 @@
 <?php
 // Heading
-$_['heading_title'] = 'Failed Payment!';
+$_['heading_title'] = 'Plată Eșuată!';
 
 // Text
-$_['text_basket']   = 'Shopping Cart';
+$_['text_basket']   = 'Coș de Cumpărături';
 $_['text_checkout'] = 'Checkout';
-$_['text_failure']  = 'Failed Payment';
+$_['text_failure']  = 'Plată Eșuată';
 $_['text_message']  = '<p>There was a problem processing your payment and the order did not complete.</p>
 
 <p>Possible reasons are:</p>
