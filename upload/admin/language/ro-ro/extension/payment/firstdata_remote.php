95ec47d010e88dd5d7981ad504ea19f8923d1bb9
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:55:12 +0800

    New translations firstdata_remote.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/firstdata_remote.php
+++ upload/admin/language/ro-ro/extension/payment/firstdata_remote.php
@@ -31,8 +31,8 @@ $_['text_refund_status']			 = 'Payment refunded';
 $_['text_order_ref']				 = 'Order ref';
 $_['text_order_total']				 = 'Total authorised';
 $_['text_total_captured']			 = 'Total captured';
-$_['text_transactions']				 = 'Transactions';
-$_['text_column_amount']			 = 'Amount';
+$_['text_transactions']				 = 'Tranzacții';
+$_['text_column_amount']			 = 'Valoare';
 $_['text_column_type']				 = 'Tip';
 $_['text_column_date_added']		 = 'Created';
 $_['text_confirm_void']				 = 'Are you sure you want to void the payment?';
@@ -48,7 +48,7 @@ $_['entry_merchant_id']				 = 'Store ID';
 $_['entry_user_id']					 = 'User ID';
 $_['entry_password']				 = 'Parolă';
 $_['entry_total']					 = 'Total';
-$_['entry_sort_order']				 = 'Ordinea de Sortare';
+$_['entry_sort_order']				 = 'Ordinea de sortare';
 $_['entry_geo_zone']				 = 'Geo zone';
 $_['entry_status']					 = 'Stare';
 $_['entry_debug']					 = 'Debug logging';
@@ -71,7 +71,7 @@ $_['help_settle']					 = 'If you use pre-auth you must complete a post-auth acti
 
 // Tab
 $_['tab_account']					 = 'API info';
-$_['tab_order_status']				 = 'Order status';
+$_['tab_order_status']				 = 'Stare comandă';
 $_['tab_payment']					 = 'Payment settings';
 
 // Button
