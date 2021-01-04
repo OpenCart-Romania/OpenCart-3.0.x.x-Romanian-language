56f246a7e5777342fa16248f9ee41f369cd84de0
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:55:27 +0800

    New translations bank_transfer.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/bank_transfer.php
+++ upload/admin/language/ro-ro/extension/payment/bank_transfer.php
@@ -1,16 +1,16 @@
 <?php
 // Heading
-$_['heading_title']      = 'Bank Transfer';
+$_['heading_title']      = 'Transfer Bancar';
 
 // Text
 $_['text_extension']     = 'Extensii';
-$_['text_success']       = 'Success: You have modified bank transfer details!';
-$_['text_edit']          = 'Edit Bank Transfer';
+$_['text_success']       = 'Succes: Ai modificat detaliile transferului bancar!';
+$_['text_edit']          = 'Editează Transfer Bancar';
 
 // Entry
-$_['entry_bank']         = 'Bank Transfer Instructions';
+$_['entry_bank']         = 'Instrucţiuni Transfer Bancar';
 $_['entry_total']        = 'Total';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_order_status'] = 'Stare Comandă';
 $_['entry_geo_zone']     = 'Geo Zone';
 $_['entry_status']       = 'Stare';
 $_['entry_sort_order']   = 'Ordinea de Sortare';
@@ -19,5 +19,5 @@ $_['entry_sort_order']   = 'Ordinea de Sortare';
 $_['help_total']         = 'The checkout total the order must reach before this payment method becomes active.'; 
 
 // Error 
-$_['error_permission']   = 'Warning: You do not have permission to modify payment bank transfer!';
-$_['error_bank']         = 'Bank Transfer Instructions Required!';
+$_['error_permission']   = 'Atenție: Nu ai permisiunea de a modifica plata prin transfer bancar!';
+$_['error_bank']         = 'Instrucţiuni Transfer Bancar necesare!';
