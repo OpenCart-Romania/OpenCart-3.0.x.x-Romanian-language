0c1f54dc647e62df0f97c4c42e072434b7868adc
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:52:31 +0800

    New translations cart.php (Romanian)

--- upload/catalog/language/ro-ro/checkout/cart.php
+++ upload/catalog/language/ro-ro/checkout/cart.php
@@ -1,24 +1,24 @@
 <?php
 // Heading
-$_['heading_title']            = 'Shopping Cart';
+$_['heading_title']            = 'Coș de Cumpărături';
 
 // Text
 $_['text_success']             = 'Succes: Ai adăugat <a href="%s">%s</a> la <a href="%s">coșul tău de cumpărături</a>!';
-$_['text_remove']              = 'Success: You have modified your shopping cart!';
+$_['text_remove']              = 'Succes: Ți-ai modificat coșul de cumpărături!';
 $_['text_login']               = 'Attention: You must <a href="%s">login</a> or <a href="%s">create an account</a> to view prices!';
-$_['text_items']               = '%s item(s) - %s';
-$_['text_points']              = 'Reward Points: %s';
-$_['text_next']                = 'What would you like to do next?';
+$_['text_items']               = '%s articol(e) - %s';
+$_['text_points']              = 'Puncte de Recompensă: %s';
+$_['text_next']                = 'Ce ai vrea să faci mai departe?';
 $_['text_next_choice']         = 'Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.';
-$_['text_empty']               = 'Your shopping cart is empty!';
-$_['text_day']                 = 'day';
-$_['text_week']                = 'week';
-$_['text_semi_month']          = 'half-month';
-$_['text_month']               = 'month';
-$_['text_year']                = 'year';
+$_['text_empty']               = 'Coșul tău de cumpărături este gol!';
+$_['text_day']                 = 'zi';
+$_['text_week']                = 'săptămână';
+$_['text_semi_month']          = 'jumătate de lună';
+$_['text_month']               = 'lună';
+$_['text_year']                = 'an';
 $_['text_trial']               = '%s every %s %s for %s payments then ';
 $_['text_recurring']           = '%s every %s %s';
-$_['text_recurring_item']      = 'Recurring Item';
+$_['text_recurring_item']      = 'Articol Recurent';
 $_['text_payment_recurring']   = 'Profil Plată';
 $_['text_trial_description']   = '%s every %d %s(s) for %d payment(s) then';
 $_['text_payment_description'] = '%s every %d %s(s) for %d payment(s)';
@@ -29,12 +29,12 @@ $_['column_image']             = 'Imagine';
 $_['column_name']              = 'Nume Produs';
 $_['column_model']             = 'Model';
 $_['column_quantity']          = 'Cantitate';
-$_['column_price']             = 'Unit Price';
+$_['column_price']             = 'Preț Unitar';
 $_['column_total']             = 'Total';
 
 // Error
 $_['error_stock']              = 'Products marked with *** are not available in the desired quantity or not in stock!';
 $_['error_minimum']            = 'Minimum order amount for %s is %s!';
-$_['error_required']           = '%s required!';
+$_['error_required']           = '%s necesar!';
 $_['error_product']            = 'Warning: There are no products in your cart!';
 $_['error_recurring_required'] = 'Please select a payment recurring!';
