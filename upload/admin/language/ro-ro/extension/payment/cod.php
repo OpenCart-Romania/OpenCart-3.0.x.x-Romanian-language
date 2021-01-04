326a97a446a95705aebd4e24e66b7207560697cb
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:55:18 +0800

    New translations cod.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/cod.php
+++ upload/admin/language/ro-ro/extension/payment/cod.php
@@ -9,7 +9,7 @@ $_['text_edit']          = 'Edit Cash On Delivery';
 
 // Entry
 $_['entry_total']        = 'Total';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_order_status'] = 'Stare Comandă';
 $_['entry_geo_zone']     = 'Geo Zone';
 $_['entry_status']       = 'Stare';
 $_['entry_sort_order']   = 'Ordinea de Sortare';
