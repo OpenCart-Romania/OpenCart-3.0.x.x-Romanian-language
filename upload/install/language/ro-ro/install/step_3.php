b212f1c3eb21d225a774c76f642a847aca67bf69
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:52:59 +0800

    New translations step_3.php (Romanian)

--- upload/install/language/ro-ro/install/step_3.php
+++ upload/install/language/ro-ro/install/step_3.php
@@ -3,9 +3,9 @@
 $_['heading_title']          = 'Configurare';
 
 // Text
-$_['text_step_3']            = 'Enter your database and administration details';
-$_['text_db_connection']     = '1. Please enter your database connection details.';
-$_['text_db_administration'] = '2. Please enter a username and password for the administration.';
+$_['text_step_3']            = 'Introdu baza de date și detaliile de administrare';
+$_['text_db_connection']     = '1. Te rog introdu detaliile conexiunii bazei de date.';
+$_['text_db_administration'] = '2. Te rog introdu un nume de utilizator și o parolă pentru administrare.';
 $_['text_mysqli']            = 'MySQLi';
 $_['text_pdo']               = 'mPDO';
 $_['text_pgsql']             = 'PostgreSQL';
@@ -15,7 +15,7 @@ $_['entry_db_driver']        = 'DB Driver';
 $_['entry_db_hostname']      = 'Hostname';
 $_['entry_db_username']      = 'Nume utilizator';
 $_['entry_db_password']      = 'Parolă';
-$_['entry_db_database']      = 'Bază de date';
+$_['entry_db_database']      = 'Bază de Date';
 $_['entry_db_port']          = 'Port';
 $_['entry_db_prefix']        = 'Prefix';
 $_['entry_username']         = 'Nume utilizator';
@@ -23,14 +23,14 @@ $_['entry_password']         = 'Parolă';
 $_['entry_email']            = 'E-Mail';
 
 // Error
-$_['error_db_driver']        = 'Database Driver required!';
+$_['error_db_driver']        = 'Driver Bază de Date necesar!';
 $_['error_db_hostname']      = 'Hostname necesar!';
 $_['error_db_username']      = 'Nume utilizator necesar!';
-$_['error_db_database']      = 'Database Name required!';
-$_['error_db_port']          = 'Database Port required!';
-$_['error_db_prefix']        = 'DB Prefix can only contain lowercase characters in the a-z range, 0-9 and underscores';
-$_['error_db_connect']       = 'Error: Could not connect to the database please make sure the database server, username and password is correct!';
+$_['error_db_database']      = 'Nume Bază de Date necesar!';
+$_['error_db_port']          = 'Port Bază de Date necesar!';
+$_['error_db_prefix']        = 'DB Prefix poate conține numai caractere minuscule în intervalul a-z, 0-9 și sublinieri';
+$_['error_db_connect']       = 'Eroare: Nu s-a putut conecta la baza de date, te rog asigură-te că serverul bazei de date, numele de utilizator și parola sunt corecte!';
 $_['error_username']         = 'Nume utilizator necesar!';
 $_['error_password']         = 'Parolă necesară!';
 $_['error_email']            = 'Adresa E-Mail nu pare să fie validă!';
-$_['error_config']           = 'Error: Could not write to config.php please check you have set the correct permissions on: ';
+$_['error_config']           = 'Eroare: Nu s-a putut scrie in config.php te rog verifică că ai setat permisiunile corecte pe: ';
