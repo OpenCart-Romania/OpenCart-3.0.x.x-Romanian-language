440c7a6dfefd3837bb1bc1786bf1eea4bb7d3b8f
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:56:31 +0800

    New translations auspost.php (Romanian)

--- upload/admin/language/ro-ro/extension/shipping/auspost.php
+++ upload/admin/language/ro-ro/extension/shipping/auspost.php
@@ -9,7 +9,7 @@ $_['text_edit']          = 'Edit Australia Post Shipping';
 
 // Entry
 $_['entry_api']          = 'API Key';
-$_['entry_postcode']     = 'Post Code';
+$_['entry_postcode']     = 'Cod Poștal';
 $_['entry_weight_class'] = 'Clasă de Greutate';
 $_['entry_tax_class']    = 'Clasă de Taxare';
 $_['entry_geo_zone']     = 'Geo Zone';
@@ -21,4 +21,4 @@ $_['help_weight_class']  = 'Set to kilograms.';
 
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify Australia Post shipping!';
-$_['error_postcode']     = 'Post Code must be 4 digits!';
+$_['error_postcode']     = 'Cod Poștal trebuie să fie 4 cifre!';
