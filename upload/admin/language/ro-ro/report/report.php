756e3fd7e6579ce52cfdebfb50c77aef6b8a8eeb
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:56:18 +0800

    New translations report.php (Romanian)

--- upload/admin/language/ro-ro/report/report.php
+++ upload/admin/language/ro-ro/report/report.php
@@ -3,7 +3,7 @@
 $_['heading_title'] = 'Rapoarte';
 
 // Text
-$_['text_success']  = 'Success: You have modified reports!';
+$_['text_success']  = 'Succes: Ai modificat rapoartele!';
 $_['text_list']     = 'Report List';
 $_['text_type']     = 'Choose the report type';
 $_['text_filter']   = 'Filtru';
