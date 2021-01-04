fc0c10cceca69f25197c7730dfeea41868fea2df
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:53:31 +0800

    New translations laybuy.php (Romanian)

--- upload/catalog/language/ro-ro/extension/payment/laybuy.php
+++ upload/catalog/language/ro-ro/extension/payment/laybuy.php
@@ -7,13 +7,13 @@ $_['text_title']			= 'PUT IT ON LAY-BUY powered by PayPal';
 $_['text_plan_preview']		= 'Plan Preview';
 $_['text_payment']			= 'Payment';
 $_['text_due_date']			= 'Due Date';
-$_['text_amount']			= 'Amount';
+$_['text_amount']			= 'Valoare';
 $_['text_downpayment']		= 'Down Payment';
-$_['text_today']			= 'Today';
+$_['text_today']			= 'Astăzi';
 $_['text_delivery_msg']		= 'Your goods/services will be delivered once your final payment has been received.';
 $_['text_fee_msg']			= 'A 0.9% admin fee is payable to Lay-Buys.com.';
 $_['text_month']			= 'Lună';
-$_['text_months']			= 'Months';
+$_['text_months']			= 'Luni';
 $_['text_status_1']			= 'Pending';
 $_['text_status_5']			= 'Completed';
 $_['text_status_7']			= 'Canceled';
@@ -23,7 +23,7 @@ $_['text_comment']			= 'Updated by Lay-Buy';
 
 // Entry
 $_['entry_initial']			= 'Initial Payment';
-$_['entry_months']			= 'Months';
+$_['entry_months']			= 'Luni';
 
 // Button
 $_['button_confirm']		= 'Confirmă Comanda';
