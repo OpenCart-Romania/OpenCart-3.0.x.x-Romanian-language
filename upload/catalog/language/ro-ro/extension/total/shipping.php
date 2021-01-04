576258be5f121787759908f7d5813166ccb95a4d
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:52:47 +0800

    New translations shipping.php (Romanian)

--- upload/catalog/language/ro-ro/extension/total/shipping.php
+++ upload/catalog/language/ro-ro/extension/total/shipping.php
@@ -8,13 +8,13 @@ $_['text_shipping']        = 'Enter your destination to get a shipping estimate.
 $_['text_shipping_method'] = 'Please select the preferred shipping method to use on this order.';
 
 // Entry
-$_['entry_country']        = 'Country';
+$_['entry_country']        = 'Țară';
 $_['entry_zone']           = 'Regiune / Judeţ';
-$_['entry_postcode']       = 'Post Code';
+$_['entry_postcode']       = 'Cod Poștal';
 
 // Error
 $_['error_postcode']       = 'Cod postal trebuie să fie între 2 și 10 de caractere!';
-$_['error_country']        = 'Please select a country!';
+$_['error_country']        = 'Te rugăm selectează o țară!';
 $_['error_zone']           = 'Te rugăm selectează o Regiune / un Județ!';
 $_['error_shipping']       = 'Warning: Shipping method required!';
 $_['error_no_shipping']    = 'Warning: No Shipping options are available. Please <a href="%s">contact us</a> for assistance!';
