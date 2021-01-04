87a58d9ba063b187887af8a0e14ccb5a164dc18c
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:56:58 +0800

    New translations upgrade.php (Romanian)

--- upload/install/language/ro-ro/upgrade/upgrade.php
+++ upload/install/language/ro-ro/upgrade/upgrade.php
@@ -5,7 +5,7 @@ $_['heading_success'] = 'Upgrade - Complete!';
 
 // Text
 $_['text_upgrade']    = 'Prepare for your upgrade';
-$_['text_server']     = 'Verificați dacă serverul dvs. este configurat corect';
+$_['text_server']     = 'Verificare că serverul este configurat corect';
 $_['text_steps']      = 'Upgrade Progress';
 $_['text_error']      = 'Post any upgrade script errors problems in the forums';
 $_['text_clear']      = 'After upgrade, clear any cookies in your browser to avoid getting token errors.';
@@ -14,7 +14,7 @@ $_['text_user']       = 'Goto Admin -> Users -> User Groups and Edit the Top Adm
 $_['text_setting']    = 'Goto Admin and Edit the main System Settings. Update all fields and click save, even if nothing changed.';
 $_['text_store']      = 'Load the store front & press Ctrl+F5 twice to force the browser to update the css changes.';
 $_['text_progress']   = 'Patch %s has been applied (%s of %s)';
-$_['text_success']    = 'Congratulations! You have successfuly upgraded your OpenCart installation!';
+$_['text_success']    = 'Felicitări! Ai actualizat cu succes instalarea OpenCart!';
 
 // Entry
 $_['entry_progress']  = 'Progress';
