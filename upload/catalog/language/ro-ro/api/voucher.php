3cdc8cbf1bffe8be61d8a62449943d12ac1ded91
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:52:33 +0800

    New translations voucher.php (Romanian)

--- upload/catalog/language/ro-ro/api/voucher.php
+++ upload/catalog/language/ro-ro/api/voucher.php
@@ -1,7 +1,7 @@
 <?php
 // Text
 $_['text_success']     = 'Success: Your gift voucher discount has been applied!';
-$_['text_cart']        = 'Success: You have modified your shopping cart!';
+$_['text_cart']        = 'Succes: Ți-ai modificat coșul de cumpărături!';
 $_['text_for']         = '%s Voucher Cadou pentru %s';
 
 // Error
