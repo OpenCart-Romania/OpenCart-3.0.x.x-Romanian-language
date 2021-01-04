14a10d05d446de346ed3a01b6169148833973100
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:53:07 +0800

    New translations register.php (Romanian)

--- upload/catalog/language/ro-ro/mail/register.php
+++ upload/catalog/language/ro-ro/mail/register.php
@@ -1,7 +1,7 @@
 <?php
 // Text
-$_['text_subject']        = '%s - Thank you for registering';
-$_['text_welcome']        = 'Welcome and thank you for registering at %s!';
+$_['text_subject']        = '%s - Îți mulțumim pentru înregistrare';
+$_['text_welcome']        = 'Bine ai venit și îți mulțumim că te-ai înregistrat la %s!';
 $_['text_login']          = 'Your account has now been created and you can log in by using your email address and password by visiting our website or at the following URL:';
 $_['text_approval']       = 'Your account must be approved before you can login. Once approved you can log in by using your email address and password by visiting our website or at the following URL:';
 $_['text_service']        = 'Upon logging in, you will be able to access other services including reviewing past orders, printing invoices and editing your account information.';
@@ -12,4 +12,4 @@ $_['text_customer_group'] = 'Customer Group:';
 $_['text_firstname']      = 'First Name:';
 $_['text_lastname']       = 'Last Name:';
 $_['text_email']          = 'E-Mail:';
-$_['text_telephone']      = 'Telephone:';
+$_['text_telephone']      = 'Telefon:';
