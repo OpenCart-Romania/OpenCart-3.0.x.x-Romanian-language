a1581fb2f7337df91ac51b7016210ff1b04b6ce5
Constantin DOBRE <admin@harddex.ro>
2021-01-04 15:50:31 +0800

    New translations payment.php (Romanian)

--- upload/admin/language/ro-ro/extension/extension/payment.php
+++ upload/admin/language/ro-ro/extension/extension/payment.php
@@ -3,12 +3,12 @@
 $_['heading_title']     = 'Payments';
 
 // Text
-$_['text_success']      = 'Success: You have modified payments!';
+$_['text_success']      = 'Succes: Ai modificat plățile!';
 $_['text_list']         = 'Payment List';
 
 
 // Column
-$_['column_name']       = 'Payment Method';
+$_['column_name']       = 'Metodă de Plată';
 $_['column_status']     = 'Stare';
 $_['column_sort_order'] = 'Ordinea de Sortare';
 $_['column_action']     = 'Acțiune';
