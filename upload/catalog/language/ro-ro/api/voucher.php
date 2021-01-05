33e507289ee959864e91f7bdceeb0d1c4db42a9d
Constantin DOBRE <admin@harddex.ro>
2021-01-05 19:02:18 +0800

    New translations voucher.php (Romanian)

--- upload/catalog/language/ro-ro/api/voucher.php
+++ upload/catalog/language/ro-ro/api/voucher.php
@@ -5,7 +5,7 @@ $_['text_cart']        = 'Succes: Ți-ai modificat coșul de cumpărături!';
 $_['text_for']         = '%s Voucher Cadou pentru %s';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to access the API!';
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a accesa API!';
 $_['error_voucher']    = 'Warning: Gift Voucher is either invalid or the balance has been used up!';
 $_['error_to_name']    = 'Recipient\'s Name must be between 1 and 64 characters!';
 $_['error_from_name']  = 'Your Name must be between 1 and 64 characters!';
