22c8b08d8e8b12c60da138451a40c235dfd98b86
Constantin DOBRE <admin@harddex.ro>
2021-01-05 19:02:40 +0800

    New translations upgrade.php (Romanian)

--- upload/install/language/ro-ro/upgrade/upgrade.php
+++ upload/install/language/ro-ro/upgrade/upgrade.php
@@ -1,20 +1,20 @@
 <?php
 // Heading
-$_['heading_title']   = 'Actualizare';
-$_['heading_success'] = 'Upgrade - Complete!';
+$_['heading_title']   = 'Upgrade';
+$_['heading_success'] = 'Upgrade - Complet!';
 
 // Text
-$_['text_upgrade']    = 'Prepare for your upgrade';
+$_['text_upgrade']    = 'Pregătește-te pentru upgrade';
 $_['text_server']     = 'Verificare că serverul este configurat corect';
-$_['text_steps']      = 'Upgrade Progress';
-$_['text_error']      = 'Post any upgrade script errors problems in the forums';
-$_['text_clear']      = 'After upgrade, clear any cookies in your browser to avoid getting token errors.';
-$_['text_admin']      = 'Load the admin page & press Ctrl+F5 twice to force the browser to update the css changes.';
-$_['text_user']       = 'Goto Admin -> Users -> User Groups and Edit the Top Adminstrator group. Check All boxes.';
-$_['text_setting']    = 'Goto Admin and Edit the main System Settings. Update all fields and click save, even if nothing changed.';
-$_['text_store']      = 'Load the store front & press Ctrl+F5 twice to force the browser to update the css changes.';
-$_['text_progress']   = 'Patch %s has been applied (%s of %s)';
+$_['text_steps']      = 'Progres Upgrade';
+$_['text_error']      = 'Postează orice problemă cu erorile scriptului de upgrade pe forumuri';
+$_['text_clear']      = 'După upgrade, șterge orice cookie-uri din browser pentru a evita apariția erorilor de token.';
+$_['text_admin']      = 'Încărcați pagina de admin & apăsați Ctrl+F5 de două ori pentru a forța browserul să actualizeze modificările css.';
+$_['text_user']       = 'Accesați Admin -> Utilizatori -> Grupuri de Utilizatori și Editați grupul Top Adminstrator. Bifați Toate casuțele.';
+$_['text_setting']    = 'Accesați Admin și Editați principale Setări de Sistem. Actualizați toate câmpurile și faceți clic pe salvare, chiar dacă nu s-a schimbat nimic.';
+$_['text_store']      = 'Încărcați prima pagină a magazinului & apăsați Ctrl+F5 de două ori pentru a forța browserul să actualizeze modificările css.';
+$_['text_progress']   = 'Patch-ul %s a fost aplicat (%s of %s)';
 $_['text_success']    = 'Felicitări! Ai actualizat cu succes instalarea OpenCart!';
 
 // Entry
-$_['entry_progress']  = 'Progress';
+$_['entry_progress']  = 'Progres';
