f3d5484bc010a4e5d297c07a36bb7a0368ec70b1
Constantin DOBRE <admin@harddex.ro>
2021-01-05 19:02:16 +0800

    New translations column_left.php (Romanian)

--- upload/install/language/ro-ro/common/column_left.php
+++ upload/install/language/ro-ro/common/column_left.php
@@ -3,6 +3,6 @@
 $_['text_license']       = 'Licenţă';
 $_['text_installation']  = 'Pre-Instalare';
 $_['text_configuration'] = 'Configurare';
-$_['text_upgrade']       = 'Actualizare';
+$_['text_upgrade']       = 'Upgrade';
 $_['text_finished']      = 'Finalizat';
 $_['text_language']      = 'Limbă';
