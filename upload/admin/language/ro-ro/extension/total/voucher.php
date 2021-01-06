71e070860c28525fdd8c42f9f101fadb66cb463d
Constantin DOBRE <admin@harddex.ro>
2021-01-07 02:05:57 +0800

    New translations voucher.php (Romanian)

--- upload/admin/language/ro-ro/extension/total/voucher.php
+++ upload/admin/language/ro-ro/extension/total/voucher.php
@@ -9,7 +9,7 @@ $_['text_edit']        = 'Edit Gift Voucher Total';
 
 // Entry
 $_['entry_status']     = 'Stare';
-$_['entry_sort_order'] = 'Ordinea de Sortare';
+$_['entry_sort_order'] = 'Ordine Sortare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify gift voucher total!';
