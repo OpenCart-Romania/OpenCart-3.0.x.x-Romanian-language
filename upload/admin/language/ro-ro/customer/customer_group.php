c6589f0e5ea07806deb99f321f839a72058abf0f
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:15:25 +0800

    New translations customer_group.php (Romanian)

---
+++ upload/admin/language/ro-ro/customer/customer_group.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']     = 'Grupuri de Clienți';
+
+// Text
+$_['text_success']      = 'Succes: Ai modificat grupurile de clienți!';
+$_['text_list']         = 'Customer Group List';
+$_['text_add']          = 'Add Customer Group';
+$_['text_edit']         = 'Edit Customer Group';
+
+// Column
+$_['column_name']       = 'Nume Grup Clienți';
+$_['column_sort_order'] = 'Ordinea de Sortare';
+$_['column_action']     = 'Acțiune';
+
+// Entry
+$_['entry_name']        = 'Nume Grup Clienți';
+$_['entry_description'] = 'Descriere';
+$_['entry_approval']    = 'Approve New Customers';
+$_['entry_sort_order']  = 'Ordinea de Sortare';
+
+// Help
+$_['help_approval']     = 'Customers must be approved by an administrator before they can login.';
+
+// Error
+$_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica grupurile de clienți!';
+$_['error_name']        = 'Nume Grup Clienți trebuie să fie între 3 și 32 de caractere!';
+$_['error_default']     = 'Warning: This customer group cannot be deleted as it is currently assigned as the default store customer group!';
+$_['error_store']       = 'Warning: This customer group cannot be deleted as it is currently assigned to %s stores!';
+$_['error_customer']    = 'Warning: This customer group cannot be deleted as it is currently assigned to %s customers!';
