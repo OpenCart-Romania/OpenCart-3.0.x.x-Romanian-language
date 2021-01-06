67366c6157d56de89d3382d77649b9c2edb5a22e
Constantin DOBRE <admin@harddex.ro>
2021-01-06 15:58:12 +0800

    New translations klarna_invoice.php (Romanian)

--- upload/catalog/language/ro-ro/extension/payment/klarna_invoice.php
+++ upload/catalog/language/ro-ro/extension/payment/klarna_invoice.php
@@ -10,6 +10,8 @@ $_['text_year']					= 'An';
 $_['text_month']				= 'Lună';
 $_['text_day']					= 'Zi';
 $_['text_comment']				= 'Klarna\'s Invoice ID: %s. ' . "\n" . '%s/%s: %.4f';
+$_['text_trems_description']    = 'Mit der Übermittlung der für die Abwicklung des Rechnungskaufes und einer Identitäts - und Bonitätsprüfung erforderlichen
+Daten an Klarna bin ich einverstanden. Meine <a href="https://online.klarna.com/consent_de.yaws" target="_blank">Einwilligung</a> kann ich jederzeit mit Wirkung für die Zukunft widerrufen.';
 
 // Entry
 $_['entry_gender']				= 'Gender';
