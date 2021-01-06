175a08dd3ab973b7cbcb2891a98e39ddd6afa2a6
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:12:42 +0800

    New translations currency.php (Romanian)

---
+++ upload/catalog/language/ro-ro/api/currency.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_success']     = 'Success: Your currency has been changed!';
+
+// Error
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a accesa API!';
+$_['error_currency']   = 'Warning: Currency code is invalid!';
