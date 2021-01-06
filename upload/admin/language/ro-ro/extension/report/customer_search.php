7bdb807aa32ff460549d8818842dd6596a7811f5
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:43:35 +0800

    New translations customer_search.php (Romanian)

--- upload/admin/language/ro-ro/extension/report/customer_search.php
+++ upload/admin/language/ro-ro/extension/report/customer_search.php
@@ -14,7 +14,7 @@ $_['text_customer']     = '<a href="%s">%s</a>';
 $_['column_keyword']    = 'Cuvânt Cheie';
 $_['column_products']   = 'Found Products';
 $_['column_category']   = 'Category';
-$_['column_customer']   = 'Customer';
+$_['column_customer']   = 'Client';
 $_['column_ip']         = 'IP';
 $_['column_date_added'] = 'Data Adăugării';
 
@@ -22,7 +22,7 @@ $_['column_date_added'] = 'Data Adăugării';
 $_['entry_date_start']  = 'Dată de Început';
 $_['entry_date_end']    = 'Dată de Sfârșit';
 $_['entry_keyword']     = 'Cuvânt Cheie';
-$_['entry_customer']    = 'Customer';
+$_['entry_customer']    = 'Client';
 $_['entry_ip']          = 'IP';
 $_['entry_status']      = 'Stare';
 $_['entry_sort_order']  = 'Ordinea de Sortare';
