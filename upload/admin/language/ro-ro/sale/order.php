dfbe61fe1893005c022ae5b30a17cbb62f8371c7
Constantin DOBRE <admin@harddex.ro>
2021-01-06 17:24:22 +0800

    New translations order.php (Romanian)

--- upload/admin/language/ro-ro/sale/order.php
+++ upload/admin/language/ro-ro/sale/order.php
@@ -129,4 +129,4 @@ $_['help_override']              = 'If the customers order is being blocked from
 $_['error_warning']              = 'Atenție: Te rugăm să verifici cu atenție formularul pentru erori!';
 $_['error_permission']           = 'Warning: You do not have permission to modify orders!';
 $_['error_action']               = 'Warning: Could not complete this action!';
-$_['error_filetype']             = 'Tip fișier nevalid!';
+$_['error_filetype']             = 'Tip fișier incorect!';
