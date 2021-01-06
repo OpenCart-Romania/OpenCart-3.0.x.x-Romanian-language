a70d9ebb5bd3ee55e41ff3597c491cddd529066e
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:13:40 +0800

    New translations address.php (Romanian)

---
+++ upload/catalog/language/ro-ro/account/address.php
@@ -0,0 +1,37 @@
+<?php
+// Heading
+$_['heading_title']      = 'Agendă';
+
+// Text
+$_['text_account']       = 'Cont';
+$_['text_address_book']  = 'Intrări Agendă';
+$_['text_address_add']   = 'Adaugă Adresă';
+$_['text_address_edit']  = 'Editează Adresă';
+$_['text_add']           = 'Adresa ta a fost adăugată cu succes';
+$_['text_edit']          = 'Adresa ta a fost actualizată cu succes';
+$_['text_delete']        = 'Adresa ta a fost ștearsă cu succes';
+$_['text_empty']         = 'Nu ai nicio adresă în contul tău.';
+
+// Entry
+$_['entry_firstname']    = 'Prenume';
+$_['entry_lastname']     = 'Nume de Familie';
+$_['entry_company']      = 'Companie';
+$_['entry_address_1']    = 'Adresă 1';
+$_['entry_address_2']    = 'Adresă 2';
+$_['entry_postcode']     = 'Cod Poștal';
+$_['entry_city']         = 'Oraș';
+$_['entry_country']      = 'Țară';
+$_['entry_zone']         = 'Regiune / Judeţ';
+$_['entry_default']      = 'Adresa Implicită';
+
+// Error
+$_['error_delete']       = 'Atenție: Trebuie să ai cel puțin o adresă!';
+$_['error_default']      = 'Atenție: Nu poți șterge adresa ta implicită!';
+$_['error_firstname']    = 'Prenumele trebuie să fie între 1 și 32 de caractere!';
+$_['error_lastname']     = 'Numele de Familie trebuie să fie între 1 și 32 de caractere!';
+$_['error_address_1']    = 'Adresa trebuie să fie între 3 și 128 de caractere!';
+$_['error_postcode']     = 'Cod postal trebuie să fie între 2 și 10 de caractere!';
+$_['error_city']         = 'Oraș trebuie să fie între 2 și 128 de caractere!';
+$_['error_country']      = 'Te rugăm selectează o țară!';
+$_['error_zone']         = 'Te rugăm selectează o Regiune / un Județ!';
+$_['error_custom_field'] = '%s necesar!';
