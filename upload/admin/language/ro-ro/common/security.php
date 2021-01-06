0d055e1785fd0a49e0649d965cc11e3fc40a8da8
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:15:33 +0800

    New translations security.php (Romanian)

---
+++ upload/admin/language/ro-ro/common/security.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']    = 'Notificare Importantă de Securitate!';
+
+// Text
+$_['text_success']     = 'Succes: Ai modificat directorul "storage"!';
+$_['text_admin']       = 'Editează admin/config.php și modifică';
+$_['text_security']    = 'Este foarte important să muți directorul "storage" în afara directorului web (e.g. public_html, www or htdocs).';
+$_['text_choose']      = 'Alege cum să muți directorul de stocare';
+$_['text_automatic']   = 'Mutare automată';
+$_['text_manual']      = 'Mutare manuală';
+$_['text_move']        = 'Mutare';
+$_['text_to']          = 'către';
+$_['text_config']      = 'Editează modificarea config.php';
+$_['text_admin']       = 'Editează admin/config.php și modifică';
+
+// Entry
+$_['entry_directory']  = 'Director';
+
+// Button
+$_['button_move']      = 'Mutare';
+$_['button_manual']    = 'Manual';
+
+// Error
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica directorul "storage"!';
+$_['error_path']       = 'Atenție: Cale invalidă!';
+$_['error_directory']  = 'Atenție: Director invalid!';
+$_['error_exists']     = 'Atenție: Directorul există deja!';
+$_['error_writable']   = 'Atenție: config.php and admin/config.php trebuie să fie editabile!';
