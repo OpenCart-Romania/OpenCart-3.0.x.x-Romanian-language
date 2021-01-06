1716113ddbcb7804a4c690b0468e0770df840b22
Constantin DOBRE <admin@harddex.ro>
2021-01-07 02:03:30 +0800

    New translations alipay_cross.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/alipay_cross.php
+++ upload/admin/language/ro-ro/extension/payment/alipay_cross.php
@@ -19,7 +19,7 @@ $_['entry_currency']                 = 'Currency Code';
 $_['entry_order_status']             = 'Completed Status';
 $_['entry_geo_zone']                 = 'Geo Zone';
 $_['entry_status']                   = 'Stare';
-$_['entry_sort_order']               = 'Ordinea de Sortare';
+$_['entry_sort_order']               = 'Ordine Sortare';
 
 // Help
 $_['help_total']                     = 'The checkout total the order must reach before this payment method becomes active';
