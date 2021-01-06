41f8d4197ca31158dc240a602466aa6a3dbb3f6e
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:15:23 +0800

    New translations layout.php (Romanian)

---
+++ upload/admin/language/ro-ro/design/layout.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']       = 'Layout-uri';
+
+// Text
+$_['text_success']        = 'Succes: Ai modificat layout-urile!';
+$_['text_list']           = 'Layout List';
+$_['text_add']            = 'Adaugă Layout';
+$_['text_edit']           = 'Edit Layout';
+$_['text_remove']         = 'Remove';
+$_['text_route']          = 'Choose the store and routes to be used with this layout';
+$_['text_module']         = 'Choose the position of the modules';
+$_['text_default']        = 'Implicit';
+$_['text_content_top']    = 'Content Top';
+$_['text_content_bottom'] = 'Content Bottom';
+$_['text_column_left']    = 'Column Left';
+$_['text_column_right']   = 'Column Right';
+
+// Column
+$_['column_name']         = 'Layout Name';
+$_['column_action']       = 'Acțiune';
+
+// Entry
+$_['entry_name']          = 'Layout Name';
+$_['entry_store']         = 'Store';
+$_['entry_route']         = 'Route';
+$_['entry_module']        = 'Module';
+
+// Error
+$_['error_permission']    = 'Atenție: Nu ai permisiunea de a modifica layout-urile!';
+$_['error_name']          = 'Layout Name must be between 3 and 64 characters!';
+$_['error_default']       = 'Warning: This layout cannot be deleted as it is currently assigned as the default store layout!';
+$_['error_store']         = 'Warning: This layout cannot be deleted as it is currently assigned to %s stores!';
+$_['error_product']       = 'Warning: This layout cannot be deleted as it is currently assigned to %s products!';
+$_['error_category']      = 'Warning: This layout cannot be deleted as it is currently assigned to %s categories!';
+$_['error_information']   = 'Warning: This layout cannot be deleted as it is currently assigned to %s information pages!';
