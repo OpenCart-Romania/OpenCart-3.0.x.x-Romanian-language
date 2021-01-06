b5e751996bd0fde25e9ba4ac621138643de4bf91
Constantin DOBRE <admin@harddex.ro>
2021-01-07 02:06:55 +0800

    New translations customer_search.php (Romanian)

--- upload/admin/language/ro-ro/extension/report/customer_search.php
+++ upload/admin/language/ro-ro/extension/report/customer_search.php
@@ -25,7 +25,7 @@ $_['entry_keyword']     = 'Cuvânt Cheie';
 $_['entry_customer']    = 'Client';
 $_['entry_ip']          = 'IP';
 $_['entry_status']      = 'Stare';
-$_['entry_sort_order']  = 'Ordinea de Sortare';
+$_['entry_sort_order']  = 'Ordine Sortare';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify customer searches report!';
