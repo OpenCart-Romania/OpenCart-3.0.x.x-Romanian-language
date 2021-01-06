a7660ab7d3a84a9d108326f190f68f0825c52299
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:14:04 +0800

    New translations map.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/dashboard/map.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    = 'Harta Lumii';
+
+// Text
+$_['text_extension']   = 'Extensii';
+$_['text_success']     = 'Succes: Ai modificat panoul hartă!';
+$_['text_edit']        = 'Editează Panoul Hartă';
+$_['text_order']       = 'Comenzi';
+$_['text_sale']        = 'Vânzări';
+
+// Entry
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordinea de Sortare';
+$_['entry_width']      = 'Lăţime';
+
+// Error
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica panoul hartă!';
