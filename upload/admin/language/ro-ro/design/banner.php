9cda3e9a9f70bd5d8b6a842d99f40a82431fd83c
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:15:15 +0800

    New translations banner.php (Romanian)

---
+++ upload/admin/language/ro-ro/design/banner.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']     = 'Bannere';
+
+// Text
+$_['text_success']      = 'Succes: Ai modificat bannerele!';
+$_['text_list']         = 'Listă Bannere';
+$_['text_add']          = 'Adaugă Banner';
+$_['text_edit']         = 'Editează Banner';
+$_['text_default']      = 'Implicit';
+
+// Column
+$_['column_name']       = 'Nume Banner';
+$_['column_status']     = 'Stare';
+$_['column_action']     = 'Acțiune';
+
+// Entry
+$_['entry_name']        = 'Nume Banner';
+$_['entry_title']       = 'Titlu';
+$_['entry_link']        = 'Link';
+$_['entry_image']       = 'Imagine';
+$_['entry_status']      = 'Stare';
+$_['entry_sort_order']  = 'Ordinea de Sortare';
+
+// Error
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica bannerele!';
+$_['error_name']       = 'Nume Banner trebuie să fie între 3 și 64 de caractere!';
+$_['error_title']      = 'Titlu Banner trebuie să fie între 2 și 64 de caractere!';
