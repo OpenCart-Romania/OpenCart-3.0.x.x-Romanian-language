0c912156c852f67908f19a512b0dbbab0e6f4cd6
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:11:42 +0800

    New translations customer_transaction.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/report/customer_transaction.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']         = 'Customer Transaction Report';
+
+// Column
+$_['text_extension']        = 'Extensii';
+$_['text_edit']             = 'Edit Customer Transaction Report';
+$_['text_success']          = 'Success: You have modified customer credit report!';
+$_['text_filter']           = 'Filtru';
+
+// Column
+$_['column_customer']       = 'Nume Client';
+$_['column_email']          = 'E-Mail';
+$_['column_customer_group'] = 'Grup Clienți';
+$_['column_status']         = 'Stare';
+$_['column_total']          = 'Total';
+$_['column_action']         = 'Acțiune';
+
+// Entry
+$_['entry_date_start']      = 'Dată de Început';
+$_['entry_date_end']        = 'Dată de Sfârșit';
+$_['entry_customer']        = 'Customer';
+$_['entry_status']          = 'Stare';
+$_['entry_sort_order']      = 'Ordinea de Sortare';
+
+// Error
+$_['error_permission']      = 'Warning: You do not have permission to modify customer credit report!';
