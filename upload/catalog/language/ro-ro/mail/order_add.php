49052baa6531ec8535419657106705ef3fa51a93
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:18:55 +0800

    New translations order_add.php (Romanian)

---
+++ upload/catalog/language/ro-ro/mail/order_add.php
@@ -0,0 +1,27 @@
+<?php
+// Text
+$_['text_subject']          = '%s - Order %s';
+$_['text_greeting']         = 'Thank you for your interest in %s products. Your order has been received and will be processed once payment has been confirmed.';
+$_['text_link']             = 'To view your order click on the link below:';
+$_['text_order_detail']     = 'Order Details';
+$_['text_instruction']      = 'Instructions';
+$_['text_order_id']         = 'Order ID:';
+$_['text_date_added']       = 'Data Adăugării:';
+$_['text_order_status']     = 'Order Status:';
+$_['text_payment_method']   = 'Metodă de Plată:';
+$_['text_shipping_method']  = 'Metodă Livrare:';
+$_['text_email']            = 'E-mail:';
+$_['text_telephone']        = 'Telefon:';
+$_['text_ip']               = 'IP Address:';
+$_['text_payment_address']  = 'Adresă Plată';
+$_['text_shipping_address'] = 'Adresă Livrare';
+$_['text_products']         = 'Produse';
+$_['text_product']          = 'Produs';
+$_['text_model']            = 'Model';
+$_['text_quantity']         = 'Cantitate';
+$_['text_price']            = 'Preț';
+$_['text_order_total']      = 'Order Totals';
+$_['text_total']            = 'Total';
+$_['text_download']         = 'Once your payment has been confirmed you can click on the link below to access your downloadable products:';
+$_['text_comment']          = 'The comments for your order are:';
+$_['text_footer']           = 'Please reply to this e-mail if you have any questions.';
