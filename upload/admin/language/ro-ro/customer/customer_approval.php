bff7c7e9206650f9b9222d0d9bcd4d8f915b6318
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:15:29 +0800

    New translations customer_approval.php (Romanian)

---
+++ upload/admin/language/ro-ro/customer/customer_approval.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']         = 'Aprobări Clienți';
+
+// Text
+$_['text_success']          = 'Succes: Ai modificat aprobările clienților!';
+$_['text_list']             = 'Listă Aprobări Clienți';
+$_['text_default']          = 'Implicit';
+$_['text_customer']         = 'Client';
+$_['text_affiliate']        = 'Afiliat';
+
+// Column
+$_['column_name']           = 'Nume Client';
+$_['column_email']          = 'E-Mail';
+$_['column_customer_group'] = 'Grup Clienți';
+$_['column_type']           = 'Tip';
+$_['column_date_added']     = 'Data Adăugării';
+$_['column_action']         = 'Acțiune';
+
+// Entry
+$_['entry_name']            = 'Nume Client';
+$_['entry_email']           = 'E-Mail';
+$_['entry_customer_group']  = 'Grup Clienți';
+$_['entry_type']            = 'Tip';
+$_['entry_date_added']      = 'Data Adăugării';
+
+// Error
+$_['error_permission']      = 'Atenție: Nu ai permisiunea de a modifica aprobările clienților!';
