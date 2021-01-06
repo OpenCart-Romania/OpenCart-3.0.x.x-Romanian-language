12da0d6ec71ef986a6384d4fc76aa409960efdc7
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:14:23 +0800

    New translations module.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/extension/module.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']    = 'Modules';
+
+// Text
+$_['text_success']     = 'Succes: Ai modificat modulele!';
+$_['text_layout']      = 'After you have installed and configured a module you can add it to a layout <a href="%s" class="alert-link">here</a>!';
+$_['text_add']         = 'Adaugă Modul';
+$_['text_list']        = 'Module List';
+
+// Column
+$_['column_name']      = 'Module Name';
+$_['column_status']    = 'Stare';
+$_['column_action']    = 'Acțiune';
+
+// Entry
+$_['entry_code']       = 'Module';
+$_['entry_name']       = 'Module Name';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify modules!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_code']       = 'Extension required!';
