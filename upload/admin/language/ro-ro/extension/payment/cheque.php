aabf1d2d2cb4ad8ccf8003c04638802afa045303
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:17:04 +0800

    New translations cheque.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/payment/cheque.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']      = 'Cec / Ordin de Plată';
+
+// Text
+$_['text_extension']     = 'Extensii';
+$_['text_success']       = 'Succes: Ai modificat detaliile contului cec / ordin de plată!';
+$_['text_edit']          = 'Editează Cec / Ordin de Plată';
+
+// Entry
+$_['entry_payable']      = 'Payable To';
+$_['entry_total']        = 'Total';
+$_['entry_order_status'] = 'Stare Comandă';
+$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_status']       = 'Stare';
+$_['entry_sort_order']   = 'Ordinea de Sortare';
+
+// Help
+$_['help_total']         = 'The checkout total the order must reach before this payment method becomes active.';
+
+// Error
+$_['error_permission']   = 'Atenție: Nu ai permisiunea de a modifica plata cec / ordin de plată!';
+$_['error_payable']      = 'Payable To Required!';
