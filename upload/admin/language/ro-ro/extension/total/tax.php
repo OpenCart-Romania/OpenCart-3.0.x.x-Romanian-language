d0963f05324c50862ec4f0bc03c7637ccd85bf7d
Constantin DOBRE <admin@harddex.ro>
2021-01-07 02:05:49 +0800

    New translations tax.php (Romanian)

--- upload/admin/language/ro-ro/extension/total/tax.php
+++ upload/admin/language/ro-ro/extension/total/tax.php
@@ -9,7 +9,7 @@ $_['text_edit']        = 'Edit Tax Total';
 
 // Entry
 $_['entry_status']     = 'Stare';
-$_['entry_sort_order'] = 'Ordinea de Sortare';
+$_['entry_sort_order'] = 'Ordine Sortare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify taxes total!';
