daf8f26477f34964811cd3aae9303b5abf6c6364
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:13:27 +0800

    New translations recurring.php (Romanian)

---
+++ upload/admin/language/ro-ro/sale/recurring.php
@@ -0,0 +1,82 @@
+<?php
+// Heading
+$_['heading_title']                        = 'Comenzi Recurente';
+
+// Text
+$_['text_success']                         = 'Succes: Ai modificat profilurile recurente!';
+$_['text_list']                            = 'Recurring Order List';
+$_['text_filter']                          = 'Filtru';
+$_['text_recurring_detail']                = 'Recurring Details';
+$_['text_order_detail']                    = 'Order Details';
+$_['text_product_detail']                  = 'Product Details';
+$_['text_transaction']                     = 'Tranzacții';
+$_['text_order_recurring_id']              = 'ID Comandă Recurentă';
+$_['text_reference']                       = 'Payment Reference';
+$_['text_recurring_name']                  = 'Profil Recurent';
+$_['text_recurring_description']           = 'Descriere';
+$_['text_recurring_status']                = 'Recurring Status';
+$_['text_payment_method']                  = 'Metodă de Plată';
+$_['text_order_id']                        = 'ID Comandă';
+$_['text_customer']                        = 'Customer';
+$_['text_email']                           = 'Email';
+$_['text_date_added']                      = 'Data Adăugării';
+$_['text_order_status']                    = 'Stare Comandă';
+$_['text_type']                            = 'Tip';
+$_['text_action']                          = 'Acțiune';
+$_['text_product']                         = 'Produs';
+$_['text_quantity']                        = 'Cantitate';
+$_['text_amount']                          = 'Valoare';
+$_['text_cancel_payment']                  = 'Cancel Payment';
+$_['text_status_1']                        = 'Active';
+$_['text_status_2']                        = 'Inactive';
+$_['text_status_3']                        = 'Cancelled';
+$_['text_status_4']                        = 'Suspended';
+$_['text_status_5']                        = 'Expired';
+$_['text_status_6']                        = 'Pending';
+
+$_['text_transactions']                    = 'Tranzacții';
+$_['text_cancel_confirm']                  = 'Profile\'s cancellation cannot be undone! Are you sure want to do this?';
+$_['text_transaction_date_added']          = 'Date added';
+$_['text_transaction_payment']             = 'Payment';
+$_['text_transaction_outstanding_payment'] = 'Outstanding payment';
+$_['text_transaction_skipped']             = 'Payment skipped';
+$_['text_transaction_failed']              = 'Payment failed';
+$_['text_transaction_cancelled']           = 'Cancelled';
+$_['text_transaction_suspended']           = 'Suspended';
+$_['text_transaction_suspended_failed']    = 'Suspended from failed payment';
+$_['text_transaction_outstanding_failed']  = 'Outstanding payment failed';
+$_['text_transaction_expired']             = 'Expired';
+$_['text_cancelled']                       = 'Recurring payment has been cancelled';
+
+// Column
+$_['column_order_recurring_id']             = 'ID Recurență';
+$_['column_order_id']                       = 'ID Comandă';
+$_['column_reference']                      = 'Payment Reference';
+$_['column_customer']                       = 'Customer';
+$_['column_date_added']                     = 'Data Adăugării';
+$_['column_status']                         = 'Stare';
+$_['column_amount']                         = 'Valoare';
+$_['column_type']                           = 'Tip';
+$_['column_action']                         = 'Acțiune';
+
+// Entry
+$_['entry_order_recurring_id']             = 'ID Recurență';
+$_['entry_order_id']                       = 'ID Comandă';
+$_['entry_reference']                      = 'Payment Reference';
+$_['entry_customer']                       = 'Customer';
+$_['entry_date_added']                     = 'Data Adăugării';
+$_['entry_status']                         = 'Stare';
+$_['entry_type']                           = 'Tip';
+$_['entry_action']                         = 'Acțiune';
+$_['entry_email']                          = 'Email';
+$_['entry_description']                    = 'Recurring Profile\'s description';
+$_['entry_product']                        = 'Produs';
+$_['entry_quantity']                       = 'Cantitate';
+$_['entry_amount']                         = 'Valoare';
+$_['entry_recurring']                      = 'Profil Recurent';
+$_['entry_payment_method']                 = 'Metodă de Plată';
+$_['entry_cancel_payment']                 = 'Cancel Payment';
+
+// Error
+$_['error_not_cancelled']                  = 'Error: %s';
+$_['error_not_found']                      = 'Could not cancel recurring profile';
