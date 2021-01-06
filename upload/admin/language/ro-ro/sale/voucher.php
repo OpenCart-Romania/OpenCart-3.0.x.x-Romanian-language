dd200b4911da318003c40bbad4f827664dda17a8
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:13:07 +0800

    New translations voucher.php (Romanian)

---
+++ upload/admin/language/ro-ro/sale/voucher.php
@@ -0,0 +1,46 @@
+<?php
+// Heading
+$_['heading_title']     = 'Vouchere Cadou';
+
+// Text
+$_['text_success']      = 'Success: You have modified vouchers!';
+$_['text_list']         = 'Gift Voucher List';
+$_['text_add']          = 'Add Gift Voucher';
+$_['text_edit']         = 'Edit Gift Voucher';
+
+// Column
+$_['column_name']       = 'Voucher Name';
+$_['column_code']       = 'Code';
+$_['column_from']       = 'From';
+$_['column_to']         = 'To';
+$_['column_theme']      = 'Temă';
+$_['column_amount']     = 'Valoare';
+$_['column_status']     = 'Stare';
+$_['column_order_id']   = 'ID Comandă';
+$_['column_customer']   = 'Customer';
+$_['column_date_added'] = 'Data Adăugării';
+$_['column_action']     = 'Acțiune';
+
+// Entry
+$_['entry_code']        = 'Code';
+$_['entry_from_name']   = 'From Name';
+$_['entry_from_email']  = 'From E-Mail';
+$_['entry_to_name']     = 'To Name';
+$_['entry_to_email']    = 'To E-Mail';
+$_['entry_theme']       = 'Temă';
+$_['entry_message']     = 'Message';
+$_['entry_amount']      = 'Valoare';
+$_['entry_status']      = 'Stare';
+
+// Help
+$_['help_code']         = 'The code the customer enters to activate the voucher.';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify vouchers!';
+$_['error_exists']      = 'Warning: Voucher code is already in use!';
+$_['error_code']        = 'Code must be between 3 and 10 characters!';
+$_['error_to_name']     = 'Recipient\'s Name must be between 1 and 64 characters!';
+$_['error_from_name']   = 'Your Name must be between 1 and 64 characters!';
+$_['error_email']       = 'Adresa E-Mail nu pare să fie validă!';
+$_['error_amount']      = 'Cantitatea trebuie să fie mai mare sau egală cu 1!';
+$_['error_order']       = 'Warning: This voucher cannot be deleted as it is part of an <a href="%s">order</a>!';
