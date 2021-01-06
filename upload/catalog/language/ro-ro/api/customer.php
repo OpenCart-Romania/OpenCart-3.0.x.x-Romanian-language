c085e1cb12f07ed0f3b2358f7bf7f156065c1989
Constantin DOBRE <admin@harddex.ro>
2021-01-06 17:24:30 +0800

    New translations customer.php (Romanian)

--- upload/catalog/language/ro-ro/api/customer.php
+++ upload/catalog/language/ro-ro/api/customer.php
@@ -1,10 +1,10 @@
 <?php
 // Text
-$_['text_success']       = 'You have successfully modified customers';
+$_['text_success']       = 'Ai modificat cu succes clienţii';
 
 // Error
 $_['error_permission']   = 'Atenție: Nu ai permisiunea de a accesa API!';
-$_['error_customer']     = 'You must select a customer!';
+$_['error_customer']     = 'Trebuie să selectezi un client!';
 $_['error_firstname']    = 'Prenumele trebuie să fie între 1 și 32 de caractere!';
 $_['error_lastname']     = 'Numele de Familie trebuie să fie între 1 și 32 de caractere!';
 $_['error_email']        = 'Adresa E-Mail nu pare să fie validă!';
