9f1e558ef7733cbbe94a5f5dc938263ac58a9a9f
Constantin Dobre <admin@harddex.ro>
2021-01-06 16:03:50 +0800

    remove ro-ro files from main (will be commited just in translation branch)

--- upload/admin/language/ro-ro/common/dashboard.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'Dashboard';
-
-// Error
-$_['error_install'] = 'Atenție: Directorul de instalare există încă și ar trebui șters din motive de securitate!';
--- upload/admin/language/ro-ro/common/footer.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' Toate drepturile rezervate.';
-$_['text_version'] = 'Versiunea %s';
