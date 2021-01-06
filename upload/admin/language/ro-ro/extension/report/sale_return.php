66083a624021962f95a2b916e9f0f0d416c5766e
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:12:03 +0800

    New translations sale_return.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/report/sale_return.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']     = 'Returns Report';
+
+// Text
+$_['text_extension']    = 'Extensii';
+$_['text_edit']         = 'Edit Returns Report';
+$_['text_success']      = 'Success: You have modified returns report!';
+$_['text_filter']       = 'Filtru';
+$_['text_year']         = 'Ani';
+$_['text_month']        = 'Luni';
+$_['text_week']         = 'Săptămâni';
+$_['text_day']          = 'Zile';
+$_['text_all_status']   = 'All Statuses';
+
+// Column
+$_['column_date_start'] = 'Dată de Început';
+$_['column_date_end']   = 'Dată de Sfârșit';
+$_['column_returns']    = 'No. Returns';
+
+// Entry
+$_['entry_date_start']  = 'Dată de Început';
+$_['entry_date_end']    = 'Dată de Sfârșit';
+$_['entry_group']       = 'Group By';
+$_['entry_status']      = 'Status';
+$_['entry_status']      = 'Stare';
+$_['entry_sort_order']  = 'Ordinea de Sortare';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify returns report!';
