d6fa5ebdc4d1319ab38c9917d399a3e0f8437d2c
Constantin DOBRE <admin@harddex.ro>
2021-01-07 02:05:18 +0800

    New translations item.php (Romanian)

--- upload/admin/language/ro-ro/extension/shipping/item.php
+++ upload/admin/language/ro-ro/extension/shipping/item.php
@@ -12,7 +12,7 @@ $_['entry_cost']       = 'Cost';
 $_['entry_tax_class']  = 'Clasă de Taxare';
 $_['entry_geo_zone']   = 'Geo Zone';
 $_['entry_status']     = 'Stare';
-$_['entry_sort_order'] = 'Ordinea de Sortare';
+$_['entry_sort_order'] = 'Ordine Sortare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify shipping per item rates!';
