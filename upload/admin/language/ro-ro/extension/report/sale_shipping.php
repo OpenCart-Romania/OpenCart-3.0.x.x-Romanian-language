453d257bffe3c4015d4b2b036208a03903790c26
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:12:12 +0800

    New translations sale_shipping.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/report/sale_shipping.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']     = 'Shipping Report';
+
+// Text
+$_['text_extension']    = 'Extensii';
+$_['text_edit']         = 'Edit Shipping Report';
+$_['text_success']      = 'Success: You have modified shipping report!';
+$_['text_filter']       = 'Filtru';
+$_['text_year']         = 'Ani';
+$_['text_month']        = 'Luni';
+$_['text_week']         = 'Săptămâni';
+$_['text_day']          = 'Zile';
+$_['text_all_status']   = 'All Statuses';
+
+// Column
+$_['column_date_start'] = 'Dată de Început';
+$_['column_date_end']   = 'Dată de Sfârșit';
+$_['column_title']      = 'Shipping Title';
+$_['column_orders']     = 'No. Orders';
+$_['column_total']      = 'Total';
+
+// Entry
+$_['entry_date_start']  = 'Dată de Început';
+$_['entry_date_end']    = 'Dată de Sfârșit';
+$_['entry_group']       = 'Group By';
+$_['entry_status']      = 'Stare Comandă';
+$_['entry_status']      = 'Stare';
+$_['entry_sort_order']  = 'Ordinea de Sortare';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify shipping report!';
