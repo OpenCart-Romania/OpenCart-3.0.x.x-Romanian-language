7c1300a9c462ea5683624802a1ea47a682ed080f
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:49:38 +0800

    New translations coupon.php (Romanian)

--- upload/catalog/language/ro-ro/extension/total/coupon.php
+++ upload/catalog/language/ro-ro/extension/total/coupon.php
@@ -4,7 +4,7 @@ $_['heading_title'] = 'Use Coupon Code';
 
 // Text
 $_['text_coupon']   = 'Coupon (%s)';
-$_['text_success']  = 'Success: Your coupon discount has been applied!';
+$_['text_success']  = 'Succes: Reducerea cuponului tău a fost aplicată!';
 
 // Entry
 $_['entry_coupon']  = 'Enter your coupon here';
