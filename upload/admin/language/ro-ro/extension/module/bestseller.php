0b694919f139b6e3dd4beb6c71e08613f0b9fabe
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:16:29 +0800

    New translations bestseller.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/module/bestseller.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']    = 'Bestsellers';
+
+// Text
+$_['text_extension']   = 'Extensii';
+$_['text_success']     = 'Success: You have modified bestsellers module!';
+$_['text_edit']        = 'Edit Bestsellers Module';
+
+// Entry
+$_['entry_name']       = 'Module Name';
+$_['entry_limit']      = 'Limit';
+$_['entry_image']      = 'Image (W x H) and Resize Type';
+$_['entry_width']      = 'Lăţime';
+$_['entry_height']     = 'Înălţime';
+$_['entry_status']     = 'Stare';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify bestsellers module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_width']      = 'Width required!';
+$_['error_height']     = 'Height required!';
