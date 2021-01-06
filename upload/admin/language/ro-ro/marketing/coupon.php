ee5defd88c017c9e1264de7b83fb8ff2eb83ba27
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:13:06 +0800

    New translations coupon.php (Romanian)

---
+++ upload/admin/language/ro-ro/marketing/coupon.php
@@ -0,0 +1,57 @@
+<?php
+// Heading
+$_['heading_title']       = 'Cupoane';
+
+// Text
+$_['text_success']        = 'Success: You have modified coupons!';
+$_['text_list']           = 'Coupon List';
+$_['text_add']            = 'Add Coupon';
+$_['text_edit']           = 'Edit Coupon';
+$_['text_percent']        = 'Procentaj';
+$_['text_amount']         = 'Valoare Fixă';
+$_['text_coupon']         = 'Coupon History';
+
+// Column
+$_['column_name']         = 'Coupon Name';
+$_['column_code']         = 'Code';
+$_['column_discount']     = 'Reducere';
+$_['column_date_start']   = 'Dată de Început';
+$_['column_date_end']     = 'Dată de Sfârșit';
+$_['column_status']       = 'Stare';
+$_['column_order_id']     = 'ID Comandă';
+$_['column_customer']     = 'Customer';
+$_['column_amount']       = 'Valoare';
+$_['column_date_added']   = 'Data Adăugării';
+$_['column_action']       = 'Acțiune';
+
+// Entry
+$_['entry_name']          = 'Coupon Name';
+$_['entry_code']          = 'Code';
+$_['entry_type']          = 'Tip';
+$_['entry_discount']      = 'Reducere';
+$_['entry_logged']        = 'Customer Login';
+$_['entry_shipping']      = 'Free Shipping';
+$_['entry_total']         = 'Total Amount';
+$_['entry_category']      = 'Category';
+$_['entry_product']       = 'Produse';
+$_['entry_date_start']    = 'Dată de Început';
+$_['entry_date_end']      = 'Dată de Sfârșit';
+$_['entry_uses_total']    = 'Uses Per Coupon';
+$_['entry_uses_customer'] = 'Uses Per Customer';
+$_['entry_status']        = 'Stare';
+
+// Help
+$_['help_code']           = 'The code the customer enters to get the discount.';
+$_['help_type']           = 'Percentage or Fixed Amount.';
+$_['help_logged']         = 'Customer must be logged in to use the coupon.';
+$_['help_total']          = 'The total amount that must be reached before the coupon is valid.';
+$_['help_category']       = 'Choose all products under selected category.';
+$_['help_product']        = 'Choose specific products the coupon will apply to. Select no products to apply coupon to entire cart.';
+$_['help_uses_total']     = 'The maximum number of times the coupon can be used by any customer. Leave blank for unlimited';
+$_['help_uses_customer']  = 'The maximum number of times the coupon can be used by a single customer. Leave blank for unlimited';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify coupons!';
+$_['error_exists']        = 'Warning: Coupon code is already in use!';
+$_['error_name']          = 'Nume Cupon trebuie să fie între 3 și 128 de caractere!';
+$_['error_code']          = 'Code must be between 3 and 10 characters!';
