dd59fcfb1c8e422c0404cd37fcfc72a98acc9b38
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:10:33 +0800

    New translations attribute.php (Romanian)

---
+++ upload/admin/language/ro-ro/catalog/attribute.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']          = 'Atribute';
+
+// Text
+$_['text_success']           = 'Succes: Ai modificat atributele!';
+$_['text_list']              = 'Listă Atribute';
+$_['text_add']               = 'Adaugă Atribut';
+$_['text_edit']              = 'Editează Atribut';
+
+// Column
+$_['column_name']            = 'Nume Atribut';
+$_['column_attribute_group'] = 'Grup Atribute';
+$_['column_sort_order']      = 'Ordinea de Sortare';
+$_['column_action']          = 'Acțiune';
+
+// Entry
+$_['entry_name']             = 'Nume Atribut';
+$_['entry_attribute_group']  = 'Grup Atribute';
+$_['entry_sort_order']       = 'Ordinea de Sortare';
+
+// Error
+$_['error_permission']       = 'Atenție: Nu ai permisiunea de a modifica atributele!';
+$_['error_attribute_group']  = 'Grup Atribute Necesar!';
+$_['error_name']             = 'Nume Atribut trebuie să fie între 1 și 64 de caractere!';
+$_['error_product']          = 'Atenție: Acest atribut nu poate fi șters deoarece în prezent este asociat cu %s produse!';
