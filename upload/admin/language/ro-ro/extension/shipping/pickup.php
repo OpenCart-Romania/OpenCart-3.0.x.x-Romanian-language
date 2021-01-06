781a7a4c52974f9cead9bf907f2a6487ca65add6
Constantin DOBRE <admin@harddex.ro>
2021-01-07 02:05:12 +0800

    New translations pickup.php (Romanian)

--- upload/admin/language/ro-ro/extension/shipping/pickup.php
+++ upload/admin/language/ro-ro/extension/shipping/pickup.php
@@ -10,7 +10,7 @@ $_['text_edit']        = 'Edit Pickup From Store Shipping';
 // Entry
 $_['entry_geo_zone']   = 'Geo Zone';
 $_['entry_status']     = 'Stare';
-$_['entry_sort_order'] = 'Ordinea de Sortare';
+$_['entry_sort_order'] = 'Ordine Sortare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify pickup from store!';
