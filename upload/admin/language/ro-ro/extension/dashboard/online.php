1676deb1fb16e99dbb3421cfa12d1e92b9fc96c4
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:14:05 +0800

    New translations online.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/dashboard/online.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'Oameni Online';
+
+// Text
+$_['text_extension']   = 'Extensii';
+$_['text_success']     = 'Succes: Ai modificat panoul online!';
+$_['text_edit']        = 'Editează Panoul Online';
+$_['text_view']        = 'Vezi mai mult...';
+
+// Entry
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordinea de Sortare';
+$_['entry_width']      = 'Lăţime';
+
+// Error
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica panoul online!';
