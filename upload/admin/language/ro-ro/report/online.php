335065393625722a387ec1286b2546c3c6382eb7
Constantin DOBRE <admin@harddex.ro>
2021-01-06 16:13:20 +0800

    New translations online.php (Romanian)

---
+++ upload/admin/language/ro-ro/report/online.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']     = 'Online Report';
+
+// Text
+$_['text_extension']    = 'Extensii';
+$_['text_success']      = 'Success: You have modified customers online report!';
+$_['text_list']         = 'Online List';
+$_['text_filter']       = 'Filtru';
+$_['text_guest']        = 'Guest';
+
+// Column
+$_['column_ip']         = 'IP';
+$_['column_customer']   = 'Customer';
+$_['column_url']        = 'Last Page Visited';
+$_['column_referer']    = 'Referer';
+$_['column_date_added'] = 'Last Click';
+$_['column_action']     = 'Acțiune';
+
+// Entry
+$_['entry_ip']          = 'IP';
+$_['entry_customer']    = 'Customer';
+$_['entry_status']      = 'Stare';
+$_['entry_sort_order']  = 'Ordinea de Sortare';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify customers online report!';
