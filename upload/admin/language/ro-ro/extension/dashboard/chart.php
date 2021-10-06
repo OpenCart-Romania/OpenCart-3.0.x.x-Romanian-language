9f725d0fc396cef3401cc15e8d9e465603aa7483
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:13:39 +0800

    New translations chart.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/dashboard/chart.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']    = 'Statistici Vânzări';
+
+// Text
+$_['text_extension']   = 'Extensii';
+$_['text_success']     = 'Succes: Ai modificat panoul diagramă!';
+$_['text_edit']        = 'Editează Panoul Diagramă';
+$_['text_order']       = 'Comenzi';
+$_['text_customer']    = 'Clienţi';
+$_['text_day']         = 'Astăzi';
+$_['text_week']        = 'Săptămână';
+$_['text_month']       = 'Lună';
+$_['text_year']        = 'An';
+
+// Entry
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordine Sortare';
+$_['entry_width']      = 'Lăţime';
+
+// Error
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica panoul diagramă!';
