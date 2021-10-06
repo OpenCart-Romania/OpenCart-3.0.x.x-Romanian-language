6f3d3686d07f5fb66753d364afb7328b929a46fe
Constantin DOBRE <admin@harddex.ro>
2021-10-06 16:43:27 +0800

    New translations register.php (Romanian)

--- upload/catalog/language/ro-ro/mail/register.php
+++ upload/catalog/language/ro-ro/mail/register.php
@@ -2,14 +2,14 @@
 // Text
 $_['text_subject']        = '%s - Îți mulțumim pentru înregistrare';
 $_['text_welcome']        = 'Bine ai venit și îți mulțumim că te-ai înregistrat la %s!';
-$_['text_login']          = 'Your account has now been created and you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_approval']       = 'Your account must be approved before you can login. Once approved you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_service']        = 'Upon logging in, you will be able to access other services including reviewing past orders, printing invoices and editing your account information.';
-$_['text_thanks']         = 'Thanks,';
-$_['text_new_customer']   = 'New customer';
-$_['text_signup']         = 'A new customer has signed up:';
-$_['text_customer_group'] = 'Customer Group:';
-$_['text_firstname']      = 'First Name:';
-$_['text_lastname']       = 'Last Name:';
+$_['text_login']          = 'Contul tău a fost creat şi te poți conecta folosindu-ți adresa de email şi parola pe site-ul nostru sau la următorul URL:';
+$_['text_approval']       = 'Contul tău trebuie să fie aprobat înainte de a te putea conecta. Odată aprobat, te poți conecta folosindu-ți adresa de email şi parola pe site-ul nostru sau la următorul URL:';
+$_['text_service']        = 'La conectare vei putea accesa și alte servicii, inclusiv recenzarea unei comenzi, imprimarea de facturi şi modificarea informaţiilor contului tău.';
+$_['text_thanks']         = 'Mulțumim';
+$_['text_new_customer']   = 'Client nou';
+$_['text_signup']         = 'Un nou client s-a înregistrat:';
+$_['text_customer_group'] = 'Grup de Clienți:';
+$_['text_firstname']      = 'Prenume:';
+$_['text_lastname']       = 'Nume de Familie:';
 $_['text_email']          = 'E-Mail:';
 $_['text_telephone']      = 'Telefon:';
