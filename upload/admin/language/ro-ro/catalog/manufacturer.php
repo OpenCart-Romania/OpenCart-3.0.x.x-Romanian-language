f7c25245ce30a2792c4308b979f11745efdb35ae
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:13:31 +0800

    New translations manufacturer.php (Romanian)

---
+++ upload/admin/language/ro-ro/catalog/manufacturer.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']     = 'Producători';
+
+// Text
+$_['text_success']      = 'Succes: Ai modificat producătorii!';
+$_['text_list']         = 'Listă Producători';
+$_['text_add']          = 'Adaugă Producător';
+$_['text_edit']         = 'Editează Producător';
+$_['text_default']      = 'Implicit';
+$_['text_percent']      = 'Procentaj';
+$_['text_amount']       = 'Valoare Fixă';
+$_['text_keyword']      = 'Nu utiliza spații, în schimb înlocuieste spațiile cu - și asigură-te că SEO URL este unic global.';
+
+// Column
+$_['column_name']       = 'Nume Producător';
+$_['column_sort_order'] = 'Ordine Sortare';
+$_['column_action']     = 'Acțiune';
+
+// Entry
+$_['entry_name']        = 'Nume Producător';
+$_['entry_store']       = 'Magazine';
+$_['entry_keyword']     = 'Cuvânt Cheie';
+$_['entry_image']       = 'Imagine';
+$_['entry_sort_order']  = 'Ordine Sortare';
+$_['entry_type']        = 'Tip';
+
+// Error
+$_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica producătorii!';
+$_['error_name']        = 'Nume Producător trebuie să fie între 1 și 64 de caractere!';
+$_['error_keyword']     = 'SEO URL este deja utilizat!';
+$_['error_unique']      = 'SEO URL trebuie să fie unic!';
+$_['error_product']     = 'Atenție: Acest producător nu poate fi șters deoarece în prezent este asociat cu %s produse!';
