aa7d49925dc3b6c26daa8d0503c109e3213d2e86
Constantin DOBRE <admin@harddex.ro>
2021-10-06 16:43:22 +0800

    New translations compare.php (Romanian)

--- upload/catalog/language/ro-ro/product/compare.php
+++ upload/catalog/language/ro-ro/product/compare.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']     = 'Product Comparison';
+$_['heading_title']     = 'Compararea Produselor';
 
 // Text
-$_['text_product']      = 'Product Details';
+$_['text_product']      = 'Detalii produs';
 $_['text_name']         = 'Produs';
 $_['text_image']        = 'Imagine';
 $_['text_price']        = 'Preț';
 $_['text_model']        = 'Model';
-$_['text_manufacturer'] = 'Brand';
-$_['text_availability'] = 'Availability';
+$_['text_manufacturer'] = 'Marca';
+$_['text_availability'] = 'Disponibilitate';
 $_['text_instock']      = 'În Stoc';
 $_['text_rating']       = 'Evaluare';
 $_['text_reviews']      = 'Bazat pe %s recenzii.';
-$_['text_summary']      = 'Summary';
+$_['text_summary']      = 'Rezumat';
 $_['text_weight']       = 'Greutate';
 $_['text_dimension']    = 'Dimensiuni (L x l x Î)';
-$_['text_compare']      = 'Product Compare (%s)';
-$_['text_success']      = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">product comparison</a>!';
-$_['text_remove']       = 'Success: You have modified your product comparison!';
-$_['text_empty']        = 'You have not chosen any products to compare.';
+$_['text_compare']      = 'Compara produs (%s)';
+$_['text_success']      = 'Succes: Ați adăugat <a href="%s">%s</a> la <a href="%s">compararea produselor</a>!';
+$_['text_remove']       = 'Succes: Ați modificat compararea produsul dvs!';
+$_['text_empty']        = 'Nu ați ales nici un produs care să fie comparat.';
