ef63f3e6e700bdd05142e081d7f7378973b87645
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:14:05 +0800

    New translations low_order_fee.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/total/low_order_fee.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'Low Order Fee';
+
+// Text
+$_['text_extension']   = 'Extensii';
+$_['text_success']     = 'Success: You have modified low order fee total!';
+$_['text_edit']        = 'Edit Low Order Fee Total';
+
+// Entry
+$_['entry_total']      = 'Order Total';
+$_['entry_fee']        = 'Fee';
+$_['entry_tax_class']  = 'Clasă de Taxare';
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordine Sortare';
+
+// Help
+$_['help_total']       = 'The checkout total the order must reach before this order total is deactivated.';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify low order fee total!';
