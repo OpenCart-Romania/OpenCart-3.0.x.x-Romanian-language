ee3a8121858d1a9dab9a94daf30f66b0c9c3a5af
Constantin DOBRE <admin@harddex.ro>
2021-10-06 16:43:21 +0800

    New translations manufacturer.php (Romanian)

--- upload/catalog/language/ro-ro/product/manufacturer.php
+++ upload/catalog/language/ro-ro/product/manufacturer.php
@@ -1,27 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Find Your Favorite Brand';
+$_['heading_title']     = 'Găsiți-vă marca preferată';
 
 // Text
-$_['text_brand']        = 'Brand';
-$_['text_index']        = 'Brand Index:';
-$_['text_error']        = 'Brand not found!';
-$_['text_empty']        = 'There are no products to list.';
-$_['text_quantity']     = 'Qty:';
-$_['text_manufacturer'] = 'Brand:';
-$_['text_model']        = 'Product Code:';
+$_['text_brand']        = 'Marca';
+$_['text_index']        = 'Indicele de Marcă:';
+$_['text_error']        = 'Marca nu a fost găsită!';
+$_['text_empty']        = 'Nu există produse de listat.';
+$_['text_quantity']     = 'Cantitate:';
+$_['text_manufacturer'] = 'Marca:';
+$_['text_model']        = 'Cod Produs:';
 $_['text_points']       = 'Puncte de Recompensă:';
-$_['text_price']        = 'Price:';
-$_['text_tax']          = 'Ex Tax:';
-$_['text_compare']      = 'Product Compare (%s)';
-$_['text_sort']         = 'Sort By:';
+$_['text_price']        = 'Preț:';
+$_['text_tax']          = 'Fără TVA:';
+$_['text_compare']      = 'Compara produs (%s)';
+$_['text_sort']         = 'Sortează după:';
 $_['text_default']      = 'Implicit';
-$_['text_name_asc']     = 'Name (A - Z)';
-$_['text_name_desc']    = 'Name (Z - A)';
-$_['text_price_asc']    = 'Price (Low &gt; High)';
-$_['text_price_desc']   = 'Price (High &gt; Low)';
-$_['text_rating_asc']   = 'Rating (Lowest)';
-$_['text_rating_desc']  = 'Rating (Highest)';
+$_['text_name_asc']     = 'De la (A - Z)';
+$_['text_name_desc']    = 'De la (Z - A)';
+$_['text_price_asc']    = 'Pret: de la mic la mare';
+$_['text_price_desc']   = 'Preț: de la mare la mic';
+$_['text_rating_asc']   = 'Rating (Mic)';
+$_['text_rating_desc']  = 'Rating (Mare)';
 $_['text_model_asc']    = 'Model (A - Z)';
 $_['text_model_desc']   = 'Model (Z - A)';
-$_['text_limit']        = 'Show:';
+$_['text_limit']        = 'Afișează:';
