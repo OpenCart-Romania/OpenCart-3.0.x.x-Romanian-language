a5c33b84741bb2b5906375b396f3ac6fe07e9414
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:15:25 +0800

    New translations customer_search.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/report/customer_search.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']     = 'Customer Searches Report';
+
+// Text
+$_['text_extension']    = 'Extensii';
+$_['text_edit']         = 'Edit Customer Searches Report';
+$_['text_success']      = 'Success: You have modified customer searches report!';
+$_['text_filter']       = 'Filtru';
+$_['text_guest']        = 'Guest';
+$_['text_customer']     = '<a href="%s">%s</a>';
+
+// Column
+$_['column_keyword']    = 'Cuvânt Cheie';
+$_['column_products']   = 'Found Products';
+$_['column_category']   = 'Category';
+$_['column_customer']   = 'Client';
+$_['column_ip']         = 'IP';
+$_['column_date_added'] = 'Data Adăugării';
+
+// Entry
+$_['entry_date_start']  = 'Dată de Început';
+$_['entry_date_end']    = 'Dată de Sfârșit';
+$_['entry_keyword']     = 'Cuvânt Cheie';
+$_['entry_customer']    = 'Client';
+$_['entry_ip']          = 'IP';
+$_['entry_status']      = 'Stare';
+$_['entry_sort_order']  = 'Ordine Sortare';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify customer searches report!';
