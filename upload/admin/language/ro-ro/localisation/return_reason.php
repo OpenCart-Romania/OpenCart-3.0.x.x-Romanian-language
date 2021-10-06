9f37873068bf30a928d38b6ca478af58a0f7eb6b
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:14:40 +0800

    New translations return_reason.php (Romanian)

---
+++ upload/admin/language/ro-ro/localisation/return_reason.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'Motive Returnare';
+
+// Text
+$_['text_success']     = 'Success: You have modified return reasons!';
+$_['text_list']        = 'Listă Motive Returnare';
+$_['text_add']         = 'Adaugă Motiv Returnare';
+$_['text_edit']        = 'Editează Motiv Returnare';
+
+// Column
+$_['column_name']      = 'Nume Motiv Returnare';
+$_['column_action']    = 'Acțiune';
+
+// Entry
+$_['entry_name']       = 'Nume Motiv Returnare';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify return reasons!';
+$_['error_name']       = 'Nume Motiv Returnare trebuie să fie între 3 și 128 de caractere!';
+$_['error_return']     = 'Warning: This return reason cannot be deleted as it is currently assigned to %s returned products!';
