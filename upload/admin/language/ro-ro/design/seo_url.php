783613dbfb1195c38faf781fb35670213eba277e
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:13:59 +0800

    New translations seo_url.php (Romanian)

---
+++ upload/admin/language/ro-ro/design/seo_url.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']    = 'SEO URL';
+
+// Text
+$_['text_success']     = 'Succes: Ai modificat SEO URL!';
+$_['text_list']        = 'Listă SEO URL';
+$_['text_add']         = 'Adaugă SEO URL';
+$_['text_edit']        = 'Editează SEO URL';
+$_['text_filter']      = 'Filtru';
+$_['text_default']     = 'Implicit';
+
+// Column
+$_['column_query']     = 'Query';
+$_['column_keyword']   = 'Cuvânt Cheie';
+$_['column_store']     = 'Magazin';
+$_['column_language']  = 'Language';
+$_['column_action']    = 'Acțiune';
+
+// Entry
+$_['entry_query']        = 'Query';
+$_['entry_keyword']      = 'Cuvânt Cheie';
+$_['entry_store']        = 'Magazin';
+$_['entry_language']     = 'Language';
+
+// Error
+$_['error_permission']   = 'Atenție: Nu ai permisiunea de a modifica SEO URL!';
+$_['error_query']        = 'Query must be between 3 and 64 characters!';
+$_['error_keyword']      = 'Keyword must be between 3 and 64 characters!';
+$_['error_exists']       = 'Cuvânt Cheie deja utilizat!';
+$_['error_query_exists'] = 'Query already in use!';
