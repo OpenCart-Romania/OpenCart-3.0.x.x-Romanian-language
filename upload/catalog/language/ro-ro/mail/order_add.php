08720b5f1ab80afaef87c6025bdacd0629908996
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:11:20 +0800

    New translations order_add.php (Romanian)

---
+++ upload/catalog/language/ro-ro/mail/order_add.php
@@ -0,0 +1,27 @@
+<?php
+// Text
+$_['text_subject']          = '%s - Comanda %s';
+$_['text_greeting']         = 'Îți mulţumim pentru interesul tău asupra acestor %s produse. Comanda ta a fost recepționată şi va fi procesată imediat ce plata a fost confirmată.';
+$_['text_link']             = 'Pentru a-ți vizualiza comanda apasă pe linkul de mai jos:';
+$_['text_order_detail']     = 'Detalii comandă';
+$_['text_instruction']      = 'Instrucțiuni';
+$_['text_order_id']         = 'Comanda ID:';
+$_['text_date_added']       = 'Data Adăugării:';
+$_['text_order_status']     = 'Statusul comenzii:';
+$_['text_payment_method']   = 'Metodă de Plată:';
+$_['text_shipping_method']  = 'Metodă Livrare:';
+$_['text_email']            = 'E-Mail:';
+$_['text_telephone']        = 'Telefon:';
+$_['text_ip']               = 'Adresă IP:';
+$_['text_payment_address']  = 'Adresă Plată';
+$_['text_shipping_address'] = 'Adresă Livrare';
+$_['text_products']         = 'Produse';
+$_['text_product']          = 'Produs';
+$_['text_model']            = 'Model';
+$_['text_quantity']         = 'Cantitate';
+$_['text_price']            = 'Preț';
+$_['text_order_total']      = 'Totaluri Comandă';
+$_['text_total']            = 'Total';
+$_['text_download']         = 'Odată ce s-a confirmat plata ta, poţi apăsa pe linkul de mai jos pentru a accesa produsele descărcabile:';
+$_['text_comment']          = 'Comentariile pentru comanda dvs. sunt:';
+$_['text_footer']           = 'Vă rugăm să răspundeţi la acest e-mail dacă aveţi întrebări.';
