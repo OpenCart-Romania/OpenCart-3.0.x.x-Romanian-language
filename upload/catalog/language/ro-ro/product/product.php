c1bbc3fbbc7c59b7df952485bb925cbffcf86b16
Constantin DOBRE <admin@harddex.ro>
2021-10-06 16:43:20 +0800

    New translations product.php (Romanian)

--- upload/catalog/language/ro-ro/product/product.php
+++ upload/catalog/language/ro-ro/product/product.php
@@ -1,26 +1,26 @@
 <?php
 // Text
 $_['text_search']              = 'Căutare';
-$_['text_brand']               = 'Brand';
-$_['text_manufacturer']        = 'Brand:';
-$_['text_model']               = 'Product Code:';
+$_['text_brand']               = 'Marca';
+$_['text_manufacturer']        = 'Marca:';
+$_['text_model']               = 'Cod Produs:';
 $_['text_reward']              = 'Puncte de Recompensă:';
-$_['text_points']              = 'Price in reward points:';
-$_['text_stock']               = 'Availability:';
+$_['text_points']              = 'Preţ în puncte recompensă:';
+$_['text_stock']               = 'Disponibilitate:';
 $_['text_instock']             = 'În Stoc';
-$_['text_tax']                 = 'Ex Tax:';
-$_['text_discount']            = ' or more ';
-$_['text_option']              = 'Available Options';
-$_['text_minimum']             = 'This product has a minimum quantity of %s';
+$_['text_tax']                 = 'Fără TVA:';
+$_['text_discount']            = ' mai mult ';
+$_['text_option']              = 'Opțiuni disponibile';
+$_['text_minimum']             = 'Acest produs are o cantitate minimă de %s';
 $_['text_reviews']             = '%s recenzii';
 $_['text_write']               = 'Scrie o recenzie';
-$_['text_login']               = 'Please <a href="%s">login</a> or <a href="%s">register</a> to review';
-$_['text_no_reviews']          = 'There are no reviews for this product.';
-$_['text_note']                = '<span class="text-danger">Note:</span> HTML is not translated!';
-$_['text_success']             = 'Thank you for your review. It has been submitted to the webmaster for approval.';
+$_['text_login']               = 'Te rugam <a href="%s">autentifica-te</a> or <a href="%s">intregistreaza-te</a> pentru a scrie o recenzie';
+$_['text_no_reviews']          = 'Momentan nu sunt recenzii pentru acest produs.';
+$_['text_note']                = '<span class="text-danger">Nota:</span> HTML nu este tradus!';
+$_['text_success']             = 'Vă mulțumim pentru comentariul dvs. A fost trimis administratorului pentru aprobare.';
 $_['text_related']             = 'Produse Asemănătoare';
-$_['text_tags']                = 'Tags:';
-$_['text_error']               = 'Product not found!';
+$_['text_tags']                = 'Tag-uri:';
+$_['text_error']               = 'Produs nu a putut fi găsit!';
 $_['text_payment_recurring']   = 'Profil Plată';
 $_['text_trial_description']   = '%s every %d %s(s) for %d payment(s) then';
 $_['text_payment_description'] = '%s every %d %s(s) for %d payment(s)';
@@ -32,19 +32,19 @@ $_['text_month']               = 'lună';
 $_['text_year']                = 'an';
 
 // Entry
-$_['entry_qty']                = 'Qty';
+$_['entry_qty']                = 'Cantitate';
 $_['entry_name']               = 'Numele Tău';
-$_['entry_review']             = 'Your Review';
+$_['entry_review']             = 'Recenzia ta';
 $_['entry_rating']             = 'Evaluare';
-$_['entry_good']               = 'Good';
-$_['entry_bad']                = 'Bad';
+$_['entry_good']               = 'Bună';
+$_['entry_bad']                = 'Rău';
 
 // Tabs
 $_['tab_description']          = 'Descriere';
-$_['tab_attribute']            = 'Specification';
+$_['tab_attribute']            = 'Specificație';
 $_['tab_review']               = 'Recenzii (%s)';
 
 // Error
-$_['error_name']               = 'Warning: Review Name must be between 3 and 25 characters!';
-$_['error_text']               = 'Warning: Review Text must be between 25 and 1000 characters!';
-$_['error_rating']             = 'Warning: Please select a review rating!';
+$_['error_name']               = 'Atenție: Textul Comentariului trebuie să fie între 3 și 25 de caractere!';
+$_['error_text']               = 'Atenție: Textul Comentariului trebuie să fie între 25 și 1000 de caractere!';
+$_['error_rating']             = 'Atenție: Selectează o evaluare a comentariilor!';
