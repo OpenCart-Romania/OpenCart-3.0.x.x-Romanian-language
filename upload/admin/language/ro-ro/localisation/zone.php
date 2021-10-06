7a033745ba48ad9000f1ed24d0a3b1101bbe3dcf
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:14:33 +0800

    New translations zone.php (Romanian)

---
+++ upload/admin/language/ro-ro/localisation/zone.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']          = 'Zone';
+
+// Text
+$_['text_success']           = 'Success: You have modified zones!';
+$_['text_list']              = 'Zone List';
+$_['text_add']               = 'Add Zone';
+$_['text_edit']              = 'Edit Zone';
+
+// Column
+$_['column_name']            = 'Zone Name';
+$_['column_code']            = 'Zone Code';
+$_['column_country']         = 'Țară';
+$_['column_action']          = 'Acțiune';
+
+// Entry
+$_['entry_name']             = 'Zone Name';
+$_['entry_code']             = 'Zone Code';
+$_['entry_country']          = 'Țară';
+$_['entry_status']           = 'Stare';
+
+// Error
+$_['error_permission']       = 'Warning: You do not have permission to modify zones!';
+$_['error_name']             = 'Nume Zonă trebuie să fie între 1 şi 128 de caractere!';
+$_['error_default']          = 'Warning: This zone cannot be deleted as it is currently assigned as the default store zone!';
+$_['error_store']            = 'Warning: This zone cannot be deleted as it is currently assigned to %s stores!';
+$_['error_address']          = 'Warning: This zone cannot be deleted as it is currently assigned to %s address book entries!';
+$_['error_affiliate']        = 'Warning: This zone cannot be deleted as it is currently assigned to %s affiliates!';
+$_['error_zone_to_geo_zone'] = 'Warning: This zone cannot be deleted as it is currently assigned to %s zones to geo zones!';
