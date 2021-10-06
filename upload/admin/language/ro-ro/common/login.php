77dfdc4dfd13f5ff5bcc16b222cbe6a33d96fad0
Constantin DOBRE <admin@harddex.ro>
2021-10-06 18:16:07 +0800

    New translations login.php (Romanian)

--- upload/admin/language/ro-ro/common/login.php
+++ upload/admin/language/ro-ro/common/login.php
@@ -17,3 +17,4 @@ $_['button_login']   = 'Autentificare';
 // Error
 $_['error_login']    = 'Nume utilizator și/sau Parolă nu se potrivesc.';
 $_['error_token']    = 'Sesiune token incorectă. Te rugăm să te autentifici din nou.';
+$_['error_attempts'] = 'Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour or reset password.';
