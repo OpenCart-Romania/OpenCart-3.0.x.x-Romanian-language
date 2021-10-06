3f49a7e95dfbfa795b59c8f68f0f500a4c523974
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:11:19 +0800

    New translations search.php (Romanian)

---
+++ upload/catalog/language/ro-ro/product/search.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title']     = 'Căutare';
+$_['heading_tag']       = 'Tag - ';
+
+// Text
+$_['text_search']       = 'Produse care îndeplinesc criteriile de căutare';
+$_['text_keyword']      = 'Cuvinte Cheie';
+$_['text_category']     = 'Toate Categoriile';
+$_['text_sub_category'] = 'Căutare în subcategorii';
+$_['text_empty']        = 'Nu există niciun produs care să corespundă criteriilor de căutare.';
+$_['text_quantity']     = 'Cantitate:';
+$_['text_manufacturer'] = 'Marca:';
+$_['text_model']        = 'Cod Produs:';
+$_['text_points']       = 'Puncte de Recompensă:';
+$_['text_price']        = 'Preț:';
+$_['text_tax']          = 'Fără TVA:';
+$_['text_reviews']      = 'Bazat pe %s recenzii.';
+$_['text_compare']      = 'Compara produs (%s)';
+$_['text_sort']         = 'Sortează după:';
+$_['text_default']      = 'Implicit';
+$_['text_name_asc']     = 'De la (A - Z)';
+$_['text_name_desc']    = 'De la (Z - A)';
+$_['text_price_asc']    = 'Pret: de la mic la mare';
+$_['text_price_desc']   = 'Preț: de la mare la mic';
+$_['text_rating_asc']   = 'Rating (Mic)';
+$_['text_rating_desc']  = 'Rating (Mare)';
+$_['text_model_asc']    = 'Model (A - Z)';
+$_['text_model_desc']   = 'Model (Z - A)';
+$_['text_limit']        = 'Afișează:';
+
+// Entry
+$_['entry_search']      = 'Căutare criterii';
+$_['entry_description'] = 'Căutați în descrierile produselor';
