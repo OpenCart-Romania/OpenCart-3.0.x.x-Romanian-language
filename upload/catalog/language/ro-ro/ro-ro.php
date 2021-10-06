b8f17f6f9dc289c9b92a9d67409b0dd67f3f0736
Constantin DOBRE <admin@harddex.ro>
2021-10-06 18:15:10 +0800

    New translations en-gb.php (Romanian)

--- upload/catalog/language/ro-ro/ro-ro.php
+++ upload/catalog/language/ro-ro/ro-ro.php
@@ -73,7 +73,6 @@ $_['error_curl']            = 'CURL: Cod de Eroare(%s): %s';
 /* When doing translations only include the matching language code */
 
 // Datepicker
-$_['datepicker']              = 'ro';
 //$_['datepicker']            = 'af';
 //$_['datepicker']            = 'ar-dz';
 //$_['datepicker']            = 'ar-kw';
@@ -101,7 +100,7 @@ $_['datepicker']              = 'ro';
 //$_['datepicker']            = 'el';
 //$_['datepicker']            = 'en-au';
 //$_['datepicker']            = 'en-ca';
-//$_['datepicker']            = 'en-gb';
+$_['datepicker']            = 'ro';
 //$_['datepicker']            = 'en-ie';
 //$_['datepicker']            = 'en-nz';
 //$_['datepicker']            = 'eo';
