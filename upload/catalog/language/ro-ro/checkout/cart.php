41466b15a921df15c2748e8bafc0c984ad36898d
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:09:15 +0800

    New translations cart.php (Romanian)

---
+++ upload/catalog/language/ro-ro/checkout/cart.php
@@ -0,0 +1,40 @@
+<?php
+// Heading
+$_['heading_title']            = 'Coș de Cumpărături';
+
+// Text
+$_['text_success']             = 'Succes: Ai adăugat <a href="%s">%s</a> la <a href="%s">coșul tău de cumpărături</a>!';
+$_['text_remove']              = 'Succes: Ți-ai modificat coșul de cumpărături!';
+$_['text_login']               = 'Atenție: Trebuie <a href="%s">să vă conectați</a> sau <a href="%s">creați un cont</a> pentru a vedea prețurile!';
+$_['text_items']               = '%s articol(e) - %s';
+$_['text_points']              = 'Puncte de Recompensă: %s';
+$_['text_next']                = 'Ce ai vrea să faci mai departe?';
+$_['text_next_choice']         = 'Alegeți dacă aveți un cod de reducere sau puncte de recompensă pe care doriți să le utilizați sau doriți să estimați costul de livrare.';
+$_['text_empty']               = 'Coșul tău de cumpărături este gol!';
+$_['text_day']                 = 'zi';
+$_['text_week']                = 'săptămână';
+$_['text_semi_month']          = 'jumătate de lună';
+$_['text_month']               = 'lună';
+$_['text_year']                = 'an';
+$_['text_trial']               = '%s every %s %s for %s payments then ';
+$_['text_recurring']           = '%s every %s %s';
+$_['text_recurring_item']      = 'Articol Recurent';
+$_['text_payment_recurring']   = 'Profil Plată';
+$_['text_trial_description']   = '%s every %d %s(s) for %d payment(s) then';
+$_['text_payment_description'] = '%s every %d %s(s) for %d payment(s)';
+$_['text_payment_cancel']      = '%s every %d %s(s) until canceled';
+
+// Column
+$_['column_image']             = 'Imagine';
+$_['column_name']              = 'Nume Produs';
+$_['column_model']             = 'Model';
+$_['column_quantity']          = 'Cantitate';
+$_['column_price']             = 'Preț Unitar';
+$_['column_total']             = 'Total';
+
+// Error
+$_['error_stock']              = 'Produsele marcate cu *** nu sunt disponibile în cantitatea dorită sau nu sunt în stoc!';
+$_['error_minimum']            = 'Suma minimă pentru comanda %s este %s!';
+$_['error_required']           = '%s necesar!';
+$_['error_product']            = 'Atenție: Nu există produse in coş!';
+$_['error_recurring_required'] = 'Vă rugăm să alegeți o modalitate de plată!';
