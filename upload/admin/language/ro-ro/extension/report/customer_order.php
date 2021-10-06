476f33de15d7e41cd39bac194c0f98c92a463674
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:15:27 +0800

    New translations customer_order.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/report/customer_order.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']         = 'Customer Orders Report';
+
+// Text
+$_['text_extension']        = 'Extensii';
+$_['text_edit']             = 'Edit Customer Orders Report';
+$_['text_success']          = 'Success: You have modified customer orders report!';
+$_['text_filter']           = 'Filtru';
+$_['text_all_status']       = 'All Statuses';
+
+// Column
+$_['column_customer']       = 'Nume Client';
+$_['column_email']          = 'E-Mail';
+$_['column_customer_group'] = 'Grup de Clienți';
+$_['column_status']         = 'Stare';
+$_['column_orders']         = 'No. Orders';
+$_['column_products']       = 'No. Products';
+$_['column_total']          = 'Total';
+$_['column_action']         = 'Acțiune';
+
+// Entry
+$_['entry_date_start']      = 'Dată de Început';
+$_['entry_date_end']        = 'Dată de Sfârșit';
+$_['entry_customer']        = 'Client';
+$_['entry_status']          = 'Stare Comandă';
+$_['entry_status']          = 'Stare';
+$_['entry_sort_order']      = 'Ordine Sortare';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify customer orders report!';
