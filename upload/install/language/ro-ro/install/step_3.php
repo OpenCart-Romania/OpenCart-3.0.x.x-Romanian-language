52a61f4e142ddf2c5d35f5e02091274620ebde20
Constantin DOBRE <admin@harddex.ro>
2021-10-06 20:58:40 +0800

    New translations step_3.php (Romanian)

---
+++ upload/install/language/ro-ro/install/step_3.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']          = 'Configurare';
+
+// Text
+$_['text_step_3']            = 'Introdu baza de date și detaliile de administrare';
+$_['text_db_connection']     = '1. Te rog introdu detaliile conexiunii bazei de date.';
+$_['text_db_administration'] = '2. Te rog introdu un nume de utilizator și o parolă pentru administrare.';
+$_['text_mysqli']            = 'MySQLi';
+$_['text_pdo']               = 'PDO';
+$_['text_pgsql']             = 'PostgreSQL';
+
+// Entry
+$_['entry_db_driver']        = 'DB Driver';
+$_['entry_db_hostname']      = 'Hostname';
+$_['entry_db_username']      = 'Nume utilizator';
+$_['entry_db_password']      = 'Parolă';
+$_['entry_db_database']      = 'Bază de Date';
+$_['entry_db_port']          = 'Port';
+$_['entry_db_prefix']        = 'Prefix';
+$_['entry_username']         = 'Nume utilizator';
+$_['entry_password']         = 'Parolă';
+$_['entry_email']            = 'E-Mail';
+
+// Error
+$_['error_db_driver']        = 'Driver Bază de Date necesar!';
+$_['error_db_hostname']      = 'Hostname necesar!';
+$_['error_db_username']      = 'Nume utilizator necesar!';
+$_['error_db_database']      = 'Nume Bază de Date necesar!';
+$_['error_db_port']          = 'Port Bază de Date necesar!';
+$_['error_db_prefix']        = 'DB Prefix poate conține numai caractere minuscule în intervalul a-z, 0-9 și sublinieri';
+$_['error_db_connect']       = 'Eroare: Nu s-a putut conecta la baza de date, te rog asigură-te că serverul bazei de date, numele de utilizator și parola sunt corecte!';
+$_['error_username']         = 'Nume utilizator necesar!';
+$_['error_password']         = 'Parolă necesară!';
+$_['error_email']            = 'Adresa E-Mail nu pare să fie validă!';
+$_['error_config']           = 'Eroare: Nu s-a putut scrie in config.php te rog verifică că ai setat permisiunile corecte pe: ';
