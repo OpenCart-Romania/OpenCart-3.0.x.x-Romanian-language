abc11c11bc18bc5770ca76771f5422a283358e82
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:09:55 +0800

    New translations voucher_theme.php (Romanian)

---
+++ upload/admin/language/ro-ro/sale/voucher_theme.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']     = 'Teme Voucher';
+
+// Text
+$_['text_success']      = 'Success: You have modified voucher themes!';
+$_['text_list']         = 'Voucher Theme List';
+$_['text_add']          = 'Adaugă Temă Voucher';
+$_['text_edit']         = 'Edit Voucher Theme';
+
+// Column
+$_['column_name']       = 'Voucher Theme Name';
+$_['column_action']     = 'Acțiune';
+
+// Entry
+$_['entry_name']        = 'Voucher Theme Name';
+$_['entry_description'] = 'Voucher Theme Description';
+$_['entry_image']       = 'Imagine';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify voucher themes!';
+$_['error_name']        = 'Voucher Theme Name must be between 3 and 32 characters!';
+$_['error_image']       = 'Image required!';
+$_['error_voucher']     = 'Warning: This voucher theme cannot be deleted as it is currently assigned to %s vouchers!';
