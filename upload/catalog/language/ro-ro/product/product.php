d61b2548b88f06c1b644e2496796a5b3835ee591
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:11:21 +0800

    New translations product.php (Romanian)

---
+++ upload/catalog/language/ro-ro/product/product.php
@@ -0,0 +1,51 @@
+<?php
+// Text
+$_['text_search']              = 'Căutare';
+$_['text_brand']               = 'Marca';
+$_['text_manufacturer']        = 'Marca:';
+$_['text_model']               = 'Cod Produs:';
+$_['text_reward']              = 'Puncte de Recompensă:';
+$_['text_points']              = 'Preţ în puncte recompensă:';
+$_['text_stock']               = 'Disponibilitate:';
+$_['text_instock']             = 'În Stoc';
+$_['text_tax']                 = 'Fără TVA:';
+$_['text_discount']            = ' mai mult ';
+$_['text_option']              = 'Opțiuni disponibile';
+$_['text_minimum']             = 'Acest produs are o cantitate minimă de %s';
+$_['text_reviews']             = '%s recenzii';
+$_['text_write']               = 'Scrie o recenzie';
+$_['text_login']               = 'Te rugam <a href="%s">autentifica-te</a> or <a href="%s">intregistreaza-te</a> pentru a scrie o recenzie';
+$_['text_no_reviews']          = 'Momentan nu sunt recenzii pentru acest produs.';
+$_['text_note']                = '<span class="text-danger">Nota:</span> HTML nu este tradus!';
+$_['text_success']             = 'Vă mulțumim pentru comentariul dvs. A fost trimis administratorului pentru aprobare.';
+$_['text_related']             = 'Produse Asemănătoare';
+$_['text_tags']                = 'Tag-uri:';
+$_['text_error']               = 'Produs nu a putut fi găsit!';
+$_['text_payment_recurring']   = 'Profil Plată';
+$_['text_trial_description']   = '%s every %d %s(s) for %d payment(s) then';
+$_['text_payment_description'] = '%s every %d %s(s) for %d payment(s)';
+$_['text_payment_cancel']      = '%s every %d %s(s) until canceled';
+$_['text_day']                 = 'zi';
+$_['text_week']                = 'săptămână';
+$_['text_semi_month']          = 'jumătate de lună';
+$_['text_month']               = 'lună';
+$_['text_year']                = 'an';
+
+// Entry
+$_['entry_qty']                = 'Cantitate';
+$_['entry_name']               = 'Numele Tău';
+$_['entry_review']             = 'Recenzia ta';
+$_['entry_rating']             = 'Evaluare';
+$_['entry_good']               = 'Bună';
+$_['entry_bad']                = 'Rău';
+
+// Tabs
+$_['tab_description']          = 'Descriere';
+$_['tab_attribute']            = 'Specificație';
+$_['tab_review']               = 'Recenzii (%s)';
+
+// Error
+$_['error_name']               = 'Atenție: Textul Comentariului trebuie să fie între 3 și 25 de caractere!';
+$_['error_text']               = 'Atenție: Textul Comentariului trebuie să fie între 25 și 1000 de caractere!';
+$_['error_rating']             = 'Atenție: Selectează o evaluare a comentariilor!';
+$_['error_product']            = "Wanring: Product required to add the review!";
