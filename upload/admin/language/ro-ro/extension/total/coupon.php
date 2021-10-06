72da7b7792d8419ad3ee89599b5dcd2db488ec8b
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:14:09 +0800

    New translations coupon.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/total/coupon.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'Coupon';
+
+// Text
+$_['text_extension']   = 'Extensii';
+$_['text_success']     = 'Success: You have modified coupon total!';
+$_['text_edit']        = 'Edit Coupon';
+
+// Entry
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordine Sortare';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify coupon total!';
