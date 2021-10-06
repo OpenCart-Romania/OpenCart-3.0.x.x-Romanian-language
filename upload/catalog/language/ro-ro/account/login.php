6cf9d741281ac3af0dc5b9501c8cc7c30f45cb88
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:10:32 +0800

    New translations login.php (Romanian)

---
+++ upload/catalog/language/ro-ro/account/login.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']                = 'Autentificare Cont';
+
+// Text
+$_['text_account']                 = 'Cont';
+$_['text_login']                   = 'Autentificare';
+$_['text_new_customer']            = 'Cont Nou';
+$_['text_register']                = 'Înregistrare Cont';
+$_['text_register_account']        = 'Prin crearea unui cont, vei putea face cumpărături mai repede, vei fi la curent cu starea comenzii și vei ține evidența comenzilor făcute anterior.';
+$_['text_returning_customer']      = 'Utilizator Existent';
+$_['text_i_am_returning_customer'] = 'Sunt un utilizator existent';
+$_['text_forgotten']               = 'Parolă Uitată';
+
+// Entry
+$_['entry_email']                  = 'Adresă de E-Mail';
+$_['entry_password']               = 'Parolă';
+
+// Error
+$_['error_login']                  = 'Atenție: Adresa de E-Mail și/sau Parola nu se potrivesc.';
+$_['error_attempts']               = 'Atenție: Contul tău a depășit numărul permis de încercări de autentificare. Te rugăm să încerci din nou peste 1 oră.';
+$_['error_approved']               = 'Atenție: Contul tău necesită aprobare înainte de a te putea autentifica.';
