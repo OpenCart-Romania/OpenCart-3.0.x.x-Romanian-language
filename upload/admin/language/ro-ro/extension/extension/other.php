97f083a853125ffebf6495e73345ef9972535a17
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:12:44 +0800

    New translations other.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/extension/other.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']     = 'Altele';
+
+// Text
+$_['text_success']      = 'Succes: Ai modificat altă extensie!';
+$_['text_list']         = 'Altă Listă';
+
+// Column
+$_['column_name']       = 'Altele';
+$_['column_status']     = 'Stare';
+$_['column_action']     = 'Acțiune';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify other extension!';
