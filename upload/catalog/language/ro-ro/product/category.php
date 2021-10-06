94e3251905d165b70f0d0f9a825fa9cecafe2a00
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:11:23 +0800

    New translations category.php (Romanian)

---
+++ upload/catalog/language/ro-ro/product/category.php
@@ -0,0 +1,24 @@
+<?php
+// Text
+$_['text_refine']       = 'Îmbunătățește Căutarea';
+$_['text_product']      = 'Produse';
+$_['text_error']        = 'Categoria nu a fost găsită!';
+$_['text_empty']        = 'Nu există produse care să fie listate in această categorie.';
+$_['text_quantity']     = 'Cantitate:';
+$_['text_manufacturer'] = 'Marca:';
+$_['text_model']        = 'Cod Produs:';
+$_['text_points']       = 'Puncte de Recompensă:';
+$_['text_price']        = 'Preț:';
+$_['text_tax']          = 'Fără TVA:';
+$_['text_compare']      = 'Compara produs (%s)';
+$_['text_sort']         = 'Sortează după:';
+$_['text_default']      = 'Implicit';
+$_['text_name_asc']     = 'De la (A - Z)';
+$_['text_name_desc']    = 'De la (Z - A)';
+$_['text_price_asc']    = 'Pret: de la mic la mare';
+$_['text_price_desc']   = 'Preț: de la mare la mic';
+$_['text_rating_asc']   = 'Rating (Mic)';
+$_['text_rating_desc']  = 'Rating (Mare)';
+$_['text_model_asc']    = 'Model (A - Z)';
+$_['text_model_desc']   = 'Model (Z - A)';
+$_['text_limit']        = 'Afișează:';
