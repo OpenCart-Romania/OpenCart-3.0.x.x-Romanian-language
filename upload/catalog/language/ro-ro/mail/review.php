92cf496d20dff52bf56649df91beb0bf235e2390
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:11:25 +0800

    New translations review.php (Romanian)

---
+++ upload/catalog/language/ro-ro/mail/review.php
@@ -0,0 +1,8 @@
+<?php
+// Text
+$_['text_subject']  = '%s - Recenzie produs';
+$_['text_waiting']  = 'Ai un produs nou care aşteaptă revizuire.';
+$_['text_product']  = 'Produse %s';
+$_['text_reviewer'] = '%s';
+$_['text_rating']   = 'Rating: %s';
+$_['text_review']   = 'Recenzie:';
