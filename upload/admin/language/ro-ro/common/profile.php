13cb5c42c215fff9b41109b414dcc9b35f6e6a9d
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:13:18 +0800

    New translations profile.php (Romanian)

---
+++ upload/admin/language/ro-ro/common/profile.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']         = 'Profil';
+
+// Text
+$_['text_success']          = 'Succes: Ți-ai modificat profilul!';
+$_['text_edit']             = 'Editează-ți Profilul';
+
+// Column
+$_['column_username']       = 'Nume utilizator';
+$_['column_status']         = 'Stare';
+$_['column_date_added']     = 'Data Adăugării';
+$_['column_action']         = 'Acțiune';
+
+// Entry
+$_['entry_username']        = 'Nume utilizator';
+$_['entry_password']        = 'Parolă';
+$_['entry_confirm']         = 'Confirmare';
+$_['entry_firstname']       = 'Prenume';
+$_['entry_lastname']        = 'Nume de Familie';
+$_['entry_email']           = 'E-Mail';
+$_['entry_image']           = 'Imagine';
+
+// Error
+$_['error_permission']      = 'Atenție: Nu ai permisiunea de a-ți modifica profilul!';
+$_['error_exists_username'] = 'Atenție: Nume utilizator este deja utilizat!';
+$_['error_username']        = 'Nume utilizator trebuie să fie între 3 și 20 de caractere!';
+$_['error_password']        = 'Parola trebuie să fie între 4 și 20 de caractere!';
+$_['error_confirm']         = 'Parola și confirmarea parolei nu se potrivesc!';
+$_['error_firstname']       = 'Prenumele trebuie să fie între 1 și 32 de caractere!';
+$_['error_lastname']        = 'Numele de Familie trebuie să fie între 1 și 32 de caractere!';
+$_['error_email']           = 'Adresa E-Mail nu pare să fie validă!';
+$_['error_exists_email']    = 'Atenție: Adresa E-Mail este deja înregistrată!';
