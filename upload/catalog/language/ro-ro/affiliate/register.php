c6c72bc6447bb51f079bb27a46e7f9c76e5beb46
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:09:27 +0800

    New translations register.php (Romanian)

---
+++ upload/catalog/language/ro-ro/affiliate/register.php
@@ -0,0 +1,52 @@
+<?php
+// Heading
+$_['heading_title']             = 'Program de Afiliere';
+
+// Text
+$_['text_account']              = 'Cont';
+$_['text_register']             = 'Registru Afiliați';
+$_['text_account_already']      = 'Dacă ai deja un cont la noi, te rugăm să te autentifici pe <a href="%s">pagina de autentificare</a>.';
+$_['text_signup']               = 'Pentru a crea un cont de afiliat, completați formularul de mai jos asigurându-vă că completați toate câmpurile necesare:';
+$_['text_your_details']         = 'Detaliile Tale Personale';
+$_['text_your_address']         = 'Detaliile Adresei Tale';
+$_['text_your_affiliate']       = 'Informațiile Tale de Afiliat';
+$_['text_your_password']        = 'Parola Ta';
+$_['text_cheque']               = 'Cec';
+$_['text_paypal']               = 'PayPal';
+$_['text_bank']                 = 'Transfer Bancar';
+$_['text_agree']                = 'Am citit și sunt de acord cu <a href="%s" class="agree"><b>%s</b></a>';
+
+// Entry
+$_['entry_customer_group']      = 'Grup de Clienți';
+$_['entry_firstname']           = 'Prenume';
+$_['entry_lastname']            = 'Nume de Familie';
+$_['entry_email']               = 'E-Mail';
+$_['entry_telephone']           = 'Telefon';
+$_['entry_company']             = 'Companie';
+$_['entry_website']             = 'Web Site';
+$_['entry_tax']                 = 'C.U.I. / Cod TVA';
+$_['entry_payment']             = 'Metodă de Plată';
+$_['entry_cheque']              = 'Nume Beneficiar Cec';
+$_['entry_paypal']              = 'Email Cont PayPal';
+$_['entry_bank_name']           = 'Nume Bancă';
+$_['entry_bank_branch_number']  = 'Număr ABA/BSB (Număr Filială)';
+$_['entry_bank_swift_code']     = 'Cod SWIFT';
+$_['entry_bank_account_name']   = 'Nume Cont';
+$_['entry_bank_account_number'] = 'Număr Cont';
+$_['entry_password']            = 'Parolă';
+$_['entry_confirm']             = 'Confirmă Parola';
+
+// Error
+$_['error_exists']              = 'Atenție: Adresa E-Mail este deja înregistrată!';
+$_['error_firstname']           = 'Prenumele trebuie să fie între 1 și 32 de caractere!';
+$_['error_lastname']            = 'Numele de Familie trebuie să fie între 1 și 32 de caractere!';
+$_['error_email']               = 'Adresa E-Mail nu pare să fie validă!';
+$_['error_telephone']           = 'Telefon trebuie să fie între 3 și 32 de caractere!';
+$_['error_custom_field']        = '%s necesar!';
+$_['error_cheque']              = 'Nume Beneficiar Cec necesar!';
+$_['error_paypal']              = 'Adresa Email PayPal nu pare să fie validă!';
+$_['error_bank_account_name']   = 'Nume Cont necesar!';
+$_['error_bank_account_number'] = 'Număr Cont necesar!';
+$_['error_password']            = 'Parola trebuie să fie între 4 și 20 de caractere!';
+$_['error_confirm']             = 'Confirmarea parolei nu se potrivește cu parola!';
+$_['error_agree']               = 'Atenție: Trebuie să fii de acord cu %s!';
