943b58986214937c4e0aa32a653636711d15baaa
Constantin DOBRE <admin@harddex.ro>
2021-10-06 16:43:26 +0800

    New translations transaction.php (Romanian)

--- upload/catalog/language/ro-ro/mail/transaction.php
+++ upload/catalog/language/ro-ro/mail/transaction.php
@@ -2,5 +2,5 @@
 // Text
 $_['text_subject']  = '%s - Comision Afiliat';
 $_['text_received'] = 'Felicitări! Ai primit o plată de comision de la programul de afiliere %s';
-$_['text_amount']   = 'You have received:';
-$_['text_total']    = 'Your total amount of commission is now:';
+$_['text_amount']   = 'Ai primit:';
+$_['text_total']    = 'Suma totală a comisionului dvs acum este %s:';
