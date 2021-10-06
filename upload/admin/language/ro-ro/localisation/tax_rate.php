27accc9cc7ef3d6a22f7522887aa794d7dfaa9a1
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:14:36 +0800

    New translations tax_rate.php (Romanian)

---
+++ upload/admin/language/ro-ro/localisation/tax_rate.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']        = 'Cote Taxare';
+
+// Text
+$_['text_success']         = 'Success: You have modified tax rates!';
+$_['text_list']            = 'Tax Rate List';
+$_['text_add']             = 'Add Tax Rate';
+$_['text_edit']            = 'Edit Tax Rate';
+$_['text_percent']         = 'Procentaj';
+$_['text_amount']          = 'Valoare Fixă';
+
+// Column
+$_['column_name']          = 'Tax Name';
+$_['column_rate']          = 'Tax Rate';
+$_['column_type']          = 'Tip';
+$_['column_geo_zone']      = 'Geo Zone';
+$_['column_date_added']    = 'Data Adăugării';
+$_['column_date_modified'] = 'Date Modified';
+$_['column_action']        = 'Acțiune';
+
+// Entry
+$_['entry_name']           = 'Tax Name';
+$_['entry_rate']           = 'Tax Rate';
+$_['entry_type']           = 'Tip';
+$_['entry_customer_group'] = 'Grup de Clienți';
+$_['entry_geo_zone']       = 'Geo Zone';
+
+// Error
+$_['error_permission']     = 'Warning: You do not have permission to modify tax rates!';
+$_['error_tax_rule']       = 'Warning: This tax rate cannot be deleted as it is currently assigned to %s tax classes!';
+$_['error_name']           = 'Tax Name must be between 3 and 32 characters!';
+$_['error_rate']           = 'Tax Rate required!';
