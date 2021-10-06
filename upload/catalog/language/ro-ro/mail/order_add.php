c9888c80847531c7b73c39abd362348e1afc9d41
Constantin DOBRE <admin@harddex.ro>
2021-10-06 16:43:29 +0800

    New translations order_add.php (Romanian)

--- upload/catalog/language/ro-ro/mail/order_add.php
+++ upload/catalog/language/ro-ro/mail/order_add.php
@@ -1,16 +1,16 @@
 <?php
 // Text
-$_['text_subject']          = '%s - Order %s';
-$_['text_greeting']         = 'Thank you for your interest in %s products. Your order has been received and will be processed once payment has been confirmed.';
-$_['text_link']             = 'To view your order click on the link below:';
-$_['text_order_detail']     = 'Order Details';
-$_['text_instruction']      = 'Instructions';
-$_['text_order_id']         = 'Order ID:';
+$_['text_subject']          = '%s - Comanda %s';
+$_['text_greeting']         = 'Îți mulţumim pentru interesul tău asupra acestor %s produse. Comanda ta a fost recepționată şi va fi procesată imediat ce plata a fost confirmată.';
+$_['text_link']             = 'Pentru a-ți vizualiza comanda apasă pe linkul de mai jos:';
+$_['text_order_detail']     = 'Detalii comandă';
+$_['text_instruction']      = 'Instrucțiuni';
+$_['text_order_id']         = 'Comanda ID:';
 $_['text_date_added']       = 'Data Adăugării:';
-$_['text_order_status']     = 'Order Status:';
+$_['text_order_status']     = 'Statusul comenzii:';
 $_['text_payment_method']   = 'Metodă de Plată:';
 $_['text_shipping_method']  = 'Metodă Livrare:';
-$_['text_email']            = 'E-mail:';
+$_['text_email']            = 'E-Mail:';
 $_['text_telephone']        = 'Telefon:';
 $_['text_ip']               = 'Adresă IP:';
 $_['text_payment_address']  = 'Adresă Plată';
@@ -20,8 +20,8 @@ $_['text_product']          = 'Produs';
 $_['text_model']            = 'Model';
 $_['text_quantity']         = 'Cantitate';
 $_['text_price']            = 'Preț';
-$_['text_order_total']      = 'Order Totals';
+$_['text_order_total']      = 'Totaluri Comandă';
 $_['text_total']            = 'Total';
-$_['text_download']         = 'Once your payment has been confirmed you can click on the link below to access your downloadable products:';
-$_['text_comment']          = 'The comments for your order are:';
-$_['text_footer']           = 'Please reply to this e-mail if you have any questions.';
+$_['text_download']         = 'Odată ce s-a confirmat plata ta, poţi apăsa pe linkul de mai jos pentru a accesa produsele descărcabile:';
+$_['text_comment']          = 'Comentariile pentru comanda dvs. sunt:';
+$_['text_footer']           = 'Vă rugăm să răspundeţi la acest e-mail dacă aveţi întrebări.';
