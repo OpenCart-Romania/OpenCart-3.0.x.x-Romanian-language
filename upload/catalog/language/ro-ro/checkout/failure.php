ecb44f7f468cfda99890820ae23ec2980c7e07a3
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:09:14 +0800

    New translations failure.php (Romanian)

---
+++ upload/catalog/language/ro-ro/checkout/failure.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title'] = 'Plată Eșuată!';
+
+// Text
+$_['text_basket']   = 'Coș de Cumpărături';
+$_['text_checkout'] = 'Checkout';
+$_['text_failure']  = 'Plată Eșuată';
+$_['text_message']  = '<p>There was a problem processing your payment and the order did not complete.</p>
+
+<p>Possible reasons are:</p>
+<ul>
+  <li>Insufficient funds</li>
+  <li>Verification failed</li>
+</ul>
+
+<p>Please try to order again using a different payment method.</p>
+
+<p>If the problem persists please <a href="%s">contact us</a> with the details of the order you are trying to place.</p>
+';
