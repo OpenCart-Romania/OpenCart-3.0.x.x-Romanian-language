55350b253b097a271bf5a39ef3d8acb24e857eb8
Constantin DOBRE <admin@harddex.ro>
2021-10-06 16:43:28 +0800

    New translations order_edit.php (Romanian)

--- upload/catalog/language/ro-ro/mail/order_edit.php
+++ upload/catalog/language/ro-ro/mail/order_edit.php
@@ -1,9 +1,9 @@
 <?php
 // Text
-$_['text_subject']      = '%s - Order Update %s';
-$_['text_order_id']     = 'Order ID:';
+$_['text_subject']      = '%s - Actualizarea Comenzi %s';
+$_['text_order_id']     = 'Numărul comenzii:';
 $_['text_date_added']   = 'Data Adăugării:';
-$_['text_order_status'] = 'Your order has been updated to the following status:';
-$_['text_comment']      = 'The comments for your order are:';
-$_['text_link']         = 'To view your order click on the link below:';
-$_['text_footer']       = 'Please reply to this email if you have any questions.';
+$_['text_order_status'] = 'Comanda dvs. a fost actualizată la următoarea stare:';
+$_['text_comment']      = 'Comentariile pentru comanda dvs. sunt:';
+$_['text_link']         = 'Pentru a-ți vizualiza comanda apasă pe linkul de mai jos:';
+$_['text_footer']       = 'Vă rugăm să răspundeţi la acest e-mail dacă aveţi întrebări.';
