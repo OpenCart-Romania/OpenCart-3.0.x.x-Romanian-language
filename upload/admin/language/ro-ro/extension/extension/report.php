f77229f43a728fb1230a9887b5fe7e4ad1fb0a09
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:12:42 +0800

    New translations report.php (Romanian)

---
+++ upload/admin/language/ro-ro/extension/extension/report.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']     = 'Rapoarte';
+
+// Text
+$_['text_success']      = 'Succes: Ai modificat rapoartele!';
+$_['text_list']         = 'Listă Rapoarte';
+
+// Column
+$_['column_name']       = 'Report Name';
+$_['column_status']     = 'Stare';
+$_['column_sort_order'] = 'Ordine Sortare';
+$_['column_action']     = 'Acțiune';
+
+// Error
+$_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica rapoartele!';
