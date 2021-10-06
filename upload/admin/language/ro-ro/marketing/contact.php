1e94b254609ed25419b54f3a23ab33b7620e8b05
Constantin DOBRE <admin@harddex.ro>
2021-10-06 21:10:13 +0800

    New translations contact.php (Romanian)

---
+++ upload/admin/language/ro-ro/marketing/contact.php
@@ -0,0 +1,37 @@
+<?php
+// Heading
+$_['heading_title']        = 'Mail';
+
+// Text
+$_['text_success']         = 'Your message has been successfully sent!';
+$_['text_sent']            = 'Your message has been successfully sent to %s - %s recipients!';
+$_['text_list']            = 'Mail List';
+$_['text_default']         = 'Implicit';
+$_['text_newsletter']      = 'All Newsletter Subscribers';
+$_['text_customer_all']    = 'All Customers';
+$_['text_customer_group']  = 'Grup de Clienți';
+$_['text_customer']        = 'Clienţi';
+$_['text_affiliate_all']   = 'Toți Afiliații';
+$_['text_affiliate']       = 'Afiliați';
+$_['text_product']         = 'Produse';
+
+// Entry
+$_['entry_store']          = 'From';
+$_['entry_to']             = 'To';
+$_['entry_customer_group'] = 'Grup de Clienți';
+$_['entry_customer']       = 'Client';
+$_['entry_affiliate']      = 'Afiliat';
+$_['entry_product']        = 'Produse';
+$_['entry_subject']        = 'Subject';
+$_['entry_message']        = 'Message';
+
+// Help
+$_['help_customer']        = '(Completare automată)';
+$_['help_affiliate']       = '(Completare automată)';
+$_['help_product']         = 'Send only to customers who have ordered products in the list. (Autocomplete)';
+
+// Error
+$_['error_permission']     = 'Warning: You do not have permission to send E-Mails!';
+$_['error_subject']        = 'E-Mail Subject required!';
+$_['error_message']        = 'E-Mail Message required!';
+$_['error_email']          = 'E-Mail required!';
