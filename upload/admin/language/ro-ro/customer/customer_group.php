a928cc30c3299b362621aa99d6c7350f4a406b91
Constantin DOBRE <admin@harddex.ro>
2021-10-29 10:21:49 +0800

    New translations customer_group.php (Romanian)

--- upload/admin/language/ro-ro/customer/customer_group.php
+++ upload/admin/language/ro-ro/customer/customer_group.php
@@ -4,27 +4,27 @@ $_['heading_title']     = 'Grupuri de Clienți';
 
 // Text
 $_['text_success']      = 'Succes: Ai modificat grupurile de clienți!';
-$_['text_list']         = 'Lista grupurilor de clienți';
-$_['text_add']          = 'Adăugați un grup de clienți';
-$_['text_edit']         = 'Editați grupul de clienți';
+$_['text_list']         = 'Lista Grupurilor de Clienți';
+$_['text_add']          = 'Adăugați Grupul de Clienți';
+$_['text_edit']         = 'Editați Grupul de Clienți';
 
 // Column
-$_['column_name']       = 'Nume Grup Clienți';
+$_['column_name']       = 'Numele Grupului de Clienți';
 $_['column_sort_order'] = 'Ordine Sortare';
 $_['column_action']     = 'Acțiune';
 
 // Entry
-$_['entry_name']        = 'Nume Grup Clienți';
+$_['entry_name']        = 'Numele Grupului de Clienți';
 $_['entry_description'] = 'Descriere';
-$_['entry_approval']    = 'Aprobați clienți noi';
+$_['entry_approval']    = 'Aprobați Clienții Noi';
 $_['entry_sort_order']  = 'Ordine Sortare';
 
 // Help
-$_['help_approval']     = 'Clienții trebuie să fie aprobați de un administrator înainte de a se putea conecta.';
+$_['help_approval']     = 'Clienții trebuie să fie aprobați de un administrator înainte de a se putea autentifica.';
 
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica grupurile de clienți!';
-$_['error_name']        = 'Nume Grup Clienți trebuie să fie între 3 și 32 de caractere!';
-$_['error_default']     = 'Avertisment: Acest grup de clienți nu poate fi șters deoarece este atribuit în prezent grupului de clienți implicit în magazin!';
-$_['error_store']       = 'Avertisment: Acest grup de clienți nu poate fi șters deoarece este în prezent alocat magazinelor %s!';
+$_['error_name']        = 'Numele Grupului de Clienți trebuie să fie între 3 și 32 de caractere!';
+$_['error_default']     = 'Avertisment: Acest grup de clienți nu poate fi șters, deoarece este atribuit în prezent ca grup implicit de clienți magazin!';
+$_['error_store']       = 'Avertisment: Acest grup de clienți nu poate fi șters, deoarece este atribuit în prezent la %s magazine!';
 $_['error_customer']    = 'Atenție: Acest grup de clienți nu poate fi șters deoarece în prezent este asociat cu %s clienți!';
