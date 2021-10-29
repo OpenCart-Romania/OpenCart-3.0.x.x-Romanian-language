f5c74758b33a394ab28dddec080a7883d5d7378e
Constantin DOBRE <admin@harddex.ro>
2021-10-29 10:21:46 +0800

    New translations not_found.php (Romanian)

--- upload/admin/language/ro-ro/error/not_found.php
+++ upload/admin/language/ro-ro/error/not_found.php
@@ -3,4 +3,4 @@
 $_['heading_title']  = 'Pagina nu a fost găsită!';
 
 // Text
-$_['text_not_found'] = 'Pagina pe care o căutați nu a fost găsită! Vă rugăm să vă contactați administratorul dacă problema persistă.';
+$_['text_not_found'] = 'Pagina pe care o căutați nu a putut fi găsită! Vă rugăm să contactați administratorul dacă problema persistă.';
