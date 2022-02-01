c79d428e0229f9985aa6aace6e316cae39b3b7da
Constantin DOBRE <admin@harddex.ro>
2022-02-01 15:29:41 +0800

    New translations order_status.php (Romanian)

--- upload/admin/language/ro-ro/localisation/order_status.php
+++ upload/admin/language/ro-ro/localisation/order_status.php
@@ -18,7 +18,7 @@ $_['entry_name']       = 'Order Status Name';
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify order statuses!';
 $_['error_name']       = 'Order Status Name must be between 3 and 32 characters!';
-$_['error_default']    = 'Warning: This order status cannot be deleted as it is currently assigned as the default store order status!';
+$_['error_default']    = 'Atenție: Această stare a comenzii nu poate fi ștearsă, deoarece în prezent este atribuită ca stare comandă implicită magazin!';
 $_['error_download']   = 'Warning: This order status cannot be deleted as it is currently assigned as the default download status!';
 $_['error_store']      = 'Warning: This order status cannot be deleted as it is currently assigned to %s stores!';
 $_['error_order']      = 'Warning: This order status cannot be deleted as it is currently assigned to %s orders!';
