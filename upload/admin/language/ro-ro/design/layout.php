b54aa216e3e9c99dac93838b295224ff8e62b5a8
Constantin DOBRE <admin@harddex.ro>
2022-02-01 14:34:23 +0800

    New translations layout.php (Romanian)

--- upload/admin/language/ro-ro/design/layout.php
+++ upload/admin/language/ro-ro/design/layout.php
@@ -8,28 +8,28 @@ $_['text_list']           = 'Listă de Layout-uri';
 $_['text_add']            = 'Adaugă Layout';
 $_['text_edit']           = 'Editează Layout';
 $_['text_remove']         = 'Elimină';
-$_['text_route']          = 'Choose the store and routes to be used with this layout';
-$_['text_module']         = 'Choose the position of the modules';
+$_['text_route']          = 'Alege magazinul și rutele care vor fi utilizate cu acest layout';
+$_['text_module']         = 'Alege poziția modulelor';
 $_['text_default']        = 'Implicit';
-$_['text_content_top']    = 'Content Top';
-$_['text_content_bottom'] = 'Content Bottom';
-$_['text_column_left']    = 'Column Left';
-$_['text_column_right']   = 'Column Right';
+$_['text_content_top']    = 'Conținut in Partea de Sus';
+$_['text_content_bottom'] = 'Conținut în Partea de Jos';
+$_['text_column_left']    = 'Coloană Stânga';
+$_['text_column_right']   = 'Coloană Dreapta';
 
 // Column
-$_['column_name']         = 'Layout Name';
+$_['column_name']         = 'Nume Layout';
 $_['column_action']       = 'Acțiune';
 
 // Entry
-$_['entry_name']          = 'Layout Name';
+$_['entry_name']          = 'Nume Layout';
 $_['entry_store']         = 'Magazin';
-$_['entry_route']         = 'Route';
+$_['entry_route']         = 'Rută';
 $_['entry_module']        = 'Modul';
 
 // Error
 $_['error_permission']    = 'Atenție: Nu ai permisiunea de a modifica layout-urile!';
-$_['error_name']          = 'Layout Name must be between 3 and 64 characters!';
-$_['error_default']       = 'Warning: This layout cannot be deleted as it is currently assigned as the default store layout!';
+$_['error_name']          = 'Nume Layout trebuie să fie între 3 și 64 de caractere!';
+$_['error_default']       = 'Atenție: Acest layout nu poate fi șters, deoarece în prezent este atribuit ca layout implicit magazin!';
 $_['error_store']         = 'Warning: This layout cannot be deleted as it is currently assigned to %s stores!';
 $_['error_product']       = 'Warning: This layout cannot be deleted as it is currently assigned to %s products!';
 $_['error_category']      = 'Warning: This layout cannot be deleted as it is currently assigned to %s categories!';
