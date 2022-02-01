5e70013154ebd12291878b72c8b5a1328f4d7c00
Constantin DOBRE <admin@harddex.ro>
2022-02-01 14:34:31 +0800

    New translations length_class.php (Romanian)

--- upload/admin/language/ro-ro/localisation/length_class.php
+++ upload/admin/language/ro-ro/localisation/length_class.php
@@ -26,5 +26,5 @@ $_['help_value']       = 'Set to 1.00000 if this is your default length.';
 $_['error_permission'] = 'Warning: You do not have permission to modify length classes!';
 $_['error_title']      = 'Length Title must be between 3 and 32 characters!';
 $_['error_unit']       = 'Length Unit must be between 1 and 4 characters!';
-$_['error_default']    = 'Warning: This length class cannot be deleted as it is currently assigned as the default store length class!';
+$_['error_default']    = 'Atenție: Această clasă de lungime nu poate fi ștearsă, deoarece în prezent este atribuită ca fiind clasă de lungime implicită magazin!';
 $_['error_product']    = 'Warning: This length class cannot be deleted as it is currently assigned to %s products!';
