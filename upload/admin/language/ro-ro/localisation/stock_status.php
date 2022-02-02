7a62047f45770a6fa581662e2d3bd9a48d5b47ca
Constantin DOBRE <admin@harddex.ro>
2022-02-03 02:15:09 +0800

    New translations stock_status.php (Romanian)

--- upload/admin/language/ro-ro/localisation/stock_status.php
+++ upload/admin/language/ro-ro/localisation/stock_status.php
@@ -18,4 +18,4 @@ $_['entry_name']       = 'Stock Status Name';
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify stock statuses!';
 $_['error_name']       = 'Stock Status Name must be between 3 and 32 characters!';
-$_['error_product']    = 'Warning: This stock status cannot be deleted as it is currently assigned to %s products!';
+$_['error_product']    = 'Atenție: Această stare a stocului nu poate fi ștearsă, deoarece în prezent este asociată cu %s produse!';
