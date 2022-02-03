f111f4ae0b8336a504264175ef4bbecdb157fc21
Constantin DOBRE <admin@harddex.ro>
2022-02-03 13:59:35 +0800

    New translations tax_class.php (Romanian)

--- upload/admin/language/ro-ro/localisation/tax_class.php
+++ upload/admin/language/ro-ro/localisation/tax_class.php
@@ -1,13 +1,13 @@
 <?php
 // Heading
-$_['heading_title']     = 'Clase Taxare';
+$_['heading_title']     = 'Clase Fiscale';
 
 // Text
-$_['text_success']      = 'Success: You have modified tax classes!';
+$_['text_success']      = 'Succes: Ai modificat clasele fiscale!';
 $_['text_list']         = 'Tax Class List';
 $_['text_add']          = 'Add Tax Class';
 $_['text_edit']         = 'Edit Tax Class';
-$_['text_tax_class']    = 'Clasă de Taxare';
+$_['text_tax_class']    = 'Clasa Fiscală';
 $_['text_tax_rate']     = 'Cote Taxare';
 $_['text_shipping']     = 'Adresă Livrare';
 $_['text_payment']      = 'Adresă Plată';
@@ -26,7 +26,7 @@ $_['entry_geo_zone']    = 'Geo Zone';
 $_['entry_priority']    = 'Prioritate';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify tax classes!';
+$_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica clasele fiscale!';
 $_['error_title']       = 'Tax Class Title must be between 3 and 32 characters!';
 $_['error_description'] = 'Description must be between 3 and 255 characters!';
 $_['error_product']     = 'Atenție: Această clasă fiscală nu poate fi ștearsă, deoarece în prezent este atribuită la %s produse!';
