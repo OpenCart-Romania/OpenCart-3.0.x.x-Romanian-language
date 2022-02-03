a26c95f53dad4c99f5fb57ad8f44cc47c9f31da6
Constantin DOBRE <admin@harddex.ro>
2022-02-03 23:36:59 +0800

    New translations securetrading_ws.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/securetrading_ws.php
+++ upload/admin/language/ro-ro/extension/payment/securetrading_ws.php
@@ -6,7 +6,7 @@ $_['tab_myst']                                   = 'MyST';
 
 $_['text_securetrading_ws']                      = '<a href="http://www.securetradingfs.com/partner/open-cart/" target="_blank"><img src="view/image/payment/secure_trading.png" alt="Secure Trading" title="Secure Trading" style="border: 1px solid #EEEEEE;" /></a>';
 $_['text_extension']                             = 'Extensii';
-$_['text_all_geo_zones']                         = 'All Geo Zones';
+$_['text_all_geo_zones']                         = 'Toate Zonele Geografice';
 $_['text_process_immediately']                   = 'Process immediately';
 $_['text_wait_x_days']                           = 'Wait %d days';
 $_['text_wait']                                  = 'Please wait.';
