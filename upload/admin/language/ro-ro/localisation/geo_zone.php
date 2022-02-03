673e114763da6f94e6d6fa89e7e98995c6e499aa
Constantin DOBRE <admin@harddex.ro>
2022-02-03 23:37:01 +0800

    New translations geo_zone.php (Romanian)

--- upload/admin/language/ro-ro/localisation/geo_zone.php
+++ upload/admin/language/ro-ro/localisation/geo_zone.php
@@ -3,7 +3,7 @@
 $_['heading_title']      = 'Zone Geografice';
 
 // Text
-$_['text_success']       = 'Success: You have modified geo zones!';
+$_['text_success']       = 'Succes: Ai modificat zonele geografice!';
 $_['text_list']          = 'Geo Zone List';
 $_['text_add']           = 'Adaugă Zonă Geografică';
 $_['text_edit']          = 'Edit Geo Zone';
@@ -21,7 +21,7 @@ $_['entry_country']      = 'Țară';
 $_['entry_zone']         = 'Zone';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify geo zones!';
+$_['error_permission']   = 'Atenție: Nu ai permisiunea de a modifica zonele geografice!';
 $_['error_name']         = 'Geo Zone Name must be between 3 and 32 characters!';
 $_['error_description']  = 'Description Name must be between 3 and 255 characters!';
 $_['error_tax_rate']     = 'Atenție: Această zonă geografică nu poate fi ștearsă, deoarece în prezent este atribuită uneia sau mai multor rate de impozitare!';
