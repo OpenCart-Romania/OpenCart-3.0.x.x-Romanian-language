8933c6a74a23fa09475e6fd6a0c468121a1879d3
Constantin DOBRE <admin@harddex.ro>
2022-02-04 01:43:56 +0800

    New translations upload.php (Romanian)

--- upload/catalog/language/ro-ro/tool/upload.php
+++ upload/catalog/language/ro-ro/tool/upload.php
@@ -3,6 +3,6 @@
 $_['text_upload']    = 'Fișierul tău a fost încărcat cu succes!';
 
 // Error
-$_['error_filename'] = 'Nume Fișier trebuie să fie între 3 și 64 de caractere!';
+$_['error_filename'] = 'Numele fișierului trebuie să fie între 3 și 64 de caractere!';
 $_['error_filetype'] = 'Tip fișier incorect!';
 $_['error_upload']   = 'Încărcare necesară!';
