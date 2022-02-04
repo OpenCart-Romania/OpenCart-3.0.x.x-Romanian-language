7c6dbbb325e05f5d31ad9b28a0cb758f7e54d393
Constantin DOBRE <admin@harddex.ro>
2022-02-04 22:48:30 +0800

    New translations alipay_cross.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/alipay_cross.php
+++ upload/admin/language/ro-ro/extension/payment/alipay_cross.php
@@ -4,7 +4,7 @@ $_['heading_title']                  = 'Alipay Cross-border';
 
 // Text
 $_['text_extension']                 = 'Extensii';
-$_['text_success']                   = 'Success: You have modified Alipay account details!';
+$_['text_success']                   = 'Succes: Ai modificat detaliile contului Alipay!';
 $_['text_edit']                      = 'Edit Alipay Pay';
 $_['text_alipay_cross']              = '<a target="_BLANK" href="https://global.alipay.com"><img src="view/image/payment/alipay-cross-border.png" alt="Alipay Pay Website" title="Alipay Pay Website" style="border: 1px solid #EEEEEE;" /></a>';
 $_['text_live']                      = 'Live';
