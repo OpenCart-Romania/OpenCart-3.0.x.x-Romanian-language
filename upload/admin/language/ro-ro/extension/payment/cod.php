b27f8c03fc039ac82741d5a1a5b58d00c51884d0
Constantin DOBRE <admin@harddex.ro>
2022-02-05 03:58:22 +0800

    New translations cod.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/cod.php
+++ upload/admin/language/ro-ro/extension/payment/cod.php
@@ -1,11 +1,11 @@
 <?php
 // Heading
-$_['heading_title']      = 'Cash On Delivery';
+$_['heading_title']      = 'Numerar La Livrare';
 
 // Text
 $_['text_extension']     = 'Extensii';
-$_['text_success']       = 'Success: You have modified Cash On Delivery payment module!';
-$_['text_edit']          = 'Edit Cash On Delivery';
+$_['text_success']       = 'Succes: Ai modificat modulul de plată Numerar La Livrare!';
+$_['text_edit']          = 'Editare Numerar La Livrare';
 
 // Entry
 $_['entry_total']        = 'Total';
@@ -18,4 +18,4 @@ $_['entry_sort_order']   = 'Ordine Sortare';
 $_['help_total']         = 'The checkout total the order must reach before this payment method becomes active.';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment Cash On Delivery!';
+$_['error_permission']   = 'Atenție: Nu ai permisiunea de a modifica plata Numerar La Livrare!';
