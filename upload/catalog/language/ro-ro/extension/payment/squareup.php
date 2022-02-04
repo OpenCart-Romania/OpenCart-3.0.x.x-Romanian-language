31bcf403ebcf414726b74db1aabbda1c65b960a4
Constantin DOBRE <admin@harddex.ro>
2022-02-04 15:40:23 +0800

    New translations squareup.php (Romanian)

--- upload/catalog/language/ro-ro/extension/payment/squareup.php
+++ upload/catalog/language/ro-ro/extension/payment/squareup.php
@@ -31,7 +31,7 @@ $_['text_expiry']                       = 'MM/YY';
 $_['text_default_squareup_name']        = 'Credit / Debit Card';
 $_['text_token_issue_customer_error']   = 'We are experiencing a technical outage in our payment system. Please try again later.';
 $_['text_token_revoked_subject']        = 'Your Square access token has been revoked!';
-$_['text_token_revoked_message']        = "The Square payment extension's access to your Square account has been revoked through the Square Dashboard. You need to verify your application credentials in the extension settings and connect again.";
+$_['text_token_revoked_message']        = "Accesul extensiei de plată Square la contul tău Square a fost revocat din Square Dashboard. Trebuie să verificați acreditările aplicației în setările extensiei și să vă conectați din nou.";
 $_['text_token_expired_subject']        = 'Your Square access token has expired!';
 $_['text_token_expired_message']        = "The Square payment extension's access token connecting it to your Square account has expired. You need to verify your application credentials and CRON job in the extension settings and connect again.";
 
