cc763570ab0f5d6f35cd2da2bdfe39985be4f5ce
Constantin DOBRE <admin@harddex.ro>
2022-02-04 15:40:19 +0800

    New translations order.php (Romanian)

--- upload/admin/language/ro-ro/extension/dashboard/order.php
+++ upload/admin/language/ro-ro/extension/dashboard/order.php
@@ -4,7 +4,7 @@ $_['heading_title']    = 'Total Comenzi';
 
 // Text
 $_['text_extension']   = 'Extensii';
-$_['text_success']     = 'Succes: Ai modificat panoul comenzi!';
+$_['text_success']     = 'Succes: Ai modificat dashboard-ul comenzi!';
 $_['text_edit']        = 'Editare Dashboard Comenzi';
 $_['text_view']        = 'Vezi mai mult...';
 
@@ -14,4 +14,4 @@ $_['entry_sort_order'] = 'Ordine Sortare';
 $_['entry_width']      = 'Lăţime';
 
 // Error
-$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica panoul comenzi!';
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica dashboard-ul comenzi!';
