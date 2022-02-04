a38e07b5b99d100b88d340814e98f88ca0a45cc9
Constantin DOBRE <admin@harddex.ro>
2022-02-04 10:00:49 +0800

    New translations affiliate.php (Romanian)

--- upload/catalog/language/ro-ro/account/affiliate.php
+++ upload/catalog/language/ro-ro/account/affiliate.php
@@ -19,7 +19,7 @@ $_['entry_website']             = 'Web Site';
 $_['entry_tax']                 = 'C.U.I. / Cod TVA';
 $_['entry_payment']             = 'Metodă de Plată';
 $_['entry_cheque']              = 'Nume Beneficiar Cec';
-$_['entry_paypal']              = 'Email Cont PayPal';
+$_['entry_paypal']              = 'Cont de Email PayPal';
 $_['entry_bank_name']           = 'Nume Bancă';
 $_['entry_bank_branch_number']  = 'Număr ABA/BSB (Număr Filială)';
 $_['entry_bank_swift_code']     = 'Cod SWIFT';
@@ -29,7 +29,7 @@ $_['entry_bank_account_number'] = 'Număr Cont';
 // Error
 $_['error_agree']               = 'Atenție: Trebuie să fii de acord cu %s!';
 $_['error_cheque']              = 'Nume Beneficiar Cec necesar!';
-$_['error_paypal']              = 'Adresa Email PayPal nu pare să fie validă!';
+$_['error_paypal']              = 'Adresa de Email PayPal nu pare să fie validă!';
 $_['error_bank_account_name']   = 'Nume Cont necesar!';
 $_['error_bank_account_number'] = 'Număr Cont necesar!';
 $_['error_custom_field']        = '%s necesar!';
