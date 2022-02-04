f83acebf20b968bbdafa25e01ce02f8d991b0eea
Constantin DOBRE <admin@harddex.ro>
2022-02-04 15:40:21 +0800

    New translations sale.php (Romanian)

--- upload/admin/language/ro-ro/extension/dashboard/sale.php
+++ upload/admin/language/ro-ro/extension/dashboard/sale.php
@@ -4,8 +4,8 @@ $_['heading_title']    = 'Total Vânzări';
 
 // Text
 $_['text_extension']   = 'Extensii';
-$_['text_success']     = 'Succes: Ai modificat panoul vânzări!';
-$_['text_edit']        = 'Editează Panoul Vânzări';
+$_['text_success']     = 'Succes: Ai modificat dashboard-ul vânzări!';
+$_['text_edit']        = 'Editare Dashboard Vânzări';
 $_['text_view']        = 'Vezi mai mult...';
 
 // Entry
@@ -14,4 +14,4 @@ $_['entry_sort_order'] = 'Ordine Sortare';
 $_['entry_width']      = 'Lăţime';
 
 // Error
-$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica panoul vânzări!';
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica dashboard-ul vânzări!';
