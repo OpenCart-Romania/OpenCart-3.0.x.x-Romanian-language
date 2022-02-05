32f16eacb592fac949e7bf87ffefdcc6d55d16a9
Constantin DOBRE <admin@harddex.ro>
2022-02-06 00:24:06 +0800

    New translations paypal_smart_button.php (Romanian)

--- upload/catalog/language/ro-ro/extension/module/paypal_smart_button.php
+++ upload/catalog/language/ro-ro/extension/module/paypal_smart_button.php
@@ -45,7 +45,7 @@ $_['entry_country']                  	= 'Țară';
 $_['entry_zone']                    	= 'Regiune / Judeţ';
 
 // Button
-$_['button_confirm']  					= 'Confirmare';
+$_['button_confirm']  					= 'Confirm';
 $_['button_shipping'] 					= 'Update shipping';
 
 // Error
