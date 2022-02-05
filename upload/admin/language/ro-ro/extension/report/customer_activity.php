15c8d52bc4abff4a645f6b41586ac4af11c21282
Constantin DOBRE <admin@harddex.ro>
2022-02-05 22:18:33 +0800

    New translations customer_activity.php (Romanian)

--- upload/admin/language/ro-ro/extension/report/customer_activity.php
+++ upload/admin/language/ro-ro/extension/report/customer_activity.php
@@ -36,7 +36,7 @@ $_['entry_ip']                     = 'IP';
 $_['entry_date_start']             = 'Dată de Început';
 $_['entry_date_end']               = 'Dată de Sfârșit';
 $_['entry_status']                 = 'Stare';
-$_['entry_sort_order']             = 'Ordine Sortare';
+$_['entry_sort_order']             = 'Ordine de Sortare';
 
 // Error
 $_['error_permission']             = 'Warning: You do not have permission to modify customer activity report!';
