34b8aef53e7732582b6082a9fd739c698beca0a4
Constantin DOBRE <admin@harddex.ro>
2022-02-05 11:58:51 +0800

    New translations category.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/category.php
+++ upload/admin/language/ro-ro/extension/module/category.php
@@ -5,7 +5,7 @@ $_['heading_title']    = 'Category';
 // Text
 $_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Succes: Ai modificat modulul categorie!';
-$_['text_edit']        = 'Edit Category Module';
+$_['text_edit']        = 'Editare Modul Categorie';
 
 // Entry
 $_['entry_status']     = 'Stare';
