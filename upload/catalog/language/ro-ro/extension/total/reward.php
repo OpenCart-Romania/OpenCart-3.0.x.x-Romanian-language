9e623425c57d93e4e4e0acba1fe5d8bd13cbf1e6
Constantin DOBRE <admin@harddex.ro>
2022-02-05 20:17:26 +0800

    New translations reward.php (Romanian)

--- upload/catalog/language/ro-ro/extension/total/reward.php
+++ upload/catalog/language/ro-ro/extension/total/reward.php
@@ -5,12 +5,12 @@ $_['heading_title'] = 'Use Reward Points (Available %s)';
 // Text
 $_['text_reward']   = 'Puncte de Recompensă (%s)';
 $_['text_order_id'] = 'Order ID: #%s';
-$_['text_success']  = 'Success: Your reward points discount has been applied!';
+$_['text_success']  = 'Succes: Reducerea din punctele tale de recompensă a fost aplicată!';
 
 // Entry
 $_['entry_reward']  = 'Points to use (Max %s)';
 
 // Error
-$_['error_reward']  = 'Warning: Please enter the amount of reward points to use!';
-$_['error_points']  = 'Warning: You don\'t have %s reward points!';
-$_['error_maximum'] = 'Warning: The maximum number of points that can be applied is %s!';
+$_['error_reward']  = 'Avertisment: Introduceți suma punctelor de recompensă de utilizat!';
+$_['error_points']  = 'Avertisment: Nu aveți %s puncte de recompensă!';
+$_['error_maximum'] = 'Atenție: Numărul maxim de puncte care poate fi aplicat este %s!';
