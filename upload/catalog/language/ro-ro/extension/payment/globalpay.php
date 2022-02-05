81f0a6adb9dedc02362b69db004169a95521ff58
Constantin DOBRE <admin@harddex.ro>
2022-02-06 00:24:40 +0800

    New translations globalpay.php (Romanian)

--- upload/catalog/language/ro-ro/extension/payment/globalpay.php
+++ upload/catalog/language/ro-ro/extension/payment/globalpay.php
@@ -3,7 +3,7 @@
 $_['text_title']				= 'Credit / Debit card (Globalpay)';
 
 // Button
-$_['button_confirm']			= 'Confirmare';
+$_['button_confirm']			= 'Confirm';
 
 // Entry
 $_['entry_cc_type']				= 'Card Type';
