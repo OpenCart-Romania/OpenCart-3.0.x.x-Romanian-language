622e77126348accf15534589e829acd9a132ad67
Constantin DOBRE <admin@harddex.ro>
2022-02-05 22:18:56 +0800

    New translations shipping.php (Romanian)

--- upload/admin/language/ro-ro/extension/total/shipping.php
+++ upload/admin/language/ro-ro/extension/total/shipping.php
@@ -10,7 +10,7 @@ $_['text_edit']        = 'Edit Shipping Total';
 // Entry
 $_['entry_estimator']  = 'Shipping Estimator';
 $_['entry_status']     = 'Stare';
-$_['entry_sort_order'] = 'Ordine Sortare';
+$_['entry_sort_order'] = 'Ordine de Sortare';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify shipping total!';
