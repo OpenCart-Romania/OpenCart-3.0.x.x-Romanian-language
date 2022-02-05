e4c77a26952f271190dc6c2c239fe926e88f27a1
Constantin DOBRE <admin@harddex.ro>
2022-02-05 20:17:35 +0800

    New translations voucher.php (Romanian)

--- upload/catalog/language/ro-ro/api/voucher.php
+++ upload/catalog/language/ro-ro/api/voucher.php
@@ -1,14 +1,14 @@
 <?php
 // Text
-$_['text_success']     = 'Success: Your gift voucher discount has been applied!';
+$_['text_success']     = 'Succes: Voucherul tău cadou a fost aplicat!';
 $_['text_cart']        = 'Succes: Ți-ai modificat coșul de cumpărături!';
 $_['text_for']         = '%s Voucher Cadou pentru %s';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a accesa API!';
-$_['error_voucher']    = 'Warning: Gift Voucher is either invalid or the balance has been used up!';
-$_['error_to_name']    = 'Recipient\'s Name must be between 1 and 64 characters!';
-$_['error_from_name']  = 'Your Name must be between 1 and 64 characters!';
+$_['error_voucher']    = 'Atenție: Voucher-ul cadou este invalid sau soldul a fost consumat!';
+$_['error_to_name']    = 'Numele destinatarului trebuie să fie între 1 și 64 de caractere!';
+$_['error_from_name']  = 'Numele Dvs. trebuie să fie între 1 și 64 de caractere!';
 $_['error_email']      = 'Adresa E-Mail nu pare să fie validă!';
 $_['error_theme']      = 'Trebuie să selectezi o temă!';
 $_['error_amount']     = 'Suma trebuie să fie între %s și %s!';
