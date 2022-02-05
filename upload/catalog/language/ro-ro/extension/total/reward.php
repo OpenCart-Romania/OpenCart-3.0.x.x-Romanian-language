c36d7c00031678b0bce11c4c4991ee486a64fe74
Constantin DOBRE <admin@harddex.ro>
2022-02-05 21:16:41 +0800

    New translations reward.php (Romanian)

--- upload/catalog/language/ro-ro/extension/total/reward.php
+++ upload/catalog/language/ro-ro/extension/total/reward.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title'] = 'Use Reward Points (Available %s)';
+$_['heading_title'] = 'Utilizează punctele de recompensă (%s disponibile)';
 
 // Text
 $_['text_reward']   = 'Puncte de Recompensă (%s)';
-$_['text_order_id'] = 'Order ID: #%s';
+$_['text_order_id'] = 'ID comandă: #%s';
 $_['text_success']  = 'Succes: Reducerea din punctele tale de recompensă a fost aplicată!';
 
 // Entry
-$_['entry_reward']  = 'Points to use (Max %s)';
+$_['entry_reward']  = 'Puncte de utilizat (Max %s)';
 
 // Error
 $_['error_reward']  = 'Avertisment: Introduceți suma punctelor de recompensă de utilizat!';
