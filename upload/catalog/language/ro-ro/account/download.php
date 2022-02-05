4de6322f6c743e5f6af1ac5b7449daee0feaa272
Constantin DOBRE <admin@harddex.ro>
2022-02-06 03:19:04 +0800

    New translations download.php (Romanian)

--- upload/catalog/language/ro-ro/account/download.php
+++ upload/catalog/language/ro-ro/account/download.php
@@ -8,7 +8,7 @@ $_['text_downloads']    = 'Descărcări';
 $_['text_empty']        = 'Nu ai făcut nicio comandă descărcabilă anterior!';
 
 // Column
-$_['column_order_id']   = 'ID Comandă';
+$_['column_order_id']   = 'Număr Comandă';
 $_['column_name']       = 'Nume';
 $_['column_size']       = 'Mărime';
 $_['column_date_added'] = 'Data Adăugării';
