d1f979dea679b78d97db02064c01a58122a63902
Constantin DOBRE <admin@harddex.ro>
2022-02-05 20:17:43 +0800

    New translations affiliate.php (Romanian)

--- upload/catalog/language/ro-ro/mail/affiliate.php
+++ upload/catalog/language/ro-ro/mail/affiliate.php
@@ -1,17 +1,17 @@
 <?php
 // Text
 $_['text_subject']        = '%s - Program de Afiliere';
-$_['text_welcome']        = 'Thank you for joining the %s Affiliate Program!';
-$_['text_login']          = 'Your account has now been created and you can log in by using your e-mail address and password by visiting our website or at the following URL:';
-$_['text_approval']       = 'Your account must be approved before you can login. Once approved you can log in by using your e-mail address and password by visiting our website or at the following URL:';
-$_['text_service']        = 'Upon logging in, you will be able to generate tracking codes, track commission payments and edit your account information.';
-$_['text_thanks']         = 'Thanks,';
+$_['text_welcome']        = 'Vă mulțumim pentru aderarea la programul de afiliere %s!';
+$_['text_login']          = 'Contul tău a fost creat şi te poți conecta folosindu-ți adresa de email şi parola pe site-ul nostru sau la următorul URL:';
+$_['text_approval']       = 'Contul tău trebuie să fie aprobat înainte de a te putea conecta. Odată aprobat, te poți conecta folosindu-ți adresa de email şi parola pe site-ul nostru sau la următorul URL:';
+$_['text_service']        = 'La conectare, o să fiți capabili să generați codurile de urmărire, plătirile comisionului de urmărire și să editați informația contului dvs.';
+$_['text_thanks']         = 'Mulțumim,';
 $_['text_new_affiliate']  = 'Afiliat Nou';
-$_['text_signup']         = 'A new affiliate has signed up:';
-$_['text_website']        = 'Web Site:';
-$_['text_customer_group'] = 'Customer Group:';
-$_['text_firstname']      = 'First Name:';
-$_['text_lastname']       = 'Last Name:';
+$_['text_signup']         = 'Un nou afiliat s-a înregistrat:';
+$_['text_website']        = 'Site Web:';
+$_['text_customer_group'] = 'Grup de Clienți:';
+$_['text_firstname']      = 'Prenume:';
+$_['text_lastname']       = 'Nume de Familie:';
 $_['text_company']        = 'Companie:';
 $_['text_email']          = 'E-Mail:';
 $_['text_telephone']      = 'Telefon:';
