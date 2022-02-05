51d71ca3251c952dd1eb99e624b126da46e0860a
Constantin DOBRE <admin@harddex.ro>
2022-02-06 03:19:41 +0800

    New translations shipping.php (Romanian)

--- upload/catalog/language/ro-ro/extension/total/shipping.php
+++ upload/catalog/language/ro-ro/extension/total/shipping.php
@@ -15,6 +15,6 @@ $_['entry_postcode']       = 'Cod Poștal';
 // Error
 $_['error_postcode']       = 'Cod postal trebuie să fie între 2 și 10 de caractere!';
 $_['error_country']        = 'Te rugăm selectează o țară!';
-$_['error_zone']           = 'Te rugăm selectează o Regiune / un Județ!';
+$_['error_zone']           = 'Te rugăm selectează o regiune / stat!';
 $_['error_shipping']       = 'Avertisment: Metoda de expediere necesară!';
 $_['error_no_shipping']    = 'Avertisment: Nu sunt disponibile opțiuni de expediere. Vă rugăm <a href="%s">să ne contactați</a> pentru asistență!';
