9075c37a81b0b550ba11fad8b021ba233993f0f4
Constantin DOBRE <admin@harddex.ro>
2022-02-05 22:19:22 +0800

    New translations sale_tax.php (Romanian)

--- upload/admin/language/ro-ro/extension/report/sale_tax.php
+++ upload/admin/language/ro-ro/extension/report/sale_tax.php
@@ -25,7 +25,7 @@ $_['entry_date_start']  = 'Dată de Început';
 $_['entry_date_end']    = 'Dată de Sfârșit';
 $_['entry_group']       = 'Group By';
 $_['entry_status']      = 'Stare';
-$_['entry_sort_order']  = 'Ordine Sortare';
+$_['entry_sort_order']  = 'Ordine de Sortare';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify tax report!';
