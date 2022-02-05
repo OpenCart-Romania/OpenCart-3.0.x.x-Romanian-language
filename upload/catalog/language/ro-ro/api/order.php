bf7c19f364fd526d221745ef6271c0ad7ba6315b
Constantin DOBRE <admin@harddex.ro>
2022-02-05 22:19:46 +0800

    New translations order.php (Romanian)

--- upload/catalog/language/ro-ro/api/order.php
+++ upload/catalog/language/ro-ro/api/order.php
@@ -5,12 +5,12 @@ $_['text_success']           = 'Succes: Ai modificat comenzile!';
 // Error
 $_['error_permission']       = 'Atenție: Nu ai permisiunea de a accesa API!';
 $_['error_customer']         = 'Atenție: Detalii client trebuie să fie stabilite!';
-$_['error_payment_address']  = 'Avertisment: Metoda de expediere necesară!';
+$_['error_payment_address']  = 'Atenție: Adresa de plată necesară!';
 $_['error_payment_method']   = 'Avertisment: Metoda de plată necesară!';
 $_['error_no_payment']       = 'Avertisment: Nici o opțiune de plată nu este disponibilă!';
-$_['error_shipping_address'] = 'Avertisment: Metoda de expediere necesară!';
-$_['error_shipping_method']  = 'Avertisment: Metoda de expediere necesară!';
+$_['error_shipping_address'] = 'Atenție: Adresa de expediere necesară!';
+$_['error_shipping_method']  = 'Atenție: Metoda de expediere necesară!';
 $_['error_no_shipping']      = 'Atenție: Nici o opțiune de expediere nu este disponibilă!';
-$_['error_stock']            = 'Produsele marcate cu *** nu sunt disponibile în cantitatea dorită sau nu sunt în stoc!';
+$_['error_stock']            = 'Atenție: Produsele marcate cu *** nu sunt disponibile in cantitatea dorita sau nu sunt in stoc!';
 $_['error_minimum']          = 'Atenție: Suma minimă pentru comanda %s este %s!';
 $_['error_not_found']        = 'Atenție: Comanda nu putu fi găsită!';
