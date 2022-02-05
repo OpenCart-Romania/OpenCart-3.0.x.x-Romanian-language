aa3b66e2ed82ed2214727de3347053e0878cd2ea
Constantin DOBRE <admin@harddex.ro>
2022-02-06 00:23:13 +0800

    New translations reset.php (Romanian)

--- upload/catalog/language/ro-ro/account/reset.php
+++ upload/catalog/language/ro-ro/account/reset.php
@@ -9,7 +9,7 @@ $_['text_success']   = 'Succes: Parola ta a fost actualizată cu succes.';
 
 // Entry
 $_['entry_password'] = 'Parolă';
-$_['entry_confirm']  = 'Confirmare';
+$_['entry_confirm']  = 'Confirm';
 
 // Error
 $_['error_password'] = 'Parola trebuie să fie între 4 și 20 de caractere!';
