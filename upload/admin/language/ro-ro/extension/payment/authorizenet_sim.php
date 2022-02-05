6df53ab793a391606ff69939b6adc02d43e5ccd5
Constantin DOBRE <admin@harddex.ro>
2022-02-06 02:18:36 +0800

    New translations authorizenet_sim.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/authorizenet_sim.php
+++ upload/admin/language/ro-ro/extension/payment/authorizenet_sim.php
@@ -27,5 +27,5 @@ $_['help_total']			= 'The checkout total the order must reach before this paymen
 
 // Error
 $_['error_permission']		= 'Warning: You do not have permission to modify payment Authorize.Net (SIM)!';
-$_['error_merchant']		= 'Merchant ID Required!';
+$_['error_merchant']		= 'ID Comerciant Necesar!';
 $_['error_key']				= 'Transaction Key Required!';
