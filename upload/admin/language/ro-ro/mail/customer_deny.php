f0a4fa1de68fb93057a1ac0ebd15c5eaf48f29d3
Constantin DOBRE <admin@harddex.ro>
2022-02-06 22:07:25 +0800

    New translations customer_deny.php (Romanian)

--- upload/admin/language/ro-ro/mail/customer_deny.php
+++ upload/admin/language/ro-ro/mail/customer_deny.php
@@ -3,4 +3,4 @@
 $_['text_subject'] = '%s - Your Account has been denied!';
 $_['text_welcome'] = 'Bine ai venit și îți mulțumim că te-ai înregistrat la %s!';
 $_['text_denied']  = 'Unfortunately your request has been denied. For more information you can contact the store owner here:';
-$_['text_thanks']  = 'Thanks,';
+$_['text_thanks']  = 'Îți mulțumim,';
