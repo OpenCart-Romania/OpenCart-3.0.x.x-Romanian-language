d1582a7642dd721362141ce4ffaf3e389c3c3444
Constantin DOBRE <admin@harddex.ro>
2022-02-06 22:07:22 +0800

    New translations affiliate_approve.php (Romanian)

--- upload/admin/language/ro-ro/mail/affiliate_approve.php
+++ upload/admin/language/ro-ro/mail/affiliate_approve.php
@@ -3,5 +3,5 @@
 $_['text_subject']  = '%s - Your affiliate account has been activated!';
 $_['text_welcome']  = 'Bine ai venit și îți mulțumim că te-ai înregistrat la %s!';
 $_['text_login']    = 'Your account has now been approved and you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_services'] = 'Upon logging in, you will be able to generate tracking codes, track commission payments and edit your account information.';
-$_['text_thanks']   = 'Thanks,';
+$_['text_services'] = 'După conectare, vei putea să generezi coduri de urmărire, să urmărești plățile comisioanelor și să editezi informațiile contului.';
+$_['text_thanks']   = 'Îți mulțumim,';
