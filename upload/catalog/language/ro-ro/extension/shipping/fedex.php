cbd4046c98bf3e50e98541774faa54b8c9396457
Constantin DOBRE <admin@harddex.ro>
2022-02-06 20:12:48 +0800

    New translations fedex.php (Romanian)

--- upload/catalog/language/ro-ro/extension/shipping/fedex.php
+++ upload/catalog/language/ro-ro/extension/shipping/fedex.php
@@ -1,7 +1,7 @@
 <?php
 // Text
 $_['text_title']                               = 'Fedex';
-$_['text_weight']                              = 'Greutate:';
+$_['text_weight']                              = 'Weight:';
 $_['text_eta']                                 = 'Estimated Time:';
 $_['text_europe_first_international_priority'] = 'Europe First International Priority';
 $_['text_fedex_1_day_freight']                 = 'Fedex 1 Day Freight';
