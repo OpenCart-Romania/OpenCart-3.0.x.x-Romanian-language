2eda3ca157397977895104e41d64ece4cfe19081
Constantin DOBRE <admin@harddex.ro>
2022-02-07 00:10:52 +0800

    New translations theme.php (Romanian)

--- upload/admin/language/ro-ro/design/theme.php
+++ upload/admin/language/ro-ro/design/theme.php
@@ -5,8 +5,8 @@ $_['heading_title']     = 'Editor Temă';
 // Text
 $_['text_success']      = 'Succes: Ai modificat temele!';
 $_['text_edit']         = 'Editare Temă';
-$_['text_store']        = 'Choose your store';
-$_['text_template']     = 'Choose a template';
+$_['text_store']        = 'Alege-ți magazinul';
+$_['text_template']     = 'Alege un șablon';
 $_['text_default']      = 'Implicit';
 $_['text_history']      = 'Istoric Temă';
 $_['text_twig']         = 'Editorul de teme utilizează limbajul pentru șabloane Twig. Poți citi despre <a href="http://twig.sensiolabs.org/documentation" target="_blank" class="alert-link">Sintaxa Twig aici</a>.';
