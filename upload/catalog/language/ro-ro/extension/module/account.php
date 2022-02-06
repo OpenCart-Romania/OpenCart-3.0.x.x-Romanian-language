5410c9ed9bf0eea38c1d513f32b9225dfde058c1
Constantin DOBRE <admin@harddex.ro>
2022-02-06 21:10:11 +0800

    New translations account.php (Romanian)

--- upload/catalog/language/ro-ro/extension/module/account.php
+++ upload/catalog/language/ro-ro/extension/module/account.php
@@ -4,7 +4,7 @@ $_['heading_title']    = 'Cont';
 
 // Text
 $_['text_register']    = 'Înregistrează-te';
-$_['text_login']       = 'Autentificare';
+$_['text_login']       = 'Conectare';
 $_['text_logout']      = 'Deconectare';
 $_['text_forgotten']   = 'Parolă Uitată';
 $_['text_account']     = 'Contul Meu';
@@ -15,7 +15,7 @@ $_['text_wishlist']    = 'Listă de Dorințe';
 $_['text_order']       = 'Istoric Comenzi';
 $_['text_download']    = 'Descărcări';
 $_['text_reward']      = 'Puncte de Recompensă';
-$_['text_return']      = 'Returnări';
+$_['text_return']      = 'Retururi';
 $_['text_transaction'] = 'Tranzacții';
 $_['text_newsletter']  = 'Newsletter';
 $_['text_recurring']   = 'Plăţi recurente';
