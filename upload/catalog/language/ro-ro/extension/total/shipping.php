fcf39cda39d4e195e3a6976dfd8ff736f0dc4e35
Constantin DOBRE <admin@harddex.ro>
2022-02-06 22:07:01 +0800

    New translations shipping.php (Romanian)

--- upload/catalog/language/ro-ro/extension/total/shipping.php
+++ upload/catalog/language/ro-ro/extension/total/shipping.php
@@ -5,7 +5,7 @@ $_['heading_title']        = 'Estimare transport &amp; taxe';
 // Text
 $_['text_success']         = 'Succes: Estimarea dumneavoastră de transport a fost aplicată!';
 $_['text_shipping']        = 'Introduceți destinația dvs. pentru a obține o estimare de expediere.';
-$_['text_shipping_method'] = 'Selectați metoda preferată de expediere pe care doriți să o utilizați pentru această comandă.';
+$_['text_shipping_method'] = 'Te rugăm să selectezi metoda de expediere preferată pentru a fi utilizată la această comandă.';
 
 // Entry
 $_['entry_country']        = 'Țară';
@@ -17,4 +17,4 @@ $_['error_postcode']       = 'Codul poștal trebuie să fie între 2 și 10 cara
 $_['error_country']        = 'Te rugăm selectează o țară!';
 $_['error_zone']           = 'Te rugăm selectează o regiune / stat!';
 $_['error_shipping']       = 'Atenție: Metoda de expediere necesară!';
-$_['error_no_shipping']    = 'Avertisment: Nu sunt disponibile opțiuni de expediere. Vă rugăm <a href="%s">să ne contactați</a> pentru asistență!';
+$_['error_no_shipping']    = 'Atenție: Nu sunt disponibile opțiuni de expediere. Te rugăm <a href="%s">contactează-ne</a> pentru asistență!';
