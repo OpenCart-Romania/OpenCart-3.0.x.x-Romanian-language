125976f1a701528e16a98efd6fbf307b518379b4
Constantin DOBRE <admin@harddex.ro>
2022-02-06 22:07:24 +0800

    New translations customer_approve.php (Romanian)

--- upload/admin/language/ro-ro/mail/customer_approve.php
+++ upload/admin/language/ro-ro/mail/customer_approve.php
@@ -4,4 +4,4 @@ $_['text_subject'] = '%s - Your Account has been activated!';
 $_['text_welcome'] = 'Bine ai venit și îți mulțumim că te-ai înregistrat la %s!';
 $_['text_login']   = 'Your account has now been created and you can log in by using your email address and password by visiting our website or at the following URL:';
 $_['text_service'] = 'La conectare, vei putea accesa alte servicii, inclusiv revizuirea comenzilor anterioare, imprimarea facturilor și editarea informațiilor contului.';
-$_['text_thanks']  = 'Thanks,';
+$_['text_thanks']  = 'Îți mulțumim,';
