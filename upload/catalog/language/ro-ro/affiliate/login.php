810c7a4df46bbffe631b97c5c8eac4c4483dd454
Constantin DOBRE <admin@harddex.ro>
2022-02-06 21:10:21 +0800

    New translations login.php (Romanian)

--- upload/catalog/language/ro-ro/affiliate/login.php
+++ upload/catalog/language/ro-ro/affiliate/login.php
@@ -4,7 +4,7 @@ $_['heading_title']                 = 'Program de Afiliere';
 
 // Text
 $_['text_account']                  = 'Cont';
-$_['text_login']                    = 'Autentificare';
+$_['text_login']                    = 'Conectare';
 $_['text_description']              = '<p>Programul de afiliere %s este gratuit și permite membrilor să câștige venituri prin plasarea unui link sau unor link-uri pe site-ul lor web care promovează %s sau produse specifice. Orice vânzare efectuată către clienții care au făcut clic pe aceste link-uri va câștiga comisionul de afiliere. Rata standard a comisionului este în prezent %s.</p><p>Pentru mai multe informații, vizitați pagina noastră de întrebări frecvente sau consultați termenii &amp; condițiile de afiliere</p>';
 $_['text_new_affiliate']            = 'Afiliat Nou';
 $_['text_register_account']         = '<p>Nu sunt afiliat în prezent.</p><p>Faceţi clic pe Continuă mai jos pentru a crea un nou cont de afiliat. Te rugam sa retii ca acesta nu este conectat in nici un fel la contul tau de client.</p>';
