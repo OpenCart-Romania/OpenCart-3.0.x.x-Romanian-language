b1bf591383a90be5c28772707780921d134d16d2
Constantin DOBRE <admin@harddex.ro>
2022-02-06 22:07:26 +0800

    New translations forgotten.php (Romanian)

--- upload/admin/language/ro-ro/mail/forgotten.php
+++ upload/admin/language/ro-ro/mail/forgotten.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_subject']  = '%s - Password reset request';
+$_['text_subject']  = '%s - Cerere de resetare a parolei';
 $_['text_greeting'] = 'A new password was requested for %s administration.';
 $_['text_change']   = 'Pentru a reseta parola, faceți clic pe linkul de mai jos:';
-$_['text_ip']       = 'The IP used to make this request was:';
+$_['text_ip']       = 'IP-ul utilizat pentru a face această solicitare a fost:';
