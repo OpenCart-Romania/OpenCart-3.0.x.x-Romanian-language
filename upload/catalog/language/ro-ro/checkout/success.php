45ff764ac6bba3036f489495f6e23bf8fde6411b
Constantin DOBRE <admin@harddex.ro>
2022-02-07 16:35:04 +0800

    New translations success.php (Romanian)

--- upload/catalog/language/ro-ro/checkout/success.php
+++ upload/catalog/language/ro-ro/checkout/success.php
@@ -7,4 +7,4 @@ $_['text_basket']          = 'Coș de Cumpărături';
 $_['text_checkout']        = 'Checkout';
 $_['text_success']         = 'Succes';
 $_['text_customer']        = '<p>Comanda ta a fost procesata cu succes!</p><p>Poți vizualiza istoricul comenzilor mergând la pagina <a href="%s">contul meu</a> și dând click pe <a href="%s">istoric</a>.</p><p>Dacă achiziția are o descărcare asociată, poți merge la pagina <a href="%s">descărcări</a> pentru a o vizualiza.</p><p>Te rugăm să direcţionezi orice întrebare către proprietarul magazinului <a href="%s"></a>.</p><p>Mulțumim pentru cumpărături online cu noi!</p>';
-$_['text_guest']           = '<p>Your order has been successfully processed!</p><p>Please direct any questions you have to the <a href="%s">store owner</a>.</p><p>Thanks for shopping with us online!</p>';
+$_['text_guest']           = '<p>Comanda ta a fost procesată cu succes!</p><p>Te rugăm să direcționezi orice întrebări pe care le ai către <a href="%s">proprietarul magazinului</a>.</p><p>Mulțumim pentru cumpărături online cu noi!</p>';
