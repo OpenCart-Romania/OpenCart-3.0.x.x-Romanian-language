2558841e8764aa68609a5faf0fb947399b94a0c1
Constantin DOBRE <admin@harddex.ro>
2022-02-07 12:48:27 +0800

    New translations failure.php (Romanian)

--- upload/catalog/language/ro-ro/checkout/failure.php
+++ upload/catalog/language/ro-ro/checkout/failure.php
@@ -6,15 +6,15 @@ $_['heading_title'] = 'Plată Eșuată!';
 $_['text_basket']   = 'Coș de Cumpărături';
 $_['text_checkout'] = 'Checkout';
 $_['text_failure']  = 'Plată Eșuată';
-$_['text_message']  = '<p>A apărut o problemă la procesarea plății și comanda nu s-a finalizat.</p>
+$_['text_message']  = '<p>A existat o problemă la procesarea plății și comanda nu s-a finalizat.</p>
 
-<p>Posibile motive sunt:</p>
+<p>Motivele posibile sunt:</p>
 <ul>
   <li>Fonduri insuficiente</li>
-  <li>Validare eșuată</li>
+  <li>Verificarea a eșuat</li>
 </ul>
 
-<p>Te rugăm să plasezi din nou comanda cu o metodă de plată diferită.</p>
+<p>Te rugăm să încerci să comanzi din nou folosind o altă metodă de plată.</p>
 
-<p>Dacă problema persistă vă rugăm <a href="%s">să ne contactați</a> cu detaliile despre comanda pe care încercați să o plasați.</p>
+<p>Dacă problema persistă, te rugăm <a href="%s">să ne contactezi</a> cu detaliile comenzii pe care încerci să o plasezi.</p>
 ';
