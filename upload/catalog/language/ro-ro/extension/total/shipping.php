c223cffdebb38dbcc670b65b61aa90494c5c9fb4
Constantin DOBRE <admin@harddex.ro>
2022-02-13 02:35:01 +0800

    New translations shipping.php (Romanian)

--- upload/catalog/language/ro-ro/extension/total/shipping.php
+++ upload/catalog/language/ro-ro/extension/total/shipping.php
@@ -4,8 +4,8 @@ $_['heading_title']        = 'Estimare transport &amp; taxe';
 
 // Text
 $_['text_success']         = 'Succes: Estimarea dumneavoastră de transport a fost aplicată!';
-$_['text_shipping']        = 'Introduceți destinația dvs. pentru a obține o estimare de expediere.';
-$_['text_shipping_method'] = 'Te rugăm să selectezi metoda de expediere preferată pentru a fi utilizată la această comandă.';
+$_['text_shipping']        = 'Introdu destinația pentru a obține o estimare de livrare.';
+$_['text_shipping_method'] = 'Te rugăm să selectezi metoda de livrare preferată pentru a fi utilizată la această comandă.';
 
 // Entry
 $_['entry_country']        = 'Țară';
@@ -16,5 +16,5 @@ $_['entry_postcode']       = 'Cod Poștal';
 $_['error_postcode']       = 'Codul poștal trebuie să fie între 2 și 10 caractere!';
 $_['error_country']        = 'Te rugăm selectează o țară!';
 $_['error_zone']           = 'Te rugăm selectează o regiune / stat!';
-$_['error_shipping']       = 'Atenție: Metoda de expediere necesară!';
-$_['error_no_shipping']    = 'Atenție: Nu sunt disponibile opțiuni de expediere. Te rugăm <a href="%s">contactează-ne</a> pentru asistență!';
+$_['error_shipping']       = 'Atenție: Metoda de livrare necesară!';
+$_['error_no_shipping']    = 'Atenție: Nu sunt disponibile opțiuni de livrare. Te rugăm <a href="%s">contactează-ne</a> pentru asistență!';
