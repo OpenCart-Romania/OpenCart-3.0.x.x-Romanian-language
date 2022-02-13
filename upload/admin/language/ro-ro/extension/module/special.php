d50721b7d9720e55959939afed57fa7bce883d12
Constantin DOBRE <admin@harddex.ro>
2022-02-13 23:59:56 +0800

    New translations special.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/special.php
+++ upload/admin/language/ro-ro/extension/module/special.php
@@ -15,7 +15,7 @@ $_['entry_height']     = 'Înălţime';
 $_['entry_status']     = 'Stare';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify specials module!';
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul speciale!';
 $_['error_name']       = 'Numele Modulului trebuie să fie între 3 și 64 de caractere!';
 $_['error_width']      = 'Width required!';
 $_['error_height']     = 'Height required!';
