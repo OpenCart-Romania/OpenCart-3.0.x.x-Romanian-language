e6713e012a55114bb73355b5a2837724a2175c73
Constantin DOBRE <admin@harddex.ro>
2022-02-14 00:00:13 +0800

    New translations klarna_account.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/klarna_account.php
+++ upload/admin/language/ro-ro/extension/payment/klarna_account.php
@@ -33,7 +33,7 @@ $_['help_secret']			= 'Shared secret to use with the Klarna service (provided by
 $_['help_total']			= 'The checkout total the order must reach before this payment method becomes active.';
 
 // Error
-$_['error_permission']		= 'Warning: You do not have permission to modify payment Klarna Part Payment!';
+$_['error_permission']		= 'Atenție: Nu ai permisiunea de a modifica plata Klarna Part Payment!';
 $_['error_pclass']			= 'Could not retrieve pClass for %s. Error Code: %s; Error Message: %s';
 $_['error_curl']			= 'Curl Error - Code: %d; Message: %s';
 $_['error_log']				= 'There were errors updating the module. Please check the log file.';
