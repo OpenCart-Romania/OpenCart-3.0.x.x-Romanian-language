d4d5782382cdc968d2b237f9861009a76f28acaf
Constantin DOBRE <admin@harddex.ro>
2022-02-13 14:56:19 +0800

    New translations pilibaba_button.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/pilibaba_button.php
+++ upload/admin/language/ro-ro/extension/module/pilibaba_button.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']    = 'Pilibaba Checkout Button';
+$_['heading_title']    = 'Buton Pilibaba Checkout';
 
 // Text
 $_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Succes: Ai modificat modulul Buton Pilibaba Checkout!';
-$_['text_edit']        = 'Edit Pilibaba Checkout Button Module';
+$_['text_edit']        = 'Editare Modul Buton Pilibaba Checkout';
 
 // Entry
 $_['entry_status']     = 'Stare';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify Pilibaba Checkout Button module!';
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul Buton Pilibaba Checkout!';
