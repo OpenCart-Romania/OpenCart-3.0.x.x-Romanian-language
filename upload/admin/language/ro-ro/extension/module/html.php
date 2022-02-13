27113f44a09ce0702e5851792fe9087caeb2cc16
Constantin DOBRE <admin@harddex.ro>
2022-02-13 14:56:23 +0800

    New translations html.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/html.php
+++ upload/admin/language/ro-ro/extension/module/html.php
@@ -14,5 +14,5 @@ $_['entry_description'] = 'Descriere';
 $_['entry_status']      = 'Stare';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify HTML Content module!';
+$_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica modulul de conținut HTML!';
 $_['error_name']        = 'Numele Modulului trebuie să fie între 3 și 64 de caractere!';
