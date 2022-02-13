78da187c6cd8dac1ae98742b635bff18c4b483b9
Constantin DOBRE <admin@harddex.ro>
2022-02-13 12:55:23 +0800

    New translations pp_braintree_button.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/pp_braintree_button.php
+++ upload/admin/language/ro-ro/extension/module/pp_braintree_button.php
@@ -10,8 +10,8 @@ $_['text_info']        = 'The button will <u>not</u> appear under certain condit
 $_['text_info_li1']    = 'The cart is empty and no vouchers have been applied';
 $_['text_info_li2']    = 'The cart has downloads or recurring payments and the user is not logged in';
 $_['text_info_li3']    = 'Out of stock item checkout is disabled and the cart contains an out of stock item';
-$_['text_layouts']     = 'After enabling the module, use the layout manager to add the button to areas on the store.';
-$_['text_layout_link'] = 'Click here to access layouts page';
+$_['text_layouts']     = 'După activarea modulului, utilizează layout manager pentru a adăuga butonul la zonele din magazin.';
+$_['text_layout_link'] = 'Click aici pentru a accesa pagina layout-uri';
 
 // Entry
 $_['entry_status']     = 'Stare';
