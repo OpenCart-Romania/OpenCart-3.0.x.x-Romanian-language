44841bad56bafbbecaf1c6ddb5ff8af338e79049
Constantin DOBRE <admin@harddex.ro>
2022-02-13 12:55:14 +0800

    New translations fraud.php (Romanian)

--- upload/admin/language/ro-ro/extension/extension/fraud.php
+++ upload/admin/language/ro-ro/extension/extension/fraud.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']    = 'Anti-Fraud';
+$_['heading_title']    = 'Anti-Fraudă';
 
 // Text
 $_['text_success']     = 'Succes: Ai modificat anti-frauda!';
-$_['text_list']        = 'Anti-Fraud List';
+$_['text_list']        = 'Lista Anti-Fraudă';
 
 // Column
-$_['column_name']      = 'Anti-Fraud Name';
+$_['column_name']      = 'Nume Anti-Fraudă';
 $_['column_status']    = 'Stare';
 $_['column_action']    = 'Acțiune';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify anti-fraud!';
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica anti-frauda!';
