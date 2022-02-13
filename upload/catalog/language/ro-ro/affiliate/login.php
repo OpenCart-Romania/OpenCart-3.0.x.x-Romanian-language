5c8b19fe4e1c99754b82537dbbda55797a42cc58
Constantin DOBRE <admin@harddex.ro>
2022-02-13 14:56:10 +0800

    New translations login.php (Romanian)

--- upload/catalog/language/ro-ro/affiliate/login.php
+++ upload/catalog/language/ro-ro/affiliate/login.php
@@ -5,9 +5,9 @@ $_['heading_title']                 = 'Program de Afiliere';
 // Text
 $_['text_account']                  = 'Cont';
 $_['text_login']                    = 'Conectare';
-$_['text_description']              = '<p>Programul de afiliere %s este gratuit și permite membrilor să câștige venituri prin plasarea unui link sau unor link-uri pe site-ul lor web care promovează %s sau produse specifice. Orice vânzare efectuată către clienții care au făcut clic pe aceste link-uri va câștiga comisionul de afiliere. Rata standard a comisionului este în prezent %s.</p><p>Pentru mai multe informații, vizitați pagina noastră de întrebări frecvente sau consultați termenii &amp; condițiile de afiliere</p>';
+$_['text_description']              = '<p>Programul de afiliere %s este gratuit și permite membrilor să câștige venituri prin plasarea unui link sau unor link-uri pe site-ul lor web care promovează %s sau produse specifice. Orice vânzare efectuată către clienții care au făcut click pe aceste link-uri va câștiga comisionul de afiliere. Rata standard a comisionului este în prezent %s.</p><p>Pentru mai multe informații, vizitați pagina noastră de întrebări frecvente sau consultați termenii &amp; condițiile de afiliere</p>';
 $_['text_new_affiliate']            = 'Afiliat Nou';
-$_['text_register_account']         = '<p>Nu sunt afiliat în prezent.</p><p>Faceţi clic pe Continuă mai jos pentru a crea un nou cont de afiliat. Te rugam sa retii ca acesta nu este conectat in nici un fel la contul tau de client.</p>';
+$_['text_register_account']         = '<p>Nu sunt afiliat în prezent.</p><p>Faceți click pe Continuă mai jos pentru a crea un nou cont de afiliat. Te rugam sa reții ca acesta nu este conectat in nici un fel la contul tău de client.</p>';
 $_['text_returning_affiliate']      = 'Autentificare Afiliat';
 $_['text_i_am_returning_affiliate'] = 'Sunt un afiliat întors.';
 $_['text_forgotten']                = 'Parolă Uitată';
