fed7e600ab63b21e09b3c9ab67472c31d7cb99b9
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:42:45 +0800

    New translations html.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/html.php
+++ upload/admin/language/ro-ro/extension/module/html.php
@@ -3,16 +3,16 @@
 $_['heading_title']     = 'HTML Content';
 
 // Text
-$_['text_extension']    = 'Extensii';
+$_['text_extension']    = 'Extensions';
 $_['text_success']      = 'Succes: Ai modificat modulul de Conținut HTML!';
 $_['text_edit']         = 'Editare Modul Conținut HTML';
 
 // Entry
 $_['entry_name']        = 'Module Name';
 $_['entry_title']       = 'Heading Title';
-$_['entry_description'] = 'Descriere';
-$_['entry_status']      = 'Stare';
+$_['entry_description'] = 'Description';
+$_['entry_status']      = 'Status';
 
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica modulul de conținut HTML!';
-$_['error_name']        = 'Numele Modulului trebuie să fie între 3 și 64 de caractere!';
+$_['error_name']        = 'Module Name must be between 3 and 64 characters!';
