e09a627af3bd19fe2608997c502fd32446e484ba
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:31:01 +0800

    New translations length_class.php (Romanian)

--- upload/admin/language/ro-ro/localisation/length_class.php
+++ upload/admin/language/ro-ro/localisation/length_class.php
@@ -12,12 +12,12 @@ $_['text_edit']        = 'Edit Length Class';
 $_['column_title']     = 'Length Title';
 $_['column_unit']      = 'Length Unit';
 $_['column_value']     = 'Value';
-$_['column_action']    = 'Action';
+$_['column_action']    = 'Acțiune';
 
 // Entry
 $_['entry_title']      = 'Length Title';
 $_['entry_unit']       = 'Length Unit';
-$_['entry_value']      = 'Value';
+$_['entry_value']      = 'Valoare';
 
 // Help
 $_['help_value']       = 'Set to 1.00000 if this is your default length.';
