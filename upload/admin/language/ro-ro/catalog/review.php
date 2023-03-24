b122d1cf3a1eecee21a6555c6e7ede273b9169c2
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:41:45 +0800

    New translations review.php (Romanian)

--- upload/admin/language/ro-ro/catalog/review.php
+++ upload/admin/language/ro-ro/catalog/review.php
@@ -7,26 +7,26 @@ $_['text_success']      = 'Succes: Ai modificat recenziile!';
 $_['text_list']         = 'Listă Recenzii';
 $_['text_add']          = 'Adaugă Recenzie';
 $_['text_edit']         = 'Editare Recenzie';
-$_['text_filter']       = 'Filtru';
+$_['text_filter']       = 'Filter';
 
 // Column
 $_['column_product']    = 'Produs';
 $_['column_author']     = 'Autor';
 $_['column_rating']     = 'Evaluare';
-$_['column_status']     = 'Stare';
-$_['column_date_added'] = 'Data Adăugării';
-$_['column_action']     = 'Acțiune';
+$_['column_status']     = 'Status';
+$_['column_date_added'] = 'Date Added';
+$_['column_action']     = 'Action';
 
 // Entry
-$_['entry_product']     = 'Produs';
-$_['entry_author']      = 'Autor';
-$_['entry_rating']      = 'Evaluare';
-$_['entry_status']      = 'Stare';
+$_['entry_product']     = 'Product';
+$_['entry_author']      = 'Author';
+$_['entry_rating']      = 'Rating';
+$_['entry_status']      = 'Status';
 $_['entry_text']        = 'Text';
-$_['entry_date_added']  = 'Data Adăugării';
+$_['entry_date_added']  = 'Date Added';
 
 // Help
-$_['help_product']      = '(Completare automată)';
+$_['help_product']      = '(Autocomplete)';
 
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica recenziile!';
