a3abd0ba338a6db70c373279972107813939e518
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:41:51 +0800

    New translations profile.php (Romanian)

--- upload/admin/language/ro-ro/common/profile.php
+++ upload/admin/language/ro-ro/common/profile.php
@@ -7,26 +7,26 @@ $_['text_success']          = 'Succes: Ți-ai modificat profilul!';
 $_['text_edit']             = 'Editează-ți Profilul';
 
 // Column
-$_['column_username']       = 'Nume de utilizator';
-$_['column_status']         = 'Stare';
-$_['column_date_added']     = 'Data Adăugării';
-$_['column_action']         = 'Acțiune';
+$_['column_username']       = 'Username';
+$_['column_status']         = 'Status';
+$_['column_date_added']     = 'Date Added';
+$_['column_action']         = 'Action';
 
 // Entry
-$_['entry_username']        = 'Nume de utilizator';
-$_['entry_password']        = 'Parolă';
-$_['entry_confirm']         = 'Confirma';
+$_['entry_username']        = 'Username';
+$_['entry_password']        = 'Password';
+$_['entry_confirm']         = 'Confirm';
 $_['entry_firstname']       = 'Prenume';
 $_['entry_lastname']        = 'Nume de Familie';
 $_['entry_email']           = 'E-Mail';
-$_['entry_image']           = 'Imagine';
+$_['entry_image']           = 'Image';
 
 // Error
 $_['error_permission']      = 'Atenție: Nu ai permisiunea de a-ți modifica profilul!';
 $_['error_exists_username'] = 'Atenție: Nume utilizator este deja utilizat!';
 $_['error_username']        = 'Nume utilizator trebuie să fie între 3 și 20 de caractere!';
-$_['error_password']        = 'Parola trebuie să fie între 4 și 20 de caractere!';
-$_['error_confirm']         = 'Parola și confirmarea parolei nu se potrivesc!';
+$_['error_password']        = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']         = 'Password and password confirmation do not match!';
 $_['error_firstname']       = 'Prenumele trebuie să fie între 1 și 32 de caractere!';
 $_['error_lastname']        = 'Numele de Familie trebuie să fie între 1 și 32 de caractere!';
 $_['error_email']           = 'Adresa E-Mail nu pare să fie validă!';
