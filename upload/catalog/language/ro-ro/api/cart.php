baddc3a82d52648f77b956295b76314c99b1a585
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:48:06 +0800

    New translations cart.php (Romanian)

--- upload/catalog/language/ro-ro/api/cart.php
+++ upload/catalog/language/ro-ro/api/cart.php
@@ -7,4 +7,4 @@ $_['error_permission'] = 'Atenție: Nu ai permisiunea de a accesa API!';
 $_['error_stock']      = 'Produsele marcate cu *** nu sunt disponibile in cantitatea dorita sau nu sunt in stoc!';
 $_['error_minimum']    = 'Valoarea minimă comandată pentru %s este %s!';
 $_['error_store']      = 'Produsul nu poate fi cumpărat de la magazinul pe care l-ai ales!';
-$_['error_required']   = '%s necesar!';
+$_['error_required']   = '%s required!';
