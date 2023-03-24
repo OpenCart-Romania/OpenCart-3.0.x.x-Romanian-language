575d9780193dc620e16db850ce62ce48a306e185
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:28:43 +0800

    New translations customer.php (Romanian)

--- upload/admin/language/ro-ro/extension/dashboard/customer.php
+++ upload/admin/language/ro-ro/extension/dashboard/customer.php
@@ -3,15 +3,15 @@
 $_['heading_title']    = 'Total Clienți';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Succes: Ai modificat dashboard-ul client!';
 $_['text_edit']        = 'Editare Dashboard Client';
 $_['text_view']        = 'Vezi mai mult...';
 
 // Entry
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordine Sortare';
+$_['entry_width']      = 'Lăţime';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica dashboard-ul client!';
