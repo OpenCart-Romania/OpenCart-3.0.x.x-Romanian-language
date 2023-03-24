e66956aea1549fc4add7e684112a3684231fadf4
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:30:51 +0800

    New translations reward.php (Romanian)

--- upload/admin/language/ro-ro/extension/total/reward.php
+++ upload/admin/language/ro-ro/extension/total/reward.php
@@ -3,13 +3,13 @@
 $_['heading_title']    = 'Reward Points';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Succes: Ai modificat totalul punctelor de recompensă!';
 $_['text_edit']        = 'Editare Total Puncte de Recompensă';
 
 // Entry
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordine Sortare';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica totalul punctelor de recompensă!';
