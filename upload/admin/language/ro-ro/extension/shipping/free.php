cf95e2058fb4c39f3ddf63d5ab12c732733d1368
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:30:38 +0800

    New translations free.php (Romanian)

--- upload/admin/language/ro-ro/extension/shipping/free.php
+++ upload/admin/language/ro-ro/extension/shipping/free.php
@@ -3,15 +3,15 @@
 $_['heading_title']    = 'Free Shipping';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified free shipping!';
 $_['text_edit']        = 'Edit Free Shipping';
 
 // Entry
 $_['entry_total']      = 'Total';
 $_['entry_geo_zone']   = 'Geo Zone';
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordine Sortare';
 
 // Help
 $_['help_total']       = 'Sub-Total amount needed before the free shipping module becomes available.';
