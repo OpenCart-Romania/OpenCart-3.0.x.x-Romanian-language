8a2c57be5d3a27fc37a417a71d3756b27519a955
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:15 +0800

    New translations divido_calculator.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/divido_calculator.php
+++ upload/admin/language/ro-ro/extension/module/divido_calculator.php
@@ -3,12 +3,12 @@
 $_['heading_title']    = 'Divido Calculator Pagină de Produs';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Succes: Ai modificat Divido Calculator Pagină de Produs!';
 $_['text_edit']        = 'Editare Divido Calculator Pagină de Produs';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul Divido Calculator Pagină de Produs!';
