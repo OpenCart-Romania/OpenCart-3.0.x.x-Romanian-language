51b8ce45cd443f0695e37ae716e3072bd2f07f9f
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:30:29 +0800

    New translations sale_coupon.php (Romanian)

--- upload/admin/language/ro-ro/extension/report/sale_coupon.php
+++ upload/admin/language/ro-ro/extension/report/sale_coupon.php
@@ -3,23 +3,23 @@
 $_['heading_title']    = 'Coupons Report';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_edit']        = 'Edit Coupons Report';
 $_['text_success']     = 'Success: You have modified coupon report!';
-$_['text_filter']      = 'Filter';
+$_['text_filter']      = 'Filtru';
 
 // Column
 $_['column_name']      = 'Coupon Name';
 $_['column_code']      = 'Code';
 $_['column_orders']    = 'Orders';
 $_['column_total']     = 'Total';
-$_['column_action']    = 'Action';
+$_['column_action']    = 'Acțiune';
 
 // Entry
-$_['entry_date_start'] = 'Date Start';
-$_['entry_date_end']   = 'Date End';
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_date_start'] = 'Dată de Început';
+$_['entry_date_end']   = 'Dată de Sfârșit';
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordine Sortare';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify coupon report!';
