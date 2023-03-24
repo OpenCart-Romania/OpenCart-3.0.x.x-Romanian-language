841bdc481a9361b6555078528712f62c6d0327b3
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:42:19 +0800

    New translations feed.php (Romanian)

--- upload/admin/language/ro-ro/extension/extension/feed.php
+++ upload/admin/language/ro-ro/extension/extension/feed.php
@@ -8,8 +8,8 @@ $_['text_list']        = 'Feed List';
 
 // Column
 $_['column_name']      = 'Product Feed Name';
-$_['column_status']    = 'Stare';
-$_['column_action']    = 'Acțiune';
+$_['column_status']    = 'Status';
+$_['column_action']    = 'Action';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica feed-urile!';
