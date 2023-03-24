3ad3d9b1cc1811b6f18af943f37a749b39bee12e
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:44:31 +0800

    New translations auspost.php (Romanian)

--- upload/admin/language/ro-ro/extension/shipping/auspost.php
+++ upload/admin/language/ro-ro/extension/shipping/auspost.php
@@ -3,18 +3,18 @@
 $_['heading_title']      = 'Australia Post';
 
 // Text
-$_['text_extension']     = 'Extensii';
+$_['text_extension']     = 'Extensions';
 $_['text_success']       = 'Success: You have modified Australia Post shipping!';
 $_['text_edit']          = 'Edit Australia Post Shipping';
 
 // Entry
 $_['entry_api']          = 'API Key';
 $_['entry_postcode']     = 'Cod Poștal';
-$_['entry_weight_class'] = 'Clasă de Greutate';
-$_['entry_tax_class']    = 'Clasa Fiscală';
+$_['entry_weight_class'] = 'Weight Class';
+$_['entry_tax_class']    = 'Tax Class';
 $_['entry_geo_zone']     = 'Geo Zone';
-$_['entry_status']       = 'Stare';
-$_['entry_sort_order']   = 'Ordine de Sortare';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order';
 
 // Help
 $_['help_weight_class']  = 'Set to kilograms.';
