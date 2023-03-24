cc933111effe9f03a49029609c12c4999e9df29b
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:31:23 +0800

    New translations marketplace.php (Romanian)

--- upload/admin/language/ro-ro/marketplace/marketplace.php
+++ upload/admin/language/ro-ro/marketplace/marketplace.php
@@ -5,20 +5,20 @@ $_['heading_title']      = 'Extension Marketplace';
 // Text
 $_['text_success']       = 'Success: You have modified extensions!';
 $_['text_list']          = 'Extension List';
-$_['text_filter']        = 'Filter';
+$_['text_filter']        = 'Filtru';
 $_['text_search']        = 'Search for extensions and themes';
 $_['text_category']      = 'Categories';
 $_['text_all']           = 'All';
 $_['text_theme']         = 'Themes';
 $_['text_marketplace']   = 'Marketplaces';
-$_['text_language']      = 'Languages';
-$_['text_payment']       = 'Payment';
+$_['text_language']      = 'Limbi';
+$_['text_payment']       = 'Plată';
 $_['text_shipping']      = 'Shipping';
 $_['text_module']        = 'Modules';
 $_['text_total']         = 'Order Totals';
 $_['text_feed']          = 'Feeds';
 $_['text_report']        = 'Reports';
-$_['text_other']         = 'Other';
+$_['text_other']         = 'Altele';
 $_['text_free']          = 'Free';
 $_['text_paid']          = 'Paid';
 $_['text_purchased']     = 'Cumpărat';
@@ -64,7 +64,7 @@ $_['button_view_all']    = 'View all extensions';
 $_['button_get_support'] = 'Get Support';
 $_['button_comment']     = 'Comment';
 $_['button_reply']       = 'Reply';
-$_['button_refresh']     = 'Refresh';
+$_['button_refresh']     = 'Reîmprospătează';
 
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify extensions!';
