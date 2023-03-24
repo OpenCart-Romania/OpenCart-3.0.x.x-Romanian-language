004dfdb30f9c0fbf1f56165ce17bb84fa6e1c6ab
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:00 +0800

    New translations shipping.php (Romanian)

--- upload/admin/language/ro-ro/extension/extension/shipping.php
+++ upload/admin/language/ro-ro/extension/extension/shipping.php
@@ -8,9 +8,9 @@ $_['text_list']         = 'Listă de Livrare';
 
 // Column
 $_['column_name']       = 'Metoda de Livrare';
-$_['column_status']     = 'Status';
-$_['column_sort_order'] = 'Sort Order';
-$_['column_action']     = 'Action';
+$_['column_status']     = 'Stare';
+$_['column_sort_order'] = 'Ordine de Sortare';
+$_['column_action']     = 'Acțiune';
 
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica livrarea!';
