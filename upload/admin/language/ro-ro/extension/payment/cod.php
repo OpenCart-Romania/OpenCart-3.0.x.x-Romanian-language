55ee2ff2dd2835b70587df669980fa825b8c5571
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:38 +0800

    New translations cod.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/cod.php
+++ upload/admin/language/ro-ro/extension/payment/cod.php
@@ -3,16 +3,16 @@
 $_['heading_title']      = 'Numerar La Livrare';
 
 // Text
-$_['text_extension']     = 'Extensions';
+$_['text_extension']     = 'Extensii';
 $_['text_success']       = 'Succes: Ai modificat modulul de plată Numerar La Livrare!';
 $_['text_edit']          = 'Editare Numerar La Livrare';
 
 // Entry
 $_['entry_total']        = 'Total';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_order_status'] = 'Stare Comandă';
 $_['entry_geo_zone']     = 'Geo Zone';
-$_['entry_status']       = 'Status';
-$_['entry_sort_order']   = 'Sort Order';
+$_['entry_status']       = 'Stare';
+$_['entry_sort_order']   = 'Ordine Sortare';
 
 // Help
 $_['help_total']         = 'The checkout total the order must reach before this payment method becomes active.';
