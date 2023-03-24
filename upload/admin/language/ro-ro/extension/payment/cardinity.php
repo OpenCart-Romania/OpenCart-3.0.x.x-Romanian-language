0b13417d730b1d55cf352b517ff300411d029a0e
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:43:05 +0800

    New translations cardinity.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/cardinity.php
+++ upload/admin/language/ro-ro/extension/payment/cardinity.php
@@ -3,7 +3,7 @@
 $_['heading_title']			= 'Cardinity';
 
 // Text
-$_['text_extension']		= 'Extensii';
+$_['text_extension']		= 'Extensions';
 $_['text_success']			= 'Succes: Ai modificat modulul de plată Cardinity!';
 $_['text_edit']             = 'Edit Cardinity';
 $_['text_cardinity']		= '<a href="http://cardinity.com/?crdp=opencart" target="_blank"><img src="view/image/payment/cardinity.png" alt="Cardinity" title="Cardinity" style="border: 1px solid #EEEEEE;" /></a>';
@@ -15,24 +15,24 @@ $_['text_confirm_refund']	= 'Are you sure you want to refund';
 $_['text_na']				= 'N/A';
 $_['text_success_action']	= 'Succes';
 $_['text_error_generic']	= 'Error: There was an error with your request. Please check the logs.';
-$_['text_amount']			= 'Valoare';
-$_['text_description']	    = 'Descriere';
+$_['text_amount']			= 'Amount';
+$_['text_description']	    = 'Description';
 
 // Column
-$_['column_refund']			= 'Stornare';
-$_['column_date']			= 'Dată';
+$_['column_refund']			= 'Refund';
+$_['column_date']			= 'Date';
 $_['column_refund_history'] = 'Refund History';
-$_['column_action']			= 'Acțiune';
-$_['column_status']			= 'Stare';
-$_['column_amount']			= 'Valoare';
-$_['column_description']	= 'Descriere';
+$_['column_action']			= 'Action';
+$_['column_status']			= 'Status';
+$_['column_amount']			= 'Amount';
+$_['column_description']	= 'Description';
 
 // Entry
 $_['entry_total']			= 'Total';
-$_['entry_order_status']	= 'Stare Comandă';
+$_['entry_order_status']	= 'Order Status';
 $_['entry_geo_zone']		= 'Geo Zone';
-$_['entry_status']			= 'Stare';
-$_['entry_sort_order']		= 'Ordine de Sortare';
+$_['entry_status']			= 'Status';
+$_['entry_sort_order']		= 'Sort Order';
 $_['entry_key']				= 'Key';
 $_['entry_secret']			= 'Secret';
 $_['entry_debug']			= 'Debug';
@@ -42,7 +42,7 @@ $_['help_debug']			= 'Enabling debug will write sensitive data to a log file. Yo
 $_['help_total']			= 'The checkout total the order must reach before this payment method becomes active.';
 
 // Button
-$_['button_refund']			= 'Stornare';
+$_['button_refund']			= 'Refund';
 
 // Error
 $_['error_key']				= 'Key Required!';
@@ -52,4 +52,4 @@ $_['error_php_version']		= 'Versiunea minimă a PHP 5.4.0 este necesară!';
 $_['error_permission']		= 'Atenție: Nu ai permisiunea de a modifica plata Cardinity!';
 $_['error_connection']		= 'There was a problem establishing a connection to the Cardinity API. Please check your Key and Secret settings.';
 $_['error_transaction']     = 'Unable to find transaction for this order.';
-$_['error_warning']			= 'Atenție: Te rugăm să verifici cu atenție formularul pentru erori!';
+$_['error_warning']			= 'Warning: Please check the form carefully for errors!';
