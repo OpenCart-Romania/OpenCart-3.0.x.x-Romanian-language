d156f65bc2e1b1ce020d5e0e3d1448973ade4f1b
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:27 +0800

    New translations marketplace.php (Romanian)

--- upload/admin/language/ro-ro/marketplace/marketplace.php
+++ upload/admin/language/ro-ro/marketplace/marketplace.php
@@ -5,35 +5,35 @@ $_['heading_title']      = 'Extension Marketplace';
 // Text
 $_['text_success']       = 'Success: You have modified extensions!';
 $_['text_list']          = 'Extension List';
-$_['text_filter']        = 'Filtru';
+$_['text_filter']        = 'Filter';
 $_['text_search']        = 'Search for extensions and themes';
-$_['text_category']      = 'Categorii';
+$_['text_category']      = 'Categories';
 $_['text_all']           = 'All';
 $_['text_theme']         = 'Themes';
 $_['text_marketplace']   = 'Marketplaces';
-$_['text_language']      = 'Limbi';
-$_['text_payment']       = 'Plată';
-$_['text_shipping']      = 'Livrare';
+$_['text_language']      = 'Languages';
+$_['text_payment']       = 'Payment';
+$_['text_shipping']      = 'Shipping';
 $_['text_module']        = 'Modules';
-$_['text_total']         = 'Totaluri Comandă';
-$_['text_feed']          = 'Feed-uri';
-$_['text_report']        = 'Rapoarte';
-$_['text_other']         = 'Altele';
+$_['text_total']         = 'Order Totals';
+$_['text_feed']          = 'Feeds';
+$_['text_report']        = 'Reports';
+$_['text_other']         = 'Other';
 $_['text_free']          = 'Free';
 $_['text_paid']          = 'Paid';
 $_['text_purchased']     = 'Cumpărat';
 $_['text_date_modified'] = 'Date Modified';
-$_['text_date_added']    = 'Data Adăugării';
-$_['text_rating']        = 'Evaluare';
+$_['text_date_added']    = 'Date Added';
+$_['text_rating']        = 'Rating';
 $_['text_reviews']       = 'recenzii';
 $_['text_compatibility'] = 'Compatibility';
 $_['text_downloaded']    = 'Descărcat';
 $_['text_member_since']  = 'Member since:';
-$_['text_price']         = 'Preț';
+$_['text_price']         = 'Price';
 $_['text_partner']       = 'Developed by OpenCart Partner';
 $_['text_support']       = '12 Luni suport gratuit';
 $_['text_documentation'] = 'Documentation Included';
-$_['text_sales']         = 'Vânzări';
+$_['text_sales']         = 'Sales';
 $_['text_comment']       = 'Comentarii';
 $_['text_download']      = 'Descarcă';
 $_['text_install']       = 'Installing';
@@ -42,29 +42,29 @@ $_['text_write']         = 'Write your comment here..';
 $_['text_purchase']      = 'Please confirm who you are!';
 $_['text_pin']           = 'Please enter your 4 digit PIN number. This PIN number is to protect your account.';
 $_['text_secure']        = 'Do not give your PIN to any one including developers. If you require an extension seller to install an extension for you then you should email them the required extension.';
-$_['text_name']          = 'Nume Descărcare';
-$_['text_progress']      = 'Progres';
+$_['text_name']          = 'Download Name';
+$_['text_progress']      = 'Progress';
 $_['text_available']     = 'Available Installs';
-$_['text_action']        = 'Acțiune';
+$_['text_action']        = 'Action';
 $_['text_see_more']      = 'see more replies...';
 
 // Entry
 $_['entry_pin']          = 'PIN';
 
 // Tab
-$_['tab_description']    = 'Descriere';
-$_['tab_documentation']  = 'Documentație';
-$_['tab_download']       = 'Descărcare';
-$_['tab_comment']        = 'Comentariu';
+$_['tab_description']    = 'Description';
+$_['tab_documentation']  = 'Documentation';
+$_['tab_download']       = 'Download';
+$_['tab_comment']        = 'Comment';
 
 // Button
 $_['button_opencart']    = 'Marketplace API';
 $_['button_purchase']    = 'Purchase';
 $_['button_view_all']    = 'View all extensions';
 $_['button_get_support'] = 'Get Support';
-$_['button_comment']     = 'Comentariu';
+$_['button_comment']     = 'Comment';
 $_['button_reply']       = 'Reply';
-$_['button_refresh']     = 'Reîmprospătează';
+$_['button_refresh']     = 'Refresh';
 
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify extensions!';
