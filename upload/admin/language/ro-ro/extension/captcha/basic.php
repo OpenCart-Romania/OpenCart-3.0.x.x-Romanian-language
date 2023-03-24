263894af1270c01cd9961a15627d1b6e03f09496
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:28:39 +0800

    New translations basic.php (Romanian)

--- upload/admin/language/ro-ro/extension/captcha/basic.php
+++ upload/admin/language/ro-ro/extension/captcha/basic.php
@@ -3,12 +3,12 @@
 $_['heading_title']    = 'Basic Captcha';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Succes: Ai modificat Basic Captcha!';
 $_['text_edit']        = 'Editare Basic Captcha';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica Basic Captcha!';
