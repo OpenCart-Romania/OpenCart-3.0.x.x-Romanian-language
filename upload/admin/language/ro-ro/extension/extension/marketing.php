f3daecf62d984a1520b8ec6749cc4ad114113b33
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:42:21 +0800

    New translations marketing.php (Romanian)

--- upload/admin/language/ro-ro/extension/extension/marketing.php
+++ upload/admin/language/ro-ro/extension/extension/marketing.php
@@ -4,12 +4,12 @@ $_['heading_title']    = 'Marketing';
 
 // Text
 $_['text_success']     = 'Succes: Ai modificat marketing-ul!';
-$_['text_list']        = 'Listă Analytics';
+$_['text_list']        = 'Analytics List';
 
 // Column
 $_['column_name']      = 'Nume Marketing';
-$_['column_status']    = 'Stare';
-$_['column_action']    = 'Acțiune';
+$_['column_status']    = 'Status';
+$_['column_action']    = 'Action';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica marketing-ul!';
