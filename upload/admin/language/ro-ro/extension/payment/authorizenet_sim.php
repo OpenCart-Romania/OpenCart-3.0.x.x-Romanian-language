469a2f3f582dfe5f5798e1aa90a82457a7d73f76
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:43:00 +0800

    New translations authorizenet_sim.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/authorizenet_sim.php
+++ upload/admin/language/ro-ro/extension/payment/authorizenet_sim.php
@@ -3,7 +3,7 @@
 $_['heading_title']			= 'Authorize.Net (SIM)';
 
 // Text
-$_['text_extension']		= 'Extensii';
+$_['text_extension']		= 'Extensions';
 $_['text_success']			= 'Succes: Ai modificat detaliile contului Authorize.Net (SIM)!';
 $_['text_edit']             = 'Edit Authorize.Net (SIM)';
 $_['text_authorizenet_sim']	= '<a href="https://account.authorize.net/signUpNow?resellerID=26357" target="_BLANK"><img src="view/image/payment/authorizenet.png" alt="Authorize.Net" title="Authorize.Net" style="border: 1px solid #EEEEEE;" /></a>';
@@ -15,10 +15,10 @@ $_['entry_callback']		= 'Relay Response URL';
 $_['entry_md5']				= 'Signature Key/Hash';
 $_['entry_test']			= 'Test Mode';
 $_['entry_total']			= 'Total';
-$_['entry_order_status']	= 'Stare Comandă';
+$_['entry_order_status']	= 'Order Status';
 $_['entry_geo_zone']		= 'Geo Zone';
-$_['entry_status']			= 'Stare';
-$_['entry_sort_order']		= 'Ordine de Sortare';
+$_['entry_status']			= 'Status';
+$_['entry_sort_order']		= 'Sort Order';
 
 // Help
 $_['help_callback']			= 'Please login and set this at <a href="https://secure.authorize.net" target="_blank" class="txtLink">https://secure.authorize.net</a>.';
@@ -26,6 +26,6 @@ $_['help_md5']				= 'The Hash feature enables you to authenticate that a transac
 $_['help_total']			= 'The checkout total the order must reach before this payment method becomes active.';
 
 // Error
-$_['error_permission']		= 'Atenție: Nu ai permisiunea de a modifica plata Authorize.Net (SIM)!';
+$_['error_permission']		= 'Warning: You do not have permission to modify payment Authorize.Net (SIM)!';
 $_['error_merchant']		= 'ID Comerciant Necesar!';
 $_['error_key']				= 'Transaction Key Required!';
