1a2d63a88b994199d25be13e2178936c6c7ee0f5
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:36 +0800

    New translations voucher.php (Romanian)

--- upload/admin/language/ro-ro/sale/voucher.php
+++ upload/admin/language/ro-ro/sale/voucher.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']     = 'Vouchere Cadou';
+$_['heading_title']     = 'Gift Vouchers';
 
 // Text
 $_['text_success']      = 'Success: You have modified vouchers!';
@@ -13,13 +13,13 @@ $_['column_name']       = 'Voucher Name';
 $_['column_code']       = 'Code';
 $_['column_from']       = 'From';
 $_['column_to']         = 'To';
-$_['column_theme']      = 'Temă';
-$_['column_amount']     = 'Valoare';
-$_['column_status']     = 'Stare';
-$_['column_order_id']   = 'Număr Comandă';
-$_['column_customer']   = 'Client';
-$_['column_date_added'] = 'Data Adăugării';
-$_['column_action']     = 'Acțiune';
+$_['column_theme']      = 'Theme';
+$_['column_amount']     = 'Amount';
+$_['column_status']     = 'Status';
+$_['column_order_id']   = 'Order ID';
+$_['column_customer']   = 'Customer';
+$_['column_date_added'] = 'Date Added';
+$_['column_action']     = 'Action';
 
 // Entry
 $_['entry_code']        = 'Code';
@@ -27,10 +27,10 @@ $_['entry_from_name']   = 'From Name';
 $_['entry_from_email']  = 'From E-Mail';
 $_['entry_to_name']     = 'To Name';
 $_['entry_to_email']    = 'To E-Mail';
-$_['entry_theme']       = 'Temă';
-$_['entry_message']     = 'Mesaj';
-$_['entry_amount']      = 'Valoare';
-$_['entry_status']      = 'Stare';
+$_['entry_theme']       = 'Theme';
+$_['entry_message']     = 'Message';
+$_['entry_amount']      = 'Amount';
+$_['entry_status']      = 'Status';
 
 // Help
 $_['help_code']         = 'The code the customer enters to activate the voucher.';
@@ -41,6 +41,6 @@ $_['error_exists']      = 'Warning: Voucher code is already in use!';
 $_['error_code']        = 'Code must be between 3 and 10 characters!';
 $_['error_to_name']     = 'Numele Destinatarului trebuie să fie între 1 și 64 de caractere!';
 $_['error_from_name']   = 'Numele Tău trebuie să fie între 1 și 64 de caractere!';
-$_['error_email']       = 'Adresa E-Mail nu pare să fie validă!';
+$_['error_email']       = 'E-Mail Address does not appear to be valid!';
 $_['error_amount']      = 'Cantitatea trebuie să fie mai mare sau egală cu 1!';
 $_['error_order']       = 'Warning: This voucher cannot be deleted as it is part of an <a href="%s">order</a>!';
