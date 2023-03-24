35e1a4486d28192998bb5871728fceda410a8c7e
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:41 +0800

    New translations upload.php (Romanian)

--- upload/admin/language/ro-ro/tool/upload.php
+++ upload/admin/language/ro-ro/tool/upload.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']     = 'Încărcări';
+$_['heading_title']     = 'Uploads';
 
 // Text
 $_['text_success']      = 'Success: You have modified uploads!';
@@ -8,14 +8,14 @@ $_['text_list']         = 'Upload List';
 
 // Column
 $_['column_name']       = 'Upload Name';
-$_['column_filename']   = 'Nume Fișier';
-$_['column_date_added'] = 'Data Adăugării';
-$_['column_action']     = 'Acțiune';
+$_['column_filename']   = 'Filename';
+$_['column_date_added'] = 'Date Added';
+$_['column_action']     = 'Action';
 
 // Entry
 $_['entry_name']        = 'Upload Name';
-$_['entry_filename']    = 'Nume Fișier';
-$_['entry_date_added']  = 'Data Adăugării';
+$_['entry_filename']    = 'Filename';
+$_['entry_date_added']  = 'Date Added';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify uploads!';
