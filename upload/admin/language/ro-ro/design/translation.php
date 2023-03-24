92bb115c4d5e7fb25494e13b73529a12645d4032
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:28:36 +0800

    New translations translation.php (Romanian)

--- upload/admin/language/ro-ro/design/translation.php
+++ upload/admin/language/ro-ro/design/translation.php
@@ -7,25 +7,25 @@ $_['text_success']     = 'Succes: Ai modificat editorul de limbă!';
 $_['text_list']        = 'Translation List';
 $_['text_edit']        = 'Editare Traducere';
 $_['text_add']         = 'Adaugă Traducere';
-$_['text_default']     = 'Default';
+$_['text_default']     = 'Implicit';
 $_['text_store']       = 'Store';
 $_['text_language']    = 'Language';
 
 // Column
 $_['column_store']     = 'Store';
-$_['column_language']  = 'Language';
+$_['column_language']  = 'Limbă';
 $_['column_route']     = 'Route';
 $_['column_key']       = 'Key';
 $_['column_value']     = 'Value';
-$_['column_action']    = 'Action';
+$_['column_action']    = 'Acțiune';
 
 // Entry
-$_['entry_store']      = 'Store';
+$_['entry_store']      = 'Magazin';
 $_['entry_language']   = 'Language';
-$_['entry_route']      = 'Route';
+$_['entry_route']      = 'Rută';
 $_['entry_key']        = 'Key';
 $_['entry_default']    = 'Default';
-$_['entry_value']      = 'Value';
+$_['entry_value']      = 'Valoare';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica editorul de limbă!';
