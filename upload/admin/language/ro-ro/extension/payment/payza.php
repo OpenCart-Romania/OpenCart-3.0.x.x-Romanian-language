11b9f7094909bd83742879a6fc86d96ec6206b08
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:55 +0800

    New translations payza.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/payza.php
+++ upload/admin/language/ro-ro/extension/payment/payza.php
@@ -3,7 +3,7 @@
 $_['heading_title']		 = 'Payza';
 
 // Text
-$_['text_extension']	 = 'Extensions';
+$_['text_extension']	 = 'Extensii';
 $_['text_success']		 = 'Success: You have modified Payza account details!';
 $_['text_edit']          = 'Edit Payza';
 
@@ -12,10 +12,10 @@ $_['entry_merchant']	 = 'Merchant ID';
 $_['entry_security']	 = 'Security Code';
 $_['entry_callback']	 = 'Alert URL';
 $_['entry_total']		 = 'Total';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_order_status'] = 'Stare Comandă';
 $_['entry_geo_zone']	 = 'Geo Zone';
-$_['entry_status']		 = 'Status';
-$_['entry_sort_order']	 = 'Sort Order';
+$_['entry_status']		 = 'Stare';
+$_['entry_sort_order']	 = 'Ordine Sortare';
 
 // Help
 $_['help_callback']		 = 'This has to be set in the Payza control panel. You will also need to check the "IPN Status" to enabled.';
@@ -23,5 +23,5 @@ $_['help_total']		 = 'The checkout total the order must reach before this paymen
 
 // Error
 $_['error_permission']	 = 'Atenție: Nu ai permisiunea de a modifica plata Payza!';
-$_['error_merchant']	 = 'Merchant ID Required!';
+$_['error_merchant']	 = 'ID Comerciant Necesar!';
 $_['error_security']	 = 'Security Code Required!';
