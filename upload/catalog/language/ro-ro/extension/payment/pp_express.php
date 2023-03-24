0aec3db3aeb0228b0f99c6278c087c311c2e975f
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:49:08 +0800

    New translations pp_express.php (Romanian)

--- upload/catalog/language/ro-ro/extension/payment/pp_express.php
+++ upload/catalog/language/ro-ro/extension/payment/pp_express.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['express_text_title']      = 'Confirmare comandă';
+$_['express_text_title']      = 'Confirm order';
 
 // Text
 $_['text_title']              = 'PayPal Express Checkout';
-$_['text_cart']               = 'Coș de Cumpărături';
+$_['text_cart']               = 'Shopping Cart';
 $_['text_shipping_updated']   = 'Shipping service updated';
-$_['text_trial']              = '%s la fiecare %s %s pentru %s plați atunci ';
-$_['text_recurring']          = '%s la fiecare %s %s';
-$_['text_recurring_item']     = 'Articol Recurent';
+$_['text_trial']              = '%s every %s %s for %s payments then ';
+$_['text_recurring']          = '%s every %s %s';
+$_['text_recurring_item']     = 'Recurring Item';
 $_['text_length']             = ' for %s payments';
 
 // Entry
@@ -16,7 +16,7 @@ $_['express_entry_coupon']    = 'Introdu cuponul tău aici:';
 
 // Button
 $_['button_express_coupon']   = 'Add';
-$_['button_express_confirm']  = 'Confirma';
+$_['button_express_confirm']  = 'Confirm';
 $_['button_express_login']    = 'Continue to PayPal';
 $_['button_express_shipping'] = 'Update shipping';
 
@@ -24,4 +24,4 @@ $_['button_express_shipping'] = 'Update shipping';
 $_['error_heading_title']	  = 'There was an error';
 $_['error_too_many_failures'] = 'Your payment has failed too many times';
 $_['error_unavailable'] 	  = 'Please use the full checkout with this order';
-$_['error_no_shipping']    	  = 'Atenție: Nu sunt disponibile opțiuni de livrare. Te rugăm <a href="%s">contactează-ne</a> pentru asistență!';
+$_['error_no_shipping']    	  = 'Warning: No Shipping options are available. Please <a href="%s">contact us</a> for assistance!';
