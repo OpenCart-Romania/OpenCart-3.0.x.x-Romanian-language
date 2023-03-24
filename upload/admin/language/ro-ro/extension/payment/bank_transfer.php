5811c74377db002b19b7ac28232f42c94a8acf91
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:43:01 +0800

    New translations bank_transfer.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/bank_transfer.php
+++ upload/admin/language/ro-ro/extension/payment/bank_transfer.php
@@ -1,19 +1,19 @@
 <?php
 // Heading
-$_['heading_title']      = 'Transfer Bancar';
+$_['heading_title']      = 'Bank Transfer';
 
 // Text
-$_['text_extension']     = 'Extensii';
+$_['text_extension']     = 'Extensions';
 $_['text_success']       = 'Succes: Ai modificat detaliile transferului bancar!';
 $_['text_edit']          = 'Editează Transfer Bancar';
 
 // Entry
 $_['entry_bank']         = 'Instrucţiuni Transfer Bancar';
 $_['entry_total']        = 'Total';
-$_['entry_order_status'] = 'Stare Comandă';
+$_['entry_order_status'] = 'Order Status';
 $_['entry_geo_zone']     = 'Geo Zone';
-$_['entry_status']       = 'Stare';
-$_['entry_sort_order']   = 'Ordine de Sortare';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order';
 
 // Help
 $_['help_total']         = 'The checkout total the order must reach before this payment method becomes active.'; 
