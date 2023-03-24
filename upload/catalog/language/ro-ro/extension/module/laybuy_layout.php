654f7c4337549c4ef04532b77c0c7ca029e444db
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:48:41 +0800

    New translations laybuy_layout.php (Romanian)

--- upload/catalog/language/ro-ro/extension/module/laybuy_layout.php
+++ upload/catalog/language/ro-ro/extension/module/laybuy_layout.php
@@ -3,11 +3,11 @@
 $_['heading_title']            = 'Informații Lay-Buy';
 
 // Text
-$_['text_reference_info']      = 'Informații Referință';
+$_['text_reference_info']      = 'Reference Information';
 $_['text_laybuy_ref_no']       = 'Număr de Referință Lay-Buy:';
 $_['text_paypal_profile_id']   = 'PayPal Profile ID:';
 $_['text_payment_plan']        = 'Payment Plan';
-$_['text_status']              = 'Stare:';
+$_['text_status']              = 'Status:';
 $_['text_amount']              = 'Amount:';
 $_['text_downpayment_percent'] = 'Down Payment Percent:';
 $_['text_months']              = 'Luni:';
@@ -16,11 +16,11 @@ $_['text_payment_amounts']     = 'Payment Amounts:';
 $_['text_first_payment_due']   = 'First Payment Due:';
 $_['text_last_payment_due']    = 'Last Payment Due:';
 $_['text_downpayment']         = 'Down Payment';
-$_['text_month']               = 'Lună';
+$_['text_month']               = 'Month';
 
 // Column
 $_['column_instalment']        = 'Instalment';
-$_['column_amount']            = 'Valoare';
-$_['column_date']              = 'Dată';
-$_['column_pp_trans_id']       = 'ID Tranzacție PayPal';
-$_['column_status']            = 'Stare';
+$_['column_amount']            = 'Amount';
+$_['column_date']              = 'Date';
+$_['column_pp_trans_id']       = 'PayPal Transaction ID';
+$_['column_status']            = 'Status';
