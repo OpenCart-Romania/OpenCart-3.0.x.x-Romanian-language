e65c2d69340e82ccf18dff088432a29e03866562
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:21 +0800

    New translations laybuy_layout.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/laybuy_layout.php
+++ upload/admin/language/ro-ro/extension/module/laybuy_layout.php
@@ -3,12 +3,12 @@
 $_['heading_title']    = 'Layout Lay-Buy';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Succes: Ai modificat modulul Layout Lay-Buy!';
 $_['text_edit']        = 'Editare Modul Layout Lay-Buy';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul Layout Lay-Buy!';
