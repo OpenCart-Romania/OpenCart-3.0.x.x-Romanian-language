9c90c20d5211db2abfc1c207dd4a9da894f972b3
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:21 +0800

    New translations latest.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/latest.php
+++ upload/admin/language/ro-ro/extension/module/latest.php
@@ -3,19 +3,19 @@
 $_['heading_title']    = 'Latest';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Succes: Ai modificat modulul cel mai recent!';
 $_['text_edit']        = 'Edit Latest Module';
 
 // Entry
 $_['entry_name']       = 'Module Name';
 $_['entry_limit']      = 'Limit';
-$_['entry_width']      = 'Width';
-$_['entry_height']     = 'Height';
-$_['entry_status']     = 'Status';
+$_['entry_width']      = 'Lăţime';
+$_['entry_height']     = 'Înălţime';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul cele mai recente!';
-$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_name']       = 'Numele Modulului trebuie să fie între 3 și 64 de caractere!';
 $_['error_width']      = 'Width required!';
 $_['error_height']     = 'Height required!';
