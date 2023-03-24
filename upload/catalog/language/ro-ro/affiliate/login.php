9a5982b35bdb301f18e06a8da0c6fd495ad9c563
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:48:03 +0800

    New translations login.php (Romanian)

--- upload/catalog/language/ro-ro/affiliate/login.php
+++ upload/catalog/language/ro-ro/affiliate/login.php
@@ -3,20 +3,20 @@
 $_['heading_title']                 = 'Program de Afiliere';
 
 // Text
-$_['text_account']                  = 'Cont';
-$_['text_login']                    = 'Conectare';
+$_['text_account']                  = 'Account';
+$_['text_login']                    = 'Login';
 $_['text_description']              = '<p>Programul de afiliere %s este gratuit și permite membrilor să câștige venituri prin plasarea unui link sau unor link-uri pe site-ul lor web care promovează %s sau produse specifice. Orice vânzare efectuată către clienții care au făcut click pe aceste link-uri va câștiga comisionul de afiliere. Rata standard a comisionului este în prezent %s.</p><p>Pentru mai multe informații, vizitați pagina noastră de întrebări frecvente sau consultați termenii &amp; condițiile de afiliere</p>';
 $_['text_new_affiliate']            = 'Afiliat Nou';
 $_['text_register_account']         = '<p>Nu sunt afiliat în prezent.</p><p>Faceți click pe Continuă mai jos pentru a crea un nou cont de afiliat. Te rugam sa reții ca acesta nu este conectat in nici un fel la contul tău de client.</p>';
 $_['text_returning_affiliate']      = 'Autentificare Afiliat';
 $_['text_i_am_returning_affiliate'] = 'Sunt un afiliat întors.';
-$_['text_forgotten']                = 'Parolă Uitată';
+$_['text_forgotten']                = 'Forgotten Password';
 
 // Entry
 $_['entry_email']                   = 'E-Mail Afiliat';
-$_['entry_password']                = 'Parolă';
+$_['entry_password']                = 'Password';
 
 // Error
-$_['error_login']                   = 'Atenție: Adresa de E-Mail și/sau Parola nu se potrivesc.';
-$_['error_attempts']                = 'Atenție: Contul tău a depășit numărul permis de încercări de conectare. Te rugăm să încerci din nou în 1 oră.';
-$_['error_approved']                = 'Atenție: Contul tău necesită aprobare înainte de a te putea autentifica.';
+$_['error_login']                   = 'Warning: No match for E-Mail Address and/or Password.';
+$_['error_attempts']                = 'Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour.';
+$_['error_approved']                = 'Warning: Your account requires approval before you can login.';
