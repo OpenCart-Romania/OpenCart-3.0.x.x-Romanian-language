2210746e07b9b88a29f1244efd7b86eea96ebca8
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:02 +0800

    New translations total.php (Romanian)

--- upload/admin/language/ro-ro/extension/extension/total.php
+++ upload/admin/language/ro-ro/extension/extension/total.php
@@ -8,9 +8,9 @@ $_['text_list']         = 'Order Total List';
 
 // Column
 $_['column_name']       = 'Order Totals';
-$_['column_status']     = 'Status';
-$_['column_sort_order'] = 'Sort Order';
-$_['column_action']     = 'Action';
+$_['column_status']     = 'Stare';
+$_['column_sort_order'] = 'Ordine de Sortare';
+$_['column_action']     = 'Acțiune';
 
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica totalurile!';
