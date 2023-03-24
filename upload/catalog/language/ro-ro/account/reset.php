5431c7efa2440d9a0cfb3dd16d95c3e7626a2c18
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:31:49 +0800

    New translations reset.php (Romanian)

--- upload/catalog/language/ro-ro/account/reset.php
+++ upload/catalog/language/ro-ro/account/reset.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']  = 'Reset your password';
+$_['heading_title']  = 'Resetează-ți parola';
 
 // Text
-$_['text_account']   = 'Account';
-$_['text_password']  = 'Enter the new password you wish to use.';
-$_['text_success']   = 'Success: Your password has been successfully updated.';
+$_['text_account']   = 'Cont';
+$_['text_password']  = 'Introdu noua parolă pe care dorești să o utilizezi.';
+$_['text_success']   = 'Succes: Parola ta a fost actualizată cu succes.';
 
 // Entry
-$_['entry_password'] = 'Password';
-$_['entry_confirm']  = 'Confirm';
+$_['entry_password'] = 'Parolă';
+$_['entry_confirm']  = 'Confirma';
 
 // Error
-$_['error_password'] = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']  = 'Password and password confirmation do not match!';
+$_['error_password'] = 'Parola trebuie să fie între 4 și 20 de caractere!';
+$_['error_confirm']  = 'Parola și confirmarea parolei nu se potrivesc!';
 $_['error_code']     = 'Codul de resetare a parolei este incorect sau a fost utilizat anterior!';
