65ececdb02177c1d7fb89ce9de728b979369ab97
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:28:40 +0800

    New translations google.php (Romanian)

--- upload/admin/language/ro-ro/extension/captcha/google.php
+++ upload/admin/language/ro-ro/extension/captcha/google.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'Google reCAPTCHA';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Succes: Ai modificat Google reCAPTCHA!';
 $_['text_edit']        = 'Editare Google reCAPTCHA';
 $_['text_signup']      = 'Accesează <a href="https://www.google.com/recaptcha/intro/index.html" target="_blank"><u>pagina Google reCAPTCHA</u></a> si înregistrează-te cu website-ul tău.';
@@ -11,7 +11,7 @@ $_['text_signup']      = 'Accesează <a href="https://www.google.com/recaptcha/i
 // Entry
 $_['entry_key']        = 'Site key';
 $_['entry_secret']     = 'Secret key';
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica Google reCAPTCHA!';
