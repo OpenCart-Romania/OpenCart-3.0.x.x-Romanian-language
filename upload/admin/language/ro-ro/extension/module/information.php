9861be3b3af7eff26f160302fa053d7f34b97548
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:42:46 +0800

    New translations information.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/information.php
+++ upload/admin/language/ro-ro/extension/module/information.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']    = 'Informații';
+$_['heading_title']    = 'Information';
 
 // Text
-$_['text_extension']   = 'Extensii';
+$_['text_extension']   = 'Extensions';
 $_['text_success']     = 'Succes: Ai modificat modulul de informare!';
 $_['text_edit']        = 'Editare Modul Informații';
 
 // Entry
-$_['entry_status']     = 'Stare';
+$_['entry_status']     = 'Status';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul de informații!';
