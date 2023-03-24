64336bb43739b4f1e100b818a8df283680f407ed
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:41:55 +0800

    New translations customer.php (Romanian)

--- upload/admin/language/ro-ro/customer/customer.php
+++ upload/admin/language/ro-ro/customer/customer.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']             = 'Clienți';
+$_['heading_title']             = 'Customers';
 
 // Text
 $_['text_success']              = 'Succes: Ai modificat clienții!';
 $_['text_list']                 = 'Listă Clienți';
 $_['text_add']                  = 'Adaugă Client';
 $_['text_edit']                 = 'Editare Client';
-$_['text_default']              = 'Implicit';
+$_['text_default']              = 'Default';
 $_['text_account']              = 'Detalii Client';
-$_['text_password']             = 'Parolă';
+$_['text_password']             = 'Password';
 $_['text_other']                = 'Altele';
 $_['text_affiliate']            = 'Detalii Afiliat';
 $_['text_payment']              = 'Detalii Plată';
@@ -21,39 +21,39 @@ $_['text_history']              = 'Istoric';
 $_['text_history_add']          = 'Adaugă Istoric';
 $_['text_transaction']          = 'Tranzacții';
 $_['text_transaction_add']      = 'Adaugă Tranzacție';
-$_['text_reward']               = 'Puncte de Recompensă';
+$_['text_reward']               = 'Reward Points';
 $_['text_reward_add']           = 'Adaugă Puncte de Recompensă';
 $_['text_ip']                   = 'IP';
-$_['text_option']               = 'Opţiuni';
+$_['text_option']               = 'Options';
 $_['text_login']                = 'Autentificare în Magazin';
 $_['text_unlock']               = 'Deblocare Cont';
 
 // Column
 $_['column_name']               = 'Numele Clientului';
 $_['column_email']              = 'E-Mail';
-$_['column_customer_group']     = 'Grupul de Clienți';
-$_['column_status']             = 'Stare';
-$_['column_date_added']         = 'Data Adăugării';
+$_['column_customer_group']     = 'Customer Group';
+$_['column_status']             = 'Status';
+$_['column_date_added']         = 'Date Added';
 $_['column_comment']            = 'Comentariu';
-$_['column_description']        = 'Descriere';
+$_['column_description']        = 'Description';
 $_['column_amount']             = 'Valoare';
-$_['column_points']             = 'Puncte';
+$_['column_points']             = 'Points';
 $_['column_ip']                 = 'IP';
 $_['column_total']              = 'Total Conturi';
-$_['column_action']             = 'Acțiune';
+$_['column_action']             = 'Action';
 
 // Entry
-$_['entry_customer_group']      = 'Grupul de Clienți';
-$_['entry_firstname']           = 'Prenume';
-$_['entry_lastname']            = 'Nume de Familie';
+$_['entry_customer_group']      = 'Customer Group';
+$_['entry_firstname']           = 'First Name';
+$_['entry_lastname']            = 'Last Name';
 $_['entry_email']               = 'E-Mail';
 $_['entry_telephone']           = 'Telefon';
 $_['entry_newsletter']          = 'Newsletter';
-$_['entry_status']              = 'Stare';
+$_['entry_status']              = 'Status';
 $_['entry_approved']            = 'Aprobat';
 $_['entry_safe']                = 'Sigur';
-$_['entry_password']            = 'Parolă';
-$_['entry_confirm']             = 'Confirma';
+$_['entry_password']            = 'Password';
+$_['entry_confirm']             = 'Confirm';
 $_['entry_company']             = 'Companie';
 $_['entry_address_1']           = 'Adresa 1';
 $_['entry_address_2']           = 'Adresa 2';
@@ -62,7 +62,7 @@ $_['entry_postcode']            = 'Cod poștal';
 $_['entry_country']             = 'Țară';
 $_['entry_zone']                = 'Regiune / Stat';
 $_['entry_default']             = 'Adresa Implicită';
-$_['entry_affiliate']           = 'Afiliat';
+$_['entry_affiliate']           = 'Affiliate';
 $_['entry_tracking']            = 'Cod de Urmărire';
 $_['entry_website']             = 'Web Site';
 $_['entry_commission']          = 'Comision (%)';
@@ -75,13 +75,13 @@ $_['entry_bank_branch_number']  = 'Număr ABA/BSB (Număr Filială)';
 $_['entry_bank_swift_code']     = 'Cod SWIFT';
 $_['entry_bank_account_name']   = 'Nume Cont';
 $_['entry_bank_account_number'] = 'Număr Cont';
-$_['entry_comment']             = 'Comentariu';
-$_['entry_description']         = 'Descriere';
-$_['entry_amount']              = 'Valoare';
-$_['entry_points']              = 'Puncte';
-$_['entry_name']                = 'Numele Clientului';
+$_['entry_comment']             = 'Comment';
+$_['entry_description']         = 'Description';
+$_['entry_amount']              = 'Amount';
+$_['entry_points']              = 'Points';
+$_['entry_name']                = 'Customer Name';
 $_['entry_ip']                  = 'IP';
-$_['entry_date_added']          = 'Data Adăugării';
+$_['entry_date_added']          = 'Date Added';
 
 // Help
 $_['help_safe']                 = 'Setați cu "adevărat" pentru a evita ca acest client să fie prins de sistemul anti-fraudă';
@@ -91,15 +91,15 @@ $_['help_commission']           = 'Procentul pe care afiliatul îl primește la 
 $_['help_points']               = 'Utilizați minus pentru a elimina punctele';
 
 // Error
-$_['error_warning']             = 'Atenție: Te rugăm să verifici cu atenție formularul pentru erori!';
+$_['error_warning']             = 'Warning: Please check the form carefully for errors!';
 $_['error_permission']          = 'Atenție: Nu ai permisiunea de a modifica clienții!';
-$_['error_exists']              = 'Atenție: Adresa de E-Mail este deja înregistrată!';
-$_['error_firstname']           = 'Prenumele trebuie să fie între 1 și 32 de caractere!';
-$_['error_lastname']            = 'Numele de Familie trebuie să fie între 1 și 32 de caractere!';
-$_['error_email']               = 'Adresa E-Mail nu pare să fie validă!';
+$_['error_exists']              = 'Warning: E-Mail Address is already registered!';
+$_['error_firstname']           = 'First Name must be between 1 and 32 characters!';
+$_['error_lastname']            = 'Last Name must be between 1 and 32 characters!';
+$_['error_email']               = 'E-Mail Address does not appear to be valid!';
 $_['error_telephone']           = 'Telefonul trebuie să fie între 3 și 32 de caractere!';
-$_['error_password']            = 'Parola trebuie să fie între 4 și 20 de caractere!';
-$_['error_confirm']             = 'Parola și confirmarea parolei nu se potrivesc!';
+$_['error_password']            = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']             = 'Password and password confirmation do not match!';
 $_['error_address_1']           = 'Adresa 1 trebuie să fie între 3 și 128 de caractere!';
 $_['error_city']                = 'Orașul trebuie să fie între 2 și 128 de caractere!';
 $_['error_postcode']            = 'Cod poștal trebuie să fie între 2 și 10 caractere pentru această țară!';
