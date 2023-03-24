3bdb3834c42173a03a5549e626a6f103ae07c69b
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:42:04 +0800

    New translations google.php (Romanian)

--- upload/admin/language/ro-ro/extension/advertise/google.php
+++ upload/admin/language/ro-ro/extension/advertise/google.php
@@ -36,7 +36,7 @@ $_['help_size_system']                      = 'With this attribute you can expla
 $_['help_size_type']                        = 'Use this attribute to describe the cut of your product. This information helps create accurate filters, which users can use to narrow search results.';
 
 // Entry
-$_['entry_action']                          = 'Acțiune';
+$_['entry_action']                          = 'Action';
 $_['entry_adult']                           = 'Adult-Only Content';
 $_['entry_age_group']                       = 'Age Group';
 $_['entry_auto_advertise']                  = 'Automatically advertise new listings?';
@@ -59,7 +59,7 @@ $_['entry_setup_confirmation']              = 'Setup Confirmation';
 $_['entry_size']                            = 'Size Option';
 $_['entry_size_system']                     = 'Size System';
 $_['entry_size_type']                       = 'Size Type';
-$_['entry_status']                          = 'Stare';
+$_['entry_status']                          = 'Status';
 
 // Texts
 $_['text_access_token']                     = 'Access token';
@@ -67,7 +67,7 @@ $_['text_acknowledge_add_campaign_1']       = 'Am luat la cunoștință că, cam
 $_['text_acknowledge_add_campaign_2']       = 'Am luat la cunoștință că campaniile mele nu vor deveni active până când nu voi configura <a href="https://support.google.com/merchants/topic/7293661?hl=en-GB&ref_topic=7259125" target="_blank"><strong>Livrare</strong></a> și <a href="https://support.google.com/merchants/topic/7294266?hl=en-GB&ref_topic=7259125" target="_blank"><strong>Taxe (doar în US)</strong></a> pentru contul meu Merchant Center.';
 $_['text_acknowledge_cron']                 = 'I confirm that I have set up an automated CRON task using one of the methods above.';
 $_['text_acknowledge_merchant_tos']         = 'By purchasing Google Shopping ads, I agree to comply with Google&apos;s terms and policies, including Google&apos;s <a href="https://support.google.com/merchants/answer/160173?hl=en" target="_blank">Merchant Center terms of service</a>, <a href="https://support.google.com/merchants/answer/6149970?hl=en" target="_blank">Shopping ads policies</a>, and <a href="https://billing.google.com/payments/u/0/paymentsinfofinder?style=:md#" target="_blank">Google Ads Terms and Conditions</a>.';
-$_['text_action']                           = 'Acțiune';
+$_['text_action']                           = 'Action';
 $_['text_active']                           = 'Activ';
 $_['text_active_states']                    = 'Select active states';
 $_['text_add_target']                       = 'New Campaign';
@@ -81,7 +81,7 @@ $_['text_age_group_toddler']                = 'Toddler (1-5 years old)';
 $_['text_all']                              = 'All';
 $_['text_app_id']                           = 'App ID';
 $_['text_app_secret']                       = 'App Secret';
-$_['text_approved']                         = 'Aprobat';
+$_['text_approved']                         = 'Approved';
 $_['text_campaign_more_info']               = '<h4>Campaign Duration</h4><p>Campaigns will run until paused. You can pause a campaign at any time.</p><hr /><h4>Campaign Optimization</h4><p>It usually takes around 30 days for Google to rank products and optimize shopping ad campaigns.</p><hr /><h4>Products in Campaign</h4><p>Google will create a unique shopping ad for each approved product synced to your Merchant Center account. Ads are optimized based to maximize your sales. Popular products will likely receive more of your budget.</p><hr /><h4>Campaign Duration</h4><p>Campaigns will run until paused. You can pause a campaign at any time.</p><hr /><h4>Shopping Ad Placement</h4><p>Your ads may appear on multiple platforms including Google Search, Google Display Network, Youtube, and Gmail.</p>';
 $_['text_campaigns']                        = 'Campaigns';
 $_['text_carrier_postcode']                 = 'Origin Postal Code';
@@ -111,27 +111,27 @@ $_['text_cron_info']                        = 'Please make sure to set up a CRON
 $_['text_cron_settings']                    = 'CRON Settings';
 $_['text_data_quality_issues']              = 'Data Quality Issues';
 $_['text_debug_log']                        = 'Debug Logging';
-$_['text_destination_status']               = 'Stare';
-$_['text_disabled']                         = 'Dezactivat';
+$_['text_destination_status']               = 'Status';
+$_['text_disabled']                         = 'Disabled';
 $_['text_disapproved']                      = 'Respins';
 $_['text_disconnect_reminder']              = 'Chiar dacă decizi să deconectezi această extensie OpenCart, vei avea în continuare acces la contul Merchant Center. Nu va fi șters. Deconectarea va face următoarele:<ul><li>Dezactivează această extensie</li><li>Elimină toate campaniile active</li><li>Șterge toate feed-urile de date din contul Google Merchant Center conectat</li><li>Elimină toate link-urile dintre contul Google Merchant Center existent și OpenCart.</li></ul>';
 $_['text_disconnecting_please_wait']        = 'Disconnecting...';
 $_['text_does_not_apply']                   = '-- Does not apply --';
 $_['text_download_debug_log']               = 'Download Debug Log';
 $_['text_edit_target']                      = 'Editare Campanie: %s';
-$_['text_enabled']                          = 'Activat';
+$_['text_enabled']                          = 'Enabled';
 $_['text_error']                            = 'Error';
 $_['text_existing_merchant']                = 'Use my own Google Merchant Center account (In case you want to use your active Google Merchant Center account.)';
 $_['text_extension_settings']               = 'Setări Extensie';
-$_['text_extensions']                       = 'Extensii';
-$_['text_filter']                           = 'Filtru';
+$_['text_extensions']                       = 'Extensions';
+$_['text_filter']                           = 'Filter';
 $_['text_gender_female']                    = 'Female';
 $_['text_gender_male']                      = 'Male';
 $_['text_gender_unisex']                    = 'Unisex';
 $_['text_google_expiration_date']           = 'Google Expiration Date';
-$_['text_heading_merchant_center_account']  = 'Cont Merchant Center';
-$_['text_home']                             = 'Acasă';
-$_['text_image']                            = 'Imagine';
+$_['text_heading_merchant_center_account']  = 'Merchant Center Account';
+$_['text_home']                             = 'Home';
+$_['text_image']                            = 'Image';
 $_['text_impressions']                      = 'Impressions';
 $_['text_info_popup_product']               = 'The information requested here is required to properly list your product on Google Shopping. <a href="https://support.google.com/merchants/answer/7052112?hl=en&ref_topic=6324338" target="_blank">Click here</a> for more information.';
 $_['text_issues']                           = 'Issues';
@@ -157,7 +157,7 @@ $_['text_merchant_intro']                   = 'Please select the account you wis
 $_['text_merchant_website_claim']           = '<p>Upon clicking <strong>Proceed</strong>, you will be asked to authorize OpenCart to manage your listing and account in Google Shopping. Your website URL will be claimed by the selected Merchant Center account.</p>';
 $_['text_na']                               = '&ndash;';
 $_['text_new_merchant']                     = 'Use an account managed by OpenCart (For beginners who do not have a Google Merchant Center account.)';
-$_['text_no']                               = 'Nu';
+$_['text_no']                               = 'No';
 $_['text_no_results']                       = 'No results found!';
 $_['text_no_targets']                       = 'No campaigns found! Click the button below to add your first campaign.';
 $_['text_panel_connect']                    = 'Step 1 of 5: Connect the Google Shopping Extension';
@@ -182,17 +182,17 @@ $_['text_popup_title_single']               = '%s (%s)';
 $_['text_product_category']                 = 'Category (incl. sub-categories)';
 $_['text_product_is_modified']              = 'Câmpuri Google Editate';
 $_['text_product_model']                    = 'Model';
-$_['text_product_name']                     = 'Produs';
+$_['text_product_name']                     = 'Product';
 $_['text_refresh_token']                    = 'Re-create token';
 $_['text_remote_cron']                      = 'Method #2 - Remote CRON:';
 $_['text_report_campaign_name']             = 'Campaign Name';
-$_['text_report_clicks']                    = 'Click-uri';
+$_['text_report_clicks']                    = 'Clicks';
 $_['text_report_conversion_value']          = 'Conversion Value';
 $_['text_report_conversions']               = 'Conversions';
 $_['text_report_cost']                      = 'Cost';
 $_['text_report_date_range']                = 'Rapoarte Campanie pentru %s';
 $_['text_report_impressions']               = 'Impressions';
-$_['text_report_status']                    = 'Stare';
+$_['text_report_status']                    = 'Status';
 $_['text_reporting_interval']               = 'Reporting Time Interval';
 $_['text_reporting_interval_LAST_14_DAYS']  = 'Ultimele 14 zile';
 $_['text_reporting_interval_LAST_30_DAYS']  = 'Ultimele 30 zile';
@@ -221,7 +221,7 @@ $_['text_size_type_maternity']              = 'Maternity';
 $_['text_size_type_petite']                 = 'Petite';
 $_['text_size_type_plus']                   = 'Plus';
 $_['text_size_type_regular']                = 'Regular';
-$_['text_status']                           = 'Stare';
+$_['text_status']                           = 'Status';
 $_['text_suggestion']                       = 'Suggestion';
 $_['text_tax_custom']                       = 'Set this up myself in the Google Merchant Center';
 $_['text_tax_not_usa']                      = 'I am not based in the USA';
@@ -235,7 +235,7 @@ $_['text_video_tutorial_url_advertise']     = 'https://youtu.be/ZN7zz8raoVM?t=18
 $_['text_video_tutorial_url_install']       = 'https://www.youtube.com/watch?v=AvkBLWAUojI';
 $_['text_video_tutorial_url_setup']         = 'https://www.youtube.com/watch?v=ZN7zz8raoVM';
 $_['text_view_issues']                      = 'Vezi Probleme';
-$_['text_yes']                              = 'Da';
+$_['text_yes']                              = 'Yes';
 
 // Placeholders
 $_['placeholder_access_token']              = 'Paste your access token here';
@@ -243,7 +243,7 @@ $_['placeholder_access_token']              = 'Paste your access token here';
 // Tabs
 $_['tab_text_ads']                          = 'Anunțuri / Rapoarte Produse';
 $_['tab_text_reports']                      = 'Rapoarte Campanie';
-$_['tab_text_settings']                     = 'Setări';
+$_['tab_text_settings']                     = 'Settings';
 
 // Buttons
 $_['button_add_feed']                       = 'New Feed';
@@ -251,18 +251,18 @@ $_['button_add_target']                     = 'New Campaign';
 $_['button_apply']                          = 'Assign Selected Products to Campaigns';
 $_['button_bulk_edit_google_fields']        = 'Editare în Masă';
 $_['button_campaign']                       = 'Smart Shopping Ad Campaigns';
-$_['button_close']                          = 'Închide';
+$_['button_close']                          = 'Close';
 $_['button_connect']                        = 'Connect';
 $_['button_disconnect']                     = 'Disconnect';
 $_['button_mapping']                        = 'Category Mapping';
 $_['button_proceed']                        = 'Proceed';
 $_['button_product_edit']                   = 'Editare Câmpuri Google';
 $_['button_product_set']                    = 'Set Google Fields';
-$_['button_save']                           = 'Salvează';
+$_['button_save']                           = 'Save';
 $_['button_save_future']                    = 'Save &amp; Do Nothing';
 $_['button_save_modify']                    = 'Save &amp; Modify Current Products';
 $_['button_select_campaigns']               = 'Select Campaigns';
-$_['button_shipping_taxes']                 = 'Livrare &amp; Taxe';
+$_['button_shipping_taxes']                 = 'Shipping &amp; Taxes';
 $_['button_video_tutorial_install']         = 'Watch Video Tutorial';
 $_['button_video_tutorial_setup']           = 'Watch Video Tutorial';
 
@@ -296,7 +296,7 @@ $_['error_cron_acknowledge']                = 'Please confirm you have set up a 
 $_['error_empty_app_id']                    = 'Please insert the App ID!';
 $_['error_empty_app_secret']                = 'Please insert the App Secret!';
 $_['error_empty_campaign_name']             = 'Please set a name for your campaign!';
-$_['error_empty_country']                   = 'Te rugăm selectează o țară!';
+$_['error_empty_country']                   = 'Please select a country!';
 $_['error_empty_feed']                      = 'Please specify at least one campaign feed!';
 $_['error_field_no_value']                  = 'Please provide a value!';
 $_['error_flat_rate']                       = 'Please insert a flat rate value. The value must be numeric.';
