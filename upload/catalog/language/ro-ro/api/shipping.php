be481f86a92c81ea3a275b1de56306479a0ecea9
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:32:04 +0800

    New translations shipping.php (Romanian)

--- upload/catalog/language/ro-ro/api/shipping.php
+++ upload/catalog/language/ro-ro/api/shipping.php
@@ -4,15 +4,15 @@ $_['text_address']       = 'Succes: Adresa de livrare a fost setată!';
 $_['text_method']        = 'Succes: Metoda de livrare a fost setată!';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to access the API!';
-$_['error_firstname']    = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']     = 'Last Name must be between 1 and 32 characters!';
-$_['error_address_1']    = 'Address 1 must be between 3 and 128 characters!';
-$_['error_city']         = 'City must be between 3 and 128 characters!';
-$_['error_postcode']     = 'Postcode must be between 2 and 10 characters for this country!';
-$_['error_country']      = 'Please select a country!';
-$_['error_zone']         = 'Please select a region / state!';
-$_['error_custom_field'] = '%s required!';
+$_['error_permission']   = 'Atenție: Nu ai permisiunea de a accesa API!';
+$_['error_firstname']    = 'Prenumele trebuie să fie între 1 și 32 de caractere!';
+$_['error_lastname']     = 'Numele de Familie trebuie să fie între 1 și 32 de caractere!';
+$_['error_address_1']    = 'Adresa 1 trebuie să fie între 3 și 128 de caractere!';
+$_['error_city']         = 'Oraș trebuie să fie între 3 și 128 de caractere!';
+$_['error_postcode']     = 'Cod poștal trebuie să fie între 2 și 10 caractere pentru această țară!';
+$_['error_country']      = 'Te rugăm selectează o țară!';
+$_['error_zone']         = 'Te rugăm selectează o regiune / stat!';
+$_['error_custom_field'] = '%s necesar!';
 $_['error_address']      = 'Warning: Shipping address required!';
 $_['error_method']       = 'Warning: Shipping method required!';
-$_['error_no_shipping']  = 'Warning: No Shipping options are available!';
+$_['error_no_shipping']  = 'Atenție: Nu sunt disponibile opțiuni de livrare!';
