3d426cff0ff7bd2aecb7e61c3f6d9c874ab59e2c
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:41:44 +0800

    New translations recurring.php (Romanian)

--- upload/admin/language/ro-ro/catalog/recurring.php
+++ upload/admin/language/ro-ro/catalog/recurring.php
@@ -13,12 +13,12 @@ $_['text_semi_month']       = 'Jumătate de lună';
 $_['text_month']            = 'Lună';
 $_['text_year']             = 'An';
 $_['text_recurring']        = '<p><i class="fa fa-info-circle"></i> Sumele recurente sunt calculate după frecvență și cicluri.</p><p>De exemplu, dacă vei utiliza frecvența de o "săptămână" și o ciclul de "2", atunci utilizatorul va fi facturat la fiecare 2 săptămâni.</p><p>Durata este de câte ori clientul va face o plată, setează 0, dacă vrei sa faca plăți până când acestea vor fi anulate.</p>';
-$_['text_profile']          = 'Profil Recurent';
+$_['text_profile']          = 'Recurring Profile';
 $_['text_trial']            = 'Profil de Probă';
 
 // Entry
 $_['entry_name']            = 'Nume';
-$_['entry_price']           = 'Preț';
+$_['entry_price']           = 'Price';
 $_['entry_duration']        = 'Durată';
 $_['entry_cycle']           = 'Ciclu';
 $_['entry_frequency']       = 'Frecvență';
@@ -27,16 +27,16 @@ $_['entry_trial_duration']  = 'Durată Probă';
 $_['entry_trial_status']    = 'Stare Probă';
 $_['entry_trial_cycle']     = 'Ciclu Probă';
 $_['entry_trial_frequency'] = 'Frecventă Probă';
-$_['entry_status']          = 'Stare';
-$_['entry_sort_order']      = 'Ordine de Sortare';
+$_['entry_status']          = 'Status';
+$_['entry_sort_order']      = 'Sort Order';
 
 // Column
-$_['column_name']           = 'Nume';
-$_['column_sort_order']     = 'Ordine de Sortare';
-$_['column_action']         = 'Acțiune';
+$_['column_name']           = 'Name';
+$_['column_sort_order']     = 'Sort Order';
+$_['column_action']         = 'Action';
 
 // Error
-$_['error_warning']         = 'Atenție: Te rugăm să verifici cu atenție formularul pentru erori!';
+$_['error_warning']         = 'Warning: Please check the form carefully for errors!';
 $_['error_permission']      = 'Atenție: Nu ai permisiunea de a modifica profilurile recurente!';
 $_['error_name']            = 'Nume Profil trebuie să fie mai mare de 3 și mai mic de 255 de caractere!';
 $_['error_product']         = 'Atenție: Acest profil recurent nu poate fi șters deoarece în prezent este asociat cu %s produse!';
