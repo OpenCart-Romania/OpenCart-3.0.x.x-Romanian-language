72415ecdd4e95cfa7c8cad30d9c266bd7cdc99e4
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:33:17 +0800

    New translations register.php (Romanian)

--- upload/catalog/language/ro-ro/mail/register.php
+++ upload/catalog/language/ro-ro/mail/register.php
@@ -1,15 +1,15 @@
 <?php
 // Text
 $_['text_subject']        = '%s - Îți mulțumim pentru înregistrare';
-$_['text_welcome']        = 'Welcome and thank you for registering at %s!';
-$_['text_login']          = 'Your account has now been created and you can log in by using your email address and password by visiting our website or at the following URL:';
+$_['text_welcome']        = 'Bine ai venit și îți mulțumim că te-ai înregistrat la %s!';
+$_['text_login']          = 'Contul tău a fost creat acum și te poți conecta folosind adresa ta de email și parola, vizitând site-ul nostru web sau la următoarea adresă URL:';
 $_['text_approval']       = 'Contul tău trebuie să fie aprobat înainte de a te putea conecta. Odată aprobat, te poți conecta folosindu-ți adresa de email şi parola pe site-ul nostru sau la următorul URL:';
-$_['text_service']        = 'Upon logging in, you will be able to access other services including reviewing past orders, printing invoices and editing your account information.';
-$_['text_thanks']         = 'Thanks,';
+$_['text_service']        = 'La conectare, vei putea accesa alte servicii, inclusiv revizuirea comenzilor anterioare, imprimarea facturilor și editarea informațiilor contului.';
+$_['text_thanks']         = 'Îți mulțumim,';
 $_['text_new_customer']   = 'Client nou';
 $_['text_signup']         = 'Un nou client s-a înregistrat:';
-$_['text_customer_group'] = 'Customer Group:';
-$_['text_firstname']      = 'First Name:';
-$_['text_lastname']       = 'Last Name:';
+$_['text_customer_group'] = 'Grup de Clienți:';
+$_['text_firstname']      = 'Prenume:';
+$_['text_lastname']       = 'Nume de Familie:';
 $_['text_email']          = 'E-Mail:';
-$_['text_telephone']      = 'Telephone:';
+$_['text_telephone']      = 'Telefon:';
