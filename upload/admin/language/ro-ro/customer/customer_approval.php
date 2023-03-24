87b1bc2d1384da2571252fc832e2e61d7b2ffc89
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:28:30 +0800

    New translations customer_approval.php (Romanian)

--- upload/admin/language/ro-ro/customer/customer_approval.php
+++ upload/admin/language/ro-ro/customer/customer_approval.php
@@ -5,23 +5,23 @@ $_['heading_title']         = 'Customer Approvals';
 // Text
 $_['text_success']          = 'Succes: Ai modificat aprobările clienților!';
 $_['text_list']             = 'Listă Aprobări Clienți';
-$_['text_default']          = 'Default';
+$_['text_default']          = 'Implicit';
 $_['text_customer']         = 'Client';
-$_['text_affiliate']        = 'Affiliate';
+$_['text_affiliate']        = 'Afiliat';
 
 // Column
-$_['column_name']           = 'Customer Name';
+$_['column_name']           = 'Numele Clientului';
 $_['column_email']          = 'E-Mail';
 $_['column_customer_group'] = 'Customer Group';
 $_['column_type']           = 'Type';
-$_['column_date_added']     = 'Date Added';
-$_['column_action']         = 'Action';
+$_['column_date_added']     = 'Data Adăugării';
+$_['column_action']         = 'Acțiune';
 
 // Entry
 $_['entry_name']            = 'Customer Name';
 $_['entry_email']           = 'E-Mail';
-$_['entry_customer_group']  = 'Customer Group';
-$_['entry_type']            = 'Type';
+$_['entry_customer_group']  = 'Grupul de Clienți';
+$_['entry_type']            = 'Tip';
 $_['entry_date_added']      = 'Date Added';
 
 // Error
