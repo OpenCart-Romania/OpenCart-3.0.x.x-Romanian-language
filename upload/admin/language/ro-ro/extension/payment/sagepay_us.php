2e98e52d6a84a95eb822dca7cd894e6bb7c83840
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:30:13 +0800

    New translations sagepay_us.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/sagepay_us.php
+++ upload/admin/language/ro-ro/extension/payment/sagepay_us.php
@@ -3,7 +3,7 @@
 $_['heading_title']		 = 'Sage Payment Solutions (US)';
 
 // Text
-$_['text_extension']	 = 'Extensions';
+$_['text_extension']	 = 'Extensii';
 $_['text_success']		 = 'Success: You have modified SagePay account details!';
 $_['text_edit']          = 'Edit Sage Payment Solutions (US)';
 
@@ -11,15 +11,15 @@ $_['text_edit']          = 'Edit Sage Payment Solutions (US)';
 $_['entry_merchant_id']	 = 'Merchant ID';
 $_['entry_merchant_key'] = 'Merchant Key';
 $_['entry_total']		 = 'Total';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_order_status'] = 'Stare Comandă';
 $_['entry_geo_zone']	 = 'Geo Zone';
-$_['entry_status']		 = 'Status';
-$_['entry_sort_order']	 = 'Sort Order';
+$_['entry_status']		 = 'Stare';
+$_['entry_sort_order']	 = 'Ordine Sortare';
 
 // Help
 $_['help_total']		 = 'The checkout total the order must reach before this payment method becomes active.';
 
 // Error
-$_['error_permission']	 = 'Warning: You do not have permission to modify payment SagePay!';
+$_['error_permission']	 = 'Atenție: Nu ai permisiunea de a modifica plata SagePay!';
 $_['error_merchant_id']	 = 'Merchant ID Required!';
 $_['error_merchant_key'] = 'Merchant Key Required!';
