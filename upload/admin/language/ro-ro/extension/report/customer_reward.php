65b08176b92721cbb36232cea0844c76e660f1d2
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:30:24 +0800

    New translations customer_reward.php (Romanian)

--- upload/admin/language/ro-ro/extension/report/customer_reward.php
+++ upload/admin/language/ro-ro/extension/report/customer_reward.php
@@ -3,27 +3,27 @@
 $_['heading_title']         = 'Raport Puncte de Recompensă Client';
 
 // Text
-$_['text_extension']        = 'Extensions';
+$_['text_extension']        = 'Extensii';
 $_['text_edit']             = 'Editare Raport Puncte de Recompensă Client';
 $_['text_success']          = 'Succes: Ai modificat raportul punctelor de recompensă ale clientului!';
-$_['text_filter']           = 'Filter';
+$_['text_filter']           = 'Filtru';
 
 // Column
 $_['column_customer']       = 'Customer Name';
 $_['column_email']          = 'E-Mail';
 $_['column_customer_group'] = 'Customer Group';
-$_['column_status']         = 'Status';
+$_['column_status']         = 'Stare';
 $_['column_points']         = 'Reward Points';
 $_['column_orders']         = 'No. Orders';
 $_['column_total']          = 'Total';
-$_['column_action']         = 'Action';
+$_['column_action']         = 'Acțiune';
 
 // Entry
-$_['entry_date_start']      = 'Date Start';
-$_['entry_date_end']        = 'Date End';
+$_['entry_date_start']      = 'Dată de Început';
+$_['entry_date_end']        = 'Dată de Sfârșit';
 $_['entry_customer']        = 'Customer';
-$_['entry_status']          = 'Status';
-$_['entry_sort_order']      = 'Sort Order';
+$_['entry_status']          = 'Stare';
+$_['entry_sort_order']      = 'Ordine Sortare';
 
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica raportul punctelor de recompensă ale clientului!';
