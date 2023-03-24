8c7b67a7bc04280ced883851b6316e02a362acbe
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:48:22 +0800

    New translations header.php (Romanian)

--- upload/catalog/language/ro-ro/common/header.php
+++ upload/catalog/language/ro-ro/common/header.php
@@ -1,16 +1,16 @@
 <?php
 // Text
-$_['text_home']          = 'Acasă';
-$_['text_wishlist']      = 'Listă de Dorințe (%s)';
-$_['text_shopping_cart'] = 'Coș de Cumpărături';
-$_['text_category']      = 'Categorii';
-$_['text_account']       = 'Contul Meu';
-$_['text_register']      = 'Înregistrează-te';
-$_['text_login']         = 'Conectare';
-$_['text_order']         = 'Istoric Comenzi';
-$_['text_transaction']   = 'Tranzacții';
-$_['text_download']      = 'Descărcări';
-$_['text_logout']        = 'Deconectare';
+$_['text_home']          = 'Home';
+$_['text_wishlist']      = 'Wish List (%s)';
+$_['text_shopping_cart'] = 'Shopping Cart';
+$_['text_category']      = 'Categories';
+$_['text_account']       = 'My Account';
+$_['text_register']      = 'Register';
+$_['text_login']         = 'Login';
+$_['text_order']         = 'Order History';
+$_['text_transaction']   = 'Transactions';
+$_['text_download']      = 'Downloads';
+$_['text_logout']        = 'Logout';
 $_['text_checkout']      = 'Checkout';
-$_['text_search']        = 'Căutare';
+$_['text_search']        = 'Search';
 $_['text_all']           = 'Afișează Tot';
