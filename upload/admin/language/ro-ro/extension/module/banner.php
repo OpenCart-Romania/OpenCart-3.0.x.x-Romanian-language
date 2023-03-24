ee2a1f1146a46a835511d95399feee7ad258b25c
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:11 +0800

    New translations banner.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/banner.php
+++ upload/admin/language/ro-ro/extension/module/banner.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'Banner';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Succes: Ai modificat modulul banner!';
 $_['text_edit']        = 'Editare Modul Banner';
 
@@ -11,12 +11,12 @@ $_['text_edit']        = 'Editare Modul Banner';
 $_['entry_name']       = 'Module Name';
 $_['entry_banner']     = 'Banner';
 $_['entry_dimension']  = 'Dimension (W x H) and Resize Type';
-$_['entry_width']      = 'Width';
-$_['entry_height']     = 'Height';
-$_['entry_status']     = 'Status';
+$_['entry_width']      = 'Lăţime';
+$_['entry_height']     = 'Înălţime';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul banner!';
-$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_name']       = 'Numele Modulului trebuie să fie între 3 și 64 de caractere!';
 $_['error_width']      = 'Width required!';
 $_['error_height']     = 'Height required!';
