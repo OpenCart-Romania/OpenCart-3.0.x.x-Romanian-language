732673d1dd9a496771793c024e691401d898706e
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:28:14 +0800

    New translations filter.php (Romanian)

--- upload/admin/language/ro-ro/catalog/filter.php
+++ upload/admin/language/ro-ro/catalog/filter.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filters';
+$_['heading_title']     = 'Filtre';
 
 // Text
 $_['text_success']      = 'Succes: Ai modificat filtrele!';
@@ -11,14 +11,14 @@ $_['text_group']        = 'Grup Filtre';
 $_['text_value']        = 'Valori Filtru';
 
 // Column
-$_['column_group']      = 'Filter Group';
-$_['column_sort_order'] = 'Sort Order';
-$_['column_action']     = 'Action';
+$_['column_group']      = 'Grup Filtre';
+$_['column_sort_order'] = 'Ordine de Sortare';
+$_['column_action']     = 'Acțiune';
 
 // Entry
 $_['entry_group']       = 'Nume Grup Filtre';
 $_['entry_name']        = 'Nume Filtru';
-$_['entry_sort_order']  = 'Sort Order';
+$_['entry_sort_order']  = 'Ordine Sortare';
 
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica filtrele!';
