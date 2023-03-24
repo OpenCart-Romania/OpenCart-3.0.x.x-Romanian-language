75d3dd81ca0ebd4e4cc2471b5e39eb9fafc5a6c2
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:09 +0800

    New translations amazon_login.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/amazon_login.php
+++ upload/admin/language/ro-ro/extension/module/amazon_login.php
@@ -4,12 +4,12 @@ $_['heading_title'] = 'Conectează-te cu Amazon';
 
 //Text
 $_['text_module'] = 'Modules';
-$_['text_extension']               = 'Extensions';
+$_['text_extension']               = 'Extensii';
 $_['text_success'] = 'Succes: Ai modificat modulul Conectează-te cu Amazon!';
-$_['text_content_top'] = 'Content Top';
-$_['text_content_bottom'] = 'Content Bottom';
-$_['text_column_left'] = 'Column Left';
-$_['text_column_right'] = 'Column Right';
+$_['text_content_top'] = 'Conținut in Partea de Sus';
+$_['text_content_bottom'] = 'Conținut în Partea de Jos';
+$_['text_column_left'] = 'Coloană Stânga';
+$_['text_column_right'] = 'Coloană Dreapta';
 $_['text_lwa_button'] = 'Login with Amazon';
 $_['text_login_button'] = 'Login';
 $_['text_a_button'] = 'A';
@@ -27,8 +27,8 @@ $_['entry_button_colour'] = 'Button Colour';
 $_['entry_button_size'] = 'Button Size';
 $_['entry_layout'] = 'Layout';
 $_['entry_position'] = 'Position';
-$_['entry_status'] = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_status'] = 'Stare';
+$_['entry_sort_order'] = 'Ordine Sortare';
 
 //Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul Conectează-te cu Amazon!';
