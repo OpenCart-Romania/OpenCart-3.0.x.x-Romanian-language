bb43a106e8a44eb4d02d3d1e25391eea57242e55
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:48:52 +0800

    New translations cheque.php (Romanian)

--- upload/catalog/language/ro-ro/extension/payment/cheque.php
+++ upload/catalog/language/ro-ro/extension/payment/cheque.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_title']       = 'Cec / Ordin de Plată';
+$_['text_title']       = 'Cheque / Money Order';
 $_['text_instruction'] = 'Instrucțiuni Cec / Ordin de Plată';
 $_['text_payable']     = 'Make Payable To: ';
 $_['text_address']     = 'Send To: ';
