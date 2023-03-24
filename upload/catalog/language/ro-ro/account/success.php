e3944ebfc0e6d1e1f6cf9b85944e14376ac786c7
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:31:52 +0800

    New translations success.php (Romanian)

--- upload/catalog/language/ro-ro/account/success.php
+++ upload/catalog/language/ro-ro/account/success.php
@@ -5,5 +5,5 @@ $_['heading_title'] = 'Contul Tău A Fost Creat!';
 // Text
 $_['text_message']  = '<p>Felicitări! Noul tău cont a fost creat cu succes!</p> <p>Acum poti profita de privilegiile de membru pentru a-ți îmbunătăți experiența de cumpărături online cu noi.</p> <p>Dacă ai ORICE întrebare despre funcționarea acestui magazin online, te rugăm să trimiți un e-mail proprietarului magazinului.</p> <p>O confirmare a fost trimisă la adresa de e-mail furnizată. Dacă nu ai primit-o în decurs de o oră, te rugăm <a href="%s">să ne contactezi</a>.</p>';
 $_['text_approval'] = '<p>Îți mulțumim pentru înregistrarea cu %s!</p><p>Vei fi notificat prin e-mail odată ce contul tău a fost activat de proprietarul magazinului.</p><p>Dacă ai ORICE întrebare despre funcționarea acestui magazin online, te rugăm <a href="%s">contactează proprietarul magazinului</a>.</p>';
-$_['text_account']  = 'Account';
+$_['text_account']  = 'Cont';
 $_['text_success']  = 'Success';
