abcb79d57c15a90eb32c158fb7be2726c7b5e1dd
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:49:57 +0800

    New translations category.php (Romanian)

--- upload/catalog/language/ro-ro/product/category.php
+++ upload/catalog/language/ro-ro/product/category.php
@@ -1,7 +1,7 @@
 <?php
 // Text
-$_['text_refine']       = 'Îmbunătățește Căutarea';
-$_['text_product']      = 'Produse';
+$_['text_refine']       = 'Refine Search';
+$_['text_product']      = 'Products';
 $_['text_error']        = 'Categoria nu a fost găsită!';
 $_['text_empty']        = 'Nu există produse care să fie listate in această categorie.';
 $_['text_quantity']     = 'Cantitate:';
@@ -9,10 +9,10 @@ $_['text_manufacturer'] = 'Marca:';
 $_['text_model']        = 'Cod Produs:';
 $_['text_points']       = 'Puncte de Recompensă:';
 $_['text_price']        = 'Preț:';
-$_['text_tax']          = 'Fără Taxe:';
+$_['text_tax']          = 'Ex Tax:';
 $_['text_compare']      = 'Compară Produs (%s)';
 $_['text_sort']         = 'Sortează după:';
-$_['text_default']      = 'Implicit';
+$_['text_default']      = 'Default';
 $_['text_name_asc']     = 'De la (A - Z)';
 $_['text_name_desc']    = 'De la (Z - A)';
 $_['text_price_asc']    = 'Preț (Mic &gt; Mare)';
