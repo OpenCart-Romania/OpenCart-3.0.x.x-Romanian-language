8f0faee8ecccd7c46fea286452d23afac100098e
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:48:32 +0800

    New translations account.php (Romanian)

--- upload/catalog/language/ro-ro/extension/module/account.php
+++ upload/catalog/language/ro-ro/extension/module/account.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title']    = 'Cont';
+$_['heading_title']    = 'Account';
 
 // Text
-$_['text_register']    = 'Înregistrează-te';
-$_['text_login']       = 'Conectare';
-$_['text_logout']      = 'Deconectare';
-$_['text_forgotten']   = 'Parolă Uitată';
-$_['text_account']     = 'Contul Meu';
+$_['text_register']    = 'Register';
+$_['text_login']       = 'Login';
+$_['text_logout']      = 'Logout';
+$_['text_forgotten']   = 'Forgotten Password';
+$_['text_account']     = 'My Account';
 $_['text_edit']        = 'Editează Cont';
-$_['text_password']    = 'Parolă';
-$_['text_address']     = 'Agendă';
-$_['text_wishlist']    = 'Listă de Dorințe';
-$_['text_order']       = 'Istoric Comenzi';
-$_['text_download']    = 'Descărcări';
-$_['text_reward']      = 'Puncte de Recompensă';
-$_['text_return']      = 'Retururi';
-$_['text_transaction'] = 'Tranzacții';
+$_['text_password']    = 'Password';
+$_['text_address']     = 'Address Book';
+$_['text_wishlist']    = 'Wish List';
+$_['text_order']       = 'Order History';
+$_['text_download']    = 'Downloads';
+$_['text_reward']      = 'Reward Points';
+$_['text_return']      = 'Returns';
+$_['text_transaction'] = 'Transactions';
 $_['text_newsletter']  = 'Newsletter';
-$_['text_recurring']   = 'Plăţi recurente';
+$_['text_recurring']   = 'Recurring payments';
