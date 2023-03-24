46a9aece18ac9527512ba9d733fc03f197d24365
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:31:35 +0800

    New translations upload.php (Romanian)

--- upload/admin/language/ro-ro/tool/upload.php
+++ upload/admin/language/ro-ro/tool/upload.php
@@ -9,12 +9,12 @@ $_['text_list']         = 'Upload List';
 // Column
 $_['column_name']       = 'Upload Name';
 $_['column_filename']   = 'Filename';
-$_['column_date_added'] = 'Date Added';
-$_['column_action']     = 'Action';
+$_['column_date_added'] = 'Data Adăugării';
+$_['column_action']     = 'Acțiune';
 
 // Entry
 $_['entry_name']        = 'Upload Name';
-$_['entry_filename']    = 'Filename';
+$_['entry_filename']    = 'Nume Fișier';
 $_['entry_date_added']  = 'Date Added';
 
 // Error
