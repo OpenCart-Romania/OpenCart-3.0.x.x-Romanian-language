d16dc96db814c9a97dfff9c4cc7ccb059030f426
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:30:44 +0800

    New translations weight.php (Romanian)

--- upload/admin/language/ro-ro/extension/shipping/weight.php
+++ upload/admin/language/ro-ro/extension/shipping/weight.php
@@ -3,16 +3,16 @@
 $_['heading_title']    = 'Weight Based Shipping';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified weight based shipping!';
 $_['text_edit']        = 'Edit Weight Based Shipping';
 
 // Entry
 $_['entry_rate']       = 'Rates';
-$_['entry_tax_class']  = 'Tax Class';
+$_['entry_tax_class']  = 'Clasa Fiscală';
 $_['entry_geo_zone']   = 'Geo Zone';
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordine Sortare';
 
 // Help
 $_['help_rate']        = 'Example: 5:10.00,7:12.00 Weight:Cost,Weight:Cost, etc..';
