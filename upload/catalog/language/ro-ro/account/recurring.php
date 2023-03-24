0a3262fe86318fa96c4c6fc75d1a26b2e5e95707
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:31:48 +0800

    New translations recurring.php (Romanian)

--- upload/catalog/language/ro-ro/account/recurring.php
+++ upload/catalog/language/ro-ro/account/recurring.php
@@ -3,46 +3,46 @@
 $_['heading_title']                        = 'Recurring Payments';
 
 // Text
-$_['text_account']                         = 'Account';
+$_['text_account']                         = 'Cont';
 $_['text_recurring']                       = 'Informații Plată Recurentă';
 $_['text_recurring_detail']                = 'Detalii Plată Recurentă';
 $_['text_order_recurring_id']              = 'ID Recurență:';
-$_['text_date_added']                      = 'Date Added:';
+$_['text_date_added']                      = 'Data Adăugării:';
 $_['text_status']                          = 'Status:';
-$_['text_payment_method']                  = 'Payment Method:';
-$_['text_order_id']                        = 'Order ID:';
+$_['text_payment_method']                  = 'Metodă de Plată:';
+$_['text_order_id']                        = 'Număr Comandă:';
 $_['text_product']                         = 'Produs:';
 $_['text_quantity']                        = 'Cantitate:';
 $_['text_description']                     = 'Description';
-$_['text_reference']                       = 'Reference';
-$_['text_transaction']                     = 'Transactions';
+$_['text_reference']                       = 'Referință';
+$_['text_transaction']                     = 'Tranzacții';
 $_['text_status_1']                        = 'Active';
-$_['text_status_2']                        = 'Inactive';
+$_['text_status_2']                        = 'Inactiv';
 $_['text_status_3']                        = 'Cancelled';
 $_['text_status_4']                        = 'Suspended';
-$_['text_status_5']                        = 'Expired';
+$_['text_status_5']                        = 'Expirat';
 $_['text_status_6']                        = 'Pending';
 $_['text_transaction_date_added']          = 'Created';
 $_['text_transaction_payment']             = 'Payment';
-$_['text_transaction_outstanding_payment'] = 'Outstanding payment';
-$_['text_transaction_skipped']             = 'Payment skipped';
-$_['text_transaction_failed']              = 'Payment failed';
+$_['text_transaction_outstanding_payment'] = 'Plată restantă';
+$_['text_transaction_skipped']             = 'Plată omisă';
+$_['text_transaction_failed']              = 'Plată eșuată';
 $_['text_transaction_cancelled']           = 'Cancelled';
 $_['text_transaction_suspended']           = 'Suspended';
-$_['text_transaction_suspended_failed']    = 'Suspended from failed payment';
-$_['text_transaction_outstanding_failed']  = 'Outstanding payment failed';
+$_['text_transaction_suspended_failed']    = 'Suspendat de la plată eșuată';
+$_['text_transaction_outstanding_failed']  = 'Plată restantă eșuată';
 $_['text_transaction_expired']             = 'Expired';
 $_['text_empty']                           = 'Nicio plată recurentă găsită!';
 $_['text_error']                           = 'Comanda recurenta pe care ai solicitat-o nu a putut fi găsită!';
-$_['text_cancelled']                       = 'Recurring payment has been cancelled';
+$_['text_cancelled']                       = 'Plata recurentă a fost anulată';
 
 // Column
-$_['column_date_added']                    = 'Date Added';
+$_['column_date_added']                    = 'Data Adăugării';
 $_['column_type']                          = 'Type';
-$_['column_amount']                        = 'Amount';
-$_['column_status']                        = 'Status';
-$_['column_product']                       = 'Product';
-$_['column_order_recurring_id']            = 'Recurring ID';
+$_['column_amount']                        = 'Valoare';
+$_['column_status']                        = 'Stare';
+$_['column_product']                       = 'Produs';
+$_['column_order_recurring_id']            = 'ID Recurență';
 
 // Error
 $_['error_not_cancelled']                  = 'Error: %s';
