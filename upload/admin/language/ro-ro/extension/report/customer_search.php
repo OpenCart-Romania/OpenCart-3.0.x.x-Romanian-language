0a599f4a2f2e083de5fa7f64c9341ca86f5129ec
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:30:24 +0800

    New translations customer_search.php (Romanian)

--- upload/admin/language/ro-ro/extension/report/customer_search.php
+++ upload/admin/language/ro-ro/extension/report/customer_search.php
@@ -3,10 +3,10 @@
 $_['heading_title']     = 'Customer Searches Report';
 
 // Text
-$_['text_extension']    = 'Extensions';
+$_['text_extension']    = 'Extensii';
 $_['text_edit']         = 'Edit Customer Searches Report';
 $_['text_success']      = 'Success: You have modified customer searches report!';
-$_['text_filter']       = 'Filter';
+$_['text_filter']       = 'Filtru';
 $_['text_guest']        = 'Guest';
 $_['text_customer']     = '<a href="%s">%s</a>';
 
@@ -16,16 +16,16 @@ $_['column_products']   = 'Found Products';
 $_['column_category']   = 'Category';
 $_['column_customer']   = 'Customer';
 $_['column_ip']         = 'IP';
-$_['column_date_added'] = 'Date Added';
+$_['column_date_added'] = 'Data Adăugării';
 
 // Entry
-$_['entry_date_start']  = 'Date Start';
-$_['entry_date_end']    = 'Date End';
-$_['entry_keyword']     = 'Keyword';
+$_['entry_date_start']  = 'Dată de Început';
+$_['entry_date_end']    = 'Dată de Sfârșit';
+$_['entry_keyword']     = 'Cuvânt Cheie';
 $_['entry_customer']    = 'Customer';
 $_['entry_ip']          = 'IP';
-$_['entry_status']      = 'Status';
-$_['entry_sort_order']  = 'Sort Order';
+$_['entry_status']      = 'Stare';
+$_['entry_sort_order']  = 'Ordine Sortare';
 
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica raportul de căutări ale clientului!';
