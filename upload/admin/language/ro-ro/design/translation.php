12aadb459646f7b66ef2317beeb2e48fa2a234b6
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:42:01 +0800

    New translations translation.php (Romanian)

--- upload/admin/language/ro-ro/design/translation.php
+++ upload/admin/language/ro-ro/design/translation.php
@@ -1,31 +1,31 @@
 <?php
 // Heading
-$_['heading_title']    = 'Editor de Limbă';
+$_['heading_title']    = 'Language Editor';
 
 // Text
 $_['text_success']     = 'Succes: Ai modificat editorul de limbă!';
 $_['text_list']        = 'Translation List';
 $_['text_edit']        = 'Editare Traducere';
 $_['text_add']         = 'Adaugă Traducere';
-$_['text_default']     = 'Implicit';
-$_['text_store']       = 'Magazin';
-$_['text_language']    = 'Limbă';
+$_['text_default']     = 'Default';
+$_['text_store']       = 'Store';
+$_['text_language']    = 'Language';
 
 // Column
-$_['column_store']     = 'Magazin';
-$_['column_language']  = 'Limbă';
-$_['column_route']     = 'Rută';
+$_['column_store']     = 'Store';
+$_['column_language']  = 'Language';
+$_['column_route']     = 'Route';
 $_['column_key']       = 'Key';
-$_['column_value']     = 'Valoare';
-$_['column_action']    = 'Acțiune';
+$_['column_value']     = 'Value';
+$_['column_action']    = 'Action';
 
 // Entry
-$_['entry_store']      = 'Magazin';
-$_['entry_language']   = 'Limbă';
-$_['entry_route']      = 'Rută';
+$_['entry_store']      = 'Store';
+$_['entry_language']   = 'Language';
+$_['entry_route']      = 'Route';
 $_['entry_key']        = 'Key';
-$_['entry_default']    = 'Implicit';
-$_['entry_value']      = 'Valoare';
+$_['entry_default']    = 'Default';
+$_['entry_value']      = 'Value';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica editorul de limbă!';
