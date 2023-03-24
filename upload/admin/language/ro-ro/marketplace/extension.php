9d288758b0d7b3bac5de02e62acf4e786ebc769d
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:31:21 +0800

    New translations extension.php (Romanian)

--- upload/admin/language/ro-ro/marketplace/extension.php
+++ upload/admin/language/ro-ro/marketplace/extension.php
@@ -6,4 +6,4 @@ $_['heading_title'] = 'Extensions';
 $_['text_success']  = 'Success: You have modified extensions!';
 $_['text_list']     = 'Extension List';
 $_['text_type']     = 'Choose the extension type';
-$_['text_filter']   = 'Filter';
+$_['text_filter']   = 'Filtru';
