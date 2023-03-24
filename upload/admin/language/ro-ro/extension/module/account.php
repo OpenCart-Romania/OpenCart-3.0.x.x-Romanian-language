e43f78cce9d5fa8f0c7afa8b54914b37b5fc5786
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:08 +0800

    New translations account.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/account.php
+++ upload/admin/language/ro-ro/extension/module/account.php
@@ -3,12 +3,12 @@
 $_['heading_title']    = 'Account';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Succes: Ai modificat modulul de cont!';
 $_['text_edit']        = 'Editare Modul Cont';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul de cont!';
