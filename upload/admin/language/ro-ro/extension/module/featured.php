8a8cdf17dcc795c8c74bb08944d5d2c6ea480ecd
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:42:42 +0800

    New translations featured.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/featured.php
+++ upload/admin/language/ro-ro/extension/module/featured.php
@@ -3,23 +3,23 @@
 $_['heading_title']    = 'Featured';
 
 // Text
-$_['text_extension']   = 'Extensii';
+$_['text_extension']   = 'Extensions';
 $_['text_success']     = 'Succes: Ai modificat modulul recomandate!';
 $_['text_edit']        = 'Editare Modul Recomandate';
 
 // Entry
 $_['entry_name']       = 'Module Name';
-$_['entry_product']    = 'Produse';
+$_['entry_product']    = 'Products';
 $_['entry_limit']      = 'Limit';
-$_['entry_width']      = 'Lăţime';
-$_['entry_height']     = 'Înălţime';
-$_['entry_status']     = 'Stare';
+$_['entry_width']      = 'Width';
+$_['entry_height']     = 'Height';
+$_['entry_status']     = 'Status';
 
 // Help
-$_['help_product']     = '(Completare automată)';
+$_['help_product']     = '(Autocomplete)';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul recomandate!';
-$_['error_name']       = 'Numele Modulului trebuie să fie între 3 și 64 de caractere!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
 $_['error_width']      = 'Width required!';
 $_['error_height']     = 'Height required!';
