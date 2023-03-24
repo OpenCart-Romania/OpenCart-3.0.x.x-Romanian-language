41c49559ba2a035edbf30039cdcfb6f5cf8ebd92
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:49:24 +0800

    New translations sagepay_server.php (Romanian)

--- upload/catalog/language/ro-ro/extension/payment/sagepay_server.php
+++ upload/catalog/language/ro-ro/extension/payment/sagepay_server.php
@@ -7,8 +7,8 @@ $_['text_card_type']			= 'Card Type: ';
 $_['text_card_name']			= 'Card Name: ';
 $_['text_card_digits']			= 'Last Digits: ';
 $_['text_card_expiry']			= 'Expiry: ';
-$_['text_trial']				= '%s la fiecare %s %s pentru %s plați atunci ';
-$_['text_recurring']			= '%s la fiecare %s %s';
+$_['text_trial']				= '%s every %s %s for %s payments then ';
+$_['text_recurring']			= '%s every %s %s';
 $_['text_length']				= ' for %s payments';
 $_['text_success']				= 'Your payment has been authorised.';
 $_['text_decline']				= 'Your payment has been declined.';
@@ -16,7 +16,7 @@ $_['text_bank_error']			= 'There was an error processing your request with the b
 $_['text_transaction_error']	= 'There was an error processing your transaction.';
 $_['text_generic_error']		= 'There was an error processing your request.';
 $_['text_hash_failed']			= 'Hash check failed. Do not try your payment again as the payment status is unknown. Please contact the merchant.';
-$_['text_link']					= 'Te rugăm click <a href="%s">aici</a> pentru a continua';
+$_['text_link']					= 'Please click <a href="%s">here</a> to continue';
 $_['text_confirm_delete']		= 'Are you sure you want to delete the card?';
 
 // Entry
