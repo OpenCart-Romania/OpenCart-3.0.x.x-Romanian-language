c976a9dcab128b9899db9a166174a70c4806e34a
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:33:11 +0800

    New translations shipping.php (Romanian)

--- upload/catalog/language/ro-ro/extension/total/shipping.php
+++ upload/catalog/language/ro-ro/extension/total/shipping.php
@@ -5,16 +5,16 @@ $_['heading_title']        = 'Estimare Livrare &amp; Taxe';
 // Text
 $_['text_success']         = 'Succes: Estimarea dumneavoastră de transport a fost aplicată!';
 $_['text_shipping']        = 'Introdu destinația pentru a obține o estimare de livrare.';
-$_['text_shipping_method'] = 'Please select the preferred shipping method to use on this order.';
+$_['text_shipping_method'] = 'Te rugăm să selectezi metoda de livrare preferată pentru a fi utilizată la această comandă.';
 
 // Entry
-$_['entry_country']        = 'Country';
-$_['entry_zone']           = 'Region / State';
-$_['entry_postcode']       = 'Post Code';
+$_['entry_country']        = 'Țară';
+$_['entry_zone']           = 'Regiune / Stat';
+$_['entry_postcode']       = 'Cod Poștal';
 
 // Error
-$_['error_postcode']       = 'Postcode must be between 2 and 10 characters!';
-$_['error_country']        = 'Please select a country!';
-$_['error_zone']           = 'Please select a region / state!';
+$_['error_postcode']       = 'Codul poștal trebuie să fie între 2 și 10 caractere!';
+$_['error_country']        = 'Te rugăm selectează o țară!';
+$_['error_zone']           = 'Te rugăm selectează o regiune / stat!';
 $_['error_shipping']       = 'Warning: Shipping method required!';
-$_['error_no_shipping']    = 'Warning: No Shipping options are available. Please <a href="%s">contact us</a> for assistance!';
+$_['error_no_shipping']    = 'Atenție: Nu sunt disponibile opțiuni de livrare. Te rugăm <a href="%s">contactează-ne</a> pentru asistență!';
