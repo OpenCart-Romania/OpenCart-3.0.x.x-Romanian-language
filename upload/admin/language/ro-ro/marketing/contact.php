d4d591e62e75755308ddecee9061d58f39ef313b
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:20 +0800

    New translations contact.php (Romanian)

--- upload/admin/language/ro-ro/marketing/contact.php
+++ upload/admin/language/ro-ro/marketing/contact.php
@@ -6,28 +6,28 @@ $_['heading_title']        = 'Mail';
 $_['text_success']         = 'Your message has been successfully sent!';
 $_['text_sent']            = 'Your message has been successfully sent to %s - %s recipients!';
 $_['text_list']            = 'Mail List';
-$_['text_default']         = 'Implicit';
+$_['text_default']         = 'Default';
 $_['text_newsletter']      = 'All Newsletter Subscribers';
 $_['text_customer_all']    = 'All Customers';
-$_['text_customer_group']  = 'Grupul de Clienți';
-$_['text_customer']        = 'Clienți';
+$_['text_customer_group']  = 'Customer Group';
+$_['text_customer']        = 'Customers';
 $_['text_affiliate_all']   = 'Toți Afiliații';
 $_['text_affiliate']       = 'Afiliați';
-$_['text_product']         = 'Produse';
+$_['text_product']         = 'Products';
 
 // Entry
 $_['entry_store']          = 'From';
 $_['entry_to']             = 'To';
-$_['entry_customer_group'] = 'Grupul de Clienți';
-$_['entry_customer']       = 'Client';
-$_['entry_affiliate']      = 'Afiliat';
-$_['entry_product']        = 'Produse';
+$_['entry_customer_group'] = 'Customer Group';
+$_['entry_customer']       = 'Customer';
+$_['entry_affiliate']      = 'Affiliate';
+$_['entry_product']        = 'Products';
 $_['entry_subject']        = 'Subject';
-$_['entry_message']        = 'Mesaj';
+$_['entry_message']        = 'Message';
 
 // Help
-$_['help_customer']        = '(Completare automată)';
-$_['help_affiliate']       = '(Completare automată)';
+$_['help_customer']        = '(Autocomplete)';
+$_['help_affiliate']       = '(Autocomplete)';
 $_['help_product']         = 'Send only to customers who have ordered products in the list. (Autocomplete)';
 
 // Error
