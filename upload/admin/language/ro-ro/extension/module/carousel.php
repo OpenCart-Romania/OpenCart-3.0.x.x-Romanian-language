eb1c99cb6f6b729abac4511e7ebbb2f1d7d8ab5d
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:13 +0800

    New translations carousel.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/carousel.php
+++ upload/admin/language/ro-ro/extension/module/carousel.php
@@ -3,19 +3,19 @@
 $_['heading_title']    = 'Carousel';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Succes: Ai modificat modulul carusel!';
 $_['text_edit']        = 'Editare Modul Carusel';
 
 // Entry
 $_['entry_name']       = 'Module Name';
 $_['entry_banner']     = 'Banner';
-$_['entry_width']      = 'Width';
-$_['entry_height']     = 'Height';
-$_['entry_status']     = 'Status';
+$_['entry_width']      = 'Lăţime';
+$_['entry_height']     = 'Înălţime';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul carusel!';
-$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_name']       = 'Numele Modulului trebuie să fie între 3 și 64 de caractere!';
 $_['error_width']      = 'Width required!';
 $_['error_height']     = 'Height required!';
