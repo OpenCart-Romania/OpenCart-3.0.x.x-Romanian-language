3c4670abc843e4e4a85c723802e762d7b6dfdacc
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:22 +0800

    New translations marketing.php (Romanian)

--- upload/admin/language/ro-ro/marketing/marketing.php
+++ upload/admin/language/ro-ro/marketing/marketing.php
@@ -7,22 +7,22 @@ $_['text_success']      = 'Succes: Ai modificat urmărirea marketing-ului!';
 $_['text_list']         = 'Listă Urmărire Marketing';
 $_['text_add']          = 'Adaugă Urmărire Marketing';
 $_['text_edit']         = 'Editare Urmărire Marketing';
-$_['text_filter']       = 'Filtru';
+$_['text_filter']       = 'Filter';
 		
 // Column
 $_['column_name']       = 'Campaign Name';
 $_['column_code']       = 'Code';
-$_['column_clicks']     = 'Click-uri';
-$_['column_orders']     = 'Comenzi';
-$_['column_date_added'] = 'Data Adăugării';
-$_['column_action']     = 'Acțiune';
+$_['column_clicks']     = 'Clicks';
+$_['column_orders']     = 'Orders';
+$_['column_date_added'] = 'Date Added';
+$_['column_action']     = 'Action';
 
 // Entry
 $_['entry_name']        = 'Campaign Name';
 $_['entry_description'] = 'Campaign Description';
-$_['entry_code']        = 'Cod de Urmărire';
+$_['entry_code']        = 'Tracking Code';
 $_['entry_example']     = 'Examples';
-$_['entry_date_added']  = 'Data Adăugării';
+$_['entry_date_added']  = 'Date Added';
 
 // Help
 $_['help_code']         = 'Codul de urmărire care va fi utilizat pentru a urmări campaniile de marketing.';
@@ -31,5 +31,5 @@ $_['help_example']      = 'So the system can track referrals you need to add the
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica urmărirea marketing-ului!';
 $_['error_name']        = 'Campaign must be between 1 and 32 characters!';
-$_['error_code']        = 'Cod de Urmărire necesar!';
+$_['error_code']        = 'Tracking Code required!';
 $_['error_exists']      = 'Tracking code is being used by another campaign!';
