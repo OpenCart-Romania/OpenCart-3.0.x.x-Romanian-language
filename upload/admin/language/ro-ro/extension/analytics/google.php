3be49cc06a5acccc80bcd87ed7bb659a94b01d88
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:28:39 +0800

    New translations google.php (Romanian)

--- upload/admin/language/ro-ro/extension/analytics/google.php
+++ upload/admin/language/ro-ro/extension/analytics/google.php
@@ -2,15 +2,15 @@
 $_['heading_title']    = 'Google Analytics';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']	   = 'Succes: Ai modificat Google Analytics!';
 $_['text_edit']        = 'Editare Google Analytics';
 $_['text_signup']      = 'Conectează-te la contul <a href="http://www.google.com/analytics/" target="_blank"><u>Google Analytics</u></a> și după crearea profilului website-ului copiați și lipiți codul de analiză în acest câmp.';
-$_['text_default']     = 'Default';
+$_['text_default']     = 'Implicit';
 
 // Entry
 $_['entry_code']       = 'Code Google Analytics';
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Stare';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica Google Analytics!';
