e62430b73524b8327fc9d98b123c8634c11169dc
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:12 +0800

    New translations tax_rate.php (Romanian)

--- upload/admin/language/ro-ro/localisation/tax_rate.php
+++ upload/admin/language/ro-ro/localisation/tax_rate.php
@@ -1,29 +1,29 @@
 <?php
 // Heading
-$_['heading_title']        = 'Cote Taxare';
+$_['heading_title']        = 'Tax Rates';
 
 // Text
 $_['text_success']         = 'Success: You have modified tax rates!';
 $_['text_list']            = 'Tax Rate List';
 $_['text_add']             = 'Add Tax Rate';
 $_['text_edit']            = 'Edit Tax Rate';
-$_['text_percent']         = 'Procent';
-$_['text_amount']          = 'Valoare Fixă';
+$_['text_percent']         = 'Percentage';
+$_['text_amount']          = 'Fixed Amount';
 
 // Column
 $_['column_name']          = 'Tax Name';
 $_['column_rate']          = 'Tax Rate';
-$_['column_type']          = 'Tip';
+$_['column_type']          = 'Type';
 $_['column_geo_zone']      = 'Geo Zone';
-$_['column_date_added']    = 'Data Adăugării';
+$_['column_date_added']    = 'Date Added';
 $_['column_date_modified'] = 'Date Modified';
-$_['column_action']        = 'Acțiune';
+$_['column_action']        = 'Action';
 
 // Entry
 $_['entry_name']           = 'Tax Name';
 $_['entry_rate']           = 'Tax Rate';
-$_['entry_type']           = 'Tip';
-$_['entry_customer_group'] = 'Grupul de Clienți';
+$_['entry_type']           = 'Type';
+$_['entry_customer_group'] = 'Customer Group';
 $_['entry_geo_zone']       = 'Geo Zone';
 
 // Error
