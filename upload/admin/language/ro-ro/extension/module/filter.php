e16b2aa4ca41a9f4286d3c36a8e55a8d871a7552
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:42:43 +0800

    New translations filter.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/filter.php
+++ upload/admin/language/ro-ro/extension/module/filter.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']    = 'Filtru';
+$_['heading_title']    = 'Filter';
 
 // Text
-$_['text_extension']   = 'Extensii';
+$_['text_extension']   = 'Extensions';
 $_['text_success']     = 'Succes: Ai modificat modulul filtru!';
 $_['text_edit']        = 'Editare Modul Filtru';
 
 // Entry
-$_['entry_status']     = 'Stare';
+$_['entry_status']     = 'Status';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul filtre!';
