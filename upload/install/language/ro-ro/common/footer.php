a8a3e914a71e1bee775efe4bdb30e65cc1a115a8
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:50:04 +0800

    New translations footer.php (Romanian)

--- upload/install/language/ro-ro/common/footer.php
+++ upload/install/language/ro-ro/common/footer.php
@@ -1,5 +1,5 @@
 <?php
 // Text
 $_['text_project']       = 'Pagina de start a proiectului';
-$_['text_documentation'] = 'Documentație';
+$_['text_documentation'] = 'Documentation';
 $_['text_support']       = 'Forumuri de Suport';
