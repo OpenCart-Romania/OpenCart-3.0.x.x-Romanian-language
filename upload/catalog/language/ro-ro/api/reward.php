cd21a0d8b75bb4a611cc2946f7de84c6588f1d0c
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:32:03 +0800

    New translations reward.php (Romanian)

--- upload/catalog/language/ro-ro/api/reward.php
+++ upload/catalog/language/ro-ro/api/reward.php
@@ -3,7 +3,7 @@
 $_['text_success']     = 'Succes: Reducerea punctelor de recompensă a fost aplicată!';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to access the API!';
+$_['error_permission'] = 'Atenție: Nu ai permisiunea de a accesa API!';
 $_['error_reward']     = 'Atenție: Te rugăm să introduci valoarea punctelor de recompensă de utilizat!';
 $_['error_points']     = 'Atenție: Nu ai %s puncte de recompensă!';
 $_['error_maximum']    = 'Atenție: Numărul maxim de puncte care pot fi aplicate este %s!';
