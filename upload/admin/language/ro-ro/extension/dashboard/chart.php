c5841138c33282e87cab21c51c2799eca077c308
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:42:09 +0800

    New translations chart.php (Romanian)

--- upload/admin/language/ro-ro/extension/dashboard/chart.php
+++ upload/admin/language/ro-ro/extension/dashboard/chart.php
@@ -3,20 +3,20 @@
 $_['heading_title']    = 'Statistici Vânzări';
 
 // Text
-$_['text_extension']   = 'Extensii';
+$_['text_extension']   = 'Extensions';
 $_['text_success']     = 'Succes: Ai modificat dashboard-ul diagramă!';
 $_['text_edit']        = 'Editare Dashboard Diagramă';
-$_['text_order']       = 'Comenzi';
-$_['text_customer']    = 'Clienți';
-$_['text_day']         = 'Astăzi';
-$_['text_week']        = 'Săptămână';
-$_['text_month']       = 'Lună';
-$_['text_year']        = 'An';
+$_['text_order']       = 'Orders';
+$_['text_customer']    = 'Customers';
+$_['text_day']         = 'Today';
+$_['text_week']        = 'Week';
+$_['text_month']       = 'Month';
+$_['text_year']        = 'Year';
 
 // Entry
-$_['entry_status']     = 'Stare';
-$_['entry_sort_order'] = 'Ordine de Sortare';
-$_['entry_width']      = 'Lăţime';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+$_['entry_width']      = 'Width';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica dashboard-ul diagramă!';
