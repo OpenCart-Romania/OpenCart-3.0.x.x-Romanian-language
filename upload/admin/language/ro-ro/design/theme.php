b09edcb32ea6640603aa6695a9c722ef441e454c
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:28:35 +0800

    New translations theme.php (Romanian)

--- upload/admin/language/ro-ro/design/theme.php
+++ upload/admin/language/ro-ro/design/theme.php
@@ -7,7 +7,7 @@ $_['text_success']      = 'Succes: Ai modificat temele!';
 $_['text_edit']         = 'Editare Temă';
 $_['text_store']        = 'Alege-ți magazinul';
 $_['text_template']     = 'Alege un șablon';
-$_['text_default']      = 'Default';
+$_['text_default']      = 'Implicit';
 $_['text_history']      = 'Istoric Temă';
 $_['text_twig']         = 'Editorul de teme utilizează limbajul pentru șabloane Twig. Poți citi despre <a href="http://twig.sensiolabs.org/documentation" target="_blank" class="alert-link">Sintaxa Twig aici</a>.';
 
@@ -15,8 +15,8 @@ $_['text_twig']         = 'Editorul de teme utilizează limbajul pentru șabloan
 $_['column_store']      = 'Store';
 $_['column_route']      = 'Route';
 $_['column_theme']      = 'Theme';
-$_['column_date_added'] = 'Date Added';
-$_['column_action']     = 'Action';
+$_['column_date_added'] = 'Data Adăugării';
+$_['column_action']     = 'Acțiune';
 
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica editorul de teme!';
