64426377bf9d61358d61db191f6d0fb0199e2a13
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:32:53 +0800

    New translations pp_express.php (Romanian)

--- upload/catalog/language/ro-ro/extension/payment/pp_express.php
+++ upload/catalog/language/ro-ro/extension/payment/pp_express.php
@@ -6,9 +6,9 @@ $_['express_text_title']      = 'Confirm order';
 $_['text_title']              = 'PayPal Express Checkout';
 $_['text_cart']               = 'Shopping Cart';
 $_['text_shipping_updated']   = 'Shipping service updated';
-$_['text_trial']              = '%s every %s %s for %s payments then ';
-$_['text_recurring']          = '%s every %s %s';
-$_['text_recurring_item']     = 'Recurring Item';
+$_['text_trial']              = '%s la fiecare %s %s pentru %s plați atunci ';
+$_['text_recurring']          = '%s la fiecare %s %s';
+$_['text_recurring_item']     = 'Articol Recurent';
 $_['text_length']             = ' for %s payments';
 
 // Entry
@@ -24,4 +24,4 @@ $_['button_express_shipping'] = 'Update shipping';
 $_['error_heading_title']	  = 'There was an error';
 $_['error_too_many_failures'] = 'Your payment has failed too many times';
 $_['error_unavailable'] 	  = 'Please use the full checkout with this order';
-$_['error_no_shipping']    	  = 'Warning: No Shipping options are available. Please <a href="%s">contact us</a> for assistance!';
+$_['error_no_shipping']    	  = 'Atenție: Nu sunt disponibile opțiuni de livrare. Te rugăm <a href="%s">contactează-ne</a> pentru asistență!';
