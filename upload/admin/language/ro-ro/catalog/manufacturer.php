e10213b24f63741a813c7fc2ef5fc565bb26869c
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:28:17 +0800

    New translations manufacturer.php (Romanian)

--- upload/admin/language/ro-ro/catalog/manufacturer.php
+++ upload/admin/language/ro-ro/catalog/manufacturer.php
@@ -7,27 +7,27 @@ $_['text_success']      = 'Succes: Ai modificat producătorii!';
 $_['text_list']         = 'Listă Producători';
 $_['text_add']          = 'Adaugă Producător';
 $_['text_edit']         = 'Editare Producător';
-$_['text_default']      = 'Default';
+$_['text_default']      = 'Implicit';
 $_['text_percent']      = 'Procent';
 $_['text_amount']       = 'Valoare Fixă';
-$_['text_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['text_keyword']      = 'Nu utiliza spații, în schimb înlocuiește spațiile cu - și asigură-te că SEO URL este unic global.';
 
 // Column
 $_['column_name']       = 'Nume Producător';
-$_['column_sort_order'] = 'Sort Order';
-$_['column_action']     = 'Action';
+$_['column_sort_order'] = 'Ordine de Sortare';
+$_['column_action']     = 'Acțiune';
 
 // Entry
-$_['entry_name']        = 'Manufacturer Name';
-$_['entry_store']       = 'Stores';
-$_['entry_keyword']     = 'Keyword';
-$_['entry_image']       = 'Image';
-$_['entry_sort_order']  = 'Sort Order';
+$_['entry_name']        = 'Nume Producător';
+$_['entry_store']       = 'Magazine';
+$_['entry_keyword']     = 'Cuvânt Cheie';
+$_['entry_image']       = 'Imagine';
+$_['entry_sort_order']  = 'Ordine Sortare';
 $_['entry_type']        = 'Tip';
 
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica producătorii!';
 $_['error_name']        = 'Nume Producător trebuie să fie între 1 și 64 de caractere!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_unique']      = 'SEO URL must be unique!';
+$_['error_keyword']     = 'SEO URL este deja utilizat!';
+$_['error_unique']      = 'SEO URL trebuie să fie unic!';
 $_['error_product']     = 'Atenție: Acest producător nu poate fi șters deoarece în prezent este asociat cu %s produse!';
