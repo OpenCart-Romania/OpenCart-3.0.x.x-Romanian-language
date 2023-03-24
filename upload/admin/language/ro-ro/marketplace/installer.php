4810566830974e6f7aa53da0fd93e67b92621ad3
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:26 +0800

    New translations installer.php (Romanian)

--- upload/admin/language/ro-ro/marketplace/installer.php
+++ upload/admin/language/ro-ro/marketplace/installer.php
@@ -10,9 +10,9 @@ $_['text_success']      = 'Success: The extension has been installed!';
 $_['text_install']      = 'Installing';
 
 // Column
-$_['column_filename']   = 'Nume Fișier';
-$_['column_date_added'] = 'Data Adăugării';
-$_['column_action']     = 'Acțiune';
+$_['column_filename']   = 'Filename';
+$_['column_date_added'] = 'Date Added';
+$_['column_action']     = 'Action';
 
 // Entry
 $_['entry_upload']      = 'Încarcă Fișier';
@@ -25,5 +25,5 @@ $_['help_upload']       = 'Requires a modification file with extension \'.ocmod.
 $_['error_permission']  = 'Warning: You do not have permission to modify extensions!';
 $_['error_install']     = 'Extension installation taking place please wait a few seconds before trying to install!';
 $_['error_upload']      = 'File could not be uploaded!';
-$_['error_filetype']    = 'Tip fișier incorect!';
+$_['error_filetype']    = 'Invalid file type!';
 $_['error_file']        = 'File could not be found!';
