76d5afce8b5677a2d080215786b985f1af7c8466
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:03 +0800

    New translations geo_zone.php (Romanian)

--- upload/admin/language/ro-ro/localisation/geo_zone.php
+++ upload/admin/language/ro-ro/localisation/geo_zone.php
@@ -1,23 +1,23 @@
 <?php
 // Heading
-$_['heading_title']      = 'Zone Geografice';
+$_['heading_title']      = 'Geo Zones';
 
 // Text
 $_['text_success']       = 'Succes: Ai modificat zonele geografice!';
 $_['text_list']          = 'Geo Zone List';
-$_['text_add']           = 'Adaugă Zonă Geografică';
+$_['text_add']           = 'Add Geo Zone';
 $_['text_edit']          = 'Edit Geo Zone';
-$_['text_geo_zone']      = 'Zone Geografice';
+$_['text_geo_zone']      = 'Geo Zones';
 
 // Column
 $_['column_name']        = 'Geo Zone Name';
-$_['column_description'] = 'Descriere';
-$_['column_action']      = 'Acțiune';
+$_['column_description'] = 'Description';
+$_['column_action']      = 'Action';
 
 // Entry
 $_['entry_name']         = 'Geo Zone Name';
-$_['entry_description']  = 'Descriere';
-$_['entry_country']      = 'Țară';
+$_['entry_description']  = 'Description';
+$_['entry_country']      = 'Country';
 $_['entry_zone']         = 'Zone';
 
 // Error
