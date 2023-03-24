14597e07598d8a4e8f15a51c1a4e595c839218d2
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:38 +0800

    New translations store.php (Romanian)

--- upload/admin/language/ro-ro/setting/store.php
+++ upload/admin/language/ro-ro/setting/store.php
@@ -1,60 +1,60 @@
 <?php
 // Heading
-$_['heading_title']                = 'Magazine';
+$_['heading_title']                = 'Stores';
 
 // Text
-$_['text_settings']                = 'Setări';
+$_['text_settings']                = 'Settings';
 $_['text_success']                 = 'Succes: Ai modificat Magazine!';
 $_['text_list']                    = 'Store List';
 $_['text_add']                     = 'Add Store';
 $_['text_edit']                    = 'Edit Store';
 $_['text_items']                   = 'Items';
-$_['text_tax']                     = 'Taxe';
-$_['text_account']                 = 'Cont';
+$_['text_tax']                     = 'Taxes';
+$_['text_account']                 = 'Account';
 $_['text_checkout']                = 'Checkout';
-$_['text_stock']                   = 'Stoc';
-$_['text_shipping']                = 'Adresă Livrare';
-$_['text_payment']                 = 'Adresă Plată';
+$_['text_stock']                   = 'Stock';
+$_['text_shipping']                = 'Shipping Address';
+$_['text_payment']                 = 'Payment Address';
 
 // Column
 $_['column_name']                  = 'Store Name';
 $_['column_url']	               = 'Store URL';
-$_['column_action']                = 'Acțiune';
+$_['column_action']                = 'Action';
 
 // Entry
 $_['entry_url']                    = 'Store URL';
 $_['entry_ssl']                    = 'SSL URL';
 $_['entry_meta_title']             = 'Meta Title';
-$_['entry_meta_description']       = 'Meta Tag Descriere';
-$_['entry_meta_keyword']           = 'Meta Tag Cuvinte Cheie';
-$_['entry_layout']                 = 'Layout Implicit';
-$_['entry_theme']                  = 'Temă';
+$_['entry_meta_description']       = 'Meta Tag Description';
+$_['entry_meta_keyword']           = 'Meta Tag Keywords';
+$_['entry_layout']                 = 'Default Layout';
+$_['entry_theme']                  = 'Theme';
 $_['entry_name']                   = 'Store Name';
 $_['entry_owner']                  = 'Store Owner';
-$_['entry_address']                = 'Adresă';
+$_['entry_address']                = 'Address';
 $_['entry_geocode']                = 'Geocode';
 $_['entry_email']                  = 'E-Mail';
-$_['entry_telephone']              = 'Telefon';
+$_['entry_telephone']              = 'Telephone';
 $_['entry_fax']                    = 'Fax';
-$_['entry_image']                  = 'Imagine';
-$_['entry_open']                   = 'Program de Lucru';
-$_['entry_comment']                = 'Comentariu';
-$_['entry_location']               = 'Locație Magazin';
-$_['entry_country']                = 'Țară';
-$_['entry_zone']                   = 'Regiune / Stat';
-$_['entry_language']               = 'Limbă';
-$_['entry_currency']               = 'Monedă';
+$_['entry_image']                  = 'Image';
+$_['entry_open']                   = 'Opening Times';
+$_['entry_comment']                = 'Comment';
+$_['entry_location']               = 'Store Location';
+$_['entry_country']                = 'Country';
+$_['entry_zone']                   = 'Region / State';
+$_['entry_language']               = 'Language';
+$_['entry_currency']               = 'Currency';
 $_['entry_tax']                    = 'Display Prices With Tax';
 $_['entry_tax_default']            = 'Use Store Tax Address';
 $_['entry_tax_customer']           = 'Use Customer Tax Address';
-$_['entry_customer_group']         = 'Grupul de Clienți';
-$_['entry_customer_group_display'] = 'Grupuri de Clienți';
+$_['entry_customer_group']         = 'Customer Group';
+$_['entry_customer_group_display'] = 'Customer Groups';
 $_['entry_customer_price']         = 'Login Display Prices';
 $_['entry_account']                = 'Account Terms';
 $_['entry_cart_weight']            = 'Display Weight on Cart Page';
-$_['entry_checkout_guest']         = 'Comandă fără Cont';
+$_['entry_checkout_guest']         = 'Guest Checkout';
 $_['entry_checkout']               = 'Checkout Terms';
-$_['entry_order_status']           = 'Stare Comandă';
+$_['entry_order_status']           = 'Order Status';
 $_['entry_stock_display']          = 'Display Stock';
 $_['entry_stock_checkout']         = 'Stock Checkout';
 $_['entry_logo']                   = 'Store Logo';
@@ -84,15 +84,15 @@ $_['help_icon']                    = 'The icon should be a PNG that is 16px x 16
 $_['help_secure']                  = 'To use SSL check with your host if a SSL certificate is installed.';
 
 // Error
-$_['error_warning']                = 'Atenție: Te rugăm să verifici cu atenție formularul pentru erori!';
+$_['error_warning']                = 'Warning: Please check the form carefully for errors!';
 $_['error_permission']             = 'Warning: You do not have permission to modify stores!';
 $_['error_url']                    = 'Store URL required!';
 $_['error_meta_title']             = 'Title must be between 3 and 32 characters!';
 $_['error_name']                   = 'Store Name must be between 3 and 32 characters!';
-$_['error_owner']                  = 'Proprietarul Magazinului trebuie să fie între 3 și 64 de caractere!';
+$_['error_owner']                  = 'Store Owner must be between 3 and 64 characters!';
 $_['error_address']                = 'Store Address must be between 10 and 256 characters!';
-$_['error_email']                  = 'Adresa E-Mail nu pare să fie validă!';
-$_['error_telephone']              = 'Telefonul trebuie să fie între 3 și 32 de caractere!';
+$_['error_email']                  = 'E-Mail Address does not appear to be valid!';
+$_['error_telephone']              = 'Telephone must be between 3 and 32 characters!';
 $_['error_customer_group_display'] = 'You must include the default customer group if you are going to use this feature!';
 $_['error_default']                = 'Warning: You can not delete your default store!';
 $_['error_store']                  = 'Atenție: Acest Magazin nu poate fi șters, deoarece în prezent este atribuit la %s comenzi!';
