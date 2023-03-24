b2df0ad54aaf21f998485841bbe937bd2f794ea7
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:44 +0800

    New translations g2apay.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/g2apay.php
+++ upload/admin/language/ro-ro/extension/payment/g2apay.php
@@ -3,7 +3,7 @@
 $_['heading_title']				 	  = 'G2APay';
 
 // Text
-$_['text_extension']				  = 'Extensions';
+$_['text_extension']				  = 'Extensii';
 $_['text_success']				 	  = 'Success: You have modified the G2APay details.';
 $_['text_edit']					 	  = 'Edit G2APay';
 $_['text_g2apay']				 	  = '<a href="https://pay.g2a.com/" target="_blank"><img src="view/image/payment/g2apay.png" alt="G2APay" title="G2APay" style="border: 1px solid #EEEEEE;" /></a>';
@@ -21,7 +21,7 @@ $_['text_refund_ok_order']		 	  = 'Refund was successfully requested, amount ful
 $_['text_refund']                     = 'Refund';
 
 // Entry
-$_['entry_username']			 	  = 'Username';
+$_['entry_username']			 	  = 'Nume de utilizator';
 $_['entry_secret']				 	  = 'Secret';
 $_['entry_api_hash']		     	  = 'API Hash';
 $_['entry_environment']			 	  = 'Environment';
@@ -29,10 +29,10 @@ $_['entry_secret_token']		 	  = 'Secret Token';
 $_['entry_ipn_url']				 	  = 'IPN URL:';
 $_['entry_total']				 	  = 'Total';
 $_['entry_geo_zone']			 	  = 'Geo Zone';
-$_['entry_status']				 	  = 'Status';
-$_['entry_sort_order']			 	  = 'Sort Order';
+$_['entry_status']				 	  = 'Stare';
+$_['entry_sort_order']			 	  = 'Ordine Sortare';
 $_['entry_debug']				 	  = 'Debug logging';
-$_['entry_order_status']			  = 'Order Status';
+$_['entry_order_status']			  = 'Stare Comandă';
 $_['entry_complete_status']			  = 'Complete Status:';
 $_['entry_rejected_status']			  = 'Rejected Status:';
 $_['entry_cancelled_status']		  = 'Canceled Status:';
@@ -54,7 +54,7 @@ $_['error_permission']			 	  = 'Atenție: Nu ai permisiunea de a modifica G2APay
 $_['error_email']				 	  = 'E-Mail Required!';
 $_['error_secret']				 	  = 'Secret Required!';
 $_['error_api_hash']			 	  = 'API Hash Required!';
-$_['entry_status']				 	  = 'Status';
+$_['entry_status']				 	  = 'Stare';
 
 //Button
 $_['btn_refund']				 	  = 'refund';
