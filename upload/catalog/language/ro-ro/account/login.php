268f312430fb796f932fdad617ffadf6bab3538e
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:50 +0800

    New translations login.php (Romanian)

--- upload/catalog/language/ro-ro/account/login.php
+++ upload/catalog/language/ro-ro/account/login.php
@@ -3,20 +3,20 @@
 $_['heading_title']                = 'Autentificare Cont';
 
 // Text
-$_['text_account']                 = 'Cont';
-$_['text_login']                   = 'Conectare';
+$_['text_account']                 = 'Account';
+$_['text_login']                   = 'Login';
 $_['text_new_customer']            = 'Client Nou';
 $_['text_register']                = 'Înregistrare Cont';
 $_['text_register_account']        = 'Prin crearea unui cont, vei putea să faci cumpărături mai rapid, să fi la curent cu starea unei comenzi și să urmărești comenzile pe care le-ai făcut anterior.';
 $_['text_returning_customer']      = 'Client Existent';
 $_['text_i_am_returning_customer'] = 'Sunt un client existent';
-$_['text_forgotten']               = 'Parolă Uitată';
+$_['text_forgotten']               = 'Forgotten Password';
 
 // Entry
-$_['entry_email']                  = 'Adresă de E-Mail';
-$_['entry_password']               = 'Parolă';
+$_['entry_email']                  = 'E-Mail Address';
+$_['entry_password']               = 'Password';
 
 // Error
 $_['error_login']                  = 'Atenție: Adresa de E-Mail și/sau Parola nu se potrivesc.';
 $_['error_attempts']               = 'Atenție: Contul tău a depășit numărul permis de încercări de conectare. Te rugăm să încerci din nou în 1 oră.';
-$_['error_approved']               = 'Atenție: Contul tău necesită aprobare înainte de a te putea autentifica.';
+$_['error_approved']               = 'Warning: Your account requires approval before you can login.';
