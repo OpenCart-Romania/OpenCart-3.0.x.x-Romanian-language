78ad7ad69cbba4eca909b3351289cba6d011958b
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:56 +0800

    New translations perpetual_payments.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/perpetual_payments.php
+++ upload/admin/language/ro-ro/extension/payment/perpetual_payments.php
@@ -3,7 +3,7 @@
 $_['heading_title']		 = 'Perpetual Payments';
 
 // Text
-$_['text_extension']	 = 'Extensions';
+$_['text_extension']	 = 'Extensii';
 $_['text_success']		 = 'Success: You have modified Perpetual Payments account details!';
 $_['text_edit']          = 'Edit Perpetual Payments';
 
@@ -12,10 +12,10 @@ $_['entry_auth_id']		 = 'Authorization ID';
 $_['entry_auth_pass']	 = 'Authorization Password';
 $_['entry_test']		 = 'Test Mode';
 $_['entry_total']		 = 'Total';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_order_status'] = 'Stare Comandă';
 $_['entry_geo_zone']	 = 'Geo Zone';
-$_['entry_status']		 = 'Status';
-$_['entry_sort_order']	 = 'Sort Order';
+$_['entry_status']		 = 'Stare';
+$_['entry_sort_order']	 = 'Ordine Sortare';
 
 // Help
 $_['help_test']			 = 'Use this module in Test (YES) or Production mode (NO)?';
