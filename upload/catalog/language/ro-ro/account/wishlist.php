0121bdeee484b1a77dbf9b02e284d403630440a8
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:31:55 +0800

    New translations wishlist.php (Romanian)

--- upload/catalog/language/ro-ro/account/wishlist.php
+++ upload/catalog/language/ro-ro/account/wishlist.php
@@ -3,7 +3,7 @@
 $_['heading_title'] = 'Lista Mea de Dorințe';
 
 // Text
-$_['text_account']  = 'Account';
+$_['text_account']  = 'Cont';
 $_['text_instock']  = 'În Stoc';
 $_['text_wishlist'] = 'Listă de Dorințe (%s)';
 $_['text_login']    = 'Trebuie să <a href="%s">te autentifici</a> sau <a href="%s">să creezi un cont</a> pentru a salva <a href="%s">%s</a> în <a href="%s">lista ta de dorințe</a>!';
@@ -12,9 +12,9 @@ $_['text_remove']   = 'Succes: Ți-ai modificat lista de dorințe!';
 $_['text_empty']    = 'Lista ta de dorințe este goala.';
 
 // Column
-$_['column_image']  = 'Image';
-$_['column_name']   = 'Product Name';
+$_['column_image']  = 'Imagine';
+$_['column_name']   = 'Nume Produs';
 $_['column_model']  = 'Model';
 $_['column_stock']  = 'Stock';
 $_['column_price']  = 'Unit Price';
-$_['column_action'] = 'Action';
+$_['column_action'] = 'Acțiune';
