348837c39cc61bcc4db42fbecb51da71ef96745e
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:45 +0800

    New translations account.php (Romanian)

--- upload/catalog/language/ro-ro/account/account.php
+++ upload/catalog/language/ro-ro/account/account.php
@@ -3,8 +3,8 @@
 $_['heading_title']       = 'Contul Meu';
 
 // Text
-$_['text_account']        = 'Cont';
-$_['text_my_account']     = 'Contul Meu';
+$_['text_account']        = 'Account';
+$_['text_my_account']     = 'My Account';
 $_['text_my_orders']      = 'Comenzile Mele';
 $_['text_my_affiliate']   = 'Contul Meu de Afiliat';
 $_['text_my_newsletter']  = 'Newsletter';
@@ -14,13 +14,13 @@ $_['text_address']        = 'Modificați intrările din agenda dvs.';
 $_['text_credit_card']    = 'Gestionați cardurile de credit stocate';
 $_['text_wishlist']       = 'Modificați lista de dorințe';
 $_['text_order']          = 'Vezi istoricul comenzilor tale';
-$_['text_download']       = 'Descărcări';
+$_['text_download']       = 'Downloads';
 $_['text_reward']         = 'Punctele Tale de Recompensă';
 $_['text_return']         = 'Vezi solicitările de returnare';
 $_['text_transaction']    = 'Tranzacțiile Tale';
 $_['text_newsletter']     = 'Abonare / dezabonare la newsletter';
 $_['text_recurring']      = 'Plăţi recurente';
-$_['text_transactions']   = 'Tranzacții';
+$_['text_transactions']   = 'Transactions';
 $_['text_affiliate_add']  = 'Înregistrați-vă pentru un cont de afiliat';
 $_['text_affiliate_edit'] = 'Editați informațiile de afiliat';
 $_['text_tracking']       = 'Cod Personalizat de Urmărire Afiliați';
