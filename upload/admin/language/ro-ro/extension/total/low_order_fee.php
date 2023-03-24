74b03552d572de17e820066682e3306d0b3c8c50
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:30:50 +0800

    New translations low_order_fee.php (Romanian)

--- upload/admin/language/ro-ro/extension/total/low_order_fee.php
+++ upload/admin/language/ro-ro/extension/total/low_order_fee.php
@@ -3,16 +3,16 @@
 $_['heading_title']    = 'Comision Comandă Scăzută';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Success: You have modified low order fee total!';
 $_['text_edit']        = 'Edit Low Order Fee Total';
 
 // Entry
 $_['entry_total']      = 'Order Total';
 $_['entry_fee']        = 'Fee';
-$_['entry_tax_class']  = 'Tax Class';
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_tax_class']  = 'Clasa Fiscală';
+$_['entry_status']     = 'Stare';
+$_['entry_sort_order'] = 'Ordine Sortare';
 
 // Help
 $_['help_total']       = 'The checkout total the order must reach before this order total is deactivated.';
