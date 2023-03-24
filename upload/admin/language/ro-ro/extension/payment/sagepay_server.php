95818b4bb5d11fd2b0911abf56bd0f8c23d3674d
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:43:40 +0800

    New translations sagepay_server.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/sagepay_server.php
+++ upload/admin/language/ro-ro/extension/payment/sagepay_server.php
@@ -3,7 +3,7 @@
 $_['heading_title']			  = 'SagePay Server';
 
 // Text
-$_['text_extension']		  = 'Extensii';
+$_['text_extension']		  = 'Extensions';
 $_['text_success']			  = 'Success: You have modified SagePay account details!';
 $_['text_edit']               = 'Edit SagePay Server';
 $_['text_sagepay_server']	  = '<a href="https://support.sagepay.com/apply/default.aspx?PartnerID=E511AF91-E4A0-42DE-80B0-09C981A3FB61" target="_blank"><img src="view/image/payment/sagepay.png" alt="SagePay" title="SagePay" style="border: 1px solid #EEEEEE;" /></a>';
@@ -12,7 +12,7 @@ $_['text_test']				  = 'Test';
 $_['text_live']				  = 'Live';
 $_['text_defered']			  = 'Defered';
 $_['text_authenticate']		  = 'Authenticate';
-$_['text_payment']		  = 'Plată';
+$_['text_payment']		  = 'Payment';
 $_['text_release_ok']		  = 'Release was successful';
 $_['text_release_ok_order']	  = 'Release was successful, order status updated to success - settled';
 $_['text_rebate_ok']		  = 'Rebate was successful';
@@ -25,15 +25,15 @@ $_['text_rebate_status']	  = 'Payment rebated';
 $_['text_order_ref']		  = 'Order ref';
 $_['text_order_total']		  = 'Total authorised';
 $_['text_total_released']	  = 'Total released';
-$_['text_transactions']		  = 'Tranzacții';
-$_['text_column_amount']	  = 'Valoare';
-$_['text_column_type']		  = 'Tip';
-$_['text_column_date_added']  = 'Creat';
+$_['text_transactions']		  = 'Transactions';
+$_['text_column_amount']	  = 'Amount';
+$_['text_column_type']		  = 'Type';
+$_['text_column_date_added']  = 'Created';
 $_['text_confirm_void']		  = 'Are you sure you want to void the payment?';
 $_['text_confirm_release']	  = 'Are you sure you want to release the payment?';
 $_['text_confirm_rebate']	  = 'Are you sure you want to rebate the payment?';
-$_['text_void']               = 'Nul';
-$_['text_payment']            = "Plată";
+$_['text_void']               = 'Void';
+$_['text_payment']            = "Payment";
 $_['text_rebate']             = 'Rebate';
 
 // Entry
@@ -41,10 +41,10 @@ $_['entry_vendor']			  = 'Vendor';
 $_['entry_test']			  = 'Test Mode';
 $_['entry_transaction']		  = 'Transaction Method';
 $_['entry_total']			  = 'Total';
-$_['entry_order_status']	  = 'Stare Comandă';
+$_['entry_order_status']	  = 'Order Status';
 $_['entry_geo_zone']		  = 'Geo Zone';
-$_['entry_status']			  = 'Stare';
-$_['entry_sort_order']		  = 'Ordine de Sortare';
+$_['entry_status']			  = 'Status';
+$_['entry_sort_order']		  = 'Sort Order';
 $_['entry_debug']			  = 'Debug logging';
 $_['entry_card']			  = 'Store Cards';
 $_['entry_cron_job_token']	  = 'Secret Token';
@@ -61,8 +61,8 @@ $_['help_cron_job_url']		  = 'Set a cron job to call this URL';
 // Button
 $_['button_release']		  = 'Release';
 $_['button_rebate']			  = 'Rebate / refund';
-$_['button_void']			  = 'Nul';
+$_['button_void']			  = 'Void';
 
 // Error
-$_['error_permission']		  = 'Atenție: Nu ai permisiunea de a modifica plata SagePay!';
+$_['error_permission']		  = 'Warning: You do not have permission to modify payment SagePay!';
 $_['error_vendor']			  = 'Vendor ID Required!';
