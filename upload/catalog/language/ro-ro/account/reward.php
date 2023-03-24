86570c5ff221b09514b84ba0f885ff90d6a78517
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:58 +0800

    New translations reward.php (Romanian)

--- upload/catalog/language/ro-ro/account/reward.php
+++ upload/catalog/language/ro-ro/account/reward.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']      = 'Punctele Tale de Recompensă';
+$_['heading_title']      = 'Your Reward Points';
 
 // Column
-$_['column_date_added']  = 'Data Adăugării';
-$_['column_description'] = 'Descriere';
-$_['column_points']      = 'Puncte';
+$_['column_date_added']  = 'Date Added';
+$_['column_description'] = 'Description';
+$_['column_points']      = 'Points';
 
 // Text
-$_['text_account']       = 'Cont';
-$_['text_reward']        = 'Puncte de Recompensă';
+$_['text_account']       = 'Account';
+$_['text_reward']        = 'Reward Points';
 $_['text_total']         = 'Numărul total de puncte de recompensă este:';
 $_['text_empty']         = 'Nu ai niciun punct de recompensă!';
