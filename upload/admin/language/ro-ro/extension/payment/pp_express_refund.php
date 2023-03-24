d31bb2323094d7ba3334d025a98fd90ee02801ba
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:43:30 +0800

    New translations pp_express_refund.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/pp_express_refund.php
+++ upload/admin/language/ro-ro/extension/payment/pp_express_refund.php
@@ -5,13 +5,13 @@ $_['heading_title']		   = 'Refund Transaction';
 // Text
 $_['text_pp_express']	   = 'PayPal Express Checkout';
 $_['text_current_refunds'] = 'Refunds have already been done for this transaction. The max refund is';
-$_['text_refund']		   = 'Stornare';
+$_['text_refund']		   = 'Refund';
 
 // Entry
 $_['entry_transaction_id'] = 'Transaction ID';
 $_['entry_full_refund']	   = 'Full refund';
-$_['entry_amount']		   = 'Valoare';
-$_['entry_message']		   = 'Mesaj';
+$_['entry_amount']		   = 'Amount';
+$_['entry_message']		   = 'Message';
 
 // Button
 $_['button_refund']		   = 'Issue refund';
