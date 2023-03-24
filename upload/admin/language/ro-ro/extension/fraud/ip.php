fdafb92c0f140ec067debee98cdc92fdb8a57fa2
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:05 +0800

    New translations ip.php (Romanian)

--- upload/admin/language/ro-ro/extension/fraud/ip.php
+++ upload/admin/language/ro-ro/extension/fraud/ip.php
@@ -3,7 +3,7 @@
 $_['heading_title']      = 'IP Anti-Fraudă';
 
 // Text
-$_['text_extension']     = 'Extensions';
+$_['text_extension']     = 'Extensii';
 $_['text_success']       = 'Succes: Ai modificat IP-ul Anti-Fraudă!';
 $_['text_edit']          = 'Editare IP Anti-Fraudă';
 $_['text_ip_add']        = 'Add IP Address';
@@ -11,14 +11,14 @@ $_['text_ip_list']       = 'Fraud IP Address List';
 
 // Column
 $_['column_ip']          = 'IP';
-$_['column_total']       = 'Total Accounts';
-$_['column_date_added']  = 'Date Added';
-$_['column_action']      = 'Action';
+$_['column_total']       = 'Total Conturi';
+$_['column_date_added']  = 'Data Adăugării';
+$_['column_action']      = 'Acțiune';
 
 // Entry
 $_['entry_ip']           = 'IP';
-$_['entry_status']       = 'Status';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_status']       = 'Stare';
+$_['entry_order_status'] = 'Stare Comandă';
 
 // Help
 $_['help_order_status']  = 'Customers that have a banned IP on their accounts will be assigned this order status and will not be allowed to reach the complete status automatically.';
