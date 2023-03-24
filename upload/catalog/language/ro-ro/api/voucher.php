88056cb2ed5c5c134ba67a94bf14cb7b2e73f4fc
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:48:13 +0800

    New translations voucher.php (Romanian)

--- upload/catalog/language/ro-ro/api/voucher.php
+++ upload/catalog/language/ro-ro/api/voucher.php
@@ -1,14 +1,14 @@
 <?php
 // Text
 $_['text_success']     = 'Succes: Reducerea voucher-ului cadou a fost aplicată!';
-$_['text_cart']        = 'Succes: Ți-ai modificat coșul de cumpărături!';
-$_['text_for']         = '%s Voucher Cadou pentru %s';
+$_['text_cart']        = 'Success: You have modified your shopping cart!';
+$_['text_for']         = '%s Gift Certificate for %s';
 
 // Error
-$_['error_permission'] = 'Atenție: Nu ai permisiunea de a accesa API!';
+$_['error_permission'] = 'Warning: You do not have permission to access the API!';
 $_['error_voucher']    = 'Atenție: Voucher-ul Cadou este fie incorect, fie soldul a fost epuizat!';
-$_['error_to_name']    = 'Numele Destinatarului trebuie să fie între 1 și 64 de caractere!';
-$_['error_from_name']  = 'Numele Tău trebuie să fie între 1 și 64 de caractere!';
-$_['error_email']      = 'Adresa E-Mail nu pare să fie validă!';
-$_['error_theme']      = 'Trebuie să selectezi o temă!';
-$_['error_amount']     = 'Suma trebuie să fie între %s și %s!';
+$_['error_to_name']    = 'Recipient\'s Name must be between 1 and 64 characters!';
+$_['error_from_name']  = 'Your Name must be between 1 and 64 characters!';
+$_['error_email']      = 'E-Mail Address does not appear to be valid!';
+$_['error_theme']      = 'You must select a theme!';
+$_['error_amount']     = 'Amount must be between %s and %s!';
