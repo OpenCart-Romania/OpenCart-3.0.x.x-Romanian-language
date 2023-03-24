d1e6ba802de435d689f5a8e9036adb53656d206c
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:31:38 +0800

    New translations user_group.php (Romanian)

--- upload/admin/language/ro-ro/user/user_group.php
+++ upload/admin/language/ro-ro/user/user_group.php
@@ -10,7 +10,7 @@ $_['text_edit']        = 'Edit User Group';
 
 // Column
 $_['column_name']      = 'User Group Name';
-$_['column_action']    = 'Action';
+$_['column_action']    = 'Acțiune';
 
 // Entry
 $_['entry_name']       = 'User Group Name';
