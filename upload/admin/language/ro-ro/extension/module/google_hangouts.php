45c3b9861ebbeacc64d0533e9b43fcc10e3addb6
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:42:44 +0800

    New translations google_hangouts.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/google_hangouts.php
+++ upload/admin/language/ro-ro/extension/module/google_hangouts.php
@@ -3,13 +3,13 @@
 $_['heading_title']    = 'Google Hangouts';
 
 // Text
-$_['text_extension']   = 'Extensii';
+$_['text_extension']   = 'Extensions';
 $_['text_success']     = 'Succes: Ai modificat modulul Google Hangouts!';
 $_['text_edit']        = 'Editare Modul Google Hangouts';
 
 // Entry
 $_['entry_code']       = 'Google Talk Code';
-$_['entry_status']     = 'Stare';
+$_['entry_status']     = 'Status';
 
 // Help
 $_['help_code']        = 'Goto <a href="https://developers.google.com/+/hangouts/button" target="_blank">Create a Google Hangout chatback badge</a> and copy &amp; paste the generated code into the text box.';
