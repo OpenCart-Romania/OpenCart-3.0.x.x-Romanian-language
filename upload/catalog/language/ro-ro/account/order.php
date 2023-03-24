732977095540843271321317f913faac8f115695
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:53 +0800

    New translations order.php (Romanian)

--- upload/catalog/language/ro-ro/account/order.php
+++ upload/catalog/language/ro-ro/account/order.php
@@ -1,37 +1,37 @@
 <?php
 // Heading
-$_['heading_title']         = 'Istoric Comenzi';
+$_['heading_title']         = 'Order History';
 
 // Text
-$_['text_account']          = 'Cont';
-$_['text_order']            = 'Informații Comandă';
-$_['text_order_detail']     = 'Detalii Comandă';
+$_['text_account']          = 'Account';
+$_['text_order']            = 'Order Information';
+$_['text_order_detail']     = 'Order Details';
 $_['text_invoice_no']       = 'Factură Nr.:';
-$_['text_order_id']         = 'Număr Comandă:';
+$_['text_order_id']         = 'Order ID:';
 $_['text_date_added']       = 'Data Adăugării:';
-$_['text_shipping_address'] = 'Adresă Livrare';
+$_['text_shipping_address'] = 'Shipping Address';
 $_['text_shipping_method']  = 'Metoda de Livrare:';
-$_['text_payment_address']  = 'Adresă Plată';
+$_['text_payment_address']  = 'Payment Address';
 $_['text_payment_method']   = 'Metodă de Plată:';
 $_['text_comment']          = 'Comentarii Comandă';
-$_['text_history']          = 'Istoric Comenzi';
+$_['text_history']          = 'Order History';
 $_['text_success']          = 'Succes: Ai adăugat <a href="%s">%s</a> la <a href="%s">coșul tău de cumpărături</a>!';
 $_['text_empty']            = 'Nu ai făcut nicio comandă anterior!';
 $_['text_error']            = 'Comanda pe care ai solicitat-o nu a putut fi găsită!';
 
 // Column
-$_['column_order_id']       = 'Număr Comandă';
-$_['column_customer']       = 'Client';
+$_['column_order_id']       = 'Order ID';
+$_['column_customer']       = 'Customer';
 $_['column_product']        = 'Nr. de Produse';
-$_['column_name']           = 'Nume Produs';
+$_['column_name']           = 'Product Name';
 $_['column_model']          = 'Model';
-$_['column_quantity']       = 'Cantitate';
-$_['column_price']          = 'Preț';
+$_['column_quantity']       = 'Quantity';
+$_['column_price']          = 'Price';
 $_['column_total']          = 'Total';
-$_['column_action']         = 'Acțiune';
-$_['column_date_added']     = 'Data Adăugării';
-$_['column_status']         = 'Stare';
-$_['column_comment']        = 'Comentariu';
+$_['column_action']         = 'Action';
+$_['column_date_added']     = 'Date Added';
+$_['column_status']         = 'Status';
+$_['column_comment']        = 'Comment';
 
 // Error
 $_['error_reorder']         = '%s nu este disponibil în prezent pentru a fi comandat din nou.';
