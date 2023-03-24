ebe666c0198e5ba5781cb713ab0b0deeee7e0c61
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:30:16 +0800

    New translations skrill.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/skrill.php
+++ upload/admin/language/ro-ro/extension/payment/skrill.php
@@ -3,7 +3,7 @@
 $_['heading_title']			  = 'Skrill';
 
 // Text
-$_['text_extension']		  = 'Extensions';
+$_['text_extension']		  = 'Extensii';
 $_['text_success']			  = 'Success: You have modified the Skrill details.';
 $_['text_edit']               = 'Edit Skrill';
 $_['text_skrill']	     	  = '<a href="https://content.skrill.com/en/ecommerce-solutions/opencart/" target="_blank"><img src="view/image/payment/skrill.png" alt="Skrill" title="Skrill" style="border: 1px solid #EEEEEE;" /></a>';
@@ -12,14 +12,14 @@ $_['text_skrill']	     	  = '<a href="https://content.skrill.com/en/ecommerce-so
 $_['entry_email']			  = 'E-Mail';
 $_['entry_secret']		      = 'Secret';
 $_['entry_total']			  = 'Total';
-$_['entry_order_status']	  = 'Order Status';
+$_['entry_order_status']	  = 'Stare Comandă';
 $_['entry_pending_status']	  = 'Pending Status ';
 $_['entry_canceled_status']	  = 'Canceled Status';
 $_['entry_failed_status']	  = 'Failed Status';
 $_['entry_chargeback_status'] = 'Chargeback Status';
 $_['entry_geo_zone']		  = 'Geo Zone';
-$_['entry_status']			  = 'Status';
-$_['entry_sort_order']		  = 'Sort Order';
+$_['entry_status']			  = 'Stare';
+$_['entry_sort_order']		  = 'Ordine Sortare';
 
 // Help
 $_['help_total']			  = 'The checkout total the order must reach before this payment method becomes active.';
