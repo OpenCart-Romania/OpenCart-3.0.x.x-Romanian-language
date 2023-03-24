64fcb90f488e86efa1086f73b35604ecf4d693af
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:31:16 +0800

    New translations voucher.php (Romanian)

--- upload/admin/language/ro-ro/mail/voucher.php
+++ upload/admin/language/ro-ro/mail/voucher.php
@@ -6,5 +6,5 @@ $_['text_greeting'] = 'Felicitări, Ai primit un Certificat Cadou în valoare de
 $_['text_from']     = 'Acest Certificat Cadou ți-a fost trimis de %s';
 $_['text_message']  = 'Cu un mesaj care spune';
 $_['text_redeem']   = 'Pentru a valorifica acest Certificat Cadou, notează codul de revendicare care este <b>%s</b>, apoi click pe link-ul de mai jos și cumpărați produsul dorit, pe care să utilizați acest certificat cadou. Puteți introduce codul certificatului cadou pe pagina coșului de cumpărături înainte de a face click pe checkout.';
-$_['text_footer']   = 'Please reply to this email if you have any questions.';
+$_['text_footer']   = 'Te rugăm să răspunzi la acest email dacă ai întrebări.';
 $_['text_sent']     = 'Success: Gift Voucher e-mail has been sent!';
