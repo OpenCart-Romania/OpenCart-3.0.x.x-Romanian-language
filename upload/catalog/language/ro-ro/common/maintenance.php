4d2732cddcdf61a709b0595b2e0334ca62db4466
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:32:14 +0800

    New translations maintenance.php (Romanian)

--- upload/catalog/language/ro-ro/common/maintenance.php
+++ upload/catalog/language/ro-ro/common/maintenance.php
@@ -3,5 +3,5 @@
 $_['heading_title']    = 'Maintenance';
 
 // Text
-$_['text_maintenance'] = 'Maintenance';
+$_['text_maintenance'] = 'Întreținere';
 $_['text_message']     = '<h1 style="text-align:center;">În prezent efectuăm o întreținere programată. <br/>Ne vom întoarce cât mai curând posibil. Te rugăm să revii în curând.</h1>';
