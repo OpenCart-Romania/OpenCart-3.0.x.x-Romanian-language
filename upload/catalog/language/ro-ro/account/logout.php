2147ec609606fb452925539f4625d9863cc64c1b
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:31:44 +0800

    New translations logout.php (Romanian)

--- upload/catalog/language/ro-ro/account/logout.php
+++ upload/catalog/language/ro-ro/account/logout.php
@@ -4,5 +4,5 @@ $_['heading_title'] = 'Deconectare Cont';
 
 // Text
 $_['text_message']  = '<p>Ai fost deconectat de la contul tău. Acum este sigur să părăsești calculatorul.</p><p>Coșul tău de cumpărături a fost salvat, articolele din el vor fi restaurate oricând te conectezi din nou în contul tău</p>';
-$_['text_account']  = 'Account';
-$_['text_logout']   = 'Logout';
+$_['text_account']  = 'Cont';
+$_['text_logout']   = 'Deconectare';
