ee0030af81126f2c2b4ffb3bc4e7bd0ea9d8a019
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:42:29 +0800

    New translations google_base.php (Romanian)

--- upload/admin/language/ro-ro/extension/feed/google_base.php
+++ upload/admin/language/ro-ro/extension/feed/google_base.php
@@ -3,7 +3,7 @@
 $_['heading_title']          = 'Google Base';
 
 // Text
-$_['text_extension']         = 'Extensii';
+$_['text_extension']         = 'Extensions';
 $_['text_success']           = 'Succes: Ai modificat feed-ul Google Base!';
 $_['text_edit']              = 'Editare Google Base';
 $_['text_import']            = 'To download the latest Google category list by <a href="https://support.google.com/merchants/answer/160081?hl=en" target="_blank" class="alert-link">clicking here</a> and choose taxonomy with numeric IDs in Plain Text (.txt) file. Upload via the green import button.';
@@ -11,15 +11,15 @@ $_['text_import']            = 'To download the latest Google category list by <
 // Column
 $_['column_google_category'] = 'Google Category';
 $_['column_category']        = 'Category';
-$_['column_action']          = 'Acțiune';
+$_['column_action']          = 'Action';
 
 // Entry
 $_['entry_google_category']  = 'Google Category';
 $_['entry_category']         = 'Category';
 $_['entry_data_feed']        = 'Data Feed Url';
-$_['entry_status']           = 'Stare';
+$_['entry_status']           = 'Status';
 
 // Error
 $_['error_permission']       = 'Atenție: Nu ai permisiunea de a modifica feed-ul Google Base!';
 $_['error_upload']           = 'File could not be uploaded!';
-$_['error_filetype']         = 'Tip fișier incorect!';
+$_['error_filetype']         = 'Invalid file type!';
