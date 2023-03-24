2f1f018b7174333caaee400f7aee150c87797329
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:44:13 +0800

    New translations marketing.php (Romanian)

--- upload/admin/language/ro-ro/extension/report/marketing.php
+++ upload/admin/language/ro-ro/extension/report/marketing.php
@@ -3,24 +3,24 @@
 $_['heading_title']    = 'Raport de Marketing';
 
 // Text
-$_['text_extension']   = 'Extensii';
+$_['text_extension']   = 'Extensions';
 $_['text_edit']        = 'Editare Raport de Marketing';
 $_['text_success']     = 'Succes: Ai modificat raportul de marketing!';
-$_['text_filter']      = 'Filtru';
+$_['text_filter']      = 'Filter';
 $_['text_all_status']  = 'All Statuses';
 
 // Column
 $_['column_campaign']  = 'Campaign Name';
 $_['column_code']      = 'Code';
-$_['column_clicks']    = 'Click-uri';
+$_['column_clicks']    = 'Clicks';
 $_['column_orders']    = 'No. Orders';
 $_['column_total']     = 'Total';
 
 // Entry
-$_['entry_date_start'] = 'Dată de Început';
-$_['entry_date_end']   = 'Dată de Sfârșit';
-$_['entry_status']     = 'Stare';
-$_['entry_sort_order'] = 'Ordine de Sortare';
+$_['entry_date_start'] = 'Date Start';
+$_['entry_date_end']   = 'Date End';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
 
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica raportul de marketing!';
