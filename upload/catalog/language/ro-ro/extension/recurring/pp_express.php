2737013af684b0527a7831c5e1996def862026a1
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:49:31 +0800

    New translations pp_express.php (Romanian)

--- upload/catalog/language/ro-ro/extension/recurring/pp_express.php
+++ upload/catalog/language/ro-ro/extension/recurring/pp_express.php
@@ -7,5 +7,5 @@ $_['text_canceled']       = 'Success: You have succesfully caneled this payment!
 $_['button_cancel']       = 'Anulează Plata Recurentă';
 
 // Error
-$_['error_not_cancelled'] = 'Eroare: %s';
-$_['error_not_found']     = 'Nu s-a putut anula profilul recurent';
+$_['error_not_cancelled'] = 'Error: %s';
+$_['error_not_found']     = 'Could not cancel recurring profile';
