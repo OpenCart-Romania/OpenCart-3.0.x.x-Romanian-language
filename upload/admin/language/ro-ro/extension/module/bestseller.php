530dfcbfe793bd318069be0fc65e75408bf2589f
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:42:39 +0800

    New translations bestseller.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/bestseller.php
+++ upload/admin/language/ro-ro/extension/module/bestseller.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'Cele mai vândute';
 
 // Text
-$_['text_extension']   = 'Extensii';
+$_['text_extension']   = 'Extensions';
 $_['text_success']     = 'Succes: Ai modificat modulul cele mai vândute!';
 $_['text_edit']        = 'Editare Modul Cele mai vândute';
 
@@ -11,12 +11,12 @@ $_['text_edit']        = 'Editare Modul Cele mai vândute';
 $_['entry_name']       = 'Module Name';
 $_['entry_limit']      = 'Limit';
 $_['entry_image']      = 'Image (W x H) and Resize Type';
-$_['entry_width']      = 'Lăţime';
-$_['entry_height']     = 'Înălţime';
-$_['entry_status']     = 'Stare';
+$_['entry_width']      = 'Width';
+$_['entry_height']     = 'Height';
+$_['entry_status']     = 'Status';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul cele mai vândute!';
-$_['error_name']       = 'Numele Modulului trebuie să fie între 3 și 64 de caractere!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
 $_['error_width']      = 'Width required!';
 $_['error_height']     = 'Height required!';
