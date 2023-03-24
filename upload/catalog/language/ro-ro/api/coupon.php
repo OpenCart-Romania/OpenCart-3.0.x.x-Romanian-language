22858f3f61abb6beb5985b9967e8bd03a9a37fee
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:48:07 +0800

    New translations coupon.php (Romanian)

--- upload/catalog/language/ro-ro/api/coupon.php
+++ upload/catalog/language/ro-ro/api/coupon.php
@@ -3,5 +3,5 @@
 $_['text_success']     = 'Succes: Cuponul tău de reducere a fost aplicat!';
 
 // Error
-$_['error_permission'] = 'Atenție: Nu ai permisiunea de a accesa API!';
+$_['error_permission'] = 'Warning: You do not have permission to access the API!';
 $_['error_coupon']     = 'Atenție: Cuponul este fie incorect, expirat sau a atins la limita de utilizare!';
