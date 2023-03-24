3e94c696e77cc862c2e343dd1210ec94ad3bd2ca
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:42:54 +0800

    New translations slideshow.php (Romanian)

--- upload/admin/language/ro-ro/extension/module/slideshow.php
+++ upload/admin/language/ro-ro/extension/module/slideshow.php
@@ -3,19 +3,19 @@
 $_['heading_title']    = 'Slideshow';
 
 // Text
-$_['text_extension']   = 'Extensii';
+$_['text_extension']   = 'Extensions';
 $_['text_success']     = 'Succes: Ai modificat modulul slideshow!';
 $_['text_edit']        = 'Edit Slideshow Module';
 
 // Entry
 $_['entry_name']       = 'Module Name';
 $_['entry_banner']     = 'Banner';
-$_['entry_width']      = 'Lăţime';
-$_['entry_height']     = 'Înălţime';
-$_['entry_status']     = 'Stare';
+$_['entry_width']      = 'Width';
+$_['entry_height']     = 'Height';
+$_['entry_status']     = 'Status';
 
 // Error
 $_['error_permission'] = 'Atenție: Nu ai permisiunea de a modifica modulul slideshow!';
-$_['error_name']       = 'Numele Modulului trebuie să fie între 3 și 64 de caractere!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
 $_['error_width']      = 'Width required!';
 $_['error_height']     = 'Height required!';
