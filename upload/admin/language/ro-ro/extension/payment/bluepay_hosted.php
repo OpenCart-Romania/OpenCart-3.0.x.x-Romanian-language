170e22fb8c65e208ac9bb73a7c15d858ab26e772
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:43:02 +0800

    New translations bluepay_hosted.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/bluepay_hosted.php
+++ upload/admin/language/ro-ro/extension/payment/bluepay_hosted.php
@@ -3,7 +3,7 @@
 $_['heading_title']			 = 'BluePay Hosted Form';
 
 // Text
-$_['text_extension']		 = 'Extensii';
+$_['text_extension']		 = 'Extensions';
 $_['text_success']			 = 'Succes: Ai modificat detaliile contului BluePay Hosted Form!';
 $_['text_edit']				 = 'Edit BluePay Hosted Form';
 $_['text_bluepay_hosted']	 = '<a href="http://www.bluepay.com/preferred-partner/opencart" target="_blank"><img src="view/image/payment/bluepay.jpg" alt="BluePay Hosted Form" title="BluePay Hosted Form" style="border: 1px solid #EEEEEE;" /></a>';
@@ -23,18 +23,18 @@ $_['text_rebate_status']	 = 'Payment rebated';
 $_['text_order_ref']		 = 'Order ref';
 $_['text_order_total']		 = 'Total authorised';
 $_['text_total_released']	 = 'Total released';
-$_['text_transactions']		 = 'Tranzacții';
-$_['text_column_amount']     = 'Valoare';
-$_['text_column_type']       = 'Tip';
-$_['text_column_date_added'] = 'Data Adăugării';
+$_['text_transactions']		 = 'Transactions';
+$_['text_column_amount']     = 'Amount';
+$_['text_column_type']       = 'Type';
+$_['text_column_date_added'] = 'Date Added';
 $_['text_confirm_void']		 = 'Are you sure you want to void the payment?';
 $_['text_confirm_release']	 = 'Are you sure you want to release the payment?';
 $_['text_confirm_rebate']	 = 'Are you sure you want to rebate the payment?';
-$_['text_payment']           = "Plată";
+$_['text_payment']           = "Payment";
 $_['text_rebate']            = "Rebate";
 
 // Entry
-$_['entry_account_name']	 = 'Nume Cont';
+$_['entry_account_name']	 = 'Account Name';
 $_['entry_account_id']		 = 'ID Cont';
 $_['entry_secret_key']		 = 'Secret Key';
 $_['entry_test']			 = 'Transaction Mode';
@@ -42,10 +42,10 @@ $_['entry_transaction']		 = 'Transaction Method';
 $_['entry_card_amex']		 = 'Amex';
 $_['entry_card_discover']	 = 'Discover';
 $_['entry_total']			 = 'Total';
-$_['entry_order_status']	 = 'Stare Comandă';
+$_['entry_order_status']	 = 'Order Status';
 $_['entry_geo_zone']		 = 'Geo Zone';
-$_['entry_status']			 = 'Stare';
-$_['entry_sort_order']		 = 'Ordine de Sortare';
+$_['entry_status']			 = 'Status';
+$_['entry_sort_order']		 = 'Sort Order';
 $_['entry_debug']			 = 'Debug logging';
 
 // Help
