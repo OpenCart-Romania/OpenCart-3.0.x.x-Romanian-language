01a4dca2b0fe5db17a4cb9268614cc6425410b64
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:11 +0800

    New translations tax_class.php (Romanian)

--- upload/admin/language/ro-ro/localisation/tax_class.php
+++ upload/admin/language/ro-ro/localisation/tax_class.php
@@ -1,29 +1,29 @@
 <?php
 // Heading
-$_['heading_title']     = 'Clase Fiscale';
+$_['heading_title']     = 'Tax Classes';
 
 // Text
 $_['text_success']      = 'Succes: Ai modificat clasele fiscale!';
 $_['text_list']         = 'Tax Class List';
 $_['text_add']          = 'Add Tax Class';
 $_['text_edit']         = 'Edit Tax Class';
-$_['text_tax_class']    = 'Clasa Fiscală';
-$_['text_tax_rate']     = 'Cote Taxare';
+$_['text_tax_class']    = 'Tax Class';
+$_['text_tax_rate']     = 'Tax Rates';
 $_['text_shipping']     = 'Adresă Livrare';
 $_['text_payment']      = 'Adresă Plată';
 $_['text_store']        = 'Store Address';
 
 // Column
 $_['column_title']      = 'Tax Class Title';
-$_['column_action']     = 'Acțiune';
+$_['column_action']     = 'Action';
 
 // Entry
 $_['entry_title']       = 'Tax Class Title';
-$_['entry_description'] = 'Descriere';
+$_['entry_description'] = 'Description';
 $_['entry_rate']        = 'Tax Rate';
 $_['entry_based']       = 'Based On';
 $_['entry_geo_zone']    = 'Geo Zone';
-$_['entry_priority']    = 'Prioritate';
+$_['entry_priority']    = 'Priority';
 
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica clasele fiscale!';
