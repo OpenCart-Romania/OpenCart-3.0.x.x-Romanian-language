7662dd3f1e17a31883861a7c2a41440496d89d5e
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:47:46 +0800

    New translations affiliate.php (Romanian)

--- upload/catalog/language/ro-ro/account/affiliate.php
+++ upload/catalog/language/ro-ro/account/affiliate.php
@@ -3,33 +3,33 @@
 $_['heading_title']             = 'Informațiile Tale de Afiliat';
 
 // Text
-$_['text_account']              = 'Cont';
-$_['text_affiliate']            = 'Afiliat';
-$_['text_my_affiliate']         = 'Contul Meu de Afiliat';
-$_['text_payment']              = 'Informații Plată';
-$_['text_cheque']               = 'Cec';
+$_['text_account']              = 'Account';
+$_['text_affiliate']            = 'Affiliate';
+$_['text_my_affiliate']         = 'My Affiliate Account';
+$_['text_payment']              = 'Payment Information';
+$_['text_cheque']               = 'Cheque';
 $_['text_paypal']               = 'PayPal';
-$_['text_bank']                 = 'Transfer Bancar';
+$_['text_bank']                 = 'Bank Transfer';
 $_['text_success']              = 'Succes: Contul tau a fost actualizat cu succes.';
 $_['text_agree']                = 'Am citit și sunt de acord cu <a href="%s" class="agree"><b>%s</b></a>';
 
 // Entry
-$_['entry_company']             = 'Companie';
+$_['entry_company']             = 'Company';
 $_['entry_website']             = 'Web Site';
-$_['entry_tax']                 = 'Cod Fiscal';
-$_['entry_payment']             = 'Metodă de Plată';
-$_['entry_cheque']              = 'Nume Beneficiar Cec';
-$_['entry_paypal']              = 'Cont de Email PayPal';
-$_['entry_bank_name']           = 'Nume Bancă';
-$_['entry_bank_branch_number']  = 'Număr ABA/BSB (Număr Filială)';
-$_['entry_bank_swift_code']     = 'Cod SWIFT';
-$_['entry_bank_account_name']   = 'Nume Cont';
-$_['entry_bank_account_number'] = 'Număr Cont';
+$_['entry_tax']                 = 'Tax ID';
+$_['entry_payment']             = 'Payment Method';
+$_['entry_cheque']              = 'Cheque Payee Name';
+$_['entry_paypal']              = 'PayPal Email Account';
+$_['entry_bank_name']           = 'Bank Name';
+$_['entry_bank_branch_number']  = 'ABA/BSB number (Branch Number)';
+$_['entry_bank_swift_code']     = 'SWIFT Code';
+$_['entry_bank_account_name']   = 'Account Name';
+$_['entry_bank_account_number'] = 'Account Number';
 
 // Error
 $_['error_agree']               = 'Atenție: Trebuie să fii de acord cu %s!';
-$_['error_cheque']              = 'Nume Beneficiar Cec necesar!';
-$_['error_paypal']              = 'Adresa de Email PayPal nu pare să fie validă!';
-$_['error_bank_account_name']   = 'Nume Cont necesar!';
-$_['error_bank_account_number'] = 'Număr Cont necesar!';
-$_['error_custom_field']        = '%s necesar!';
+$_['error_cheque']              = 'Cheque Payee Name required!';
+$_['error_paypal']              = 'PayPal Email Address does not appear to be valid!';
+$_['error_bank_account_name']   = 'Account Name required!';
+$_['error_bank_account_number'] = 'Account Number required!';
+$_['error_custom_field']        = '%s required!';
