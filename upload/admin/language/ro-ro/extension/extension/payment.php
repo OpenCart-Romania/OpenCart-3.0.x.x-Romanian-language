b4c26e562e21d98b806990cfa517600f12a8b78c
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:28:58 +0800

    New translations payment.php (Romanian)

--- upload/admin/language/ro-ro/extension/extension/payment.php
+++ upload/admin/language/ro-ro/extension/extension/payment.php
@@ -9,9 +9,9 @@ $_['text_list']         = 'Payment List';
 
 // Column
 $_['column_name']       = 'Payment Method';
-$_['column_status']     = 'Status';
-$_['column_sort_order'] = 'Sort Order';
-$_['column_action']     = 'Action';
+$_['column_status']     = 'Stare';
+$_['column_sort_order'] = 'Ordine de Sortare';
+$_['column_action']     = 'Acțiune';
 
 // Error
 $_['error_permission']  = 'Atenție: Nu ai permisiunea de a modifica plățile!';
