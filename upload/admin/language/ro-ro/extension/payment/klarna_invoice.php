b660dab390276b6ac4ebd22d198dfb055e699b15
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:29:49 +0800

    New translations klarna_invoice.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/klarna_invoice.php
+++ upload/admin/language/ro-ro/extension/payment/klarna_invoice.php
@@ -3,7 +3,7 @@
 $_['heading_title']			= 'Klarna Invoice';
 
 // Text
-$_['text_extension']		= 'Extensions';
+$_['text_extension']		= 'Extensii';
 $_['text_success']			= 'Success: You have modified Klarna Payment module!';
 $_['text_edit']             = 'Edit Klarna Invoice';
 $_['text_klarna_invoice']	= '<a href="https://merchants.klarna.com/signup?locale=en&partner_id=d5c87110cebc383a826364769047042e777da5e8&utm_campaign=Platform&utm_medium=Partners&utm_source=Opencart" target="_blank"><img src="https://cdn.klarna.com/public/images/global/logos/v1/basic/global_basic_logo_std_blue-black.png?width=60&eid=opencart" alt="Klarna Invoice" title="Klarna Invoice" style="border: 1px solid #EEEEEE;" /></a>';
@@ -24,8 +24,8 @@ $_['entry_total']			= 'Total';
 $_['entry_pending_status']	= 'Pending Status';
 $_['entry_accepted_status'] = 'Accepted Status';
 $_['entry_geo_zone']		= 'Geo Zone';
-$_['entry_status']			= 'Status';
-$_['entry_sort_order']		= 'Sort Order';
+$_['entry_status']			= 'Stare';
+$_['entry_sort_order']		= 'Ordine Sortare';
 
 // Help
 $_['help_merchant']			= '(estore id) to use for the Klarna service (provided by Klarna).';
@@ -33,4 +33,4 @@ $_['help_secret']			= 'Shared secret to use with the Klarna service (provided by
 $_['help_total']			= 'The checkout total the order must reach before this payment method becomes active.';
 
 // Error
-$_['error_permission']		= 'Warning: You do not have permission to modify payment Klarna Part Payment!';
+$_['error_permission']		= 'Atenție: Nu ai permisiunea de a modifica plata Klarna Part Payment!';
