99c0984cbdd0647edff3b3e6f0aa19d4741de8a4
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:30:14 +0800

    New translations securetrading_pp.php (Romanian)

--- upload/admin/language/ro-ro/extension/payment/securetrading_pp.php
+++ upload/admin/language/ro-ro/extension/payment/securetrading_pp.php
@@ -2,8 +2,8 @@
 $_['heading_title']                              = 'Secure Trading Payment Pages';
 
 $_['text_securetrading_pp']                      = '<a href="http://www.securetradingfs.com/partner/open-cart/" target="_blank"><img src="view/image/payment/secure_trading.png" alt="Secure Trading" title="Secure Trading" style="border: 1px solid #EEEEEE;" /></a>';
-$_['text_extension']                             = 'Extensions';
-$_['text_all_geo_zones']                         = 'All Geo Zones';
+$_['text_extension']                             = 'Extensii';
+$_['text_all_geo_zones']                         = 'Toate Zonele Geografice';
 $_['text_process_immediately']                   = 'Process immediately';
 $_['text_wait_x_days']                           = 'Wait %d days';
 $_['text_success']                               = 'Success: You have modified Secure Trading module!';
@@ -14,25 +14,25 @@ $_['text_pending_settlement_manually_overriden'] = 'Pending Settlement, manually
 $_['text_pending_suspended']                     = 'Suspendat';
 $_['text_pending_settled']                       = 'Settled';
 $_['text_reversed']                              = 'Reversed';
-$_['text_payment']                               = "Payment";
+$_['text_payment']                               = "Plată";
 $_['text_rebate']                                = 'Rebate';
 
 $_['entry_site_reference']                       = 'Site reference';
 $_['entry_version']                              = 'Version';
-$_['entry_username']                             = 'Username';
-$_['entry_password']                             = 'Password';
+$_['entry_username']                             = 'Nume de utilizator';
+$_['entry_password']                             = 'Parolă';
 $_['entry_site_security_status']                 = 'Use Site Security hash';
 $_['entry_site_security_password']               = 'Site Security password';
 $_['entry_notification_password']                = 'Notification password';
-$_['entry_order_status']                         = 'Order status';
+$_['entry_order_status']                         = 'Stare comandă';
 $_['entry_declined_order_status']                = 'Declined order status';
 $_['entry_refunded_order_status']                = 'Refunded order status';
 $_['entry_authorisation_reversed_order_status']  = 'Authorisation reversed order status';
 $_['entry_settle_status']                        = 'Settlement status';
 $_['entry_settle_due_date']                      = 'Settlement due date';
 $_['entry_geo_zone']                             = 'Geo Zone';
-$_['entry_sort_order']                           = 'Sort Order';
-$_['entry_status']                               = 'Status';
+$_['entry_sort_order']                           = 'Ordine Sortare';
+$_['entry_status']                               = 'Stare';
 $_['entry_total']                                = 'Total';
 $_['entry_parent_css']                           = 'Parent CSS';
 $_['entry_child_css']                            = 'Child CSS';
@@ -58,7 +58,7 @@ $_['help_refund']                                = 'Please include the decimal p
 $_['help_total']                                 = 'The checkout total the order must reach before this payment method becomes active';
 
 $_['button_reverse_authorisation']               = 'Reverse Authorisation';
-$_['button_refund']                              = 'Refund';
+$_['button_refund']                              = 'Stornare';
 
 
 // Order page - payment tab
