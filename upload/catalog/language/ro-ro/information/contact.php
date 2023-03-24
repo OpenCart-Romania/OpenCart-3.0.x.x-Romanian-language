31107e474a6c049c5b2dcc68acea6f4554d82f61
Constantin DOBRE <admin@harddex.ro>
2023-03-24 08:49:48 +0800

    New translations contact.php (Romanian)

--- upload/catalog/language/ro-ro/information/contact.php
+++ upload/catalog/language/ro-ro/information/contact.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title']  = 'Contactează-ne';
+$_['heading_title']  = 'Contact Us';
 
 // Text
 $_['text_location']  = 'Locația Noastră';
 $_['text_store']     = 'Magazinele Noastre';
 $_['text_contact']   = 'Formular de Contact';
-$_['text_address']   = 'Adresă';
-$_['text_telephone'] = 'Telefon';
+$_['text_address']   = 'Address';
+$_['text_telephone'] = 'Telephone';
 $_['text_fax']       = 'Fax';
-$_['text_open']      = 'Program de Lucru';
-$_['text_comment']   = 'Comentarii';
+$_['text_open']      = 'Opening Times';
+$_['text_comment']   = 'Comments';
 $_['text_message']   = '<p>Solicitarea ta a fost trimisă cu succes proprietarului magazinului!</p>'; 
 
 // Entry
-$_['entry_name']     = 'Numele Tău';
-$_['entry_email']    = 'Adresă de E-Mail';
+$_['entry_name']     = 'Your Name';
+$_['entry_email']    = 'E-Mail Address';
 $_['entry_enquiry']  = 'Solicitare';
 
 // Email
@@ -23,5 +23,5 @@ $_['email_subject']  = 'Solicitare %s';
 
 // Errors
 $_['error_name']     = 'Numele trebuie să fie între 3 și 32 de caractere!';
-$_['error_email']    = 'Adresa E-Mail nu pare să fie validă!';
+$_['error_email']    = 'E-Mail Address does not appear to be valid!';
 $_['error_enquiry']  = 'Solicitare trebuie să fie între 10 și 3000 de caractere!';
