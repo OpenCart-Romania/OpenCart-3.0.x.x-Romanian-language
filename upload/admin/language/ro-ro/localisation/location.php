83e680d99f9210dfcba9927f78950e2734590989
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:31:02 +0800

    New translations location.php (Romanian)

--- upload/admin/language/ro-ro/localisation/location.php
+++ upload/admin/language/ro-ro/localisation/location.php
@@ -7,22 +7,22 @@ $_['text_success']     = 'Success: You have modified store locations!';
 $_['text_list']        = 'Store Location List';
 $_['text_add']         = 'Add Store Location';
 $_['text_edit']        = 'Edit Store Location';
-$_['text_default']     = 'Default';
+$_['text_default']     = 'Implicit';
 $_['text_time']        = 'Program de Lucru';
 $_['text_geocode']     = 'Geocode was not successful for the following reason:';
 
 // Column
 $_['column_name']      = 'Store Name';
 $_['column_address']   = 'Address';
-$_['column_action']    = 'Action';
+$_['column_action']    = 'Acțiune';
 
 // Entry
 $_['entry_name']       = 'Store Name';
 $_['entry_address']    = 'Address';
 $_['entry_geocode']    = 'Geocode';
-$_['entry_telephone']  = 'Telephone';
+$_['entry_telephone']  = 'Telefon';
 $_['entry_fax']        = 'Fax';
-$_['entry_image']      = 'Image';
+$_['entry_image']      = 'Imagine';
 $_['entry_open']       = 'Opening Times';
 $_['entry_comment']    = 'Comment';
 
@@ -35,4 +35,4 @@ $_['help_comment']     = 'This field is for any special notes you would like to 
 $_['error_permission'] = 'Warning: You do not have permission to modify store locations!';
 $_['error_name']       = 'Store name must be between 3 and 32 characters!';
 $_['error_address']    = 'Adresa trebuie să fie între 3 și 128 de caractere!';
-$_['error_telephone']  = 'Telephone must be between 3 and 32 characters!';
+$_['error_telephone']  = 'Telefonul trebuie să fie între 3 și 32 de caractere!';
