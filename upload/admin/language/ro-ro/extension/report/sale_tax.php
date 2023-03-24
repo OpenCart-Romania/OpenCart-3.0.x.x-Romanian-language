7c45e5169ae5e190504f9c2ef00fa420a6b8755d
Constantin DOBRE <admin@harddex.ro>
2023-03-24 09:30:33 +0800

    New translations sale_tax.php (Romanian)

--- upload/admin/language/ro-ro/extension/report/sale_tax.php
+++ upload/admin/language/ro-ro/extension/report/sale_tax.php
@@ -3,14 +3,14 @@
 $_['heading_title']     = 'Tax Report';
 
 // Text
-$_['text_extension']    = 'Extensions';
+$_['text_extension']    = 'Extensii';
 $_['text_edit']         = 'Edit Tax Report';
 $_['text_success']      = 'Success: You have modified tax report!';
-$_['text_filter']       = 'Filter';
-$_['text_year']         = 'Years';
+$_['text_filter']       = 'Filtru';
+$_['text_year']         = 'Ani';
 $_['text_month']        = 'Months';
-$_['text_week']         = 'Weeks';
-$_['text_day']          = 'Days';
+$_['text_week']         = 'Săptămâni';
+$_['text_day']          = 'Zile';
 $_['text_all_status']   = 'All Statuses';
 
 // Column
@@ -21,11 +21,11 @@ $_['column_orders']     = 'No. Orders';
 $_['column_total']      = 'Total';
 
 // Entry
-$_['entry_date_start']  = 'Date Start';
-$_['entry_date_end']    = 'Date End';
+$_['entry_date_start']  = 'Dată de Început';
+$_['entry_date_end']    = 'Dată de Sfârșit';
 $_['entry_group']       = 'Group By';
-$_['entry_status']      = 'Status';
-$_['entry_sort_order']  = 'Sort Order';
+$_['entry_status']      = 'Stare';
+$_['entry_sort_order']  = 'Ordine Sortare';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify tax report!';
